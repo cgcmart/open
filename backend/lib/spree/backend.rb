98fb82bd13b6f8635b438ed8c020e30ffc9f71ca
99cm <zyizyao@china-guide.com>
2018-06-25 12:23:17 -0400

    Update backend.rb

--- backend/lib/spree/backend.rb
+++ backend/lib/spree/backend.rb
@@ -1,13 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails/all'
 require 'jquery-rails'
-require 'jquery-ui-rails'
-require 'deface'
-require 'select2-rails'
+require 'coffee-rails'
+require 'sass-rails'
+require 'handlebars_assets'
+require 'font-awesome-rails'
+require 'autoprefixer-rails'
+require 'jbuilder'
+require 'kaminari'
 
 require 'spree_core'
 require 'spree_api'
 
-require 'spree/responder'
 require 'spree/backend/action_callbacks'
 require 'spree/backend/callbacks'
 require 'spree/backend/engine'
