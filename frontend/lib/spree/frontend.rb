9f22945913a6bfdc0baa25b4bcec16acb401e28c
99cm <zyizyao@china-guide.com>
2018-06-26 11:36:40 -0400

    Add missing requires

--- frontend/lib/spree/frontend.rb
+++ frontend/lib/spree/frontend.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails/all'
 require 'sprockets/rails'
-
 require 'bootstrap-sass'
 require 'canonical-rails'
-require 'deface'
 require 'jquery-rails'
+require 'sass-rails'
+require 'font-awesome-rails'
+require 'responders'
+require 'kaminari'
+
 require 'spree/core'
+require 'spree/api'
 require 'spree/frontend/middleware/seo_assist'
 require 'spree/frontend/engine'
-require 'spree/responder'
