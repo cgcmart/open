5265fda75d16bfbb1f1e6eeb90d26e87283674b6
99cm <zyizyao@china-guide.com>
2018-06-28 11:46:47 -0400

    remove clear_cache

--- backend/app/controllers/spree/admin/general_settings_controller.rb
+++ backend/app/controllers/spree/admin/general_settings_controller.rb
@@ -1,26 +1,10 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     class GeneralSettingsController < Spree::Admin::BaseController
-      include Spree::Backend::Callbacks
-
       def edit
-        @preferences_security = []
-      end
-
-      def update
-        params.each do |name, value|
-          next unless Spree::Config.has_preference? name
-          Spree::Config[name] = value
-        end
-
-        flash[:success] = Spree.t(:successfully_updated, resource: Spree.t(:general_settings))
-        redirect_to edit_admin_general_settings_path
-      end
-
-      def clear_cache
-        Rails.cache.clear
-        invoke_callbacks(:clear_cache, :after)
-        head :no_content
+        redirect_to admin_stores_path
       end
     end
   end
