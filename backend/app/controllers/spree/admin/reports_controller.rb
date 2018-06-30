f6d80b1aaa424180cb8b9859e9567e345e669cad
99cm <zyizyao@china-guide.com>
2018-06-30 11:31:00 -0400

    Update reports_controller.rb

--- backend/app/controllers/spree/admin/reports_controller.rb
+++ backend/app/controllers/spree/admin/reports_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     class ReportsController < Spree::Admin::BaseController
@@ -12,7 +14,8 @@ module Spree
           if report_description_key.nil?
             report_description_key = "#{report_key}_description"
           end
-          @@available_reports[report_key] = { name: Spree.t(report_key), description: Spree.t(report_description_key) }
+
+          @@available_reports[report_key] = { name: I18n.t(report_key, scope: 'spree'), description: I18n.t(report_description_key, scope: 'spree') }
         end
       end
 
@@ -26,47 +29,51 @@ module Spree
       end
 
       def sales_total
-        params[:q] = {} unless params[:q]
+        params[:q] = search_params
 
-        params[:q][:completed_at_gt] = if params[:q][:completed_at_gt].blank?
-                                         Time.zone.now.beginning_of_month
-                                       else
-                                         begin
-                                            Time.zone.parse(params[:q][:completed_at_gt]).beginning_of_day
-                                         rescue
-                                            Time.zone.now.beginning_of_month
-                                         end
-                                       end
-
-        if params[:q] && !params[:q][:completed_at_lt].blank?
-          params[:q][:completed_at_lt] = begin
-                                           Time.zone.parse(params[:q][:completed_at_lt]).end_of_day
-                                         rescue
-                                           ''
-                                         end
-        end
-
-        params[:q][:s] ||= 'completed_at desc'
-
-        @search = Order.complete.ransack(params[:q])
+        @search = Order.complete.not_canceled.ransack(params[:q])
         @orders = @search.result
 
         @totals = {}
         @orders.each do |order|
-          @totals[order.currency] = { item_total: ::Money.new(0, order.currency), adjustment_total: ::Money.new(0, order.currency), sales_total: ::Money.new(0, order.currency) } unless @totals[order.currency]
+          unless @totals[order.currency]
+            @totals[order.currency] = {
+              item_total: ::Money.new(0, order.currency),
+              adjustment_total: ::Money.new(0, order.currency),
+              sales_total: ::Money.new(0, order.currency)
+            }
+          end
+
           @totals[order.currency][:item_total] += order.display_item_total.money
           @totals[order.currency][:adjustment_total] += order.display_adjustment_total.money
           @totals[order.currency][:sales_total] += order.display_total.money
         end
       end
 
+      @@available_reports = {}
+
       private
 
-      def model_class
-        Spree::Admin::ReportsController
+      def search_params
+        params.fetch(:q, {}).tap do |q|
+          q[:completed_at_gt] = adjust_start_date q[:completed_at_gt]
+          q[:completed_at_lt] = adjust_end_date(q[:completed_at_lt]) if q[:completed_at_lt].present?
+          q[:s] ||= 'completed_at desc'
+        end
       end
 
-      @@available_reports = {}
+      def adjust_start_date(string_date = nil)
+        return Time.current.beginning_of_month if string_date.blank?
+        Time.zone.parse(string_date).beginning_of_day
+      rescue ArgumentError
+        Time.current.beginning_of_month
+      end
+
+      def adjust_end_date(string_date)
+        Time.zone.parse(string_date).end_of_day
+      rescue ArgumentError
+        ''
+      end
     end
   end
 end
