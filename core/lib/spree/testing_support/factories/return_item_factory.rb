476bd82e744b0a42952fc3d1a6779fb9e1bc9b42
99cm <zyizyao@china-guide.com>
2018-07-02 16:56:23 -0400

    Update return_item_factory.rb

--- core/lib/spree/testing_support/factories/return_item_factory.rb
+++ core/lib/spree/testing_support/factories/return_item_factory.rb
@@ -1,7 +1,16 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/inventory_unit_factory'
+require 'spree/testing_support/factories/return_reason_factory'
+require 'spree/testing_support/factories/return_authorization_factory'
+
 FactoryBot.define do
   factory :return_item, class: Spree::ReturnItem do
     association(:inventory_unit, factory: :inventory_unit, state: :shipped)
-    association(:return_authorization, factory: :return_authorization)
+    association(:return_reason, factory: :return_reason)
+    return_authorization do |_return_item|
+      build(:return_authorization, order: inventory_unit.order)
+    end
 
     factory :exchange_return_item do
       after(:build) do |return_item|
