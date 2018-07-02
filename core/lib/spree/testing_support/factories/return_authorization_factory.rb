61852d2fef0b2d43e408b98234dbac2bb6170e87
99cm <zyizyao@china-guide.com>
2018-07-02 16:47:24 -0400

    Update return_authorization_factory.rb

--- core/lib/spree/testing_support/factories/return_authorization_factory.rb
+++ core/lib/spree/testing_support/factories/return_authorization_factory.rb
@@ -1,20 +1,20 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/order_factory'
+require 'spree/testing_support/factories/stock_location_factory'
+require 'spree/testing_support/factories/return_reason_factory'
+
 FactoryBot.define do
   factory :return_authorization, class: Spree::ReturnAuthorization do
     association(:order, factory: :shipped_order)
     association(:stock_location, factory: :stock_location)
-    association(:reason, factory: :return_authorization_reason)
+    association(:reason, factory: :return_reason)
     memo 'Items were broken'
   end
 
   factory :new_return_authorization, class: Spree::ReturnAuthorization do
     association(:order, factory: :shipped_order)
     association(:stock_location, factory: :stock_location)
-    association(:reason, factory: :return_authorization_reason)
-  end
-
-  factory :return_authorization_reason, class: Spree::ReturnAuthorizationReason do
-    sequence(:name) { |n| "Defect ##{n}" }
-    active true
-    mutable false
+    association(:reason, factory: :return_reason)
   end
 end
