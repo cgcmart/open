e169d2f09f893887686a89b7743b3c8e15e8db25
99cm <zyizyao@china-guide.com>
2018-07-04 10:00:55 -0400

    Update adjustment.rb

--- core/app/models/spree/adjustment.rb
+++ core/app/models/spree/adjustment.rb
@@ -1,65 +1,41 @@
-# Adjustments represent a change to the +item_total+ of an Order. Each adjustment
-# has an +amount+ that can be either positive or negative.
-#
-# Adjustments can be "opened" or "closed".
-# Once an adjustment is closed, it will not be automatically updated.
-#
-# Boolean attributes:
-#
-# +mandatory+
-#
-# If this flag is set to true then it means the the charge is required and will not
-# be removed from the order, even if the amount is zero. In other words a record
-# will be created even if the amount is zero. This is useful for representing things
-# such as shipping and tax charges where you may want to make it explicitly clear
-# that no charge was made for such things.
-#
-# +eligible?+
-#
-# This boolean attributes stores whether this adjustment is currently eligible
-# for its order. Only eligible adjustments count towards the order's adjustment
-# total. This allows an adjustment to be preserved if it becomes ineligible so
-# it might be reinstated.
+# frozen_string_literal: true
+
 module Spree
+  # Adjustments represent a change to the +item_total+ of an Order. Each adjustment
+  # has an +amount+ that can be either positive or negative.
+  #
+  # Adjustments can be "opened" or "closed".
+  # Once an adjustment is closed, it will not be automatically updated.
+  #
+  # Boolean attributes:
+  #
+  # +eligible?*
+  #
+  # This boolean attributes stores whether this adjustment is currently eligible
+  # for its order. Only eligible adjustments count towards the order's adjustment
+  # total. This allows an adjustment to be preserved if it becomes ineligible so
+  # it might be reinstated.
   class Adjustment < Spree::Base
-    with_options polymorphic: true do
-      belongs_to :adjustable, touch: true
-      belongs_to :source
-    end
+    belongs_to :adjustable, polymorphic: true, touch: true
+    belongs_to :source, polymorphic: true
     belongs_to :order, class_name: 'Spree::Order', inverse_of: :all_adjustments
+    belongs_to :promotion_code, class_name: 'Spree::PromotionCode'
+    belongs_to :adjustment_reason, class_name: 'Spree::AdjustmentReason', inverse_of: :adjustments
 
-    validates :adjustable, :order, :label, presence: true
+    validates :adjustable, presence: true
+    validates :order, presence: true
+    validates :label, presence: true
     validates :amount, numericality: true
+    validates :promotion_code, presence: true, if: :require_promotion_code?
 
-    state_machine :state, initial: :open do
-      event :close do
-        transition from: :open, to: :closed
-      end
+    # We need to use `after_commit` here because otherwise it's too early to
+    # tell if any repair is needed.
+    after_commit :repair_adjustments_associations_on_create, on: [:create]
+    after_commit :repair_adjustments_associations_on_destroy, on: [:destroy]
 
-      event :open do
-        transition from: :closed, to: :open
-      end
-    end
-
-    after_create :update_adjustable_adjustment_total
-    after_destroy :update_adjustable_adjustment_total
-
-    class_attribute :competing_promos_source_types
-
-    self.competing_promos_source_types = ['Spree::PromotionAction']
-
-    scope :open, -> {
-      ActiveSupport::Deprecation.warn 'Adjustment.open is deprecated. Please use Adjustment.not_finalized instead', caller
-      not_finalized
-    }
-
-    scope :closed, -> {
-      ActiveSupport::Deprecation.warn 'Adjustment.closed is deprecated. Please use Adjustment.finalized instead', caller
-      finalized
-    }
-
-    scope :not_finalized, -> { where(state: 'open') }
-    scope :finalized, -> { where(state: 'closed') }
+    scope :not_finalized, -> { where(finalized: false) }
+    scope :finalized, -> { where(finalized: true) }
+    scope :cancellation, -> { where(source_type: 'Spree::UnitCancel') }
     scope :tax, -> { where(source_type: 'Spree::TaxRate') }
     scope :non_tax, -> do
       source_type = arel_table[:source_type]
@@ -67,24 +43,33 @@ module Spree
     end
     scope :price, -> { where(adjustable_type: 'Spree::LineItem') }
     scope :shipping, -> { where(adjustable_type: 'Spree::Shipment') }
-    scope :optional, -> { where(mandatory: false) }
     scope :eligible, -> { where(eligible: true) }
     scope :charge, -> { where("#{quoted_table_name}.amount >= 0") }
     scope :credit, -> { where("#{quoted_table_name}.amount < 0") }
     scope :nonzero, -> { where("#{quoted_table_name}.amount != 0") }
     scope :promotion, -> { where(source_type: 'Spree::PromotionAction') }
+    scope :non_promotion, -> { where.not(source_type: 'Spree::PromotionAction') }
     scope :return_authorization, -> { where(source_type: 'Spree::ReturnAuthorization') }
     scope :is_included, -> { where(included: true) }
     scope :additional, -> { where(included: false) }
-    scope :competing_promos, -> { where(source_type: competing_promos_source_types) }
-    scope :for_complete_order, -> { joins(:order).merge(Spree::Order.complete) }
-    scope :for_incomplete_order, -> { joins(:order).merge(Spree::Order.incomplete) }
 
     extend DisplayMoney
     money_methods :amount
 
-    def amount=(amount)
-      self[:amount] = Spree::LocalizedNumber.parse(amount)
+    def finalize!
+      update_attributes!(finalized: true)
+    end
+
+    def unfinalize!
+      update_attributes!(finalized: false)
+    end
+
+    def finalize
+      update_attributes(finalized: true)
+    end
+
+    def unfinalize
+      update_attributes(finalized: false)
     end
 
     def currency
@@ -95,23 +80,88 @@ module Spree
       source_type == 'Spree::PromotionAction'
     end
 
-    # Passing a target here would always be recommended as it would avoid
-    # hitting the database again and would ensure you're compute values over
-    # the specific object amount passed here.
-    def update!(target = adjustable)
-      return amount if closed? || source.blank?
-      amount = source.compute_amount(target)
-      attributes = { amount: amount, updated_at: Time.current }
-      attributes[:eligible] = source.promotion.eligible?(target) if promotion?
-      update_columns(attributes)
+    # @return [Boolean] true when this is a tax adjustment (Tax adjustments have a {TaxRate} source)
+    def tax?
+      source_type == 'Spree::TaxRate'
+    end
+
+    # @return [Boolean] true when this is a cancellation adjustment (Cancellation adjustments have a {UnitCancel} source)
+    def cancellation?
+      source_type == 'Spree::UnitCancel'
+    end
+
+    # Recalculate and persist the amount from this adjustment's source based on
+    # the adjustable ({Order}, {Shipment}, or {LineItem})
+    #
+    # If the adjustment has no source (such as when created manually from the
+    # admin) or is closed, this is a noop.
+    #
+    # @return [BigDecimal] New amount of this adjustment
+    def recalculate
+      if finalized? && !tax?
+        return amount
+      end
+
+      # If the adjustment has no source, do not attempt to re-calculate the
+      # amount.
+      # Some scenarios where this happens:
+      #   - Adjustments that are manually created via the admin backend
+      #   - PromotionAction adjustments where the PromotionAction was deleted
+      #     after the order was completed.
+      if source.present?
+        self.amount = source.compute_amount(adjustable)
+
+        if promotion?
+          self.eligible = calculate_eligibility
+        end
+
+        # Persist only if changed
+        # This is only not a save! to avoid the extra queries to load the order
+        # (for validations) and to touch the adjustment.
+        update_columns(eligible: eligible, amount: amount, updated_at: Time.current) if changed?
+      end
       amount
+    end
+
+    def update!(*args)
+      if args.empty?
+        Spree::Deprecation.warn "Calling adjustment.update! with no arguments to recalculate amounts and eligibility is deprecated, since it conflicts with AR::Base#update! Please use adjustment.recalculate instead"
+        recalculate
+      else
+        super
+      end
+    end
+
+    # Calculates based on attached promotion (if this is a promotion
+    # adjustment) whether this promotion is still eligible.
+    # @api private
+    # @return [true,false] Whether this adjustment is eligible
+    def calculate_eligibility
+      if !finalized? && source && promotion?
+        source.promotion.eligible?(adjustable, promotion_code: promotion_code)
+      else
+        eligible?
+      end
     end
 
     private
 
-    def update_adjustable_adjustment_total
-      # Cause adjustable's total to be recalculated
-      Adjustable::AdjustmentsUpdater.update(adjustable)
+    def require_promotion_code?
+      promotion? && source.promotion.codes.any?
+    end
+
+    def repair_adjustments_associations_on_create
+      if adjustable.adjustments.loaded? && !adjustable.adjustments.include?(self) && !destroyed?
+        Spree::Deprecation.warn("Adjustment #{id} was not added to #{adjustable.class} #{adjustable.id}. Add adjustments via `adjustable.adjustments.create!`. Partial call stack: #{caller.select { |line| line =~ %r(/(app|spec)/) }}.", caller)
+        adjustable.adjustments.proxy_association.add_to_target(self)
+      end
+    end
+
+    def repair_adjustments_associations_on_destroy
+      if adjustable.adjustments.loaded? && adjustable.adjustments.include?(self)
+        Spree::Deprecation.warn("Adjustment #{id} was not removed from #{adjustable.class} #{adjustable.id}. Remove adjustments via `adjustable.adjustments.destroy`. Partial call stack: #{caller.select { |line| line =~ %r(/(app|spec)/) }}.", caller)
+        adjustable.adjustments.proxy_association.target.delete(self)
+      end
     end
   end
 end
