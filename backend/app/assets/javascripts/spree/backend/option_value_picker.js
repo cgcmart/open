12a15122fe36f2ecc87422cc8d8944091e2f294e
99cm <zyizyao@china-guide.com>
2018-07-13 15:00:34 -0400

    Update option_value_picker.js

--- backend/app/assets/javascripts/spree/backend/option_value_picker.js
+++ backend/app/assets/javascripts/spree/backend/option_value_picker.js
@@ -1,17 +1,20 @@
 $.fn.optionValueAutocomplete = function (options) {
-  /* globals Spree */
   'use strict';
 
   // Default options
-  options = options || {};
-  var multiple = typeof(options.multiple) !== 'undefined' ? options.multiple : true;
-  var productSelect = options.productSelect;
+  options = options || {}
+  var multiple = typeof(options['multiple']) !== 'undefined' ? options['multiple'] : true;
+  var productSelect = options['productSelect'];
+
+  function formatOptionValue(option_value) {
+    return Select2.util.escapeMarkup(option_value.name);
+  }
 
   this.select2({
     minimumInputLength: 3,
     multiple: multiple,
     initSelection: function (element, callback) {
-      $.get(Spree.routes.option_values_api, {
+      $.get(Spree.routes.option_value_search, {
         ids: element.val().split(','),
         token: Spree.api_key
       }, function (data) {
@@ -19,9 +22,10 @@ $.fn.optionValueAutocomplete = function (options) {
       });
     },
     ajax: {
-      url: Spree.routes.option_values_api,
+      url: Spree.routes.option_value_search,
       datatype: 'json',
-      data: function (term) {
+      data: function (term, page) {
+        // Note: This doesn't work. variants_product_id isn't an allowed filter
         var productId = typeof(productSelect) !== 'undefined' ? $(productSelect).select2('val') : null;
         return {
           q: {
@@ -31,15 +35,11 @@ $.fn.optionValueAutocomplete = function (options) {
           token: Spree.api_key
         };
       },
-      results: function (data) {
+      results: function (data, page) {
         return { results: data };
       }
     },
-    formatResult: function (optionValue) {
-      return optionValue.name;
-    },
-    formatSelection: function (optionValue) {
-      return optionValue.name;
-    }
+    formatResult: formatOptionValue,
+    formatSelection: formatOptionValue
   });
 };
