adf0c3f8869084e13bc4232d7f8a10bcc31ad9d3
99cm <zyizyao@china-guide.com>
2018-07-14 10:42:06 -0400

    Update gateway.js

--- backend/app/assets/javascripts/spree/backend/gateway.js
+++ backend/app/assets/javascripts/spree/backend/gateway.js
@@ -1,13 +1,22 @@
-$(function() {
-  var original_gtwy_type = $('#gtwy-type').prop('value');
-  $('div#gateway-settings-warning').hide();
-  $('#gtwy-type').change(function() { 
-    if ($('#gtwy-type').prop('value') == original_gtwy_type) {
-      $('div.gateway-settings').show();
-      $('div#gateway-settings-warning').hide();
+Spree.ready(function() {
+  var $gateway_type = $('select.js-gateway-type');
+  var $preference_source = $('select.js-preference-source');
+  var original_gateway_type = $gateway_type.val();
+  var original_preference_source = $preference_source.val();
+
+  var render = function() {
+    var gateway_type = $gateway_type.val();
+    var preference_source = $preference_source.val();
+    $('.js-preference-source-wrapper').toggle(gateway_type === original_gateway_type);
+    if (gateway_type === original_gateway_type && preference_source === original_preference_source) {
+      $('.js-gateway-settings').show();
+      $('.js-gateway-settings-warning').hide();
     } else {
-      $('div.gateway-settings').hide();
-      $('div#gateway-settings-warning').show();
+      $('.js-gateway-settings').hide();
+      $('.js-gateway-settings-warning').show();
     }
-  });
-})
+  };
+  $gateway_type.change(render);
+  $preference_source.change(render);
+  render();
+});
