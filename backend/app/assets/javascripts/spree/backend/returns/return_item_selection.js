e0f9293f8ca5f9a437ca7b0463e1722e3531c488
99cm <zyizyao@china-guide.com>
2018-07-14 11:59:45 -0400

    Use Spree.ready to run DOC

--- backend/app/assets/javascripts/spree/backend/returns/return_item_selection.js
+++ backend/app/assets/javascripts/spree/backend/returns/return_item_selection.js
@@ -1,29 +1,24 @@
-$(document).ready(function() {
+Spree.ready(function() {
+  function checkAddItemBox() {
+    $(this).closest('tr').find('input.add-item').attr('checked', 'checked');
+    updateSuggestedAmount();
+  }
+
+  function updateSuggestedAmount() {
+    var totalPretaxRefund = 0;
+    var checkedItems = formFields.find('input.add-item:checked');
+    $.each(checkedItems, function(i, checkbox) {
+      totalPretaxRefund += parseFloat($(checkbox).parents('tr').find('.refund-amount-input').val());
+    });
+
+    var displayTotal = isNaN(totalPretaxRefund) ? '' : totalPretaxRefund.toFixed(2);
+    formFields.find('span#total_pre_tax_refund').html(displayTotal);
+  }
+
   var formFields = $("[data-hook='admin_customer_return_form_fields'], \
                      [data-hook='admin_return_authorization_form_fields']");
 
   if(formFields.length > 0) {
-    function checkAddItemBox() {
-      $(this).closest('tr').find('input.add-item').attr('checked', 'checked');
-      updateSuggestedAmount();
-    }
-
-    function updateSuggestedAmount() {
-      var totalPretaxRefund = 0;
-      var checkedItems = formFields.find('input.add-item:checked');
-      $.each(checkedItems, function(i, checkbox) {
-        var returnItemRow  = $(checkbox).parents('tr'), returnQuantity, amount, purchasedQuantity;
-        returnQuantity = parseInt(returnItemRow.find('.refund-quantity-input').val(), 10);
-        purchasedQuantity = parseInt(returnItemRow.find('.purchased-quantity').text(), 10);
-        amount = (returnQuantity / purchasedQuantity) * parseFloat(returnItemRow.find('.charged-amount').data('chargedAmount'));
-        returnItemRow.find('.refund-amount-input').val(amount.toFixed(2));
-        totalPretaxRefund += amount;
-      });
-
-      var displayTotal = isNaN(totalPretaxRefund) ? '' : totalPretaxRefund.toFixed(2);
-      formFields.find('span#total_pre_tax_refund').html(displayTotal);
-    }
-
     updateSuggestedAmount();
 
     formFields.find('input#select-all').on('change', function(ev) {
@@ -34,7 +29,6 @@ $(document).ready(function() {
 
     formFields.find('input.add-item').on('change', updateSuggestedAmount);
     formFields.find('.refund-amount-input').on('keyup', updateSuggestedAmount);
-    formFields.find('.refund-quantity-input').on('keyup mouseup', updateSuggestedAmount);
 
     formFields.find('input, select').not('.add-item').on('change', checkAddItemBox);
   }
