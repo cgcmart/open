2fe71097ee67c30c8f62c0bb505ccd563a403b53
99cm <zyizyao@china-guide.com>
2018-07-14 10:55:31 -0400

    Update handlebar_extensions.js

--- backend/app/assets/javascripts/spree/backend/handlebar_extensions.js
+++ backend/app/assets/javascripts/spree/backend/handlebar_extensions.js
@@ -1,10 +1,26 @@
-Handlebars.registerHelper("t", function(key) {
-  if (Spree.translations[key]) {
-    return Spree.translations[key]
-  } else {
-    console.error("No translation found for " + key + ". Does it exist within spree/admin/shared/_translations.html.erb?")
-  }
+//= require handlebars
+//= require spree/backend/translation
+
+Handlebars.registerHelper("t", function(key, options) {
+  return Spree.t(key, options.hash);
 });
-Handlebars.registerHelper("edit_product_url", function(product_id) {
-  return Spree.routes.edit_product(product_id)
+
+Handlebars.registerHelper("human_attribute_name", function(model, attr) {
+  return Spree.human_attribute_name(model, attr);
+});
+
+Handlebars.registerHelper("human_model_name", function(model) {
+  return Spree.human_model_name(model);
+});
+
+Handlebars.registerHelper("admin_url", function() {
+  return Spree.pathFor("admin")
+});
+
+Handlebars.registerHelper("concat", function() {
+  return Array.prototype.slice.call(arguments, 0, -1).join('');
+});
+
+Handlebars.registerHelper("format_money", function(amount, currency) {
+  return Spree.formatMoney(amount, currency);
 });
