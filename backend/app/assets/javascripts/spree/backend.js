e9fe31bf4a2fc93865ceb299b94feda315c5a2b5
99cm <zyizyao@china-guide.com>
2018-07-15 14:36:23 -0400

    Add routes.js to backend.js for routes section

--- backend/app/assets/javascripts/spree/backend.js
+++ backend/app/assets/javascripts/spree/backend.js
@@ -1,89 +1,71 @@
-//= require modernizr
-//= require bootstrap-sprockets
 //= require handlebars
 //= require jquery
-//= require js.cookie
-//= require jquery.jstree/jquery.jstree
 //= require jquery_ujs
-//= require solidus_admin/select2
-//= require solidus_admin/underscore
+//= require jquery.sticky-kit.min
+//= require prism
 //= require solidus_admin/backbone
 //= require solidus_admin/backbone-nested-models
-//= require solidus_admin/popover
+//= require solidus_admin/bind-polyfill
 //= require solidus_admin/bootstrap
 //= require solidus_admin/flatpickr/flatpickr
-//= require jquery-ui/widgets/datepicker
-//= require jquery-ui/widgets/sortable
-//= require jquery-ui/widgets/autocomplete
-//= require select2
-//= require underscore-min.js
+//= require solidus_admin/popover
+//= require solidus_admin/select2
+//= require solidus_admin/underscore
 
 //= require spree
-//= require spree/backend/spree-select2
-//= require spree/backend/address_states
+//= require spree/backend/addresses
 //= require spree/backend/adjustments
 //= require spree/backend/admin
+//= require spree/backend/backbone-overrides
 //= require spree/backend/calculator
 //= require spree/backend/checkouts/edit
+//= require spree/backend/collections
+//= require spree/backend/components/editable_table
+//= require spree/backend/components/number_with_currency
+//= require spree/backend/components/sortable_table
+//= require spree/backend/components/tabs
+//= require spree/backend/components/tooltips
+//= require spree/backend/datepicker
+//= require spree/backend/flash
+//= require spree/backend/format_money
 //= require spree/backend/gateway
-//= require spree/backend/general_settings
 //= require spree/backend/handlebar_extensions
-//= require spree/backend/line_items
-//= require spree/backend/line_items_on_order_edit
+//= require spree/backend/images/index
+//= require spree/backend/images/upload
 //= require spree/backend/locale_selection
+//= require spree/backend/models
+//= require spree/backend/namespaces
+//= require spree/backend/navigation
 //= require spree/backend/option_type_autocomplete
 //= require spree/backend/option_value_picker
-//= require spree/backend/orders/edit
+//= require spree/backend/orders
 //= require spree/backend/payments/edit
 //= require spree/backend/payments/new
 //= require spree/backend/product_picker
 //= require spree/backend/progress
 //= require spree/backend/promotions
-//= require spree/backend/returns/expedited_exchanges_warning
+//= require spree/backend/promotions/activation
 //= require spree/backend/returns/return_item_selection
+//= require spree/backend/routes
 //= require spree/backend/shipments
 //= require spree/backend/spree-select2
-//= require spree/backend/states
-//= require spree/backend/stock_location
 //= require spree/backend/stock_management
+
 //= require spree/backend/stock_movement
 //= require spree/backend/stock_transfer
+
+//= require spree/backend/store_credits
+//= require spree/backend/style_guide
 //= require spree/backend/taxon_autocomplete
+
 //= require spree/backend/taxon_permalink_preview
 //= require spree/backend/taxon_tree_menu
 //= require spree/backend/taxonomy
 //= require spree/backend/taxons
+//= require spree/backend/translation
 //= require spree/backend/users/edit
 //= require spree/backend/user_picker
 //= require spree/backend/tag_picker
 //= require spree/backend/variant_autocomplete
 //= require spree/backend/variant_management
 //= require spree/backend/zone
-
-Spree.routes.clear_cache = Spree.adminPathFor('general_settings/clear_cache')
-Spree.routes.checkouts_api = Spree.pathFor('api/v1/checkouts')
-Spree.routes.classifications_api = Spree.pathFor('api/v1/classifications')
-Spree.routes.option_types_api = Spree.pathFor('api/v1/option_types')
-Spree.routes.option_values_api = Spree.pathFor('api/v1/option_values')
-Spree.routes.orders_api = Spree.pathFor('api/v1/orders')
-Spree.routes.products_api = Spree.pathFor('api/v1/products')
-Spree.routes.shipments_api = Spree.pathFor('api/v1/shipments')
-Spree.routes.checkouts_api = Spree.pathFor('api/v1/checkouts')
-Spree.routes.stock_locations_api = Spree.pathFor('api/v1/stock_locations')
-Spree.routes.taxon_products_api = Spree.pathFor('api/v1/taxons/products')
-Spree.routes.taxons_api = Spree.pathFor('api/v1/taxons')
-Spree.routes.users_api = Spree.pathFor('api/v1/users')
-Spree.routes.tags_api = Spree.pathFor('api/v1/tags')
-Spree.routes.variants_api = Spree.pathFor('api/v1/variants')
-
-Spree.routes.edit_product = function(product_id) {
-  return Spree.adminPathFor('products/' + product_id + '/edit')
-}
-
-Spree.routes.payments_api = function(order_id) {
-  return Spree.pathFor('api/v1/orders/' + order_id + '/payments')
-}
-
-Spree.routes.stock_items_api = function(stock_location_id) {
-  return Spree.pathFor('api/v1/stock_locations/' + stock_location_id + '/stock_items')
-}
