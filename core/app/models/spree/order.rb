4f21cb2e946b38791a7f71a4350555b5aaa4606f
99cm <zyizyao@china-guide.com>
2018-07-21 12:14:07 -0400

    Add config order mailer class

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -378,7 +378,7 @@ module Spree
     end
 
     def deliver_order_confirmation_email
-      Spree::OrderMailer.confirm_email(self).deliver_later
+      Spree::Config.order_mailer_class.confirm_email(self).deliver_later
       update_column(:confirmation_delivered, true)
     end
 
