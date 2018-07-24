a28b1e8477bd2072a9a970339df36cc105159bbd
99cm <zyizyao@china-guide.com>
2018-07-24 10:42:31 -0400

    Create user_stock_location.rb

---
+++ core/app/models/spree/user_stock_location.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Spree
+  class UserStockLocation < Spree::Base
+    belongs_to :user, class_name: Spree::UserClassHandle.new, inverse_of: :user_stock_locations
+    belongs_to :stock_location, class_name: "Spree::StockLocation", inverse_of: :user_stock_locations
+  end
+end
