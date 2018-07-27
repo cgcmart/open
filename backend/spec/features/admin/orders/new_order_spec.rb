8acdd7b57460ccca0e5621e376ee68c806b7088e
99cm <zyizyao@china-guide.com>
2018-07-26 21:05:15 -0400

    Update new_order_spec.rb

--- backend/spec/features/admin/orders/new_order_spec.rb
+++ backend/spec/features/admin/orders/new_order_spec.rb
@@ -1,68 +1,109 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe 'New Order', type: :feature do
+require 'rails_helper'
+
+RSpec.describe 'New Order', type: :feature do
+  include OrderFeatureHelper
+
   let!(:product) { create(:product_in_stock) }
-  let!(:state) { create(:state) }
+  let!(:state) { create(:state, state_code: 'NY') }
+  let!(:store) { create(:store) }
   let!(:user) { create(:user, ship_address: create(:address), bill_address: create(:address)) }
+  let!(:payment_method) { create(:check_payment_method) }
+  let!(:shipping_method) { create(:shipping_method, cost: 0.0) }
 
   stub_authorization!
 
   before do
-    create(:check_payment_method)
-    create(:shipping_method)
-    # create default store
-    allow(Spree.user_class).to receive(:find_by).and_return(user)
-    create(:store)
     visit spree.new_admin_order_path
+    click_on 'Orders'
+    click_on 'New Order'
   end
 
   it 'does check if you have a billing address before letting you add shipments' do
     click_on 'Shipments'
     expect(page).to have_content 'Please fill in customer info'
-    expect(page).to have_current_path(spree.edit_admin_order_customer_path(Spree::Order.last))
+    expect(current_path).to eql(spree.edit_admin_order_customer_path(Spree::Order.last))
+  end
+
+  it 'default line item quantity is 1', js: true do
+    within '.line-items' do
+      expect(page).to have_field 'quantity', with: '1'
+    end
   end
 
   it 'completes new order successfully without using the cart', js: true do
-    select2_search product.name, from: Spree.t(:name_or_sku)
-    click_icon :add
-    wait_for_ajax
-    click_on 'Customer'
-    select_customer
+    add_line_item product.name
 
-    check 'order_use_billing'
+    click_on 'Customer'
+
+    within '#select-customer' do
+      targetted_select2_search user.email, from: '#s2id_customer_search'
+    end
+
+    expect(page).to have_checked_field('order_use_billing')
     fill_in_address
     click_on 'Update'
 
     click_on 'Payments'
     click_on 'Update'
 
-    expect(page).to have_current_path(spree.admin_order_payments_path(Spree::Order.last))
+    expect(current_path).to eql(spree.admin_order_payments_path(Spree::Order.last))
+
+    click_on 'Confirm'
+    click_on 'Complete Order'
+
+    expect(current_path).to eql(spree.edit_admin_order_path(Spree::Order.last))
+
+    click_on 'Payments'
     click_icon 'capture'
 
     click_on 'Shipments'
     click_on 'Ship'
-    wait_for_ajax
 
-    expect(page).to have_content('shipped')
+    expect(page).to have_content('Shipped')
+  end
+
+  it 'can create split payments', js: true do
+    add_line_item product.name
+
+    click_on 'Customer'
+
+    within '#select-customer' do
+      targetted_select2_search user.email, from: '#s2id_customer_search'
+    end
+
+    expect(page).to have_checked_field('order_use_billing')
+    fill_in_address
+    click_on 'Update'
+
+    click_on 'Payments'
+    fill_in 'Amount', with: '10.00'
+    click_on 'Update'
+
+    click_on 'New Payment'
+    fill_in 'mount', with: '29.98'
+    click_on 'Update'
+
+    expect(page).to have_content('$10.00')
+    expect(page).to have_content('$29.98')
   end
 
   context 'adding new item to the order', js: true do
     it 'inventory items show up just fine and are also registered as shipments' do
-      select2_search product.name, from: Spree.t(:name_or_sku)
-
-      within('table.stock-levels') do
-        fill_in 'variant_quantity', with: 2
-        click_icon :add
-      end
+      add_line_item product.name
 
       within('.line-items') do
         expect(page).to have_content(product.name)
       end
 
       click_on 'Customer'
-      select_customer
 
-      check 'order_use_billing'
+      within '#select-customer' do
+        targetted_select2_search user.email, from: '#s2id_customer_search'
+      end
+
+      expect(page).to have_checked_field('order_use_billing')
       fill_in_address
       click_on 'Update'
 
@@ -74,41 +115,15 @@ describe 'New Order', type: :feature do
     end
   end
 
-  context "adding new item to the order which isn't available", js: true do
-    before do
-      product.update(available_on: nil)
-      select2_search product.name, from: Spree.t(:name_or_sku)
-    end
-
-    it 'inventory items is displayed' do
-      expect(page).to have_content(product.name)
-      expect(page).to have_css('#stock_details')
-    end
-
-    context 'on increase in quantity the product should be removed from order', js: true do
-      before do
-        spree_accept_alert do
-          within('table.stock-levels') do
-            fill_in 'variant_quantity', with: 2
-            click_icon :add
-            wait_for_ajax
-          end
-        end
-      end
-
-      it { expect(page).not_to have_css('#stock_details') }
-    end
-  end
-
-  # Regression test for #3958
+  # Regression test for https://github.com/spree/spree/issues/3958
   context 'without a delivery step', js: true do
     before do
       allow(Spree::Order).to receive_messages checkout_step_names: [:address, :payment, :confirm, :complete]
     end
 
     it 'can still see line items' do
-      select2_search product.name, from: Spree.t(:name_or_sku)
-      click_icon :add
+      add_line_item product.name
+
       within('.line-items') do
         within('.line-item-name') do
           expect(page).to have_content(product.name)
@@ -123,64 +138,75 @@ describe 'New Order', type: :feature do
     end
   end
 
-  # Regression test for #3336
+  # Regression test for https://github.com/spree/spree/issues/3336
   context 'start by customer address' do
     it 'completes order fine', js: true do
       click_on 'Customer'
-      select_customer
 
-      check 'order_use_billing'
+      within '#select-customer' do
+        targetted_select2_search user.email, from: '#s2id_customer_search'
+      end
+
+      expect(page).to have_checked_field('order_use_billing')
       fill_in_address
       click_on 'Update'
 
-      click_on 'Shipments'
-      select2_search product.name, from: Spree.t(:name_or_sku)
-      click_icon :add
-      wait_for_ajax
+      # Automatically redirected to Shipments page
+      select2_search product.name, from: t('spree.name_or_sku')
+
+      click_icon :plus
+
+      expect(page).to have_css('.stock-item')
 
       click_on 'Payments'
       click_on 'Continue'
 
-      within('.additional-info .state') do
-        expect(page).to have_content('complete')
+      within('.additional-info') do
+        expect(page).to have_content('Confirm')
       end
     end
   end
 
-  # Regression test for #5327
+  # Regression test for https://github.com/spree/spree/issues/5327
   context 'customer with default credit card', js: true do
+    let!(:credit_card) { create(:credit_card, user: user) }
+
     before do
-      allow(Spree.user_class).to receive(:find_by).and_return(user)
-      create(:credit_card, default: true, user: user)
+      user.wallet.add(credit_card)
     end
+
     it 'transitions to delivery not to complete' do
-      select2_search product.name, from: Spree.t(:name_or_sku)
-      within('table.stock-levels') do
-        fill_in 'variant_quantity', with: 1
-        click_icon :add
-      end
-      wait_for_ajax
+      add_line_item product.name
+
+      expect(page).to have_css('.line-item')
+
       click_link 'Customer'
-      select_customer
+      targetted_select2 user.email, from: '#s2id_customer_search'
       click_button 'Update'
-      expect(Spree::Order.last.state).to eq 'delivery'
+      expect(page).to have_css('.order-state', text: 'Delivery')
     end
   end
 
-  def fill_in_address(kind = 'bill')
+  context 'customer with attempted XSS', js: true do
+    let(:xss_string) { %(<script>throw('XSS')</script>) }
+    before do
+      user.update!(email: xss_string)
+    end
+    it "displays the user's email escaped without executing" do
+      click_on 'Customer'
+      targetted_select2_search user.email, from: '#s2id_customer_search'
+      expect(page).to have_field('Customer E-Mail', with: xss_string)
+    end
+  end
+
+  def fill_in_address
     fill_in 'First Name',                with: 'John 99'
     fill_in 'Last Name',                 with: 'Doe'
     fill_in 'Street Address',            with: '100 first lane'
     fill_in "Street Address (cont'd)",   with: '#101'
     fill_in 'City',                      with: 'Bethesda'
     fill_in 'Zip',                       with: '20170'
-    targetted_select2_search state.name, from: "#s2id_order_#{kind}_address_attributes_state_id"
+    select state.name, from: 'State'
     fill_in 'Phone',                     with: '123-456-7890'
-  end
-
-  def select_customer
-    within 'div#select-customer' do
-      targetted_select2_search user.email, from: '#s2id_customer_search'
-    end
   end
 end
