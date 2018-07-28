f40434fcb7b88b411e447482a7bdfa19b6f75a85
99cm <zyizyao@china-guide.com>
2018-07-28 11:13:34 -0400

    Add rescue insufficient stock

--- backend/app/controllers/spree/admin/payments_controller.rb
+++ backend/app/controllers/spree/admin/payments_controller.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     class PaymentsController < Spree::Admin::BaseController
-      include Spree::Backend::Callbacks
+      rescue_from Spree::Order::InsufficientStock, with: :insufficient_stock_error
 
       before_action :load_order, only: [:create, :new, :index, :fire]
-      before_action :load_payment, except: [:create, :new, :index]
+      before_action :load_payment, except: [:create, :new, :index, :fire]
+      before_action :load_payment_for_fire, only: :fire
       before_action :load_data
-      before_action :can_not_transition_without_customer_info
+      before_action :require_bill_address, only: [:index]
 
       respond_to :html
 
@@ -17,59 +20,47 @@ module Spree
       end
 
       def new
-        # Move order to payment state in order to capture tax generated on shipments
-        @order.next if @order.can_go_to_state?('payment')
-        @payment = @order.payments.build
+        @payment = @order.payments.build(amount: @order.outstanding_balance)
       end
 
       def create
-        invoke_callbacks(:create, :before)
+        @payment = PaymentCreate.new(@order, object_params).build
+        if @payment.payment_method.source_required? && params[:card].present? && params[:card] != 'new'
+          @payment.source = @payment.payment_method.payment_source_class.find_by(id: params[:card])
+        end
 
         begin
-          if @payment_method.store_credit?
-            payments = @order.add_store_credit_payments
-          else
-            @payment ||= @order.payments.build(object_params)
-            if @payment.payment_method.source_required? && params[:card].present? && params[:card] != 'new'
-              @payment.source = @payment.payment_method.payment_source_class.find_by(id: params[:card])
+          if @payment.save
+            if @order.completed?
+              # If the order was already complete then go ahead and process the payment
+              # (auth and/or capture depending on payment method configuration)
+              @payment.process! if @payment.checkout?
+            else
+              # Transition order as far as it will go.
+              while @order.next; end
             end
-            @payment.save
-            payments = [@payment]
-          end
 
-          if payments && (saved_payments = payments.select &:persisted?).any?
-            invoke_callbacks(:create, :after)
-
-            # Transition order as far as it will go.
-            while @order.next; end
-            # If "@order.next" didn't trigger payment processing already (e.g. if the order was
-            # already complete) then trigger it manually now
-
-            saved_payments.each { |payment| payment.process! if payment.reload.checkout? && @order.complete? }
-            flash[:success] = flash_message_for(saved_payments.first, :successfully_created)
+            flash[:success] = flash_message_for(@payment, :successfully_created)
             redirect_to admin_order_payments_path(@order)
           else
-            @payment ||= @order.payments.build(object_params)
-            invoke_callbacks(:create, :fails)
-            flash[:error] = Spree.t(:payment_could_not_be_created)
+            flash[:error] = t('spree.payment_could_not_be_created')
             render :new
           end
         rescue Spree::Core::GatewayError => e
-          invoke_callbacks(:create, :fails)
           flash[:error] = e.message.to_s
           redirect_to new_admin_order_payment_path(@order)
         end
       end
 
       def fire
-        return unless event = params[:e] and @payment.payment_source
+        return unless event = params[:e] && @payment.payment_source
 
         # Because we have a transition method also called void, we do this to avoid conflicts.
         event = 'void_transaction' if event == 'void'
         if @payment.send("#{event}!")
-          flash[:success] = Spree.t(:payment_updated)
+          flash[:success] = t('spree.payment_updated')
         else
-          flash[:error] = Spree.t(:cannot_perform_operation)
+          flash[:error] = t('spree.cannot_perform_operation')
         end
       rescue Spree::Core::GatewayError => ge
         flash[:error] = ge.message.to_s
@@ -80,7 +71,7 @@ module Spree
       private
 
       def object_params
-        if params[:payment] and params[:payment_source] and source_params = params.delete(:payment_source)[params[:payment][:payment_method_id]]
+        if params[:payment] && params[:payment_source] && (source_params = params.delete(:payment_source)[params[:payment][:payment_method_id]])
           params[:payment][:source_attributes] = source_params
         end
 
@@ -89,27 +80,43 @@ module Spree
 
       def load_data
         @amount = params[:amount] || load_order.total
-        @payment_methods = @order.collect_backend_payment_methods
+        @payment_methods = Spree::PaymentMethod.active.available_to_admin
         if @payment and @payment.payment_method
           @payment_method = @payment.payment_method
         else
-          @payment_method = @payment_methods.find { |payment_method| payment_method.id == params[:payment][:payment_method_id].to_i } if params[:payment]
           @payment_method ||= @payment_methods.first
         end
       end
 
       def load_order
-        @order = Order.find_by!(number: params[:order_id])
+        @order = Spree::Order.find_by!(number: params[:order_id])
         authorize! action, @order
         @order
       end
 
       def load_payment
-        @payment = Payment.find_by!(number: params[:id])
+        @payment = Spree::Payment.find_by!(number: params[:id])
+      end
+
+      def load_payment_for_fire
+        load_payment
+        authorize! params[:e].to_sym, @payment
       end
 
       def model_class
         Spree::Payment
+      end
+
+      def require_bill_address
+        if Spree::Config[:order_bill_address_used] && @order.bill_address.nil?
+          flash[:notice] = t('spree.fill_in_customer_info')
+          redirect_to edit_admin_order_customer_url(@order)
+        end
+      end
+
+      def insufficient_stock_error
+        flash[:error] = t('spree.insufficient_stock_for_order')
+        redirect_to new_admin_order_payment_url(@order)
       end
     end
   end
