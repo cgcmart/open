d14ed908b8cef893f32b1fe2e021601ca0defa03
99cm <zyizyao@china-guide.com>
2018-07-28 17:24:01 -0400

    Create prices_controller_spec.rb

---
+++ backend/spec/controllers/spree/admin/prices_controller_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Spree::Admin::PricesController do
+  stub_authorization!
+
+  let!(:product) { create(:product) }
+
+  describe '#index' do
+    context "when only given a product" do
+      let(:product) { create(:product) }
+
+      subject { get :index, params: { product_id: product.slug } }
+
+      it { is_expected.to be_successful }
+
+      it 'assigns usable instance variables' do
+        subject
+        expect(assigns(:search)).to be_a(Ransack::Search)
+        expect(assigns(:variant_prices)).to eq(product.prices.for_variant)
+        expect(assigns(:master_prices)).to eq(product.prices.for_master)
+        expect(assigns(:product)).to eq(product)
+      end
+    end
+
+    context 'when given a product and a variant' do
+      let(:variant) { create(:variant) }
+      let(:product) { variant.product }
+
+      subject { get :index, params: { product_id: product.slug, variant_id: variant.id } }
+
+      it { is_expected.to be_successful }
+
+      it 'assigns usable instance variables' do
+        subject
+        expect(assigns(:search)).to be_a(Ransack::Search)
+        expect(assigns(:variant_prices)).to eq(product.prices.for_variant)
+        expect(assigns(:master_prices)).to eq(product.prices.for_master)
+        expect(assigns(:variant_prices)).to include(variant.default_price)
+        expect(assigns(:product)).to eq(product)
+      end
+    end
+  end
+end
