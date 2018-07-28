27897a976478ef4f3df7fedf0a08898795a7bb2a
99cm <zyizyao@china-guide.com>
2018-07-28 13:12:38 -0400

    Update state_factory.rb

--- core/lib/spree/testing_support/factories/state_factory.rb
+++ core/lib/spree/testing_support/factories/state_factory.rb
@@ -1,13 +1,28 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/country_factory'
+
 FactoryBot.define do
   factory :state, class: Spree::State do
-    sequence(:name) { |n| "STATE_NAME_#{n}" }
-    sequence(:abbr) { |n| "STATE_ABBR_#{n}" }
-    country do |country|
-      if usa = Spree::Country.find_by(numcode: 840)
-        country = usa
-      else
-        country.association(:country)
+    transient do
+      country_iso 'US'
+      state_code 'AL'
+
+      carmen_subregion do
+        carmen_country = Carmen::Country.coded(country.iso)
+
+        carmen_country.subregions.coded(state_code) ||
+          carmen_country.subregions.sort_by(&:name).first ||
+          fail("Country #{country.iso} has no subregions")
       end
+    end
+
+    abbr { carmen_subregion.code }
+    name { carmen_subregion.name }
+
+    country do |country|
+      Spree::Country.find_by(iso: country_iso) ||
+        country.association(:country, iso: country_iso)
     end
   end
 end
