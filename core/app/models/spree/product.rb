2d77b89b1bda355d88f509f9e3ee6d9110232422
99cm <zyizyao@china-guide.com>
2018-07-29 18:06:49 -0400

    Update product.rb

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -1,24 +1,11 @@
-# PRODUCTS
-# Products represent an entity for sale in a store.
-# Products can have variations, called variants
-# Products properties include description, permalink, availability,
-#   shipping category, etc. that do not change by variant.
-#
-# MASTER VARIANT
-# Every product has one master variant, which stores master price and sku, size and weight, etc.
-# The master variant does not have option values associated with it.
-# Price, SKU, size, weight, etc. are all delegated to the master variant.
-# Contains on_hand inventory levels only when there are no variants for the product.
-#
-# VARIANTS
-# All variants can access the product properties directly (via reverse delegation).
-# Inventory units are tied to Variant.
-# The master variant can have inventory units, but not option values.
-# All other variants have option values and may have inventory units.
-# Sum of on_hand each variant's inventory level determine "on_hand" level for the product.
-#
+# frozen_string_literal: true
+
+require 'discard'
 
 module Spree
+  # Products represent an entity for sale in a store.
+  # Products can have variations, called variants.
+  # Products properties include description, permalink, availability, shipping category, etc. that do not change by variant.
   class Product < Spree::Base
     extend FriendlyId
     include ActsAsTaggable
@@ -26,34 +13,39 @@ module Spree
 
     acts_as_paranoid
 
-    # we need to have this callback before any dependent: :destroy associations
-    # https://github.com/rails/rails/issues/3458
-    before_destroy :ensure_no_line_items
+    include Discard::Model
+    self.discard_column = :deleted_at
+
+    after_discard do
+      variants_including_master.discard_all
+      self.product_option_types = []
+      self.product_properties = []
+      self.classifications = []
+      self.product_promotion_rules = []
+    end
 
     has_many :product_option_types, dependent: :destroy, inverse_of: :product
     has_many :option_types, through: :product_option_types
     has_many :product_properties, dependent: :destroy, inverse_of: :product
     has_many :properties, through: :product_properties
+    has_many :variant_property_rules
+    has_many :variant_property_rule_values, through: :variant_property_rules, source: :values
+    has_many :variant_property_rule_conditions, through: :variant_property_rules, source: :conditions
 
     has_many :classifications, dependent: :delete_all, inverse_of: :product
     has_many :taxons, through: :classifications, before_remove: :remove_taxon
 
-    has_many :product_promotion_rules, class_name: 'Spree::ProductPromotionRule'
-    has_many :promotion_rules, through: :product_promotion_rules, class_name: 'Spree::PromotionRule'
-
-    has_many :promotions, through: :promotion_rules, class_name: 'Spree::Promotion'
-
-    has_many :possible_promotions, -> { advertised.active }, through: :promotion_rules,
-                                                             class_name: 'Spree::Promotion',
-                                                             source: :promotion
+    has_many :product_promotion_rules, dependent: :destroy
+    has_many :promotion_rules, through: :product_promotion_rules
 
     belongs_to :tax_category, class_name: 'Spree::TaxCategory'
     belongs_to :shipping_category, class_name: 'Spree::ShippingCategory', inverse_of: :products
 
     has_one :master,
-            -> { where is_master: true },
+            -> { where(is_master: true).with_deleted },
             inverse_of: :product,
-            class_name: 'Spree::Variant'
+            class_name: 'Spree::Variant',
+            autosave: true
 
     has_many :variants,
              -> { where(is_master: false).order(:position) },
@@ -66,67 +58,77 @@ module Spree
              class_name: 'Spree::Variant',
              dependent: :destroy
 
-    has_many :prices, -> { order('spree_variants.position, spree_variants.id, currency') }, through: :variants
+    has_many :prices, -> { order(Spree::Variant.arel_table[:position].asc, Spree::Variant.arel_table[:id].asc, :currency) }, through: :variants_including_master
 
     has_many :stock_items, through: :variants_including_master
 
     has_many :line_items, through: :variants_including_master
     has_many :orders, through: :line_items
 
+    def find_or_build_master
+      master || build_master
+    end
+
+    MASTER_ATTRIBUTES = [
+      :cost_currency,
+      :cost_price,
+      :depth,
+      :height,
+      :price,
+      :sku,
+      :weight,
+      :width,
+    ]
+    MASTER_ATTRIBUTES.each do |attr|
+      delegate :"#{attr}", :"#{attr}=", to: :find_or_build_master
+    end
+
+    delegate :amount_in,
+             :display_amount,
+             :display_price,
+             :has_default_price?,
+             :images,
+             :price_for,
+             :price_in,
+             :rebuild_vat_prices=,
+             to: :find_or_build_master
+
+    alias_method :master_images, :images
+
     has_many :variant_images, -> { order(:position) }, source: :images, through: :variants_including_master
 
-    after_create :add_associations_from_prototype
     after_create :build_variants_from_option_values_hash, if: :option_values_hash
 
     after_destroy :punch_slug
-    after_restore :update_slug_history
+    after_discard :punch_slug
 
     after_initialize :ensure_master
 
-    after_save :save_master
-    after_save :run_touch_callbacks, if: :anything_changed?
-    after_save :reset_nested_changes
+    after_save :run_touch_callbacks, if: :saved_changes?
     after_touch :touch_taxons
 
     before_validation :normalize_slug, on: :update
     before_validation :validate_master
 
-    with_options length: { maximum: 255 }, allow_blank: true do
-      validates :meta_keywords
-      validates :meta_title
-    end
-    with_options presence: true do
-      validates :name, :shipping_category
-      validates :price, if: proc { Spree::Config[:require_master_price] }
-    end
-
+    validates :meta_keywords, length: { maximum: 255 }
+    validates :meta_title, length: { maximum: 255 }
+    validates :name, presence: true
+    validates :price, presence: true, if: proc { Spree::Config[:require_master_price] }
+    validates :shipping_category_id, presence: true
     validates :slug, presence: true, uniqueness: { allow_blank: true }
-    validate :discontinue_on_must_be_later_than_available_on, if: -> { available_on && discontinue_on }
 
     attr_accessor :option_values_hash
 
-    accepts_nested_attributes_for :product_properties, allow_destroy: true, reject_if: ->(pp) { pp[:property_name].blank? }
+    accepts_nested_attributes_for :variant_property_rules, allow_destroy: true
+    accepts_nested_attributes_for :product_properties, allow_destroy: true, reject_if: lambda { |pp| pp[:property_name].blank? }
 
     alias options product_option_types
 
     self.whitelisted_ransackable_associations = %w[stores variants_including_master master variants]
-    self.whitelisted_ransackable_attributes = %w[description name slug discontinue_on]
-    self.whitelisted_ransackable_scopes = %w[not_discontinued]
+    self.whitelisted_ransackable_attributes = %w[slug]
 
-    [
-      :sku, :price, :currency, :weight, :height, :width, :depth, :is_master,
-      :cost_currency, :price_in, :amount_in, :cost_price
-    ].each do |method_name|
-      delegate method_name, :"#{method_name}=", to: :find_or_build_master
-    end
-
-    delegate :display_amount, :display_price, :has_default_price?,
-             :images, to: :find_or_build_master
-
-    alias master_images images
-
-    def find_or_build_master
-      master || build_master
+    def self.ransackable_scopes(_auth_object = nil)
+      %i(with_deleted with_variant_sku_cont)
     end
 
     # the master variant is not a member of the variants array
@@ -148,10 +150,7 @@ module Spree
     def ensure_option_types_exist_for_values_hash
       return if option_values_hash.nil?
       required_option_type_ids = option_values_hash.keys.map(&:to_i)
-      missing_option_type_ids = required_option_type_ids - option_type_ids
-      missing_option_type_ids.each do |id|
-        product_option_types.create(option_type_id: id)
-      end
+      self.option_type_ids |= required_option_type_ids
     end
 
     # for adding products which are closely related to existing ones
@@ -214,23 +213,27 @@ module Spree
     end
 
     def empty_option_values?
-      options.empty? || options.any? do |opt|
-        opt.option_type.option_values.empty?
-      end
+      options.empty? || !option_types.left_joins(:option_values).where('spree_option_values.id IS NULL').empty?
     end
 
     def property(property_name)
-      product_properties.joins(:property).find_by(spree_properties: { name: property_name }).try(:value)
+      return nil unless prop = properties.find_by(name: property_name)
+      product_properties.find_by(property: prop).try(:value)
     end
 
-    def set_property(property_name, property_value, property_presentation = property_name)
+    def set_property(property_name, property_value)
       ApplicationRecord.transaction do
         # Works around spree_i18n #301
-        property = Property.create_with(presentation: property_presentation).find_or_create_by(name: property_name)
-        product_property = ProductProperty.where(product: self, property: property).first_or_initialize
+        property = Spree::Property.create_with(presentation: property_name).find_or_create_by(name: property_name)
+        product_property = Spree::ProductProperty.where(product: self, property: property).first_or_initialize
         product_property.value = property_value
         product_property.save!
       end
+    end
+
+    def possible_promotions
+      promotion_ids = promotion_rules.map(&:promotion_id).uniq
+      Spree::Promotion.advertised.where(id: promotion_ids).reject(&:inactive?)
     end
 
     def total_on_hand
@@ -241,19 +244,14 @@ module Spree
       end
     end
 
-    # Master variant may be deleted (i.e. when the product is deleted)
-    # which would make AR's default finder return nil.
-    # This is a stopgap for that little problem.
-    def master
-      super || variants_including_master.with_deleted.find_by(is_master: true)
+    def display_image
+      images.first || variant_images.first || Spree::Image.new
     end
 
-    def brand
-      taxons.joins(:taxonomy).find_by(spree_taxonomies: { name: Spree.t(:taxonomy_brands_name) })
-    end
-
-    def category
-      taxons.joins(:taxonomy).find_by(spree_taxonomies: { name: Spree.t(:taxonomy_categories_name) })
+    def find_variant_property_rule(option_value_ids)
+      variant_property_rules.find do |rule|
+        rule.matches_option_value_ids?(option_value_ids)
+      end
     end
 
     private
@@ -269,11 +267,10 @@ module Spree
     end
 
     def any_variants_not_track_inventory?
-      return true unless Spree::Config.track_inventory_levels
       if variants_including_master.loaded?
-        variants_including_master.any? { |v| !v.track_inventory? }
+        variants_including_master.any? { |v| !v.should_track_inventory? }
       else
-        variants_including_master.where(track_inventory: false).exists?
+        !Spree::Config.track_inventory_levels || variants_including_master.where(track_inventory: false).exists?
       end
     end
 
@@ -289,12 +286,12 @@ module Spree
           price: master.price
         )
       end
-      throw(:abort) unless save
+      save
     end
 
     def ensure_master
       return unless new_record?
-      self.master ||= build_master
+      find_or_build_master
     end
 
     def normalize_slug
@@ -303,51 +300,12 @@ module Spree
 
     def punch_slug
       # punch slug with date prefix to allow reuse of original
-      update_column :slug, "#{Time.current.to_i}_#{slug}"[0..254] unless frozen?
+      update_column :slug, "#{Time.current.to_i}_#{slug}" unless frozen?
     end
 
-    def update_slug_history
-      save!
-    end
-
-    def anything_changed?
-      saved_changes? || @nested_changes
-    end
-
-    def reset_nested_changes
-      @nested_changes = false
-    end
-
-    def master_updated?
-      master && (
-        master.new_record? ||
-        master.changed? ||
-        (
-          master.default_price &&
-          (
-            master.default_price.new_record? ||
-            master.default_price.changed?
-          )
-        )
-      )
-    end
-
-    # there's a weird quirk with the delegate stuff that does not automatically save the delegate object
-    # when saving so we force a save using a hook
-    # Fix for issue #5306
-    def save_master
-      if master_updated?
-        master.save!
-        @nested_changes = true
-      end
-    end
-
-    # If the master cannot be saved, the Product object will get its errors
-    # and will be destroyed
+    # If the master is invalid, the Product object will get its errors
     def validate_master
-      # We call master.default_price here to ensure price is initialized.
-      # Required to avoid Variant#check_price validation failing on create.
-      unless master.default_price && master.valid?
+      unless master.valid?
         master.errors.each do |att, error|
           errors.add(att, error)
         end
@@ -366,26 +324,14 @@ module Spree
       run_callbacks(:touch)
     end
 
-    def taxon_and_ancestors
-      taxons.map(&:self_and_ancestors).flatten.uniq
-    end
-
-    # Get the taxonomy ids of all taxons assigned to this product and their ancestors.
-    def taxonomy_ids
-      taxon_and_ancestors.map(&:taxonomy_id).flatten.uniq
-    end
-
     # Iterate through this products taxons and taxonomies and touch their timestamps in a batch
     def touch_taxons
-      Spree::Taxon.where(id: taxon_and_ancestors.map(&:id)).update_all(updated_at: Time.current)
-      Spree::Taxonomy.where(id: taxonomy_ids).update_all(updated_at: Time.current)
-    end
+      taxons_to_touch = taxons.map(&:self_and_ancestors).flatten.uniq
+      unless taxons_to_touch.empty?
+        Spree::Taxon.where(id: taxons_to_touch.map(&:id)).update_all(updated_at: Time.current)
 
-    def ensure_no_line_items
-      if line_items.any?
-        errors.add(:base, :cannot_destroy_if_attached_to_line_items)
-        throw(:abort)
-      end
+        taxonomy_ids_to_touch = taxons_to_touch.map(&:taxonomy_id).flatten.uniq
+        Spree::Taxonomy.where(id: taxonomy_ids_to_touch).update_all(updated_at: Time.current)
     end
 
     def remove_taxon(taxon)
