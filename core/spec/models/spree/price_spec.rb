da3da2d45940c44a2be885484ed65e4de73c65ec
99cm <zyizyao@china-guide.com>
2018-07-29 11:15:41 -0400

    Update price_spec.rb

--- core/spec/models/spree/price_spec.rb
+++ core/spec/models/spree/price_spec.rb
@@ -1,50 +1,30 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Price, type: :model do
-  describe '#amount=' do
-    let(:price) { Spree::Price.new }
-    let(:amount) { '3,0A0' }
+require 'rails_helper'
 
-    before do
-      price.amount = amount
-    end
-
-    it 'is expected to equal to localized number' do
-      expect(price.amount).to eq(Spree::LocalizedNumber.parse(amount))
-    end
-  end
-
-  describe '#price' do
-    let(:price) { Spree::Price.new }
-    let(:amount) { 3000.00 }
-
-    context 'when amount is changed' do
-      before do
-        price.amount = amount
-      end
-
-      it 'is expected to equal to price' do
-        expect(price.amount).to eq(price.price)
-      end
+RSpec.describe Spree::Price, type: :model do
+  describe 'searchable columns' do
+    subject { described_class.whitelisted_ransackable_attributes }
+    it 'allows searching by variant_id' do
+      expect(subject).to include("variant_id")
     end
   end
 
   describe 'validations' do
-    subject { Spree::Price.new variant: variant, amount: amount }
-
     let(:variant) { stub_model Spree::Variant }
+    subject { Spree::Price.new variant: variant, amount: amount }
 
     context 'when the amount is nil' do
       let(:amount) { nil }
-
       it { is_expected.to be_valid }
     end
 
     context 'when the amount is less than 0' do
       let(:amount) { -1 }
 
-      it 'has 1 error_on' do
-        expect(subject.error_on(:amount).size).to eq(1)
+      it 'has 1 error_on amount' do
+        subject.valid?
+        expect(subject.errors(:amount).size).to eq(1)
       end
       it 'populates errors' do
         subject.valid?
@@ -55,8 +35,9 @@ describe Spree::Price, type: :model do
     context 'when the amount is greater than maximum amount' do
       let(:amount) { Spree::Price::MAXIMUM_AMOUNT + 1 }
 
-      it 'has 1 error_on' do
-        expect(subject.error_on(:amount).size).to eq(1)
+      it 'has 1 error_on amount' do
+        subject.valid?
+        expect(subject.errors(:amount).size).to eq(1)
       end
       it 'populates errors' do
         subject.valid?
@@ -66,67 +47,104 @@ describe Spree::Price, type: :model do
 
     context 'when the amount is between 0 and the maximum amount' do
       let(:amount) { Spree::Price::MAXIMUM_AMOUNT }
-
       it { is_expected.to be_valid }
     end
+
+    context '#country_iso' do
+      subject(:price) { build(:price, country_iso: country_iso) }
+
+      context 'when country iso is nil' do
+        let(:country_iso) { nil }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'when country iso is an empty string' do
+        let(:country_iso) { "" }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'when country iso is a country code' do
+        let!(:country) { create(:country, iso: "DE") }
+        let(:country_iso) { "DE" }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'when country iso is not a country code' do
+        let(:country_iso) { "ZZ" }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe "country_iso=" do
+      let(:price) { Spree::Price.new(country_iso: "de") }
+
+      it "assigns nil if passed nil" do
+        price.country_iso = nil
+        expect(price.country_iso).to be_nil
+      end
+    end
+
+    describe '#country' do
+      let!(:country) { create(:country, iso: "DE") }
+      let(:price) { create(:price, country_iso: "DE") }
+
+      it 'returns the country object' do
+        expect(price.country).to eq(country)
+      end
+    end
   end
 
-  describe '#price_including_vat_for(zone)' do
-    subject(:price_with_vat) { price.price_including_vat_for(price_options) }
-
+  describe "#currency" do
     let(:variant) { stub_model Spree::Variant }
-    let(:default_zone) { Spree::Zone.new }
-    let(:zone) { Spree::Zone.new }
-    let(:amount) { 10 }
-    let(:tax_category) { Spree::TaxCategory.new }
-    let(:price) { Spree::Price.new variant: variant, amount: amount }
-    let(:price_options) { { tax_zone: zone } }
+    subject { Spree::Price.new variant: variant, amount: 10, currency: currency }
 
-    context 'when called with a non-default zone' do
-      before do
-        allow(variant).to receive(:tax_category).and_return(tax_category)
-        expect(price).to receive(:default_zone).at_least(:once).and_return(default_zone)
-        allow(price).to receive(:apply_foreign_vat?).and_return(true)
-        allow(price).to receive(:included_tax_amount).with(tax_zone: default_zone, tax_category: tax_category).and_return(0.19)
-        allow(price).to receive(:included_tax_amount).with(tax_zone: zone, tax_category: tax_category).and_return(0.25)
+    describe "validation" do
+      context "with an invalid currency" do
+        let(:currency) { "XYZ" }
+
+        it { is_expected.to be_invalid }
+
+        it "has an understandable error message" do
+          subject.valid?
+          expect(subject.errors.messages[:currency].first).to eq("is not a valid currency code")
+        end
       end
 
-      it 'returns the correct price including another VAT to two digits' do
-        expect(price_with_vat).to eq(10.50)
-      end
-    end
+      context "with a valid currency" do
+        let(:currency) { "USD" }
 
-    context 'when called from the default zone' do
-      before do
-        allow(variant).to receive(:tax_category).and_return(tax_category)
-        expect(price).to receive(:default_zone).at_least(:once).and_return(zone)
-      end
-
-      it 'returns the correct price' do
-        expect(price).to receive(:price).and_call_original
-        expect(price_with_vat).to eq(10.00)
-      end
-    end
-
-    context 'when no default zone is set' do
-      before do
-        allow(variant).to receive(:tax_category).and_return(tax_category)
-        expect(price).to receive(:default_zone).at_least(:once).and_return(nil)
-      end
-
-      it 'returns the correct price' do
-        expect(price).to receive(:price).and_call_original
-        expect(price.price_including_vat_for(tax_zone: zone)).to eq(10.00)
+        it { is_expected.to be_valid }
       end
     end
   end
 
-  describe '#display_price_including_vat_for(zone)' do
-    subject { Spree::Price.new amount: 10 }
+  describe 'scopes' do
+    describe '.for_any_country' do
+      let(:country) { create(:country) }
+      let!(:fallback_price) { create(:price, country_iso: nil) }
+      let!(:country_price) { create(:price, country: country) }
 
-    it 'calls #price_including_vat_for' do
-      expect(subject).to receive(:price_including_vat_for)
-      subject.display_price_including_vat_for(nil)
+      subject { described_class.for_any_country }
+
+      it { is_expected.to include(fallback_price) }
     end
+  end
+
+  describe 'net_amount' do
+    let(:country) { create(:country, iso: "DE") }
+    let(:zone) { create(:zone, countries: [country]) }
+    let!(:tax_rate) { create(:tax_rate, included_in_price: true, zone: zone, tax_categories: [variant.tax_category]) }
+
+    let(:variant) { create(:product).master }
+
+    let(:price) { variant.prices.create(amount: 20, country: country) }
+
+    subject { price.net_amount }
+
+    it { is_expected.to eq(BigDecimal(20) / 1.1) }
   end
 end
