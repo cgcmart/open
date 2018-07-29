65bb86800960939867b6564d4a71d084fc153cd1
99cm <zyizyao@china-guide.com>
2018-07-29 12:07:29 -0400

    Update store_credit_event.rb

--- core/app/models/spree/store_credit_event.rb
+++ core/app/models/spree/store_credit_event.rb
@@ -1,34 +1,57 @@
+# frozen_string_literal: true
+
+require 'discard'
+
 module Spree
   class StoreCreditEvent < Spree::Base
     acts_as_paranoid
 
+    include Discard::Model
+    self.discard_column = :deleted_at
+
     belongs_to :store_credit
     belongs_to :originator, polymorphic: true
+    belongs_to :update_reason, class_name: "Spree::StoreCreditUpdateReason"
 
-    scope :exposed_events, -> { where.not(action: [Spree::StoreCredit::ELIGIBLE_ACTION, Spree::StoreCredit::AUTHORIZE_ACTION]) }
+    validates_presence_of :update_reason, if: :action_requires_reason?
+
+    NON_EXPOSED_ACTIONS = [Spree::StoreCredit::ELIGIBLE_ACTION, Spree::StoreCredit::AUTHORIZE_ACTION]
+
+    scope :exposed_events, -> { exposable_actions.not_invalidated }
+    scope :exposable_actions, -> { where.not(action: NON_EXPOSED_ACTIONS) }
+    scope :not_invalidated, -> { joins(:store_credit).where(spree_store_credits: { invalidated_at: nil }) }
+    scope :chronological, -> { order(:created_at) }
     scope :reverse_chronological, -> { order(created_at: :desc) }
 
     delegate :currency, to: :store_credit
 
+    def capture_action?
+      action == Spree::StoreCredit::CAPTURE_ACTION
+    end
+
+    def authorization_action?
+      action == Spree::StoreCredit::AUTHORIZE_ACTION
+    end
+
+    def action_requires_reason?
+      [Spree::StoreCredit::ADJUSTMENT_ACTION, Spree::StoreCredit::INVALIDATE_ACTION].include?(action)
+    end
+
     def display_amount
-      Spree::Money.new(amount, currency: currency)
+      Spree::Money.new(amount, { currency: currency })
     end
 
     def display_user_total_amount
-      Spree::Money.new(user_total_amount, currency: currency)
+      Spree::Money.new(user_total_amount, { currency: currency })
+    end
+
+    def display_event_date
+      I18n.l(created_at.to_date, format: :long)
     end
 
     def display_action
-      case action
-      when Spree::StoreCredit::CAPTURE_ACTION
-        Spree.t('store_credit.captured')
-      when Spree::StoreCredit::AUTHORIZE_ACTION
-        Spree.t('store_credit.authorized')
-      when Spree::StoreCredit::ALLOCATION_ACTION
-        Spree.t('store_credit.allocated')
-      when Spree::StoreCredit::VOID_ACTION, Spree::StoreCredit::CREDIT_ACTION
-        Spree.t('store_credit.credit')
-      end
+      return if NON_EXPOSED_ACTIONS.include?(action)
+      t("spree.store_credit.display_action.#{action}")
     end
 
     def order
