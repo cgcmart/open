da54ee30175ef33e77de144888e08502bd5f4079
99cm <zyizyao@china-guide.com>
2018-08-01 16:29:40 -0400

    Create promotion_codes_controller.rb

---
+++ backend/app/controllers/spree/admin/promotion_codes_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module Spree
+  module Admin
+    class PromotionCodesController < Spree::Admin::ResourceController
+      def index
+        @promotion = Spree::Promotion.accessible_by(current_ability, :read).find(params[:promotion_id])
+        @promotion_codes = @promotion.promotion_codes
+
+        respond_to do |format|
+          format.html do
+            @promotion_codes = @promotion_codes.page(params[:page]).per(50)
+          end
+          format.csv do
+            filename = "promotion-code-list-#{@promotion.id}.csv"
+            headers["Content-Type"] = "text/csv"
+            headers["Content-disposition"] = "attachment; filename=\"#{filename}\""
+          end
+        end
+      end
+    end
+  end
+end
