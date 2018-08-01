f8a338e5173e1104ccde4ba9ef6a20c9db1b97c8
99cm <zyizyao@china-guide.com>
2018-08-01 12:48:41 -0400

    Update spec_helper.rb

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -1,28 +1,26 @@
+# frozen_string_literal: true
+
 if ENV['COVERAGE']
-  # Run Coverage report
   require 'simplecov'
-  SimpleCov.start do
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Helpers', 'app/helpers'
-    add_group 'Mailers', 'app/mailers'
-    add_group 'Models', 'app/models'
-    add_group 'Views', 'app/views'
-    add_group 'Libraries', 'lib'
-  end
+  SimpleCov.start('rails')
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 
-begin
-  require File.expand_path('../dummy/config/environment', __FILE__)
-rescue LoadError
-  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
-  exit
-end
+require 'spree_api'
+require 'spree/testing_support/dummy_app'
+DummyApp.setup(
+  gem_root: File.expand_path('..', __dir__),
+  lib_name: 'spree_api'
+)
 
+require 'rails-controller-testing'
 require 'rspec/rails'
-require 'ffaker'
+require 'rspec-activemodel-mocks'
+
+require 'database_cleaner'
+require 'with_model'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -30,29 +28,45 @@ Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
 
 require 'spree/testing_support/factories'
 require 'spree/testing_support/preferences'
+require 'spree/testing_support/authorization_helpers'
 
 require 'spree/api/testing_support/caching'
 require 'spree/api/testing_support/helpers'
 require 'spree/api/testing_support/setup'
 
+ActiveJob::Base.queue_adapter = :test
+
 RSpec.configure do |config|
   config.backtrace_exclusion_patterns = [/gems\/activesupport/, /gems\/actionpack/, /gems\/rspec/]
   config.color = true
-  config.fail_fast = ENV['FAIL_FAST'] || false
   config.infer_spec_type_from_file_location!
-  config.raise_errors_for_deprecations!
-  config.use_transactional_fixtures = true
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = :expect
+  end
 
-  config.include JSONAPI::RSpec
   config.include FactoryBot::Syntax::Methods
-  config.include Spree::Api::TestingSupport::Helpers, type: :controller
   config.include Spree::Api::TestingSupport::Helpers, type: :request
+  config.extend Spree::Api::TestingSupport::Setup, type: :request
+  config.include Spree::Api::TestingSupport::Helpers, type: :controller
   config.extend Spree::Api::TestingSupport::Setup, type: :controller
-  config.include Spree::TestingSupport::Preferences, type: :controller
+  config.include Spree::TestingSupport::Preferences
 
-  config.before do
+  config.extend WithModel
+
+  config.before(:each) do
+    Rails.cache.clear
+    reset_spree_preferences
     Spree::Api::Config[:requires_authentication] = true
   end
+
+  config.include ActiveJob::TestHelper
+
+  config.use_transactional_fixtures = true
+
+  config.example_status_persistence_file_path = "./spec/examples.txt"
 
   # Config for running specs while have transition period from Paperclip to ActiveStorage
   if Rails.application.config.use_paperclip
