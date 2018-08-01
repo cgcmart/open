02643abdf97c5ab0c4b0644a35f6abe5a0a3df3a
99cm <zyizyao@china-guide.com>
2018-08-01 11:50:56 -0400

    Update base_controller_spec.rb

--- api/spec/controllers/spree/api/base_controller_spec.rb
+++ api/spec/controllers/spree/api/base_controller_spec.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 class FakesController < Spree::Api::BaseController
 end
 
-describe Spree::Api::BaseController, type: :controller do
+RSpec.describe Spree::Api::BaseController, type: :controller do
   render_views
   controller(Spree::Api::BaseController) do
+    rescue_from Spree::Order::InsufficientStock, with: :insufficient_stock_error
+
     def index
-      render plain: { 'products' => [] }.to_json
+      render plain: { 'products' => [] }
     end
   end
 
@@ -22,19 +26,19 @@ describe Spree::Api::BaseController, type: :controller do
 
     context 'with a correct order token' do
       it 'succeeds' do
-        api_get :index, order_token: order.guest_token, order_id: order.number
+        get :index, params: { order_token: order.guest_token, order_id: order.number }
         expect(response.status).to eq(200)
       end
 
       it 'succeeds with an order_number parameter' do
-        api_get :index, order_token: order.guest_token, order_number: order.number
+        get :index, params: { order_token: order.guest_token, order_number: order.number }
         expect(response.status).to eq(200)
       end
     end
 
     context 'with an incorrect order token' do
       it 'returns unauthorized' do
-        api_get :index, order_token: 'NOT_A_TOKEN', order_id: order.number
+        get :index, params: { order_token: 'NOT_A_TOKEN', order_id: order.number }
         expect(response.status).to eq(401)
       end
     end
@@ -42,43 +46,73 @@ describe Spree::Api::BaseController, type: :controller do
 
   context 'cannot make a request to the API' do
     it 'without an API key' do
-      api_get :index
-      expect(json_response).to eq('error' => 'You must specify an API key.')
+      get :index
+      expect(json_response).to eq({ 'error' => 'You must specify an API key.' })
       expect(response.status).to eq(401)
     end
 
     it 'with an invalid API key' do
       request.headers['X-Spree-Token'] = 'fake_key'
       get :index
-      expect(json_response).to eq('error' => 'Invalid API key (fake_key) specified.')
+      expect(json_response).to eq({ 'error' => 'Invalid API key (fake_key) specified.' })
       expect(response.status).to eq(401)
     end
 
     it 'using an invalid token param' do
       get :index, params: { token: 'fake_key' }
-      expect(json_response).to eq('error' => 'Invalid API key (fake_key) specified.')
+      expect(json_response).to eq({ 'error' => 'Invalid API key (fake_key) specified.' })
     end
   end
 
-  it 'handles parameter missing exceptions' do
-    expect(subject).to receive(:authenticate_user).and_return(true)
-    expect(subject).to receive(:load_user_roles).and_return(true)
-    expect(subject).to receive(:index).and_raise(ActionController::ParameterMissing.new('foo'))
-    get :index, params: { token: 'exception-message' }
-    expect(json_response).to eql('exception' => 'param is missing or the value is empty: foo')
-  end
-
-  it 'handles record invalid exceptions' do
-    expect(subject).to receive(:authenticate_user).and_return(true)
-    expect(subject).to receive(:load_user_roles).and_return(true)
-    resource = Spree::Product.new
-    resource.valid? # get some errors
-    expect(subject).to receive(:index).and_raise(ActiveRecord::RecordInvalid.new(resource))
-    get :index, params: { token: 'exception-message' }
-    expect(json_response).to eql('exception' => "Validation failed: Name can't be blank, Shipping Category can't be blank, Price can't be blank")
-  end
-
-  it 'lets a subclass override the product associations that are eager-loaded' do
+  it "lets a subclass override the product associations that are eager-loaded" do
     expect(controller.respond_to?(:product_includes, true)).to be
+  end
+
+  context 'insufficient stock' do
+    before do
+      expect(subject).to receive(:authenticate_user).and_return(true)
+      expect(subject).to receive(:index).and_raise(Spree::Order::InsufficientStock)
+      get :index, params: { token: "fake_key" }
+    end
+
+    it "should return a 422" do
+      expect(response.status).to eq(422)
+    end
+
+    it "returns an error message" do
+      expect(json_response).to eq(
+        { "errors" => ["Quantity is not available for items in your order"], "type" => "insufficient_stock" }
+      )
+    end
+  end
+
+  context 'lock_order' do
+    let!(:order) { create :order }
+
+    controller(Spree::Api::BaseController) do
+      around_action :lock_order
+
+      def index
+        render json: { "products" => [] }
+      end
+    end
+
+    context 'without an existing lock' do
+      it 'succeeds' do
+        get :index, params: { order_token: order.guest_token, order_id: order.number }
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'with an existing lock' do
+      around do |example|
+        Spree::OrderMutex.with_lock!(order) { example.run }
+      end
+
+      it 'returns a 409 conflict' do
+        get :index, params: { order_token: order.guest_token, order_id: order.number }
+        expect(response.status).to eq(409)
+      end
+    end
   end
 end
