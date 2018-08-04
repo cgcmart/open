7927e875702892bbdecd6dfd7a212d2f655ecf9f
99cm <zyizyao@china-guide.com>
2018-08-04 14:32:34 -0400

    Update factories.rb

--- core/lib/spree/testing_support/factories.rb
+++ core/lib/spree/testing_support/factories.rb
@@ -1,19 +1,7 @@
+# frozen_string_literal: true
+
 require 'factory_bot'
 
-Spree::Zone.class_eval do
-  def self.global
-    find_by(name: 'GlobalZone') || FactoryBot.create(:global_zone)
-  end
-end
-
 Dir["#{File.dirname(__FILE__)}/factories/**"].each do |f|
-  load File.expand_path(f)
-end
-
-FactoryBot.define do
-  sequence(:random_string) { FFaker::Lorem.sentence }
-  sequence(:random_description) { FFaker::Lorem.paragraphs(1 + Kernel.rand(5)).join("\n") }
-  sequence(:random_email) { FFaker::Internet.email }
-
-  sequence(:sku) { |n| "SKU-#{n}" }
+  require File.expand_path(f)
 end
