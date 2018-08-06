c57c4a918f330aed166e987608dd7209392a0744
99cm <zyizyao@china-guide.com>
2018-08-06 13:10:22 -0400

    Create variant_property_rule_value.rb

---
+++ core/app/models/spree/variant_property_rule_value.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Spree
+  class VariantPropertyRuleValue < Spree::Base
+    include Spree::OrderedPropertyValueList
+
+    acts_as_list scope: :variant_property_rule
+
+    belongs_to :property
+    belongs_to :variant_property_rule, touch: true
+  end
+end
