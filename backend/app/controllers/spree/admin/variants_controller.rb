106098a0e44178d7c59c5d8b8267b5615938b879
99cm <zyizyao@china-guide.com>
2018-08-06 11:01:39 -0400

    Update variants_controller.rb

--- backend/app/controllers/spree/admin/variants_controller.rb
+++ backend/app/controllers/spree/admin/variants_controller.rb
@@ -1,56 +1,41 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     class VariantsController < ResourceController
+      helper 'spree/admin/products'
+
       belongs_to 'spree/product', find_by: :slug
       new_action.before :new_before
       before_action :redirect_on_empty_option_values, only: [:new]
       before_action :load_data, only: [:new, :create, :edit, :update]
 
-      # override the destroy method to set deleted_at value
-      # instead of actually deleting the product.
-      def destroy
-        @variant = Variant.find(params[:id])
-        if @variant.destroy
-          flash[:success] = Spree.t('notice_messages.variant_deleted')
-        else
-          flash[:error] = Spree.t('notice_messages.variant_not_deleted', error: @variant.errors.full_messages.to_sentence)
-        end
-
-        respond_with(@variant) do |format|
-          format.html { redirect_to admin_product_variants_url(params[:product_id]) }
-          format.js { render_js_for_destroy }
-        end
-      end
-
-      protected
+      private
 
       def new_before
-        master = @object.product.master
-        @object.attributes = master.attributes.except(
+        @object.attributes = @object.product.master.attributes.except(
           'id', 'created_at', 'deleted_at', 'sku', 'is_master'
         )
 
         # Shallow Clone of the default price to populate the price field.
-        @object.default_price = master.default_price.clone if master.default_price.present?
+        @object.default_price = @object.product.master.default_price.clone
       end
 
       def parent
-        @product = Product.with_deleted.friendly.find(params[:product_id])
+        @parent ||= Spree::Product.with_deleted.find_by(slug: params[:product_id])
+        @product = @parent
       end
 
       def collection
-        @deleted = params.key?(:deleted) && params[:deleted] == 'on' ? 'checked' : ''
+        if params[:deleted] == "on"
+          base_variant_scope ||= super.with_deleted
+        else
+          base_variant_scope ||= super
+        end
 
-        @collection ||=
-          if @deleted.blank?
-            super.includes(:default_price, option_values: :option_type)
-          else
-            Variant.only_deleted.where(product_id: parent.id)
-          end
-        @collection
+        search = Spree::Config.variant_search_class.new(params[:variant_search_term], scope: base_variant_scope)
+        @collection = search.results.includes(variant_includes).page(params[:page]).per(Spree::Config[:admin_variants_per_page])
       end
-
-      private
 
       def load_data
         @tax_categories = TaxCategory.order(:name)
