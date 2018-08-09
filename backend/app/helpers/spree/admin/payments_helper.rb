be6f0f1cfe31ab773f2436667501ac5da469f6c0
99cm <zyizyao@china-guide.com>
2018-08-09 17:46:34 -0400

    Update payments_helper.rb

--- backend/app/helpers/spree/admin/payments_helper.rb
+++ backend/app/helpers/spree/admin/payments_helper.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     module PaymentsHelper
       def payment_method_name(payment)
-        # HACK: to allow us to retrieve the name of a "deleted" payment method
-        id = payment.payment_method_id
-        Spree::PaymentMethod.find_with_destroyed(id).name
+        payment.payment_method.name
       end
     end
   end
