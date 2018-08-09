96d9c666b17c8bdab2364b7201257d7a181adb4b
99cm <zyizyao@china-guide.com>
2018-08-09 17:31:14 -0400

    Update base_helper.rb

--- backend/app/helpers/spree/admin/base_helper.rb
+++ backend/app/helpers/spree/admin/base_helper.rb
@@ -1,28 +1,15 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     module BaseHelper
-      def flash_alert(flash)
-        if flash.present?
-          close_button = button_tag(class: 'close', 'data-dismiss' => 'alert', 'aria-label' => Spree.t(:close)) do
-            content_tag('span', '&times;'.html_safe, 'aria-hidden' => true)
-          end
-          message = flash[:error] || flash[:notice] || flash[:success]
-          flash_class = 'danger' if flash[:error]
-          flash_class = 'info' if flash[:notice]
-          flash_class = 'success' if flash[:success]
-          flash_div = content_tag(:div, (close_button + message), class: "alert alert-#{flash_class} alert-auto-disappear")
-          content_tag(:div, flash_div, class: 'col-xs-12')
-        end
-      end
-
       def field_container(model, method, options = {}, &block)
         css_classes = options[:class].to_a
         css_classes << 'field'
-        css_classes << 'withError' if error_message_on(model, method).present?
-        content_tag(
-          :div, capture(&block),
-          options.merge(class: css_classes.join(' '), id: "#{model}_#{method}_field")
-        )
+        if error_message_on(model, method).present?
+          css_classes << 'withError'
+        end
+        content_tag(:div, capture(&block), class: css_classes.join(' '), id: "#{model}_#{method}_field")
       end
 
       def error_message_on(object, method, _options = {})
@@ -37,45 +24,26 @@ module Spree
         end
       end
 
+      def admin_hint(title, text)
+        content_tag(:span, class: 'hint-tooltip', title: title, data: { content: text }) do
+          content_tag(:i, '', class: 'fa fa-info-circle')
+        end
+      end
+
       def datepicker_field_value(date)
         unless date.blank?
-          l(date, format: Spree.t('date_picker.format', default: '%Y/%m/%d'))
+          l(date, format: t('spree.date_picker.format', default: '%Y/%m/%d'))
         end
       end
 
       def preference_field_tag(name, value, options)
-        case options[:type]
-        when :integer
-          text_field_tag(name, value, preference_field_options(options))
-        when :boolean
-          hidden_field_tag(name, 0, id: "#{name}_hidden") +
-            check_box_tag(name, 1, value, preference_field_options(options))
-        when :string
-          text_field_tag(name, value, preference_field_options(options))
-        when :password
-          password_field_tag(name, value, preference_field_options(options))
-        when :text
-          text_area_tag(name, value, preference_field_options(options))
-        else
-          text_field_tag(name, value, preference_field_options(options))
-        end
+        type = options.delete(:type) || :text
+        render "spree/admin/shared/preference_fields/#{type}", name: name, value: value, html_options: options
       end
 
       def preference_field_for(form, field, options)
-        case options[:type]
-        when :integer
-          form.text_field(field, preference_field_options(options))
-        when :boolean
-          form.check_box(field, preference_field_options(options))
-        when :string
-          form.text_field(field, preference_field_options(options))
-        when :password
-          form.password_field(field, preference_field_options(options))
-        when :text
-          form.text_area(field, preference_field_options(options))
-        else
-          form.text_field(field, preference_field_options(options))
-        end
+        type = options.delete(:type) || :text
+        render "spree/admin/shared/preference_fields/#{type}", form: form, attribute: field, html_options: options
       end
 
       def preference_field_options(options)
@@ -83,66 +51,70 @@ module Spree
                         when :integer
                           {
                             size: 10,
-                            class: 'input_integer form-control'
+                            class: 'input_integer'
                           }
                         when :boolean
                           {}
                         when :string
                           {
                             size: 10,
-                            class: 'input_string form-control'
+                            class: 'input_string fullwidth'
                           }
                         when :password
                           {
                             size: 10,
-                            class: 'password_string form-control'
+                            class: 'password_string fullwidth'
                           }
                         when :text
                           {
                             rows: 15,
                             cols: 85,
-                            class: 'form-control'
+                            class: 'fullwidth'
                           }
                         else
                           {
                             size: 10,
-                            class: 'input_string form-control'
+                            class: 'input_string fullwidth'
                           }
         end
 
-        field_options.merge!(readonly: options[:readonly],
+        field_options.merge!({ readonly: options[:readonly],
                              disabled: options[:disabled],
-                             size:     options[:size])
+                             size:     options[:size] })
       end
 
       def preference_fields(object, form)
         return unless object.respond_to?(:preferences)
-        fields = object.preferences.keys.map do |key|
-          if object.has_preference?(key)
-            form.label("preferred_#{key}", Spree.t(key) + ': ') +
-              preference_field_for(form, "preferred_#{key}", type: object.preference_type(key))
+        capture do
+          object.preferences.keys.each do |key|
+            concat render("spree/admin/shared/preference_fields/#{object.preference_type(key)}", form: form, attribute: "preferred_#{key}", label: t(key, scope: 'spree'))
           end
         end
-        safe_join(fields, '<br />'.html_safe)
+      end
+
+      def link_to_add_fields(name, target, options = {})
+        name = '' if options[:no_text]
+        css_classes = options[:class] ? options[:class] + " spree_add_fields" : "spree_add_fields"
+        link_to_with_icon('plus', name, 'javascript:', data: { target: target }, class: css_classes)
       end
 
       # renders hidden field and link to remove record using nested_attributes
-      def link_to_icon_remove_fields(f)
-        url = f.object.persisted? ? [:admin, f.object] : '#'
-        link_to_with_icon('delete', '', url, class: 'spree_remove_fields btn btn-sm btn-danger', data: { action: 'remove' }, title: Spree.t(:remove)) + f.hidden_field(:_destroy)
+      def link_to_remove_fields(name, form, options = {})
+        name = '' if options[:no_text]
+        options[:class] = '' unless options[:class]
+        options[:class] += 'no-text with-tip' if options[:no_text]
+        url = form.object.persisted? ? [:admin, form.object] : '#'
+        link_to_with_icon('trash', name, url, class: "spree_remove_fields #{options[:class]}", data: { action: 'remove' }, title: t('spree.actions.remove')) + form.hidden_field(:_destroy)
       end
+        
 
       def spree_dom_id(record)
         dom_id(record, 'spree')
       end
 
-      I18N_PLURAL_MANY_COUNT = 2.1
-      def plural_resource_name(resource_class)
-        resource_class.model_name.human(count: I18N_PLURAL_MANY_COUNT)
-      end
-
-      def order_time(time)
-        [I18n.l(time.to_date), time.strftime('%l:%M %p').strip].join(' ')
+      def admin_layout(layout = nil)
+        @admin_layout = layout if layout
+        @admin_layout
       end
     end
   end
