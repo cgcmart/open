d6a225af7110de6ac8510a7e4795554624a449cf
99cm <zyizyao@china-guide.com>
2018-08-10 12:18:59 -0400

    Create promotions_controller.rb

---
+++ api/app/controllers/spree/api/promotions_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Spree
+  module Api
+    class PromotionsController < Spree::Api::BaseController
+      before_action :requires_admin
+      before_action :load_promotion
+
+      def show
+        if @promotion
+          respond_with(@promotion, default_template: :show)
+        else
+          raise ActiveRecord::RecordNotFound
+        end
+      end
+
+      private
+
+      def requires_admin
+        return if @current_user_roles.include?("admin")
+        unauthorized && return
+      end
+
+      def load_promotion
+        @promotion = Spree::Promotion.find_by(id: params[:id]) || Spree::Promotion.with_coupon_code(params[:id])
+      end
+    end
+  end
+end
