05c1491e25010abe47dfc6c1aacc3e6e7b91ce17
99cm <zyizyao@china-guide.com>
2018-08-11 11:40:47 -0400

    Create with_translations.js

---
+++ backend/spec/javascripts/support/with_translations.js
@@ -0,0 +1,10 @@
+support.withTranslations = function(translations) {
+  beforeEach(function() {
+    this._oldTranslations = Spree.translations;
+    Spree.translations = translations;
+  });
+
+  afterEach(function() {
+    Spree.translations = this._oldTranslations;
+  });
+};
