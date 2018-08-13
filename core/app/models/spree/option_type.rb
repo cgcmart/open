9094a5ae61a168eab6e8272a3079b58b7afd9c30
99cm <zyizyao@china-guide.com>
2018-08-13 13:25:29 -0400

    Update option_type.rb

--- core/app/models/spree/option_type.rb
+++ core/app/models/spree/option_type.rb
@@ -1,32 +1,30 @@
+# frozen_string_literal: true
+
 module Spree
   class OptionType < Spree::Base
     acts_as_list
 
-    with_options dependent: :destroy, inverse_of: :option_type do
-      has_many :option_values, -> { order(:position) }
-      has_many :product_option_types
-    end
-
+    has_many :option_values, -> { order(:position) }, dependent: :destroy, inverse_of: :option_type
+    has_many :product_option_types, dependent: :destroy, inverse_of: :option_type
     has_many :products, through: :product_option_types
 
-    has_many :option_type_prototypes, class_name: 'Spree::OptionTypePrototype'
-    has_many :prototypes, through: :option_type_prototypes, class_name: 'Spree::Prototype'
+    has_many :option_type_prototypes
+    has_many :prototypes, through: :option_type_prototypes
 
-    with_options presence: true do
-      validates :name, uniqueness: { case_sensitive: false, allow_blank: true }
-      validates :presentation
-    end
+    validates :name, presence: true, uniqueness: { allow_blank: true }
+    validates :presentation, presence: true
 
     default_scope { order(:position) }
 
-    accepts_nested_attributes_for :option_values, reject_if: ->(ov) { ov[:name].blank? || ov[:presentation].blank? }, allow_destroy: true
+    accepts_nested_attributes_for :option_values, reject_if: lambda { |ov| ov[:name].blank? && ov[:presentation].blank? }, allow_destroy: true
 
     after_touch :touch_all_products
+    after_save :touch_all_products
 
     private
 
     def touch_all_products
-      products.update_all(updated_at: Time.current)
+      products.find_each(&:touch)
     end
   end
 end
