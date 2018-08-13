50c4c621975c42a8a66d45edfa234c765e395ddd
99cm <zyizyao@china-guide.com>
2018-08-13 13:13:29 -0400

    Update option_type_prototype.rb

--- core/app/models/spree/option_type_prototype.rb
+++ core/app/models/spree/option_type_prototype.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 module Spree
   class OptionTypePrototype < Spree::Base
-    belongs_to :option_type, class_name: 'Spree::OptionType'
-    belongs_to :prototype, class_name: 'Spree::Prototype'
-
+    belongs_to :option_type
+    belongs_to :prototype
     validates :prototype, :option_type, presence: true
     validates :prototype_id, uniqueness: { scope: :option_type_id }
   end
