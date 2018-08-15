15ffacf436e02287f5ccecb0474f6c95c5a0a90a
99cm <zyizyao@china-guide.com>
2018-08-15 10:57:26 -0400

    Update product_spec.rb

--- core/spec/models/spree/product_spec.rb
+++ core/spec/models/spree/product_spec.rb
@@ -491,9 +491,11 @@ RSpec.describe Spree::Product, type: :model do
 
     context 'when prototype with option types is supplied' do
       def build_option_type_with_values(name, values)
-        values.each_with_object(create(:option_type, name: name)) do |val, ot|
+        ot = create(:option_type, name: name)
+        Values.each do |val|
           ot.option_values.create(name: val.downcase, presentation: val)
         end
+        ot
       end
 
       let(:prototype) do
@@ -509,34 +511,33 @@ RSpec.describe Spree::Product, type: :model do
         hash
       end
 
-      it 'creates option types based on the prototype' do
+      it 'should create option types based on the prototype' do
         product.save
         expect(product.option_type_ids.length).to eq(1)
         expect(product.option_type_ids).to eq(prototype.option_type_ids)
       end
 
-      it 'creates product option types based on the prototype' do
+      it 'should create product option types based on the prototype' do
         product.save
         expect(product.product_option_types.pluck(:option_type_id)).to eq(prototype.option_type_ids)
       end
 
-      it 'creates variants from an option values hash with one option type' do
+      it 'should create variants from an option values hash with one option type' do
         product.option_values_hash = option_values_hash
         product.save
         expect(product.variants.length).to eq(3)
       end
 
-      it 'stills create variants when option_values_hash is given but prototype id is nil' do
+      it 'should still create variants when option_values_hash is given but prototype id is nil' do
         product.option_values_hash = option_values_hash
         product.prototype_id = nil
         product.save
-        product.reload
         expect(product.option_type_ids.length).to eq(1)
         expect(product.option_type_ids).to eq(prototype.option_type_ids)
         expect(product.variants.length).to eq(3)
       end
 
-      it 'creates variants from an option values hash with multiple option types' do
+      it 'should create variants from an option values hash with multiple option types' do
         color = build_option_type_with_values('color', %w(Red Green Blue))
         logo  = build_option_type_with_values('logo', %w(Ruby Rails Nginx))
         option_values_hash[color.id.to_s] = color.option_value_ids
