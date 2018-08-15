3f8a0e0243f76cda2a5a523c64d23b81f035f0d0
99cm <zyizyao@china-guide.com>
2018-08-15 11:07:54 -0400

    Update prototypes.rb

--- sample/db/samples/prototypes.rb
+++ sample/db/samples/prototypes.rb
@@ -14,9 +14,8 @@ prototypes = [
 ]
 
 prototypes.each do |prototype_attrs|
-  prototype = Spree::Prototype.where(name: prototype_attrs[:name]).first_or_create!
-  prototype_attrs[:properties].each do |property_name|
-    property = Spree::Property.where(name: property_name).first
-    prototype.properties << property unless prototype.properties.include?(property)
+  prototype = Spree::Prototype.create!(name: prototype_attrs[:name])
+  prototype_attrs[:properties].each do |property|
+    prototype.properties << Spree::property.find_by_name!(property)
   end
 end
