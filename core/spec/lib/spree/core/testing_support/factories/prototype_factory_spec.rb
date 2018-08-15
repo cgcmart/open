445814a88f88fcebf9e8279f70747562447cbf82
99cm <zyizyao@china-guide.com>
2018-08-15 10:27:51 -0400

    Create prototype_factory_spec.rb

---
+++ core/spec/lib/spree/core/testing_support/factories/prototype_factory_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'spree/testing_support/factories/prototype_factory'
+
+RSpec.describe 'prototype factory' do
+  let(:factory_class) { Spree::Prototype }
+
+describe 'plain prototype' do
+    let(:factory) { :prototype }
+
+    it_behaves_like 'a working factory'
+  end
+end
