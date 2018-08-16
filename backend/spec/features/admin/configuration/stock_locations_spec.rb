5745e0a918b41bf54a008c9b2807b44ac313f81f
99cm <zyizyao@china-guide.com>
2018-08-16 16:23:31 -0400

    Update stock_locations_spec.rb

--- backend/spec/features/admin/configuration/stock_locations_spec.rb
+++ backend/spec/features/admin/configuration/stock_locations_spec.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'Stock Locations', type: :feature do
   stub_authorization!
 
-  before do
+  before(:each) do
     create(:country)
-    visit spree.admin_stock_locations_path
+    visit spree.admin_path
+    click_link "Settings"
+    click_link "Shipping"
+    click_link "Stock Locations"
   end
 
   it 'can create a new stock location' do
@@ -24,16 +29,17 @@ describe 'Stock Locations', type: :feature do
     visit current_path
 
     expect(find('#listing_stock_locations')).to have_content('NY Warehouse')
-    spree_accept_alert do
-      click_icon :delete
-      # Wait for API request to complete.
-      wait_for_ajax
+    accept_alert do
+      click_icon :trash
     end
+
+    expect(page).to have_content('Stock Location "NY Warehouse" has been successfully removed')
+
     visit current_path
     expect(page).to have_content('No Stock Locations found')
   end
 
-  it 'can update an existing stock location', js: true do
+  it 'can update an existing stock location' do
     create(:stock_location)
     visit current_path
 
