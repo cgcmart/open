2b0d6dec5971163e2802415ca8daec7c2cb4af88
99cm <zyizyao@china-guide.com>
2018-08-16 18:37:30 -0400

    Update line_items_spec.rb

--- backend/spec/features/admin/orders/line_items_spec.rb
+++ backend/spec/features/admin/orders/line_items_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-# Tests for #3958's features
+# Tests for https://github.com/spree/spree/issues/3958's features
 describe 'Order Line Items', type: :feature, js: true do
   stub_authorization!
 
@@ -19,34 +21,32 @@ describe 'Order Line Items', type: :feature, js: true do
   it "can edit a line item's quantity" do
     visit spree.edit_admin_order_path(order)
     within('.line-items') do
-      within_row(1) do
-        find('.edit-line-item').click
-        fill_in 'quantity', with: 10
-        find('.save-line-item').click
-        within '.line-item-qty-show' do
-          expect(page).to have_content('10')
-        end
-        within '.line-item-total' do
-          expect(page).to have_content('$199.90')
-        end
-      end
+      find('.edit-line-item').click
+      fill_in 'quantity', with: 10
+      find('.save-line-item').click
+    end
+    within '.line-item-qty-show' do
+      expect(page).to have_content('10')
+    end
+    within '.line-item-total' do
+      expect(page).to have_content('$199.90')
     end
   end
 
   it 'can delete a line item' do
-    visit spree.edit_admin_order_path(order)
+    visit spree.cart_admin_order_path(order)
 
     product_name = find('.line-items tr:nth-child(1) .line-item-name').text
 
     within('.line-items') do
       within_row(1) do
-        spree_accept_alert do
+        accept_alert do
           find('.delete-line-item').click
-          wait_for_ajax
         end
       end
     end
 
+    expect(find('#order_total')).to have_content("$0.00")
     expect(page).not_to have_content(product_name)
   end
 end
