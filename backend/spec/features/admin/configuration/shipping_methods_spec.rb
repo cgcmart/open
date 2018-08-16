a554d475d9b6f21d2ad92a5a264f7255ce8d4ca6
99cm <zyizyao@china-guide.com>
2018-08-16 16:13:51 -0400

    Update shipping_methods_spec.rb

--- backend/spec/features/admin/configuration/shipping_methods_spec.rb
+++ backend/spec/features/admin/configuration/shipping_methods_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'Shipping Methods', type: :feature do
@@ -5,32 +7,25 @@ describe 'Shipping Methods', type: :feature do
   let!(:zone) { create(:global_zone) }
   let!(:shipping_method) { create(:shipping_method, zones: [zone]) }
 
-  after do
-    Capybara.ignore_hidden_elements = true
-  end
-
   before do
-    Capybara.ignore_hidden_elements = false
-    # HACK: To work around no email prompting on check out
-    allow_any_instance_of(Spree::Order).to receive_messages(require_email: false)
-    create(:check_payment_method)
-
     visit spree.admin_shipping_methods_path
+    click_link "Settings"
+    click_link "Shipping"
   end
 
   context 'show' do
-    it 'displays existing shipping methods' do
+    it 'should display existing shipping methods' do
       within_row(1) do
         expect(column_text(1)).to eq(shipping_method.name)
         expect(column_text(2)).to eq(zone.name)
         expect(column_text(3)).to eq('Flat rate')
-        expect(column_text(4)).to eq('Both')
+        expect(column_text(4)).to eq('Yes')
       end
     end
   end
 
-  context 'create' do
-    it 'is able to create a new shipping method' do
+  context 'create', js: true do
+    it 'should be able to create a new shipping method' do
       click_link 'New Shipping Method'
 
       fill_in 'shipping_method_name', with: 'bullock cart'
@@ -40,23 +35,80 @@ describe 'Shipping Methods', type: :feature do
       end
 
       click_on 'Create'
-      expect(page).to have_current_path(spree.edit_admin_shipping_method_path(Spree::ShippingMethod.last))
+      expect(current_path).to eql(spree.edit_admin_shipping_method_path(Spree::ShippingMethod.last))
+    end
+
+    context 'with shipping method having a calculator with array or hash preference type' do
+      before do
+        class ComplexShipments < Spree::ShippingCalculator
+          preference :amount, :decimal
+          preference :currency, :string
+          preference :mapping, :hash
+          preference :list, :array
+
+          def self.description
+            "Complex Shipments"
+          end
+        end
+        @calculators = Rails.application.config.spree.calculators.shipping_methods
+        Rails.application.config.spree.calculators.shipping_methods = [ComplexShipments]
+      end
+
+      after do
+        Rails.application.config.spree.calculators.shipping_methods = @calculators
+      end
+
+      it "does not show array and hash form fields" do
+        click_link "New Shipping Method"
+
+        fill_in "shipping_method_name", with: "bullock cart"
+
+        within("#shipping_method_categories_field") do
+          check first("input[type='checkbox']")["name"]
+        end
+
+        click_on "Create"
+        select 'Complex Shipments', from: 'Base Calculator'
+        click_on "Update"
+
+        expect(page).to have_field('Amount')
+        expect(page).to have_field('Currency')
+        expect(page).to_not have_field('Mapping')
+        expect(page).to_not have_field('List')
+      end
     end
   end
 
-  # Regression test for #1331
+  # Regression test for https://github.com/spree/spree/issues/1331
   context 'update' do
-    it 'can change the calculator', js: true do
+    it 'can change the existing calculator', js: true do
       within('#listing_shipping_methods') do
         click_icon :edit
       end
 
-      expect(find(:css, '.calculator-settings-warning')).not_to be_visible
-      select2_search('Flexible Rate', from: 'Calculator')
-      expect(find(:css, '.calculator-settings-warning')).to be_visible
+      fill_in 'Amount', with: 20
 
-      click_button 'Update'
-      expect(page).not_to have_content('Shipping method is not found')
+      click_button "Update"
+
+      expect(page).to have_content 'successfully updated'
+      expect(page).to have_field 'Amount', with: '20.0'
+    end
+
+    it "can change the calculator", js: true do
+      within("#listing_shipping_methods") do
+        click_icon :edit
+      end
+
+      select 'Flexible Rate per package item', from: 'Base Calculator'
+
+      fill_in 'First Item', with: 10
+      fill_in 'Additional Item', with: 20
+
+      click_button "Update"
+
+      expect(page).to have_content 'successfully updated'
+      expect(page).to have_field 'First Item', with: '10.0'
+      expect(page).to have_field 'Additional Item', with: '20.0'
     end
   end
 end
