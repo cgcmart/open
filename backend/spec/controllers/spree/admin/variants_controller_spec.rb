413e5b5a9ba660cccf059e42d2d15ebd03928b13
99cm <zyizyao@china-guide.com>
2018-08-16 15:47:37 -0400

    Update variants_controller_spec.rb

--- backend/spec/controllers/spree/admin/variants_controller_spec.rb
+++ backend/spec/controllers/spree/admin/variants_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 module Spree
@@ -7,99 +9,61 @@ module Spree
 
       describe '#index' do
         let(:product) { create(:product) }
-        let!(:variant_1) { create(:variant, product: product) }
-        let!(:variant_2) { create(:variant, product: product) }
+        let(:params) { { product_id: product.slug } }
 
-        context 'deleted is not requested' do
-          it 'assigns the variants for a requested product' do
-            spree_get :index, product_id: product.slug
-            expect(assigns(:collection)).to include variant_1
-            expect(assigns(:collection)).to include variant_2
+        subject { get :index, params: params }
+
+        context 'the value of @parent' do
+          it 'is the product' do
+            subject
+            expect(assigns(:parent)).to eq product
+          end
+
+          context 'with a deleted product' do
+            before { product.discard }
+
+            it 'is the product' do
+            subject
+            expect(assigns(:parent)).to eq product
           end
         end
+      end
 
-        context 'deleted is requested' do
-          before { variant_2.destroy }
+      context "the value of @collection" do
+          let!(:variant) { create(:variant, product: product) }
+          let!(:deleted_variant) { create(:variant, product: product) }
 
-          it 'assigns only deleted variants for a requested product' do
-            spree_get :index, product_id: product.slug, deleted: 'on'
-            expect(assigns(:collection)).not_to include variant_1
-            expect(assigns(:collection)).to include variant_2
+          context "with soft-deleted variants" do
+            before { deleted_variant.discard }
+
+            context "when deleted is not requested" do
+              it "excludes deleted variants" do
+                subject
+                expect(assigns(:collection)).to include variant
+                expect(assigns(:collection)).not_to include deleted_variant
+              end
+            end
+
+            context "when deleted is requested" do
+              let(:params) { { product_id: product.slug, deleted: "on" } }
+
+              it "includes deleted variants" do
+                subject
+                expect(assigns(:collection)).to include variant
+                expect(assigns(:collection)).to include deleted_variant
+              end
+            end
           end
         end
       end
 
       describe '#destroy' do
-        subject(:send_request) do
-          spree_delete :destroy, product_id: product, id: variant, format: :js
-        end
+        let!(:variant) { create(:variant) }
+        let(:product) { variant.product }
 
-        let(:variant) { mock_model(Spree::Variant) }
-        let(:variants) { double(ActiveRecord::Relation) }
-        let(:product) { mock_model(Spree::Product) }
-        let(:products) { double(ActiveRecord::Relation) }
-
-        before do
-          allow(Spree::Product).to receive(:friendly).and_return(products)
-          allow(products).to receive(:find).with(product.id.to_s).and_return(product)
-          allow(product).to receive_message_chain(:variants, :find).with(variant.id.to_s).and_return(variants)
-
-          allow(Spree::Variant).to receive(:find).with(variant.id.to_s).and_return(variant)
-        end
-
-        describe 'expects to receive' do
-          after { send_request }
-
-          it { expect(Spree::Product).to receive(:friendly).and_return(products) }
-          it { expect(products).to receive(:find).with(product.id.to_s).and_return(product) }
-          it { expect(product).to receive_message_chain(:variants, :find).with(variant.id.to_s).and_return(variants) }
-          it { expect(Spree::Variant).to receive(:find).with(variant.id.to_s).and_return(variant) }
-        end
-
-        shared_examples 'correct response' do
-          it { expect(assigns(:variant)).to eq(variant) }
-          it { expect(response).to have_http_status(:ok) }
-        end
-
-        context 'will successfully destroy variant' do
-          before { allow(variant).to receive(:destroy).and_return(true) }
-
-          describe 'expects to receive' do
-            after { send_request }
-
-            it { expect(variant).to receive(:destroy).and_return(true) }
-          end
-
-          describe 'returns response' do
-            before { send_request }
-
-            it_behaves_like 'correct response'
-            it { expect(flash[:success]).to eq(Spree.t('notice_messages.variant_deleted')) }
-          end
-        end
-
-        context 'will not successfully destroy product' do
-          let(:error_msg) { 'Failed to delete' }
-
-          before do
-            allow(variant).to receive_message_chain(:errors, :full_messages).and_return([error_msg])
-            allow(variant).to receive(:destroy).and_return(false)
-          end
-
-          describe 'expects to receive' do
-            after { send_request }
-
-            it { expect(variant).to receive_message_chain(:errors, :full_messages).and_return([error_msg]) }
-            it { expect(variant).to receive(:destroy).and_return(false) }
-          end
-
-          describe 'returns response' do
-            before { send_request }
-
-            it_behaves_like 'correct response'
-
-            it { expect(flash[:error]).to eq(Spree.t('notice_messages.variant_not_deleted', error: error_msg)) }
-          end
+        it "can be deleted" do
+          delete :destroy, params: { product_id: product.to_param, id: variant.to_param }
+          expect(variant.reload).to be_discarded
         end
       end
     end
