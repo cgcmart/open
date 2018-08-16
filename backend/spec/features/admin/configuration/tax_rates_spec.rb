d590555b0cc9f167437eecd1edad4a1bce8b1fa6
99cm <zyizyao@china-guide.com>
2018-08-16 17:47:10 -0400

    Update tax_rates_spec.rb

--- backend/spec/features/admin/configuration/tax_rates_spec.rb
+++ backend/spec/features/admin/configuration/tax_rates_spec.rb
@@ -1,14 +1,27 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'Tax Rates', type: :feature do
   stub_authorization!
 
-  before { create(:tax_rate, calculator: stub_model(Spree::Calculator)) }
+  let!(:tax_rate) { create(:tax_rate, calculator: stub_model(Spree::Calculator)) }
 
-  # Regression test for #1422
-  it 'can create a new tax rate' do
+  before do
     visit spree.admin_path
-    click_link 'Configuration'
+    click_link "Taxes"
+  end
+
+  # Regression test for https://github.com/spree/spree/issues/535
+  it "can see a tax rate in the list if the tax category has been deleted" do
+    tax_rate.tax_categories.first.update_column(:deleted_at, Time.current)
+    click_link "Tax Rates"
+
+    expect(find("table tbody td:nth-child(3)")).to have_content('N/A')
+  end
+
+  # Regression test for https://github.com/spree/spree/issues/1422
+  it 'can create a new tax rate' do
     click_link 'Tax Rates'
     click_link 'New Tax Rate'
     fill_in 'Rate', with: '0.05'
