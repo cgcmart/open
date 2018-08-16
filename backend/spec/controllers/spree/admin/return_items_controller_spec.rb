62b2bfe78c643e9a9ce6638eaf56c1994bf09c00
99cm <zyizyao@china-guide.com>
2018-08-16 12:10:00 -0400

    Update return_items_controller_spec.rb

--- backend/spec/controllers/spree/admin/return_items_controller_spec.rb
+++ backend/spec/controllers/spree/admin/return_items_controller_spec.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Spree::Admin::ReturnItemsController, type: :controller do
   stub_authorization!
 
   describe '#update' do
-    subject do
-      spree_put :update, id: return_item.to_param, return_item: { acceptance_status: new_acceptance_status }
-    end
-
     let(:customer_return) { create(:customer_return) }
     let(:return_item) { customer_return.return_items.first }
-    let(:old_acceptance_status) { 'accepted' }
+    let(:old_acceptance_status) { 'pending' }
     let(:new_acceptance_status) { 'rejected' }
 
+    subject do
+      put :update, params: { id: return_item.to_param, return_item: { acceptance_status: new_acceptance_status } }
+    end
+
     it 'updates the return item' do
-      expect do
+      expect {
         subject
-      end.to change { return_item.reload.acceptance_status }.from(old_acceptance_status).to(new_acceptance_status)
+      }end.to change { return_item.reload.acceptance_status }.from(old_acceptance_status).to(new_acceptance_status)
     end
 
     it 'redirects to the custome return' do
