a21b36d6623d6c67e88e203a7bcf02337f37fb43
99cm <zyizyao@china-guide.com>
2018-08-17 16:47:15 -0400

    Update taxons_spec.rb

--- backend/spec/features/admin/products/edit/taxons_spec.rb
+++ backend/spec/features/admin/products/edit/taxons_spec.rb
@@ -1,41 +1,47 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe 'Product Taxons', type: :feature, js: true do
+describe 'Product Taxons', type: :feature do
   stub_authorization!
 
-  after do
-    Capybara.ignore_hidden_elements = true
-  end
+  context 'managing display order', js: true do
+    def assert_selected_taxons(taxons)
+      # Regression test for https://github.com/spree/spree/issues/2139
+      taxons.each do |taxon|
+        expect(page).to have_css('.select2-search-choice', text: taxon.name)
+      end
 
-  before do
-    Capybara.ignore_hidden_elements = false
-  end
-
-  context 'managing taxons' do
-    def selected_taxons
-      find('#product_taxon_ids').value.split(',').map(&:to_i).uniq
+      expected_value = taxons.map(&:id).join(',')
+      expect(page).to have_xpath("//*[@id = 'product_taxon_ids' and @value = '#{expected_value}']", visible: :all)
     end
 
-    it 'allows an admin to manage taxons' do
+    let(:product) { create(:product) }
+
+    it 'should allow an admin to manage display order (taxons)' do
       taxon_1 = create(:taxon)
       taxon_2 = create(:taxon, name: 'Clothing')
-      product = create(:product)
       product.taxons << taxon_1
 
-      visit spree.admin_products_path
-      within_row(1) { click_icon :edit }
+      visit spree.edit_admin_product_path(product)
 
-      expect(find('.select2-search-choice').text).to eq("#{taxon_1.parent.name} -> #{taxon_1.name}")
-      expect(selected_taxons).to match_array([taxon_1.id])
-
+      assert_selected_taxons([taxon_1])
+      
       select2_search 'Clothing', from: 'Taxons'
       click_button 'Update'
-      expect(selected_taxons).to match_array([taxon_1.id, taxon_2.id])
+      assert_selected_taxons([taxon_1, taxon_2])
+    end
 
-      # Regression test for #2139
-      sleep(1)
-      expect(first('.select2-search-choice', text: taxon_1.name)).to be_present
-      expect(first('.select2-search-choice', text: taxon_2.name)).to be_present
+    context 'with an XSS attempt', skip: true do
+      let(:taxon_name) { %(<script>throw('XSS')</script>) }
+      let!(:taxon) { create(:taxon, name: taxon_name) }
+      it 'displays the escaped HTML without executing it' do
+        visit spree.edit_admin_product_path(product)
+
+        select2_search '<script>', from: 'Taxon'
+
+        expect(page).to have_content(taxon_name)
+      end
     end
   end
 end
