234cc47bd7b7299605dab8da453173865bbaa16e
99cm <zyizyao@china-guide.com>
2018-08-17 16:27:38 -0400

    Update products_spec.rb

--- backend/spec/features/admin/products/edit/products_spec.rb
+++ backend/spec/features/admin/products/edit/products_spec.rb
@@ -1,42 +1,75 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe 'Product Details', type: :feature, js: true do
+describe 'Product Details', type: :feature do
   stub_authorization!
 
   context 'editing a product' do
-    before do
+    it 'should list the product details' do
       create(:product, name: 'Bún thịt nướng', sku: 'A100',
                        description: 'lorem ipsum', available_on: '2013-08-14 01:02:03')
 
-      visit spree.admin_products_path
+      visit spree.admin_path
+      click_nav 'Products'
       within_row(1) { click_icon :edit }
+    
+      click_link 'Product Details'
+
+      expect(page).to have_content('Products / Bún thịt nướng')
+      expect(page).to have_field('product_name', with: 'Bún thịt nướng')
+      expect(page).to have_field('product_slug', with: 'bun-th-t-n-ng')
+      expect(page).to have_field('product_description', with: 'lorem ipsum')
+      expect(page).to have_field('product_price', with: '19.99')
+      expect(page).to have_field('product_cost_price', with: '17.00')
+      expect(page).to have_field('product_available_on', with: "2013/08/14")
+      expect(page).to have_field('product_sku', with: 'A100')
     end
 
-    it 'lists the product details' do
-      click_link 'Details'
+    it 'should handle slug changes' do
+      create(:product, name: 'Bún thịt nướng', sku: 'A100',
+              description: 'lorem ipsum', available_on: '2011-01-01 01:01:01')
 
-      expect(find('.content-header h1').text.strip).to eq('Products / Bún thịt nướng')
-      expect(find('input#product_name').value).to eq('Bún thịt nướng')
-      expect(find('input#product_slug').value).to eq('bun-th-t-n-ng')
-      expect(find('textarea#product_description').text.strip).to eq('lorem ipsum')
-      expect(find('input#product_price').value).to eq('19.99')
-      expect(find('input#product_cost_price').value).to eq('17.00')
-      expect(find('input#product_available_on').value).to eq('2013/08/14')
-      expect(find('input#product_sku').value).to eq('A100')
-    end
+      visit spree.admin_path
+      click_nav 'Products'
+      within('table.index tbody tr:nth-child(1)') do
+        click_icon(:edit)
+      end
 
-    it 'handles slug changes' do
       fill_in 'product_slug', with: 'random-slug-value'
       click_button 'Update'
       expect(page).to have_content('successfully updated!')
+
+      fill_in 'product_slug', with: ''
+      click_button 'Update'
+      within('#product_slug_field') { expect(page).to have_content("can't be blank") }
+
+      fill_in 'product_slug', with: 'x'
+      click_button 'Update'
+      expect(page).to have_content("successfully updated!")
     end
 
     it 'has a link to preview a product' do
       allow(Spree::Core::Engine).to receive(:frontend_available?).and_return(true)
       allow_any_instance_of(Spree::BaseHelper).to receive(:product_url).and_return('http://example.com/products/product-slug')
-      click_link 'Details'
+      click_link 'Product Details'
       expect(page).to have_css('#admin_preview_product')
       expect(page).to have_link Spree.t(:preview_product), href: 'http://example.com/products/product-slug'
+    end
+  end
+
+  # Regression test for https://github.com/spree/spree/issues/3385
+  context 'deleting a product', js: true do
+    it 'is still able to find the master variant' do
+      create(:product)
+
+      visit spree.admin_products_path
+      within_row(1) do
+        accept_alert do
+          click_icon :trash
+        end
+      end
+      expect(page).to have_content('Product has been deleted')
     end
   end
 end
