5161dc675a52705c850d56e1b7158034f409673d
99cm <zyizyao@china-guide.com>
2018-08-18 11:12:40 -0400

    Update properties_spec.rb

--- backend/spec/features/admin/products/properties_spec.rb
+++ backend/spec/features/admin/products/properties_spec.rb
@@ -1,21 +1,24 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe 'Properties', type: :feature, js: true do
+describe 'Properties', type: :feature do
   stub_authorization!
 
-  before do
-    visit spree.admin_products_path
+  before(:each) do
+    visit spree.admin_path
+    click_nav 'Products'
   end
 
   context 'Property index' do
     before do
       create(:property, name: 'shirt size', presentation: 'size')
       create(:property, name: 'shirt fit', presentation: 'fit')
-      visit spree.admin_properties_path
+      click_link 'Property Types'
     end
 
     context 'listing product properties' do
-      it 'lists the existing product properties' do
+      it 'should list the existing product properties' do
         within_row(1) do
           expect(column_text(1)).to eq('shirt size')
           expect(column_text(2)).to eq('size')
@@ -29,10 +32,9 @@ describe 'Properties', type: :feature, js: true do
     end
 
     context 'searching properties' do
-      it 'lists properties matching search query' do
-        click_on 'Filter'
+      it 'should list properties matching search query', js: true do
         fill_in 'q_name_cont', with: 'size'
-        click_on 'Search'
+        click_button 'Search'
 
         expect(page).to have_content('shirt size')
         expect(page).not_to have_content('shirt fit')
@@ -41,10 +43,10 @@ describe 'Properties', type: :feature, js: true do
   end
 
   context 'creating a property' do
-    it 'allows an admin to create a new product property' do
-      visit spree.admin_properties_path
+    it 'should allow an admin to create a new product property', js: true do
+      click_link 'Property Types'
       click_link 'new_property_link'
-      within('.content-header') { expect(page).to have_content('New Property') }
+      within('#new_property') { expect(page).to have_content('New Property Type') }
 
       fill_in 'property_name', with: 'color of band'
       fill_in 'property_presentation', with: 'color'
@@ -54,61 +56,58 @@ describe 'Properties', type: :feature, js: true do
   end
 
   context 'editing a property' do
-    before do
+    before(:each) do
       create(:property)
-      visit spree.admin_properties_path
+      click_link 'Property Types'
       within_row(1) { click_icon :edit }
     end
 
-    it 'allows an admin to edit an existing product property' do
+    it 'should allow an admin to edit an existing product property' do
       fill_in 'property_name', with: 'model 99'
       click_button 'Update'
       expect(page).to have_content('successfully updated!')
       expect(page).to have_content('model 99')
     end
 
-    it 'shows validation errors' do
+    it 'should show validation errors' do
       fill_in 'property_name', with: ''
       click_button 'Update'
       expect(page).to have_content("Name can't be blank")
     end
   end
 
-  context 'linking a property to a product' do
+  context 'linking a property to a product', js: true do
     before do
       create(:product)
       visit spree.admin_products_path
       click_icon :edit
-      within('#sidebar') do
-        click_link 'Properties'
-      end
+      click_link 'Product Properties'
     end
 
-    # Regression test for #2279
+    # Regression test for https://github.com/spree/spree/issues/2279
     it 'successfully create and then remove product property' do
       fill_in_property
-      # Sometimes the page doesn't load before the all check is done
-      # lazily finding the element gives the page 10 seconds
-      expect(page).to have_css('tbody#product_properties tr:nth-child(2)')
-      expect(all('tbody#product_properties tr').count).to eq(2)
+      check_property_row_count(2)
 
       delete_product_property
 
       check_property_row_count(1)
     end
 
-    # Regression test for #4466
+    # Regression test for https://github.com/spree/spree/issues/4466
     it 'successfully remove and create a product property at the same time' do
       fill_in_property
 
-      fill_in 'product_product_properties_attributes_1_property_name', with: 'New Property'
-      fill_in 'product_product_properties_attributes_1_value', with: 'New Value'
+      expect(page).to have_css('tr.product_property', count: 2)
 
+      within '#spree_new_product_property' do
+        find('[id$=_property_name]').set('New Property')
+        find('[id$=_value]').set('New Value')
+      end
       delete_product_property
 
       # Give fadeOut time to complete
-      expect(page).not_to have_selector('#product_product_properties_attributes_0_property_name')
-      expect(page).not_to have_selector('#product_product_properties_attributes_0_value')
+      expect(page).to have_css('tr.product_property', count: 1)
 
       click_button 'Update'
 
@@ -118,27 +117,23 @@ describe 'Properties', type: :feature, js: true do
     end
 
     def fill_in_property
+      expect(page).to have_content('Products')
       fill_in 'product_product_properties_attributes_0_property_name', with: 'A Property'
       fill_in 'product_product_properties_attributes_0_value', with: 'A Value'
       click_button 'Update'
-      within('#sidebar') do
-        click_link 'Properties'
-      end
+      click_link 'Product Properties'
     end
 
     def delete_product_property
-      spree_accept_alert do
-        click_icon :delete
-        wait_for_ajax
+      accept_alert do
+        click_icon :trash
       end
+      expect(page).to have_content 'successfully removed'
     end
 
     def check_property_row_count(expected_row_count)
-      within('#sidebar') do
-        click_link 'Properties'
-      end
-      expect(page).to have_css('tbody#product_properties')
-      expect(all('tbody#product_properties tr').count).to eq(expected_row_count)
+      click_link 'Product Properties'
+      expect(page).to have_css('tbody#product_properties', count: expected_row_count)
     end
   end
 end
