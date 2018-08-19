c426725a53a7bfc4b6b4d526f10a975d9ab4527e
99cm <zyizyao@china-guide.com>
2018-08-19 11:48:26 -0400

    Create base_feature_helper.rb

---
+++ backend/spec/support/feature/base_feature_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module BaseFeatureHelper
+  def click_nav(nav_text, subnav_text = nil)
+    primary_nav = find(".admin-nav-menu>ul>li>a", text: /#{nav_text}/i)
+    if subnav_text
+      primary_nav.find('+ul>li>a', text: /#{subnav_text}/i).click
+    else
+      primary_nav.click
+    end
+  end
+end
