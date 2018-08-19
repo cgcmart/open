425f65df7339629b44c37478dd45baa1df14894c
99cm <zyizyao@china-guide.com>
2018-08-19 11:34:17 -0400

    Update navigation_helper_spec.rb

--- backend/spec/helpers/spree/admin/navigation_helper_spec.rb
+++ backend/spec/helpers/spree/admin/navigation_helper_spec.rb
@@ -1,47 +1,38 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Spree::Admin::NavigationHelper, type: :helper do
-  before do
-    # `spree` route helper is not accessible in `type: :helper` hence extending it explicitly
-    # https://github.com/rspec/rspec-rails/issues/1626
-    helper.extend Spree::TestingSupport::UrlHelpers
-  end
-
   describe '#tab' do
-    before do
-      allow(helper).to receive(:cannot?).and_return false
-    end
-
     context 'creating an admin tab' do
-      it "capitalizes the first letter of each word in the tab's label" do
+      it "should capitalize the first letter of each word in the tab's label" do
         admin_tab = helper.tab(:orders)
         expect(admin_tab).to include('Orders')
       end
-    end
-
-    it 'accepts options with label and capitalize each word of it' do
-      admin_tab = helper.tab(:orders, label: 'delivered orders')
-      expect(admin_tab).to include('Delivered Orders')
-    end
-
-    it 'capitalizes words with unicode characters' do
-      # overview
-      admin_tab = helper.tab(:orders, label: 'přehled')
-      expect(admin_tab).to include('Přehled')
     end
 
     describe 'selection' do
       context 'when match_path option is not supplied' do
         subject(:tab) { helper.tab(:orders) }
 
-        it 'is selected if the controller matches' do
+        it 'should be selected if the controller matches' do
           allow(controller).to receive(:controller_name).and_return('orders')
-          expect(subject).to include('selected')
+          expect(subject).to include('class="selected"')
         end
 
-        it 'is not selected if the controller does not match' do
+        it 'should be not selected if the controller does not match' do
           allow(controller).to receive(:controller_name).and_return('bonobos')
-          expect(subject).not_to include('selected')
+          expect(subject).not_to include('class="selected"')
+        end
+
+        it 'should be selected if the current path' do
+          allow(helper).to receive(:request).and_return(double(ActionDispatch::Request, fullpath: '/admin/orders'))
+          expect(subject).to include('class="selected"')
+        end
+
+        it 'should not be selected if not current path' do
+          allow(helper).to receive(:request).and_return(double(ActionDispatch::Request, fullpath: '/admin/products'))
+          expect(subject).not_to include('class="selected"')
         end
       end
 
@@ -50,57 +41,88 @@ describe Spree::Admin::NavigationHelper, type: :helper do
           allow(helper).to receive(:request).and_return(double(ActionDispatch::Request, fullpath: '/admin/orders/edit/1'))
         end
 
-        it 'is selected if the fullpath matches' do
+        it 'should be selected if the fullpath matches' do
           allow(controller).to receive(:controller_name).and_return('bonobos')
-          tab = helper.tab(:orders, label: 'delivered orders', match_path: '/orders')
-          expect(tab).to include('selected')
+          tab = helper.tab(:orders, match_path: '/orders')
+          expect(tab).to include('class="selected"')
         end
 
-        it 'is selected if the fullpath matches a regular expression' do
+        it 'should be selected if the fullpath matches a regular expression' do
           allow(controller).to receive(:controller_name).and_return('bonobos')
-          tab = helper.tab(:orders, label: 'delivered orders', match_path: /orders$|orders\//)
-          expect(tab).to include('selected')
+          tab = helper.tab(:orders, match_path: /orders$|orders\//)
+          expect(tab).to include('class="selected"')
         end
 
-        it 'is not selected if the fullpath does not match' do
+        it 'should not selected if the fullpath does not match' do
           allow(controller).to receive(:controller_name).and_return('bonobos')
-          tab = helper.tab(:orders, label: 'delivered orders', match_path: '/shady')
-          expect(tab).not_to include('selected')
+          tab = helper.tab(:orders, match_path: '/shady')
+          expect(tab).not_to include('class="selected"')
         end
 
         it 'is not selected if the fullpath does not match a regular expression' do
           allow(controller).to receive(:controller_name).and_return('bonobos')
-          tab = helper.tab(:orders, label: 'delivered orders', match_path: /shady$|shady\//)
-          expect(tab).not_to include('selected')
+          tab = helper.tab(:orders, match_path: /shady$|shady\//)
+          expect(tab).not_to include('class="selected"')
         end
       end
+    end
+
+    it 'should accept a block of content to append' do
+      admin_tab = helper.tab(:orders){ 'foo' }
+      expect(admin_tab).to end_with('foo</li>')
     end
   end
 
-  describe '#klass_for' do
-    it 'returns correct klass for Spree model' do
-      expect(klass_for(:products)).to eq(Spree::Product)
-      expect(klass_for(:product_properties)).to eq(Spree::ProductProperty)
+  describe "#link_to_delete" do
+    let!(:item) { create(:stock_item) }
+    let(:options) { {} }
+    let(:link) { subject }
+
+    subject { helper.link_to_delete(item, options) }
+
+    # object_url is provided by the ResourceController abstract controller.
+    # as we cannot set a custom controller for helper tests, we need to fake it
+    before do
+      allow(helper).to receive(:object_url) do |o|
+        "/stock_items/#{o.to_param}"
+      end
     end
 
-    it 'returns correct klass for non-spree model' do
-      class MyUser
-      end
-      expect(klass_for(:my_users)).to eq(MyUser)
-
-      Object.send(:remove_const, 'MyUser')
+    it "generates a deletion link for the resource" do
+      expect(link).to include("href=\"/stock_items/#{item.to_param}\"")
+      expect(link).to include("data-action=\"remove\"")
+      expect(link).to include("data-confirm=\"Are you sure?\"")
+      expect(link).to include("<span class=\"text\">Delete</span>")
     end
 
-    it 'returns correct namespaced klass for non-spree model' do
-      module My
-        class User
-        end
-      end
+    it "allows customization of the url" do
+      options[:url] = "/test/url"
+      expect(link).to include("href=\"/test/url\"")
+    end
 
-      expect(klass_for(:my_users)).to eq(My::User)
+    it "allows customization of the link name" do
+      options[:name] = "Delete Item"
+      expect(link).to include("name=\"Delete Item\"")
+      expect(link).to include("<span class=\"text\">Delete Item</span>")
+    end
 
-      My.send(:remove_const, 'User')
-      Object.send(:remove_const, 'My')
+    it "allows customization of the confirmation message" do
+      options[:confirm] = "Please confirm."
+      expect(link).to include("data-confirm=\"Please confirm.\"")
+    end
+  end
+
+  describe "#spree_icon" do
+    context "if given an icon_name" do
+      subject(:spree_icon) { helper.spree_icon('example-icon-name') }
+
+      it { is_expected.to eq "<i class=\"example-icon-name\"></i>" }
+    end
+
+    context "if not given nil icon_name" do
+      subject(:spree_icon) { helper.spree_icon(nil) }
+
+      it { is_expected.to eq '' }
     end
   end
 end
