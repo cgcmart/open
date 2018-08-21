1262e516d5512743b4ad18d7824ee21306106523
99cm <zyizyao@china-guide.com>
2018-08-21 11:46:30 -0400

    Update free_shipping_spec.rb

--- core/spec/models/spree/promotion/actions/free_shipping_spec.rb
+++ core/spec/models/spree/promotion/actions/free_shipping_spec.rb
@@ -1,12 +1,14 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Promotion::Actions::FreeShipping, type: :model do
+require 'rails_helper'
+
+RSpec.describe Spree::Promotion::Actions::FreeShipping, type: :model do
   let(:order) { create(:completed_order_with_totals) }
-  let(:promotion) { create(:promotion) }
-  let(:action) { Spree::Promotion::Actions::FreeShipping.create }
-  let(:payload) { { order: order } }
-
-  it_behaves_like 'an adjustment source'
+  let(:shipment) { order.shipments.to_a.first }
+  let(:promotion) { create(:promotion, code: 'somecode', promotion_actions: [action]) }
+  let(:action) { Spree::Promotion::Actions::FreeShipping.new }
+  let(:payload) { { order: order, promotion_code: promotion_code } }
+  let(:promotion_code) { promotion.codes.first! }
 
   # From promotion spec:
   context '#perform' do
@@ -15,22 +17,45 @@ describe Spree::Promotion::Actions::FreeShipping, type: :model do
       promotion.promotion_actions << action
     end
 
-    it 'creates a discount with correct negative amount' do
-      expect(order.shipments.count).to eq(2)
-      expect(order.shipments.first.cost).to eq(100)
-      expect(order.shipments.last.cost).to eq(100)
-      expect(action.perform(payload)).to be true
-      expect(promotion.credits_count).to eq(2)
-      expect(order.shipment_adjustments.count).to eq(2)
-      expect(order.shipment_adjustments.first.amount.to_i).to eq(-100)
-      expect(order.shipment_adjustments.last.amount.to_i).to eq(-100)
+    context "when valid" do
+      it 'should create a discount with correct negative amount' do
+        expect(order.shipments.count).to eq(2)
+        expect(order.shipments.first.cost).to eq(100)
+        expect(order.shipments.last.cost).to eq(100)
+        expect(action.perform(payload)).to be true
+        expect(promotion.usage_count).to eq(1)
+        expect(order.shipment_adjustments.count).to eq(2)
+        expect(order.shipment_adjustments.first.amount.to_i).to eq(-100)
+        expect(order.shipment_adjustments.last.amount.to_i).to eq(-100)
+        expect(order.shipment_adjustments.map(&:promotion_code)).to eq [promotion_code, promotion_code]
+      end
     end
 
-    it 'does not create a discount when order already has one from this promotion' do
-      expect(action.perform(payload)).to be true
-      expect(action.perform(payload)).to be false
-      expect(promotion.credits_count).to eq(2)
-      expect(order.shipment_adjustments.count).to eq(2)
+    context "when order already has one from this promotion" do
+      it 'does not create a discount' do
+        expect(action.perform(payload)).to be true
+        expect(action.perform(payload)).to be false
+        expect(promotion.usage_count).to eq(1)
+        expect(order.shipment_adjustments.count).to eq(2)
+      end
+    end
+  end
+
+  describe '#remove_from' do
+    # this adjustment should not get removed
+    let!(:other_adjustment) { create(:adjustment, adjustable: shipment, order: order, source: nil) }
+
+    before do
+      action.perform(payload)
+      @action_adjustment = shipment.adjustments.where(source: action).first!
+    end
+
+    it 'removes the action adjustment' do
+      expect(shipment.adjustments).to match_array([other_adjustment, @action_adjustment])
+
+      action.remove_from(order)
+
+      expect(shipment.adjustments).to eq([other_adjustment])
     end
   end
 end
