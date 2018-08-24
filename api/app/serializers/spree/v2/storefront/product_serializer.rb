ba16c7b5e2545b2d7e8a8457dcb81492915d7097
99cm <zyizyao@china-guide.com>
2018-08-24 10:42:25 -0400

    Create product_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/product_serializer.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class ProductSerializer < BaseSerializer
+        set_type :product
+
+        attributes :name, :description, :price, :currency, :display_price,
+                   :available_on, :slug, :meta_description, :meta_keywords,
+                   :updated_at
+
+        attribute :purchasable,   &:purchasable?
+        attribute :in_stock,      &:in_stock?
+        attribute :backorderable, &:backorderable?
+
+        has_many :variants
+        has_many :option_types
+        has_many :product_properties
+
+        has_one  :default_variant,
+          object_method_name: :master,
+          id_method_name:     :master_id,
+          record_type:        :variant,
+          serializer:         :variant
+      end
+    end
+  end
+end
