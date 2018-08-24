86332aefd3683d5a9f746f40812e6e05696265f5
99cm <zyizyao@china-guide.com>
2018-08-24 10:44:22 -0400

    Update variant_serializer.rb

--- api/app/serializers/spree/v2/storefront/variant_serializer.rb
+++ api/app/serializers/spree/v2/storefront/variant_serializer.rb
@@ -1,10 +1,20 @@
+# frozen_string_literal: true
+
 module Spree
   module V2
     module Storefront
       class VariantSerializer < BaseSerializer
         set_type :variant
-        attributes :name, :sku, :price, :weight, :height, :width, :depth, :is_master,
-          :slug, :description, :track_inventory
+
+        attributes :name, :sku, :price, :currency, :display_price, :weight, :height,
+                   :width, :depth, :is_master, :options_text, :slug, :description,
+                   :track_inventory
+
+        attribute :purchasable,   &:purchasable?
+        attribute :in_stock,      &:in_stock?
+        attribute :backorderable, &:backorderable?
+
+        has_many :images
       end
     end
   end
