fdf2c4de8c813a0b2faa08e39ff1857c1c73cbbd
99cm <zyizyao@china-guide.com>
2018-08-24 10:36:32 -0400

    Create image_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/image_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class ImageSerializer < BaseSerializer
+        set_type :image
+
+        attributes :viewable_type, :viewable_id, :styles
+      end
+    end
+  end
+end
