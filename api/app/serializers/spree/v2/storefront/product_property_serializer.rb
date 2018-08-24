efa52d74ddf79c10bd585654443d4769d3f1e34a
99cm <zyizyao@china-guide.com>
2018-08-24 10:41:14 -0400

    Create product_property_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/product_property_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class ProductPropertySerializer < BaseSerializer
+        set_type  :product_property
+
+        attribute :value
+
+        attribute :name,        &:property_name
+        attribute :description, &:property_presentation
+      end
+    end
+  end
+end
