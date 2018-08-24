211c9a8daa9d55ae9c0ad25b4945ca509fa9e6b0
99cm <zyizyao@china-guide.com>
2018-08-24 14:08:04 -0400

    Update line_item.rb

--- core/app/models/spree/line_item.rb
+++ core/app/models/spree/line_item.rb
@@ -1,87 +1,88 @@
+# frozen_string_literal: true
+
 module Spree
+  # Variants placed in the Order at a particular price.
+  #
+  # `Spree::LineItem` is an ActiveRecord model which records which `Spree::Variant`
+  # a customer has chosen to place in their order. It also acts as the permenent
+  # record of the customer's order by recording relevant price, taxation, and inventory
+  # concerns. Line items can also have adjustments placed on them as part of the
+  # promotion system.
+  #
   class LineItem < Spree::Base
-    before_validation :ensure_valid_quantity
+    class CurrencyMismatch < StandardError; end
 
     with_options inverse_of: :line_items do
       belongs_to :order, class_name: 'Spree::Order', touch: true
-      belongs_to :variant, class_name: 'Spree::Variant'
+      belongs_to :variant, -> { with_deleted }, class_name: 'Spree::Variant'
     end
     belongs_to :tax_category, class_name: 'Spree::TaxCategory'
 
     has_one :product, through: :variant
 
-    has_many :adjustments, as: :adjustable, dependent: :destroy
+    has_many :adjustments, as: :adjustable, inverse_of: :adjustable, dependent: :destroy
     has_many :inventory_units, inverse_of: :line_item
 
-    before_validation :copy_price
-    before_validation :copy_tax_category
+    has_many :line_item_actions, dependent: :destroy
+    has_many :actions, through: :line_item_actions
+
+    before_validation :normalize_quantity
+    before_validation :set_required_attributes
 
     validates :variant, :order, presence: true
-    validates :quantity, numericality: { only_integer: true, message: Spree.t('validation.must_be_int') }
+    validates :quantity, numericality: { only_integer: true, greater_than: -1 }
     validates :price, numericality: true
 
-    validates_with Stock::AvailabilityValidator
-    validate :ensure_proper_currency, if: -> { order.present? }
+    after_save :update_inventory
 
-    before_destroy :verify_order_inventory_before_destroy, if: -> { order.has_checkout_step?('delivery') }
-
+    before_destroy :update_inventory
     before_destroy :destroy_inventory_units
 
-    after_save :update_inventory
-    after_save :update_adjustments
-
-    after_create :update_tax_charge
-
-    delegate :name, :description, :sku, :should_track_inventory?, :product, :options_text, to: :variant
-    delegate :brand, :category, to: :product
-    delegate :tax_zone, to: :order
+    delegate :name, :description, :sku, :should_track_inventory?, to: :variant
+    delegate :currency, to: :order, allow_nil: true
 
     attr_accessor :target_shipment
 
     self.whitelisted_ransackable_associations = ['variant']
     self.whitelisted_ransackable_attributes = ['variant_id']
 
-    def copy_price
-      if variant
-        update_price if price.nil?
-        self.cost_price = variant.cost_price if cost_price.nil?
-        self.currency = variant.currency if currency.nil?
-      end
-    end
-
-    def update_price
-      self.price = variant.price_including_vat_for(tax_zone: tax_zone)
-    end
-
-    def copy_tax_category
-      self.tax_category = variant.tax_category if variant
-    end
-
-    extend DisplayMoney
-    money_methods :amount, :subtotal, :discounted_amount, :final_amount, :total, :price
-
-    alias single_money display_price
-    alias single_display_amount display_price
-
     def amount
       price * quantity
     end
-
     alias subtotal amount
 
-    def taxable_amount
-      amount + taxable_adjustment_total
+    def discounted_amount
+      amount + promo_total
     end
 
-    alias discounted_money display_discounted_amount
-    alias discounted_amount taxable_amount
-
-    def final_amount
+    def total
       amount + adjustment_total
     end
+    alias final_amount total
 
-    alias total final_amount
-    alias money display_total
+    def total_before_tax
+      amount + adjustments.select { |a| !a.tax? && a.eligible? }.sum(&:amount)
+    end
+
+    extend DisplayMoney
+    money_methods :amount, :discounted_amount, :price,
+                  :total, :adjustment_total, :additional_tax_total, :promo_total, :included_tax_total
+
+    alias money_price display_price
+    alias single_money display_price
+    alias single_display_amount display_price
+
+    alias money display_amount
+
+    def money_price=(money)
+      if !money
+        self.price = nil
+      elsif money.currency.iso_code != currency
+        raise CurrencyMismatch, "Line item price currency must match order currency!"
+      else
+        self.price = money.to_d
+      end
+    end
 
     def sufficient_stock?
       Stock::Quantifier.new(variant).can_supply? quantity
@@ -94,54 +95,65 @@ module Spree
     def options=(options = {})
       return unless options.present?
 
-      opts = options.dup # we will be deleting from the hash, so leave the caller's copy intact
+      assign_attributes options
 
-      currency = opts.delete(:currency) || order.try(:currency)
-
-      update_price_from_modifier(currency, opts)
-      assign_attributes opts
+      # When price is part of the options we are not going to fetch
+      # it from the variant. Please note that this always allows to set
+      # a price for this line item, even if there is no existing price
+      # for the associated line item in the order currency.
+      unless options.key?(:price) || options.key?('price')
+        self.money_price = variant.price_for(pricing_options)
+      end
     end
 
-    # Remove variant default_scope `deleted_at: nil`
-    def variant
-      Spree::Variant.unscoped { super }
+    def pricing_options
+      Spree::Config.pricing_options_class.from_line_item(self)
+    end
+
+    def currency=(_currency)
     end
 
     private
 
-    def ensure_valid_quantity
+    def normalize_quantity
       self.quantity = 0 if quantity.nil? || quantity < 0
     end
 
-    def update_price_from_modifier(currency, opts)
-      if currency
-        self.currency = currency
-        # variant.price_in(currency).amount can be nil if
-        # there's no price for this currency
-        self.price = (variant.price_in(currency).amount || 0) +
-          variant.price_modifier_amount_in(currency, opts)
-      else
-        self.price = variant.price +
-          variant.price_modifier_amount(opts)
+    def set_required_attributes
+      return unless variant
+      self.tax_category ||= variant.tax_category
+      set_pricing_attributes
+    end
+
+    def set_pricing_attributes
+      # If the legacy method #copy_price has been overridden, handle that gracefully
+      return handle_copy_price_override if respond_to?(:copy_price)
+
+      self.cost_price ||= variant.cost_price
+      self.money_price = variant.price_for(pricing_options) if price.nil?
+      true
+    end
+
+    def handle_copy_price_override
+      copy_price
+    end
+
+    def copy_price
+      if variant
+        update_price if price.nil?
+        self.cost_price = variant.cost_price if cost_price.nil?
+        self.currency = variant.currency if currency.nil?
       end
     end
 
     def update_inventory
       if (saved_changes? || target_shipment.present?) && order.has_checkout_step?('delivery')
-        verify_order_inventory
+        Spree::OrderInventory.new(order, self).verify(target_shipment)
       end
     end
 
-    def verify_order_inventory
-      Spree::OrderInventory.new(order, self).verify(target_shipment, is_updated: true)
-    end
-
-    def verify_order_inventory_before_destroy
-      Spree::OrderInventory.new(order, self).verify(target_shipment)
-    end
-
     def destroy_inventory_units
-      throw(:abort) unless inventory_units.destroy_all
+      inventory_units.destroy_all
     end
 
     def update_adjustments
