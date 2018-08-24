72303748566140c9b9b3647f8d2b7faed87ccc5f
99cm <zyizyao@china-guide.com>
2018-08-24 10:39:28 -0400

    Create option_value_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/option_value_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class OptionValueSerializer < BaseSerializer
+        set_type   :option_value
+
+        attributes :id, :name, :presentation, :position
+      end
+    end
+  end
+end
