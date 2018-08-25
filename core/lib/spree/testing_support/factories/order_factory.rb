cd924b7384fb0e2a289077fd72dc7835f6aece72
99cm <zyizyao@china-guide.com>
2018-08-25 16:11:52 -0400

    Update order_factory.rb

--- core/lib/spree/testing_support/factories/order_factory.rb
+++ core/lib/spree/testing_support/factories/order_factory.rb
@@ -1,30 +1,37 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/address_factory'
+require 'spree/testing_support/factories/shipment_factory'
+require 'spree/testing_support/factories/store_factory'
+require 'spree/testing_support/factories/user_factory'
+require 'spree/testing_support/factories/line_item_factory'
+require 'spree/testing_support/factories/payment_factory'
+
 FactoryBot.define do
   factory :order, class: Spree::Order do
     user
     bill_address
+    ship_address
     completed_at nil
     email { user.email }
     store
+    currency 'USD'
 
     transient do
       line_items_price BigDecimal.new(10)
     end
 
     factory :order_with_totals do
-      after(:create) do |order, evaluator|
-        create(:line_item, order: order, price: evaluator.line_items_price)
-        order.line_items.reload # to ensure order.line_items is accessible after
-      end
-    end
-
-    factory :order_with_line_item_quantity do
-      transient do
-        line_items_quantity 1
+      after(:build) do |order, evaluator|
+        order.line_items << build(
+          :line_item,
+          order: order,
+          price: evaluator.line_items_price
+        )
       end
 
-      after(:create) do |order, evaluator|
-        create(:line_item, order: order, price: evaluator.line_items_price, quantity: evaluator.line_items_quantity)
-        order.line_items.reload # to ensure order.line_items is accessible after
+      after(:create) do |order|
+        order.recalculate
       end
     end
 
@@ -34,28 +41,71 @@ FactoryBot.define do
 
       transient do
         line_items_count 1
-        without_line_items false
+        line_items_attributes { [{}] * line_items_count }
         shipment_cost 100
-        shipping_method_filter Spree::ShippingMethod::DISPLAY_ON_FRONT_END
+        shipping_method nil
+        stock_location { create(:stock_location) }
       end
 
-      after(:create) do |order, evaluator|
-        unless evaluator.without_line_items
-          create_list(:line_item, evaluator.line_items_count, order: order, price: evaluator.line_items_price)
-          order.line_items.reload
-        end
+      after(:build) do |order, evaluator|
+        evaluator.stock_location # must evaluate before creating line items
 
-        create(:shipment, order: order, cost: evaluator.shipment_cost)
+        evaluator.line_items_attributes.each do |attributes|
+          attributes = { order: order, price: evaluator.line_items_price }.merge(attributes)
+          create(:line_item, attributes)
+        end
+        order.line_items.reload
+
+        create(:shipment, order: order, cost: evaluator.shipment_cost, shipping_method: evaluator.shipping_method, stock_location: evaluator.stock_location)
         order.shipments.reload
 
-        order.update_with_updater!
+        order.recalculate
+      end
+
+      factory :completed_order_with_promotion do
+        transient do
+          promotion nil
+        end
+
+        after(:create) do |order, evaluator|
+          promotion = evaluator.promotion || create(:promotion, code: "test")
+          promotion_code = promotion.codes.first || create(:promotion_code, promotion: promotion)
+
+          promotion.activate(order: order, promotion_code: promotion_code)
+          order.order_promotions.create!(promotion: promotion, promotion_code: promotion_code)
+
+          # Complete the order after the promotion has been activated
+          order.update_column(:completed_at, Time.current)
+          order.update_column(:state, "complete")
+        end
+      end
+
+      factory :order_ready_to_complete do
+        state 'confirm'
+        payment_state 'checkout'
+
+        transient do
+          payment_type :credit_card_payment
+        end
+
+        after(:create) do |order, evaluator|
+          create(evaluator.payment_type, {
+            amount: order.total,
+            order: order,
+            state: order.payment_state
+          })
+
+          order.payments.reload
+        end
       end
 
       factory :completed_order_with_totals do
         state 'complete'
 
-        after(:create) do |order, evaluator|
-          order.refresh_shipment_rates(evaluator.shipping_method_filter)
+        after(:create) do |order|
+          order.shipments.each do |shipment|
+            shipment.inventory_units.update_all state: 'on_hand', pending: false
+          end
           order.update_column(:completed_at, Time.current)
         end
 
@@ -65,9 +115,9 @@ FactoryBot.define do
           end
         end
 
-        factory :completed_order_with_store_credit_payment do
+        factory :completed_order_with_pending_payment do
           after(:create) do |order|
-            create(:store_credit_payment, amount: order.total, order: order)
+            create(:payment, amount: order.total, order: order, state: 'pending')
           end
         end
 
@@ -75,20 +125,31 @@ FactoryBot.define do
           payment_state 'paid'
           shipment_state 'ready'
 
-          after(:create) do |order|
-            create(:payment, amount: order.total, order: order, state: 'completed')
+          transient do
+            payment_type :credit_card_payment
+          end
+
+          after(:create) do |order, evaluator|
+            create(evaluator.payment_type, amount: order.total, order: order, state: 'completed')
             order.shipments.each do |shipment|
-              shipment.inventory_units.update_all state: 'on_hand'
               shipment.update_column('state', 'ready')
             end
             order.reload
           end
 
           factory :shipped_order do
-            after(:create) do |order|
+            after(:create) do |order, evaluator|
               order.shipments.each do |shipment|
                 shipment.inventory_units.update_all state: 'shipped'
-                shipment.update_column('state', 'shipped')
+                shipment.update_columns(state: 'shipped', shipped_at: Time.current)
+                next unless evaluator.with_cartons
+                Spree::Carton.create!(
+                  stock_location: shipment.stock_location,
+                  address: order.ship_address,
+                  shipping_method: shipment.shipping_method,
+                  inventory_units: shipment.inventory_units,
+                  shipped_at: Time.current
+                )
               end
               order.update_column('shipment_state', 'shipped')
               order.reload
