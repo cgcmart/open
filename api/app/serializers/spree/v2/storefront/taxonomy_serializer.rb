a81abdeee63bc34f486e5350ece9b7e900db5be8
99cm <zyizyao@china-guide.com>
2018-08-25 10:37:12 -0400

    Create taxonomy_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/taxonomy_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class TaxonomySerializer < BaseSerializer
+        set_type   :taxonomy
+
+        attributes :name, :position
+      end
+    end
+  end
+end
