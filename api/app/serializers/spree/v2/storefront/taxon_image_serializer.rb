606912bb55ffa514e13dadabd335015c7f42c305
99cm <zyizyao@china-guide.com>
2018-08-25 10:16:42 -0400

    Create taxon_image_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/taxon_image_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class TaxonImageSerializer < BaseSerializer
+        set_type   :taxon_image
+
+        attributes :viewable_type, :viewable_id, :styles
+      end
+    end
+  end
+end
