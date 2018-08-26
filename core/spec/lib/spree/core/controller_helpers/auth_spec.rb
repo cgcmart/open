d22083a54b51c31b89a2587433e20fa60dee3e06
99cm <zyizyao@china-guide.com>
2018-08-26 15:00:33 -0400

    Update auth_spec.rb

--- core/spec/lib/spree/core/controller_helpers/auth_spec.rb
+++ core/spec/lib/spree/core/controller_helpers/auth_spec.rb
@@ -1,5 +1,6 @@
-require 'spec_helper'
-require 'spree/testing_support/url_helpers'
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 class FakesController < ApplicationController
   include Spree::Core::ControllerHelpers::Auth
@@ -8,9 +9,8 @@ class FakesController < ApplicationController
   end
 end
 
-describe Spree::Core::ControllerHelpers::Auth, type: :controller do
+RSpec.describe Spree::Core::ControllerHelpers::Auth, type: :controller do
   controller(FakesController) {}
-  include Spree::TestingSupport::UrlHelpers
 
   describe '#current_ability' do
     it 'returns Spree::Ability instance' do
@@ -29,27 +29,22 @@ describe Spree::Core::ControllerHelpers::Auth, type: :controller do
       get :index
       expect(response).to redirect_to('/redirect')
     end
-    it 'redirects to HTTP_REFERER' do
-      request.env['HTTP_REFERER'] = '/dummy_redirect'
-      get :index
-      expect(response).to redirect_to('/dummy_redirect')
-    end
     it 'redirects to default page' do
       get :index
       expect(response).to redirect_to('/')
     end
   end
 
-  describe '#set_guest_token' do
+  describe '#set_token' do
     controller(FakesController) do
       def index
-        set_guest_token
+        set_token
         render plain: 'index'
       end
     end
     it 'sends cookie header' do
       get :index
-      expect(response.cookies['guest_token']).not_to be_nil
+      expect(response.cookies['token']).not_to be_nil
     end
     it 'sets httponly flag' do
       get :index
@@ -76,38 +71,6 @@ describe Spree::Core::ControllerHelpers::Auth, type: :controller do
     end
     it 'returns nil' do
       expect(controller.try_spree_current_user).to eq nil
-    end
-  end
-
-  describe '#redirect_unauthorized_access' do
-    controller(FakesController) do
-      def index
-        redirect_unauthorized_access
-      end
-    end
-    context 'when logged in' do
-      before do
-        allow(controller).to receive_messages(try_spree_current_user: double('User', id: 1, last_incomplete_spree_order: nil))
-      end
-      it 'redirects forbidden path' do
-        get :index
-        expect(response).to redirect_to(spree.forbidden_path)
-      end
-    end
-    context 'when guest user' do
-      before do
-        allow(controller).to receive_messages(try_spree_current_user: nil)
-      end
-      it 'redirects login path' do
-        allow(controller).to receive_messages(spree_login_path: '/login')
-        get :index
-        expect(response).to redirect_to('/login')
-      end
-      it 'redirects root path' do
-        allow(controller).to receive_message_chain(:spree, :root_path).and_return('/root_path')
-        get :index
-        expect(response).to redirect_to('/root_path')
-      end
     end
   end
 end
