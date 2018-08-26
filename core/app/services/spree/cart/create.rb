ef6613bae9e8e18d3a1d292cbf6eefb56fa29cb2
99cm <zyizyao@china-guide.com>
2018-08-26 12:04:17 -0400

    Update create.rb

--- core/app/services/spree/cart/create.rb
+++ core/app/services/spree/cart/create.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 module Spree
   module Cart
     class Create
       prepend Spree::ServiceModule::Base
 
-      def call(user:, store:, order_params: nil)
+      def call(user:, store:, currency:, order_params: nil)
         order_params ||= {}
 
         default_params = {
           user: user,
           store: store,
-          currency: Spree::Config[:currency],
-          guest_token: GenerateToken.new.call(Spree::Order)
+          currency: currency,
+          token: GenerateToken.new.call(Spree::Order)
         }
 
         order = Spree::Order.create!(default_params.merge(order_params))
