8882f2f0af9d19e456c7a94d45c4859b12e2ecec
99cm <zyizyao@china-guide.com>
2018-08-26 14:19:32 -0400

    Update store.rb

--- core/lib/spree/core/controller_helpers/store.rb
+++ core/lib/spree/core/controller_helpers/store.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Spree
   module Core
     module ControllerHelpers
@@ -5,9 +7,7 @@ module Spree
         extend ActiveSupport::Concern
 
         included do
-          helper_method :current_currency
           helper_method :current_store
-          helper_method :current_price_options
         end
 
         def current_currency
@@ -15,35 +15,7 @@ module Spree
         end
 
         def current_store
-          @current_store ||= Spree::Store.current(request.env['SERVER_NAME'])
-        end
-
-        # Return a Hash of things that influence the prices displayed in your shop.
-        #
-        # By default, the only thing that influences prices that is the current order's +tax_zone+
-        # (to facilitate differing prices depending on VAT rate for digital products in Europe, see
-        # https://github.com/spree/spree/pull/6295 and https://github.com/spree/spree/pull/6662).
-        #
-        # If your prices depend on something else, overwrite this method and add
-        # more key/value pairs to the Hash it returns.
-        #
-        # Be careful though to also patch the following parts of Spree accordingly:
-        #
-        # * `Spree::VatPriceCalculation#gross_amount`
-        # * `Spree::LineItem#update_price`
-        # * `Spree::Stock::Estimator#taxation_options_for`
-        # * Subclass the `DefaultTax` calculator
-        #
-        def current_price_options
-          {
-            tax_zone: current_tax_zone
-          }
-        end
-
-        private
-
-        def current_tax_zone
-          current_order.try(:tax_zone) || Spree::Zone.default_tax
+          @current_store ||= Spree::Config.current_store_selector_class.new(request).store
         end
       end
     end
