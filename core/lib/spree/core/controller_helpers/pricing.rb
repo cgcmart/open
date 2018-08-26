3d1ba8d804f2f42017126c4d9c692173c3196469
99cm <zyizyao@china-guide.com>
2018-08-26 14:04:21 -0400

    Create pricing.rb

---
+++ core/lib/spree/core/controller_helpers/pricing.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'spree/deprecation'
+
+module Spree
+  module Core
+    module ControllerHelpers
+      module Pricing
+        extend ActiveSupport::Concern
+
+        included do
+          helper_method :current_currency
+          helper_method :current_pricing_options
+        end
+
+        def current_pricing_options
+          Spree::Config.pricing_options_class.new(
+            currency: current_store.try!(:default_currency).presence || Spree::Config[:currency],
+            country_iso: current_store.try!(:cart_tax_country_iso).presence
+          )
+        end
+
+        def current_currency
+          current_pricing_options.currency
+        end
+        deprecate current_currency: :current_pricing_options, deprecator: Spree::Deprecation
+      end
+    end
+  end
+end
