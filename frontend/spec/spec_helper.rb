d4f9f89158cb4ab55bbabb083928e5d6cfb833b4
99cm <zyizyao@china-guide.com>
2018-08-28 16:31:06 -0400

    Update spec_helper.rb

--- frontend/spec/spec_helper.rb
+++ frontend/spec/spec_helper.rb
@@ -1,38 +1,30 @@
+# frozen_string_literal: true
+
 if ENV['COVERAGE']
-  # Run Coverage report
   require 'simplecov'
-  SimpleCov.start do
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Helpers', 'app/helpers'
-    add_group 'Mailers', 'app/mailers'
-    add_group 'Models', 'app/models'
-    add_group 'Views', 'app/views'
-    add_group 'Libraries', 'lib'
-  end
+  SimpleCov.start('rails')
 end
 
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV['RAILS_ENV'] ||= 'test'
 
-begin
-  require File.expand_path('../dummy/config/environment', __FILE__)
-rescue LoadError
-  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
-  exit
-end
+require 'spree_frontend'
+require 'spree/testing_support/dummy_app'
+DummyApp.setup(
+  gem_root: File.expand_path('..', __dir__),
+  lib_name: 'spree_frontend'
+)
 
+require 'rails-controller-testing'
 require 'rspec/rails'
-require 'ffaker'
+require 'rspec-activemodel-mocks'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 require 'database_cleaner'
-require 'rspec/retry'
-
-require 'spree/testing_support/i18n' if ENV['CHECK_TRANSLATIONS']
 
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/capybara_ext'
@@ -47,38 +39,29 @@ require 'spree/testing_support/caching'
 require 'capybara-screenshot/rspec'
 
 Capybara.save_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
+Capybara.default_max_wait_time = ENV['DEFAULT_MAX_WAIT_TIME'].to_f if ENV['DEFAULT_MAX_WAIT_TIME'].present?
 
-if ENV['WEBDRIVER'] == 'accessible'
-  require 'capybara/accessible'
-  Capybara.javascript_driver = :accessible
-else
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new app,
-      browser: :chrome,
-      options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu window-size=1920,1080])
-  end
-  Capybara.javascript_driver = :chrome
+require "selenium/webdriver"
+Capybara.javascript_driver = (ENV['CAPYBARA_DRIVER'] || :selenium_chrome_headless).to_sym
 
-  Capybara::Screenshot.register_driver(:chrome) do |driver, path|
-    driver.browser.save_screenshot(path)
-  end
-end
-Capybara.configure do |config|
-  config.default_max_wait_time = 20
-end
+ActiveJob::Base.queue_adapter = :test
 
 RSpec.configure do |config|
   config.color = true
-  config.fail_fast = ENV['FAIL_FAST'] || false
-  config.fixture_path = File.join(__dir__, 'fixtures')
   config.infer_spec_type_from_file_location!
-  config.mock_with :rspec
-  config.raise_errors_for_deprecations!
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.fixture_path = File.join(__dir__, "fixtures")
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, comment the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # Config for running specs while have transition period from Paperclip to ActiveStorage
   if Rails.application.config.use_paperclip
@@ -97,26 +80,18 @@ RSpec.configure do |config|
   # Ensure DB is clean, so that transaction isolated specs see
   # pristine state.
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean
+   DatabaseCleaner.clean_with :truncation
   end
 
-  config.before do
-    WebMock.disable!
-    DatabaseCleaner.strategy = if RSpec.current_example.metadata[:js]
-                                 :truncation
-                               else
-                                 :transaction
-                               end
-    # TODO: Find out why open_transactions ever gets below 0
-    # See issue #3428
-    ApplicationRecord.connection.increment_open_transactions if ApplicationRecord.connection.open_transactions < 0
-    DatabaseCleaner.start
+  config.before(:each) do
+     Rails.cache.clear
     reset_spree_preferences
   end
 
-  config.after do
-    DatabaseCleaner.clean
+  config.before(:each, type: :feature) do
+    if page.driver.browser.respond_to?(:url_blacklist)
+      page.driver.browser.url_blacklist = ['http://fonts.googleapis.com']
+    end
   end
 
   config.after(:each, type: :feature) do |example|
@@ -134,13 +109,8 @@ RSpec.configure do |config|
   config.include Spree::TestingSupport::ControllerRequests, type: :controller
   config.include Spree::TestingSupport::Flash
 
+  config.example_status_persistence_file_path = "./spec/examples.txt"
+
   config.order = :random
   Kernel.srand config.seed
-
-  config.verbose_retry = true
-  config.display_try_failure_messages = true
-
-  config.around :each, type: :feature do |ex|
-    ex.run_with_retry retry: 3
-  end
 end
