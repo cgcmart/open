61cefefa60428399a2c6bfb92b72a22c73a84bba
99cm <zyizyao@china-guide.com>
2018-08-28 16:41:04 -0400

    Update spec_helper.rb

--- sample/spec/spec_helper.rb
+++ sample/spec/spec_helper.rb
@@ -1,33 +1,39 @@
+# frozen_string_literal: true
+
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../dummy/config/environment', __FILE__)
-require 'rspec/rails'
-require 'ffaker'
 require 'spree_sample'
+require 'spree/testing_support/dummy_app'
+DummyApp.setup(
+  gem_root: File.expand_path('..', __dir__),
+  lib_name: 'spree_sample'
+)
+
+require 'rspec/rails'
+require 'database_cleaner'
 
 RSpec.configure do |config|
   config.color = true
-  config.fail_fast = ENV['FAIL_FAST'] || false
   config.infer_spec_type_from_file_location!
-  config.mock_with :rspec
-  config.raise_errors_for_deprecations!
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, comment the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
-  # Config for running specs while have transition period from Paperclip to ActiveStorage
-  if Rails.application.config.use_paperclip
-    config.filter_run_excluding :active_storage
-  else
-    config.filter_run_including :active_storage
-    config.run_all_when_everything_filtered = true
-  end
-
-  config.include FactoryBot::Syntax::Methods
+  config.example_status_persistence_file_path = "./spec/examples.txt"
 
   config.order = :random
   Kernel.srand config.seed
