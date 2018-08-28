c7083fa3c43b75d969222eddf42901e63182d823
99cm <zyizyao@china-guide.com>
2018-08-28 11:55:12 -0400

    Update setup.rb

--- api/lib/spree/api/testing_support/setup.rb
+++ api/lib/spree/api/testing_support/setup.rb
@@ -1,13 +1,12 @@
+# frozen_string_literal: true
+
 module Spree
   module Api
     module TestingSupport
       module Setup
         def sign_in_as_admin!
           let!(:current_api_user) do
-            user = stub_model(Spree.user_class)
-            allow(user).to receive_message_chain(:spree_roles, :pluck).and_return(['admin'])
-            allow(user).to receive(:has_spree_role?).with('admin').and_return(true)
-            user
+            stub_model(Spree::LegacyUser, spree_roles: [Spree::Role.find_or_initialize_by(name: 'admin')])
           end
         end
       end
