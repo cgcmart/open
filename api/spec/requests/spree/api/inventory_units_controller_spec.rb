d70d121a6d4068637d6150a58f80779d2f5fdeb6
99cm <zyizyao@china-guide.com>
2018-08-29 11:38:58 -0400

    Update inventory_units_controller_spec.rb

--- api/spec/requests/spree/api/inventory_units_controller_spec.rb
+++ api/spec/requests/spree/api/inventory_units_controller_spec.rb
@@ -1,46 +1,53 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 module Spree
-  describe Api::V1::InventoryUnitsController, type: :controller do
-    render_views
+  describe Api::InventoryUnitsController, type: :request do
+    let!(:inventory_unit) { create(:inventory_unit) }
 
     before do
       stub_authentication!
-      @inventory_unit = create(:inventory_unit)
     end
 
     context 'as an admin' do
       sign_in_as_admin!
+      let(:variant) { create(:variant) }
 
       it 'gets an inventory unit' do
-        api_get :show, id: @inventory_unit.id
-        expect(json_response['state']).to eq @inventory_unit.state
+        get spree.api_inventory_unit_path(inventory_unit)
+        expect(json_response['state']).to eq inventory_unit.state
       end
 
       it 'updates an inventory unit' do
-        api_put :update, id: @inventory_unit.id,
-                         inventory_unit: { shipment_id: nil }
-        expect(json_response['shipment_id']).to be_nil
+        put spree.api_inventory_unit_path(inventory_unit), params: {
+          inventory_unit: { variant_id: variant.id }
+        }
+        expect(json_response['variant_id']).to eq variant.id
       end
 
       context 'fires state event' do
         it 'if supplied with :fire param' do
-          api_put :update, id: @inventory_unit.id,
-                           fire: 'ship',
-                           inventory_unit: { shipment: { tracking: 'foobar' } }
+          put spree.api_inventory_unit_path(inventory_unit), params: {
+            fire: 'ship',
+            inventory_unit: { variant_id: variant.id }
+          }
+
           expect(json_response['state']).to eq 'shipped'
         end
 
         it 'and returns exception if cannot fire' do
-          api_put :update, id: @inventory_unit.id,
-                           fire: 'return'
-          expect(json_response['exception']).to match(/cannot transition to return/)
+          put spree.api_inventory_unit_path(inventory_unit), params: {
+            fire: 'return'
+          }
+          expect(json_response['exception']).to match /cannot transition to return/
         end
 
         it 'and returns exception bad state' do
-          api_put :update, id: @inventory_unit.id,
-                           fire: 'bad'
-          expect(json_response['exception']).to match(/cannot transition to bad/)
+          put spree.api_inventory_unit_path(inventory_unit), params: {
+            fire: 'bad'
+          }
+          expect(json_response['exception']).to match /cannot transition to bad/
         end
       end
     end
