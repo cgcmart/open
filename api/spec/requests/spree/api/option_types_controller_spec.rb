7e2b3d979b644d2d510513bb6a5b3d48b92b1150
99cm <zyizyao@china-guide.com>
2018-08-29 12:05:42 -0400

    Update option_types_controller_spec.rb

--- api/spec/requests/spree/api/option_types_controller_spec.rb
+++ api/spec/requests/spree/api/option_types_controller_spec.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 module Spree
-  describe Api::V1::OptionTypesController, type: :controller do
-    render_views
-
-    let(:attributes) { [:id, :name, :presentation, :position] }
+  describe Api::OptionTypesController, type: :request do
+    let(:attributes) { [:id, :name, :position, :presentation] }
     let!(:option_value) { create(:option_value) }
     let!(:option_type) { option_value.option_type }
 
@@ -19,7 +19,7 @@ module Spree
     end
 
     it 'can list all option types' do
-      api_get :index
+      get spree.api_option_types_path
       expect(json_response.count).to eq(1)
       expect(json_response.first).to have_attributes(attributes)
 
@@ -28,53 +28,49 @@ module Spree
 
     it 'can search for an option type' do
       create(:option_type, name: 'buzz')
-      api_get :index, q: { name_cont: option_type.name }
+      get spree.api_option_types_path, params: { q: { name_cont: option_type.name } }
       expect(json_response.count).to eq(1)
       expect(json_response.first).to have_attributes(attributes)
     end
 
     it 'can retrieve a list of specific option types' do
       option_type_1 = create(:option_type)
-      create(:option_type) # option_type_2
-
-      api_get :index, ids: "#{option_type.id},#{option_type_1.id}"
+      create(:option_type)
+      get spree.api_option_types_path, params: { ids: "#{option_type.id},#{option_type_1.id}" }
       expect(json_response.count).to eq(2)
 
       check_option_values(json_response.first['option_values'])
     end
 
     it 'can list a single option type' do
-      api_get :show, id: option_type.id
+      get spree.api_option_type_path(option_type)
       expect(json_response).to have_attributes(attributes)
       check_option_values(json_response['option_values'])
     end
 
-    it 'can learn how to create a new option type' do
-      api_get :new
-      expect(json_response['attributes']).to eq(attributes.map(&:to_s))
-      expect(json_response['required_attributes']).not_to be_empty
-    end
-
     it 'cannot create a new option type' do
-      api_post :create, option_type: {
-        name: 'Option Type',
-        presentation: 'Option Type'
-      }
+      post spree.api_option_types_path, params: {
+        option_type: {
+          name: 'Option Type',
+          presentation: 'Option Type'
+        }
+      } 
       assert_unauthorized!
     end
 
     it 'cannot alter an option type' do
       original_name = option_type.name
-      api_put :update, id: option_type.id,
-                       option_type: {
-                         name: 'Option Type'
-                       }
+      put spree.api_option_type_path(option_type), params: {
+        option_type: {
+          name: 'Option Type'
+        }
+      }
       assert_not_found!
       expect(option_type.reload.name).to eq(original_name)
     end
 
     it 'cannot delete an option type' do
-      api_delete :destroy, id: option_type.id
+      delete spree.api_option_type_path(option_type)
       assert_not_found!
       expect { option_type.reload }.not_to raise_error
     end
@@ -83,32 +79,36 @@ module Spree
       sign_in_as_admin!
 
       it 'can create an option type' do
-        api_post :create, option_type: {
-          name: 'Option Type',
-          presentation: 'Option Type'
+        post spree.api_option_types_path, params: {
+          option_type: {
+            name: 'Option Type',
+            presentation: 'Option Type'
+          }
         }
         expect(json_response).to have_attributes(attributes)
         expect(response.status).to eq(201)
       end
 
       it 'cannot create an option type with invalid attributes' do
-        api_post :create, option_type: {}
+        post spree.api_option_types_path, params: { option_type: {} }
         expect(response.status).to eq(422)
       end
 
       it 'can update an option type' do
-        api_put :update, id: option_type.id, option_type: { name: 'Option Type' }
+        put spree.api_option_type_path(option_type.id), params: { option_type: { name: 'Option Type' } }
         expect(response.status).to eq(200)
+
+        option_type.reload
         expect(option_type.reload.name).to eq('Option Type')
       end
 
       it 'cannot update an option type with invalid attributes' do
-        api_put :update, id: option_type.id, option_type: { name: '' }
+        put spree.api_option_type_path(option_type.id), params: { option_type: { name: '' } }
         expect(response.status).to eq(422)
       end
 
       it 'can delete an option type' do
-        api_delete :destroy, id: option_type.id
+        delete spree.api_option_type_path(option_type.id)
         expect(response.status).to eq(204)
       end
     end
