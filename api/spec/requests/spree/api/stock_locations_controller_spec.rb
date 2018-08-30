7a5311ae9abf96259cb6ba2a1ad6f5e7519f33b7
99cm <zyizyao@china-guide.com>
2018-08-30 11:46:53 -0400

    Update stock_locations_controller_spec.rb

--- api/spec/requests/spree/api/stock_locations_controller_spec.rb
+++ api/spec/requests/spree/api/stock_locations_controller_spec.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 module Spree
-  describe Api::V1::StockLocationsController, type: :controller do
-    render_views
-
+  describe Api::StockLocationsController, type: :request do
     let!(:stock_location) { create(:stock_location) }
     let!(:attributes) { [:id, :name, :address1, :address2, :city, :state_id, :state_name, :country_id, :zipcode, :phone, :active] }
 
@@ -12,100 +12,158 @@ module Spree
     end
 
     context 'as a user' do
-      it 'cannot see stock locations' do
-        api_get :index
-        expect(response.status).to eq(401)
+      describe '#index' do
+        it 'can see active stock locations' do
+          get spree.api_stock_locations_path
+          expect(response).to be_successful
+          stock_locations = json_response['stock_locations'].map { |sl| sl['name'] }
+          expect(stock_locations).to include stock_location.name
+        end
+
+        it 'cannot see inactive stock locations' do
+          stock_location.update_attributes!(active: false)
+          get spree.api_stock_locations_path
+          expect(response).to be_successful
+          stock_locations = json_response['stock_locations'].map { |sl| sl['name'] }
+          expect(stock_locations).not_to include stock_location.name
+        end
       end
 
-      it 'cannot see a single stock location' do
-        api_get :show, id: stock_location.id
-        expect(response.status).to eq(404)
+      describe '#show' do
+        it 'can see active stock locations' do
+          get spree.api_stock_location_path(stock_location)
+          expect(response).to be_successful
+          expect(json_response['name']).to eq stock_location.name
+        end
+
+        it 'cannot see inactive stock locations' do
+          stock_location.update_attributes!(active: false)
+          get spree.api_stock_location_path(stock_location)
+          expect(response).to be_not_found
+        end
       end
 
-      it 'cannot create a new stock location' do
-        params = {
-          stock_location: {
-            name: 'North Pole',
-            active: true
+      describe '#create' do
+        it 'cannot create a new stock location' do
+          params = {
+            stock_location: {
+              name: 'North Pole',
+              active: true
+            }
           }
-        }
 
-        api_post :create, params
-        expect(response.status).to eq(401)
+          post spree.api_stock_locations_path, params: params
+          expect(response.status).to eq(401)
+        end
       end
 
-      it 'cannot update a stock location' do
-        api_put :update, stock_location: { name: 'South Pole' }, id: stock_location.to_param
-        expect(response.status).to eq(404)
+      describe '#update' do
+        it 'cannot update a stock location' do
+          put spree.api_stock_location_path(stock_location), params: { stock_location: { name: 'South Pole' } }
+          expect(response.status).to eq(401)
+        end
       end
 
-      it 'cannot delete a stock location' do
-        api_put :destroy, id: stock_location.to_param
-        expect(response.status).to eq(404)
+      describe '#destroy' do
+        it 'cannot delete a stock location' do
+          delete spree.api_stock_location_path(stock_location)
+          expect(response.status).to eq(401)
+        end
       end
     end
 
     context 'as an admin' do
       sign_in_as_admin!
 
-      it 'gets list of stock locations' do
-        api_get :index
-        expect(json_response['stock_locations'].first).to have_attributes(attributes)
-        expect(json_response['stock_locations'].first['country']).not_to be_nil
-        expect(json_response['stock_locations'].first['state']).not_to be_nil
+      describe '#index' do
+        it 'can see active stock locations' do
+          get spree.api_stock_locations_path
+          expect(response).to be_successful
+          stock_locations = json_response['stock_locations'].map { |sl| sl['name'] }
+          expect(stock_locations).to include stock_location.name
+        end
+
+        it 'can see inactive stock locations' do
+          stock_location.update_attributes!(active: false)
+          get spree.api_stock_locations_path
+          expect(response).to be_successful
+          stock_locations = json_response['stock_locations'].map { |sl| sl['name'] }
+          expect(stock_locations).to include stock_location.name
+        end
+
+        it 'gets stock location information' do
+          get spree.api_stock_locations_path
+          expect(json_response['stock_locations'].first).to have_attributes(attributes)
+          expect(json_response['stock_locations'].first['country']).not_to be_nil
+          expect(json_response['stock_locations'].first['state']).not_to be_nil
+        end
+
+        it 'can control the page size through a parameter' do
+          create(:stock_location)
+          get spree.api_stock_locations_path, params: { per_page: 1 }
+          expect(json_response['count']).to eq(1)
+          expect(json_response['current_page']).to eq(1)
+          expect(json_response['pages']).to eq(2)
+        end
+
+        it 'can query the results through a paramter' do
+          expected_result = create(:stock_location, name: 'South America')
+          get spree.api_stock_locations_path, params: { q: { name_cont: 'south' } }
+          expect(json_response['count']).to eq(1)
+          expect(json_response['stock_locations'].first['name']).to eq expected_result.name
+        end
       end
 
-      it 'can control the page size through a parameter' do
-        create(:stock_location)
-        api_get :index, per_page: 1
-        expect(json_response['count']).to eq(1)
-        expect(json_response['current_page']).to eq(1)
-        expect(json_response['pages']).to eq(2)
+      describe '#show' do
+        it 'can see active stock locations' do
+          get spree.api_stock_location_path(stock_location)
+          expect(response).to be_successful
+          expect(json_response['name']).to eq stock_location.name
+        end
+
+        it 'can see inactive stock locations' do
+          stock_location.update_attributes!(active: false)
+          get spree.api_stock_location_path(stock_location)
+          expect(response).to be_successful
+          expect(json_response['name']).to eq stock_location.name
+        end
       end
 
-      it 'can query the results through a paramter' do
-        expected_result = create(:stock_location, name: 'South America')
-        api_get :index, q: { name_cont: 'south' }
-        expect(json_response['count']).to eq(1)
-        expect(json_response['stock_locations'].first['name']).to eq expected_result.name
-      end
-
-      it 'gets a stock location' do
-        api_get :show, id: stock_location.to_param
-        expect(json_response).to have_attributes(attributes)
-        expect(json_response['name']).to eq stock_location.name
-      end
-
-      it 'can create a new stock location' do
-        params = {
-          stock_location: {
-            name: 'North Pole',
-            active: true
+      describe '#create' do
+        it 'can create a new stock location' do
+          params = {
+            stock_location: {
+              name: 'North Pole',
+              active: true
+            }
           }
-        }
 
-        api_post :create, params
-        expect(response.status).to eq(201)
-        expect(json_response).to have_attributes(attributes)
+          post spree.api_stock_locations_path, params: params
+          expect(response.status).to eq(201)
+          expect(json_response).to have_attributes(attributes)
+        end
       end
 
-      it 'can update a stock location' do
-        params = {
-          id: stock_location.to_param,
-          stock_location: {
-            name: 'South Pole'
+      describe '#update' do
+        it 'can update a stock location' do
+          params = {
+            stock_location: {
+              name: "South Pole"
+            }
           }
-        }
 
-        api_put :update, params
-        expect(response.status).to eq(200)
-        expect(json_response['name']).to eq 'South Pole'
+          put spree.api_stock_location_path(stock_location), params: params
+          expect(response.status).to eq(200)
+          expect(json_response['name']).to eq 'South Pole'
+        end
       end
 
-      it 'can delete a stock location' do
-        api_delete :destroy, id: stock_location.to_param
-        expect(response.status).to eq(204)
-        expect { stock_location.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      describe '#destroy' do
+        it 'can delete a stock location' do
+          delete spree.api_stock_location_path(stock_location)
+          expect(response.status).to eq(204)
+          expect { stock_location.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        end
       end
     end
   end
