3088697c715c2bf570917b7fcf9828e5994395ed
99cm <zyizyao@china-guide.com>
2018-08-31 15:36:31 -0400

    Update line_item_factory.rb

--- core/lib/spree/testing_support/factories/line_item_factory.rb
+++ core/lib/spree/testing_support/factories/line_item_factory.rb
@@ -1,11 +1,17 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/order_factory'
+require 'spree/testing_support/factories/product_factory'
+
 FactoryBot.define do
   factory :line_item, class: Spree::LineItem do
     quantity 1
     price { BigDecimal.new('10.00') }
     order
+    currency { order.currency }
     transient do
-      association :product
+      product nil
     end
-    variant { product.master }
+    variant { (product || create(:product)).master }
   end
 end
