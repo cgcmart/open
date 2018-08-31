6148b11e001b837dc5e64e348d025f44067935b3
99cm <zyizyao@china-guide.com>
2018-08-31 15:41:23 -0400

    Update store_factory.rb

--- core/lib/spree/testing_support/factories/store_factory.rb
+++ core/lib/spree/testing_support/factories/store_factory.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :store, class: Spree::Store do
     sequence(:code) { |i| "spree_#{i}" }
-    name 'Spree Test Store'
-    url 'www.example.com'
+    sequence(:name) { |i| "Spree Test Store #{i}" }
+    sequence(:url) { |i| "www.example#{i}.com" }
     mail_from_address 'spree@example.org'
+    default_currency 'USD'
   end
 end
