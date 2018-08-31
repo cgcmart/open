e9b1ea8a7266db719dc379de61c69decd6c65b43
99cm <zyizyao@china-guide.com>
2018-08-31 11:21:25 -0400

    Update database_cleaner.rb

--- api/spec/support/database_cleaner.rb
+++ api/spec/support/database_cleaner.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before do
+  config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.after do
+  config.after(:each) do
     DatabaseCleaner.clean
   end
 end
