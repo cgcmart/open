7ac6d3fc83d7fb815b9a7250728c7b701be6d1f1
99cm <zyizyao@china-guide.com>
2018-09-01 12:40:11 -0400

    Update asset.rb

--- core/app/models/spree/asset.rb
+++ core/app/models/spree/asset.rb
@@ -1,15 +1,6 @@
-module Spree
-  if Rails.application.config.use_paperclip
-    ActiveSupport::Deprecation.warn(<<-EOS, caller)
-     Paperclip support is deprecated, and will be removed in Spree 4.0.
-     Please migrate to ActiveStorage, to avoid problems after update
-     https://github.com/thoughtbot/paperclip/blob/master/MIGRATING.md
-    EOS
-    Paperclip.interpolates :viewable_id do |attachment, _style|
-      attachment.instance.viewable_id
-    end
-  end
+# frozen_string_literal: true
 
+module Spree
   class Asset < Spree::Base
     include Support::ActiveStorage unless Rails.application.config.use_paperclip
 
