2a35d3042ee91b7772defc161cb2df2a8a240a35
99cm <zyizyao@china-guide.com>
2018-09-03 14:01:17 -0400

    Update named_type.rb

--- core/app/models/concerns/spree/named_type.rb
+++ core/app/models/concerns/spree/named_type.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 module Spree
   module NamedType
     extend ActiveSupport::Concern
 
     included do
       scope :active, -> { where(active: true) }
-      default_scope { order(Arel.sql("LOWER(#{table_name}.name)")) }
+      default_scope -> { order(arel_table[:name].lower) }
 
-      validates :name, presence: true, uniqueness: { case_sensitive: false }
+      validates :name, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
     end
   end
 end
