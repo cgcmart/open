d73f3e4fdbfc4d277c6bc6d81d2de610f84cec63
99cm <zyizyao@china-guide.com>
2018-09-03 17:31:57 -0400

    Update flat_rate.rb

--- core/app/models/spree/calculator/flat_rate.rb
+++ core/app/models/spree/calculator/flat_rate.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 require_dependency 'spree/calculator'
 
 module Spree
   class Calculator::FlatRate < Calculator
     preference :amount, :decimal, default: 0
     preference :currency, :string, default: -> { Spree::Config[:currency] }
-
-    def self.description
-      Spree.t(:flat_rate_per_order)
-    end
 
     def compute(object = nil)
       if object && preferred_currency.casecmp(object.currency.upcase).zero?
