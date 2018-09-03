0175630850845d33691b070b07e3072f55b0077e
99cm <zyizyao@china-guide.com>
2018-09-03 14:09:20 -0400

    Update ransackable_attributes.rb

--- core/app/models/concerns/spree/ransackable_attributes.rb
+++ core/app/models/concerns/spree/ransackable_attributes.rb
@@ -1,23 +1,22 @@
+# frozen_string_literal: true
+
 module Spree::RansackableAttributes
   extend ActiveSupport::Concern
   included do
     class_attribute :whitelisted_ransackable_associations
     class_attribute :whitelisted_ransackable_attributes
-    class_attribute :whitelisted_ransackable_scopes
-
+    
     class_attribute :default_ransackable_attributes
-    self.default_ransackable_attributes = %w[id name updated_at created_at]
+    self.default_ransackable_attributes = %w[id name]
+  end
 
-    def self.ransackable_associations(*_args)
+  class_methods do
+    def ransackable_associations(*_args)
       whitelisted_ransackable_associations || []
     end
 
-    def self.ransackable_attributes(*_args)
+    def ransackable_attributes(*_args)
       default_ransackable_attributes | (whitelisted_ransackable_attributes || [])
-    end
-
-    def self.ransackable_scopes(*_args)
-      whitelisted_ransackable_scopes || []
     end
   end
 end
