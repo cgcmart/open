64902be3e085640ae29ec9e89ac15aec09faa5ea
99cm <zyizyao@china-guide.com>
2018-09-03 17:48:18 -0400

    Update tiered_flat_rate.rb

--- core/app/models/spree/calculator/tiered_flat_rate.rb
+++ core/app/models/spree/calculator/tiered_flat_rate.rb
@@ -1,29 +1,43 @@
+# frozen_string_literal: true
+
 require_dependency 'spree/calculator'
 
 module Spree
   class Calculator::TieredFlatRate < Calculator
     preference :base_amount, :decimal, default: 0
     preference :tiers, :hash, default: {}
+    preference :currency, :string, default: -> { Spree::Config[:currency] }
 
     before_validation do
       # Convert tier values to decimals. Strings don't do us much good.
       if preferred_tiers.is_a?(Hash)
-        self.preferred_tiers = Hash[*preferred_tiers.flatten.map(&:to_f)]
+        self.preferred_tiers = preferred_tiers.map do |k, v|
+          [cast_to_d(k.to_s), cast_to_d(v.to_s)]
+        end.to_h
       end
     end
 
     validate :preferred_tiers_content
 
-    def self.description
-      Spree.t(:tiered_flat_rate)
-    end
-
     def compute(object)
-      base, amount = preferred_tiers.sort.reverse.detect { |b, _| object.amount >= b }
-      amount || preferred_base_amount
+      _base, amount = preferred_tiers.sort.reverse.detect do |b, _|
+        object.amount >= b
+      end
+
+      if preferred_currency.casecmp(object.currency).zero?
+        amount || preferred_base_amount
+       else
+        0
+      end
     end
 
     private
+
+    def cast_to_d(value)
+      value.to_s.to_d
+    rescue ArgumentError
+      BigDecimal(0)
+    end
 
     def preferred_tiers_content
       if preferred_tiers.is_a? Hash
