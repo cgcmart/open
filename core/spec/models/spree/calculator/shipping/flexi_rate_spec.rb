b1c6d75d59f8439bdfceb68ee5f260097a2736a3
99cm <zyizyao@china-guide.com>
2018-09-03 16:42:52 -0400

    Update flexi_rate_spec.rb

--- core/spec/models/spree/calculator/shipping/flexi_rate_spec.rb
+++ core/spec/models/spree/calculator/shipping/flexi_rate_spec.rb
@@ -1,10 +1,15 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'shared_examples/calculator_shared_examples'
 
 module Spree
   module Calculator::Shipping
-    describe FlexiRate, type: :model do
+    RSpec.describe FlexiRate, type: :model do
       let(:variant1) { build(:variant, price: 10) }
       let(:variant2) { build(:variant, price: 20) }
+
+      it_behaves_like 'a calculator with a description'
 
       let(:package) do
         build(:stock_package, variants_contents: { variant1 => 4, variant2 => 6 })
@@ -13,27 +18,27 @@ module Spree
       let(:subject) { FlexiRate.new }
 
       context 'compute' do
-        it 'computes amount correctly when all fees are 0' do
+        it 'should compute amount correctly when all fees are 0' do
           expect(subject.compute(package).round(2)).to eq(0.0)
         end
 
-        it 'computes amount correctly when first_item has a value' do
+        it 'should compute amount correctly when first_item has a value' do
           subject.preferred_first_item = 1.0
           expect(subject.compute(package).round(2)).to eq(1.0)
         end
 
-        it 'computes amount correctly when additional_items has a value' do
+        it 'should compute amount correctly when additional_items has a value' do
           subject.preferred_additional_item = 1.0
           expect(subject.compute(package).round(2)).to eq(9.0)
         end
 
-        it 'computes amount correctly when additional_items and first_item have values' do
+        it 'should compute amount correctly when additional_items and first_item have values' do
           subject.preferred_first_item = 5.0
           subject.preferred_additional_item = 1.0
           expect(subject.compute(package).round(2)).to eq(14.0)
         end
 
-        it 'computes amount correctly when additional_items and first_item have values AND max items has value' do
+        it 'should compute amount correctly when additional_items and first_item have values AND max items has value' do
           subject.preferred_first_item = 5.0
           subject.preferred_additional_item = 1.0
           subject.preferred_max_items = 3
