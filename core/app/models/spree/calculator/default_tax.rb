b9ab3affdbafde319f0dbcdb84183a97ebc252e4
99cm <zyizyao@china-guide.com>
2018-09-03 17:19:32 -0400

    Update default_tax.rb

--- core/app/models/spree/calculator/default_tax.rb
+++ core/app/models/spree/calculator/default_tax.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: true
+
 require_dependency 'spree/calculator'
 
 module Spree
   class Calculator::DefaultTax < Calculator
-    include VatPriceCalculation
-    def self.description
-      Spree.t(:default_tax)
-    end
+    include Spree::Tax::TaxHelpers
 
     # Default tax calculator still needs to support orders for legacy reasons
     # Orders created before Spree 2.1 had tax adjustments applied to the order, as a whole.
     # Orders created with Spree 2.2 and after, have them applied to the line items individually.
     def compute_order(order)
+      return 0 unless rate.active?
       matched_line_items = order.line_items.select do |line_item|
-        line_item.tax_category == rate.tax_category
+        rate.tax_categories.include?(line_item.tax_category)
       end
 
-      line_items_total = matched_line_items.sum(&:total)
+      line_items_total = matched_line_items.sum(&:total_before_tax)
       if rate.included_in_price
         round_to_two_places(line_items_total - (line_items_total / (1 + rate.amount)))
       else
@@ -25,25 +25,17 @@ module Spree
 
     # When it comes to computing shipments or line items: same same.
     def compute_shipment_or_line_item(item)
+      return 0 unless rate.active?
       if rate.included_in_price
-        deduced_total_by_rate(item.pre_tax_amount, rate)
+        deduced_total_by_rate(item, rate)
       else
-        round_to_two_places(item.discounted_amount * rate.amount)
+        round_to_two_places(item.total_before_tax * rate.amount)
       end
     end
 
-    alias compute_shipment compute_shipment_or_line_item
-    alias compute_line_item compute_shipment_or_line_item
-
-    def compute_shipping_rate(shipping_rate)
-      if rate.included_in_price
-        pre_tax_amount = shipping_rate.cost / (1 + rate.amount)
-        deduced_total_by_rate(pre_tax_amount, rate)
-      else
-        with_tax_amount = shipping_rate.cost * rate.amount
-        round_to_two_places(with_tax_amount)
-      end
-    end
+    alias_method :compute_shipment, :compute_item
+    alias_method :compute_line_item, :compute_item
+    alias_method :compute_shipping_rate, :compute_item
 
     private
 
@@ -51,8 +43,16 @@ module Spree
       calculable
     end
 
-    def deduced_total_by_rate(pre_tax_amount, rate)
-      round_to_two_places(pre_tax_amount * rate.amount)
+    def round_to_two_places(amount)
+      BigDecimal(amount.to_s).round(2, BigDecimal::ROUND_HALF_UP)
+    end
+
+    def deduced_total_by_rate(item, rate)
+      round_to_two_places(rate.amount * item.total_before_tax / (1 + sum_of_included_tax_rates(item)))
+    end
+
+    def sum_of_included_tax_rates(item)
+      rates_for_item(item).map(&:amount).sum
     end
   end
 end
