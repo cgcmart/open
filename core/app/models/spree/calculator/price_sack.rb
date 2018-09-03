5667f2250e83bf0a421afcea6927c3e36c3a9a5d
99cm <zyizyao@china-guide.com>
2018-09-03 17:41:12 -0400

    Update price_sack.rb

--- core/app/models/spree/calculator/price_sack.rb
+++ core/app/models/spree/calculator/price_sack.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_dependency 'spree/calculator'
 
 module Spree
@@ -7,17 +9,13 @@ module Spree
     preference :discount_amount, :decimal, default: 0
     preference :currency, :string, default: -> { Spree::Config[:currency] }
 
-    def self.description
-      Spree.t(:price_sack)
-    end
-
     # as object we always get line items, as calculable we have Coupon, ShippingMethod
     def compute(object)
-      base = if object.is_a?(Array)
-               object.map { |o| o.respond_to?(:amount) ? o.amount : BigDecimal(o.to_s) }.sum
-             else
-               object.respond_to?(:amount) ? object.amount : BigDecimal(object.to_s)
-             end
+      if object.is_a?(Array)
+        base = object.map { |o| o.respond_to?(:amount) ? o.amount : BigDecimal(o.to_s) }.sum
+      else
+        object.respond_to?(:amount) ? object.amount : BigDecimal(object.to_s)
+      end
 
       if base < preferred_minimal_amount
         preferred_normal_amount
