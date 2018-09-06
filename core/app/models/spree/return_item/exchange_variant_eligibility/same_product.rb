1a465c35e6c52ac1dd95f7dcbbce5624557d68b0
99cm <zyizyao@china-guide.com>
2018-09-06 14:57:54 -0400

    Update same_product.rb

--- core/app/models/spree/return_item/exchange_variant_eligibility/same_product.rb
+++ core/app/models/spree/return_item/exchange_variant_eligibility/same_product.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 module Spree
-  module ReturnItem::ExchangeVariantEligibility
-    class SameProduct
-      def self.eligible_variants(variant)
-        Spree::Variant.where(product_id: variant.product_id, is_master: variant.is_master?).in_stock
+  class ReturnItem < Spree::Base
+    module ExchangeVariantEligibility
+      class SameProduct
+        def self.eligible_variants(variant)
+          Spree::Variant.where(product_id: variant.product_id, is_master: variant.is_master?).in_stock_or_backorderable
+        end
       end
     end
   end
