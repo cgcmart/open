21296c277eb5a5c70712432e20e799cbac454ec6
99cm <zyizyao@china-guide.com>
2018-09-06 15:57:39 -0400

    Update shipping_category.rb

--- core/app/models/spree/stock/splitter/shipping_category.rb
+++ core/app/models/spree/stock/splitter/shipping_category.rb
@@ -1,29 +1,32 @@
+# frozen_string_literal: true
+
 module Spree
   module Stock
     module Splitter
       class ShippingCategory < Spree::Stock::Splitter::Base
         def split(packages)
-          split_packages = packages.flat_map(&method(:split_by_category))
+          split_packages = []
+          packages.each do |package|
+            split_packages += split_by_category(package)
           return_next(split_packages)
         end
 
         private
 
         def split_by_category(package)
-          # group package items by shipping category
-          grouped_packages = package.contents.group_by(&method(:shipping_category_for))
-          hash_to_packages(grouped_packages)
+          categories = Hash.new { |hash, key| hash[key] = [] }
+          package.contents.each do |item|
+            categories[item.variant.shipping_category_id] << item
+          end
+          hash_to_packages(categories)
         end
 
-        def hash_to_packages(grouped_packages)
-          # select values from packages grouped by shipping categories and build new packages
-          grouped_packages.values.map(&method(:build_package))
-        end
-
-        # optimization: save variant -> shipping_category correspondence
-        def shipping_category_for(item)
-          @item_shipping_category ||= {}
-          @item_shipping_category[item.inventory_unit.variant_id] ||= item.variant.shipping_category_id
+        def hash_to_packages(categories)
+          packages = []
+          categories.each do |_id, contents|
+            packages << build_package(contents)
+          end
+          packages
         end
       end
     end
