771f2b6dd50cbf1c9cc5cea6714c3a9c781528f2
99cm <zyizyao@china-guide.com>
2018-09-06 15:35:04 -0400

    Update time_since_purchase.rb

--- core/app/models/spree/return_item/eligibility_validator/time_since_purchase.rb
+++ core/app/models/spree/return_item/eligibility_validator/time_since_purchase.rb
@@ -1,16 +1,22 @@
-module Spree
-  class ReturnItem::EligibilityValidator::TimeSincePurchase < Spree::ReturnItem::EligibilityValidator::BaseValidator
-    def eligible_for_return?
-      if (@return_item.inventory_unit.order.completed_at + Spree::Config[:return_eligibility_number_of_days].days) > Time.current
-        true
-      else
-        add_error(:number_of_days, Spree.t('return_item_time_period_ineligible'))
-        false
-      end
-    end
+# frozen_string_literal: true
 
-    def requires_manual_intervention?
-      false
+module Spree
+  class ReturnItem < Spree::Base
+    module EligibilityValidator
+      class TimeSincePurchase < Spree::ReturnItem::EligibilityValidator::BaseValidator
+        def eligible_for_return?
+          if (@return_item.inventory_unit.order.completed_at + Spree::Config[:return_eligibility_number_of_days].days) > Time.current
+            true
+          else
+            add_error(:number_of_days, I18n.t('spree.return_item_time_period_ineligible'))
+            false
+          end
+        end
+
+        def requires_manual_intervention?
+          false
+        end
+      end
     end
   end
 end
