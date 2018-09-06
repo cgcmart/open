48be296e3ebc11635ae7c3ad518f8dc56874ac48
99cm <zyizyao@china-guide.com>
2018-09-06 18:23:34 -0400

    Update db_maximum_length_validator.rb

--- core/app/models/spree/validations/db_maximum_length_validator.rb
+++ core/app/models/spree/validations/db_maximum_length_validator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Spree
   module Validations
     ##
@@ -6,11 +8,10 @@ module Spree
       def initialize(options)
         super
         @field = options[:field].to_s
-        raise ArgumentError, 'a field must be specified to the validator' if @field.blank?
+        raise ArgumentError.new('a field must be specified to the validator') if @field.blank?
       end
 
       def validate(record)
-        warn '`Spree::Validations::DbMaximumLengthValidator` is deprecated. Use `DbMaximumLengthValidator` instead.'
         limit = record.class.columns_hash[@field].limit
         value = record[@field.to_sym]
         if value && limit && value.to_s.length > limit
