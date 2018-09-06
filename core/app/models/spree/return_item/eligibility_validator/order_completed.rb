68f599f7376b1bb33a468885942389f393d7ca9a
99cm <zyizyao@china-guide.com>
2018-09-06 15:32:47 -0400

    Update order_completed.rb

--- core/app/models/spree/return_item/eligibility_validator/order_completed.rb
+++ core/app/models/spree/return_item/eligibility_validator/order_completed.rb
@@ -1,16 +1,22 @@
-module Spree
-  class ReturnItem::EligibilityValidator::OrderCompleted < Spree::ReturnItem::EligibilityValidator::BaseValidator
-    def eligible_for_return?
-      if @return_item.inventory_unit.order.completed?
-        true
-      else
-        add_error(:order_not_completed, Spree.t('return_item_order_not_completed'))
-        false
-      end
-    end
+# frozen_string_literal: true
 
-    def requires_manual_intervention?
-      false
+module Spree
+  class ReturnItem < Spree::Base
+    module EligibilityValidator
+      class OrderCompleted < Spree::ReturnItem::EligibilityValidator::BaseValidator
+        def eligible_for_return?
+          if @return_item.inventory_unit.order.completed?
+            true
+          else
+            add_error(:order_not_completed, I18n.t('spree.return_item_order_not_completed'))
+            false
+          end
+        end
+
+        def requires_manual_intervention?
+          false
+        end
+      end
     end
   end
 end
