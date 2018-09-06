e2dddc131fcca2d5c75b10bc457aa34d51b2ed35
99cm <zyizyao@china-guide.com>
2018-09-06 14:05:20 -0400

    Update credit.rb

--- core/app/models/spree/reimbursement_type/credit.rb
+++ core/app/models/spree/reimbursement_type/credit.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Spree
   class ReimbursementType::Credit < Spree::ReimbursementType
     extend Spree::ReimbursementType::ReimbursementHelpers
 
     class << self
       def reimburse(reimbursement, return_items, simulate)
-        unpaid_amount = return_items.map { |ri| ri.total.to_d.round(2) }.sum
+        unpaid_amount = return_items.sum(&:total).round(2, :down)
         reimbursement_list, unpaid_amount = create_credits(reimbursement, unpaid_amount, simulate)
         reimbursement_list
       end
