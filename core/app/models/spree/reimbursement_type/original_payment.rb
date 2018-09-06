1b3a3c48dd4e118e5bc03b190b92a4257775e8d4
99cm <zyizyao@china-guide.com>
2018-09-06 14:11:35 -0400

    Update original_payment.rb

--- core/app/models/spree/reimbursement_type/original_payment.rb
+++ core/app/models/spree/reimbursement_type/original_payment.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Spree::ReimbursementType::OriginalPayment < Spree::ReimbursementType
   extend Spree::ReimbursementType::ReimbursementHelpers
 
   class << self
     def reimburse(reimbursement, return_items, simulate)
-      unpaid_amount = return_items.map { |ri| ri.total.to_d.round(2) }.sum
+      unpaid_amount = return_items.sum(&:total).round(2, :down)
       payments = reimbursement.order.payments.completed
 
       reimbursement_list, unpaid_amount = create_refunds(reimbursement, payments, unpaid_amount, simulate)
