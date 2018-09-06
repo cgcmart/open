da64ca1fa6aedebdf387e594951ea8c21d6e41f8
99cm <zyizyao@china-guide.com>
2018-09-06 14:21:55 -0400

    Update reimbursement_helpers.rb

--- core/app/models/spree/reimbursement_type/reimbursement_helpers.rb
+++ core/app/models/spree/reimbursement_type/reimbursement_helpers.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
+
 module Spree
   module ReimbursementType::ReimbursementHelpers
+    # Ordered list of payment methods that are valid for the reimbursement
+    # type to use for reimbursing. Leaving this nil allows anything.
+    attr_accessor :eligible_refund_methods
+
     def create_refunds(reimbursement, payments, unpaid_amount, simulate, reimbursement_list = [])
-      payments.each do |payment|
+      payments = sorted_eligible_refund_payments(payments)
+      payments.map do |payment|
         break if unpaid_amount <= 0
         next unless payment.can_credit?
 
@@ -24,11 +31,11 @@ module Spree
     private
 
     def create_refund(reimbursement, payment, amount, simulate)
-      refund = reimbursement.refunds.build(
+      refund = reimbursement.refunds.build({
         payment: payment,
         amount: amount,
         reason: Spree::RefundReason.return_processing_reason
-      )
+      })
 
       simulate ? refund.readonly! : refund.save!
       refund
@@ -37,27 +44,29 @@ module Spree
     # If you have multiple methods of crediting a customer, overwrite this method
     # Must return an array of objects the respond to #description, #display_amount
     def create_credit(reimbursement, unpaid_amount, simulate)
-      category = Spree::StoreCreditCategory.default_reimbursement_category(category_options(reimbursement))
-      creditable = Spree::StoreCredit.new(store_credit_params(category, reimbursement, unpaid_amount))
+      creditable = create_creditable(reimbursement, unpaid_amount)
       credit = reimbursement.credits.build(creditable: creditable, amount: unpaid_amount)
       simulate ? credit.readonly! : credit.save!
       credit
     end
 
-    def store_credit_params(category, reimbursement, unpaid_amount)
-      {
+    def create_creditable(reimbursement, unpaid_amount)
+      Spree::Reimbursement::Credit.default_creditable_class.new(
         user: reimbursement.order.user,
         amount: unpaid_amount,
-        category: category,
+        category: Spree::StoreCreditCategory.reimbursement_category(reimbursement),
         created_by: Spree::StoreCredit.default_created_by,
         memo: "Refund for uncreditable payments on order #{reimbursement.order.number}",
         currency: reimbursement.order.currency
-      }
+      )
     end
 
-    # overwrite if you need options for the default reimbursement category
-    def category_options(_reimbursement)
-      {}
+    def sorted_eligible_refund_payments(payments)
+      if eligible_refund_methods = self.eligible_refund_methods
+        payments = payments.select { |p| eligible_refund_methods.include? p.payment_method.class }
+        payments = payments.sort_by { |p| eligible_refund_methods.index(p.payment_method.class) }
+      end
+      payments
     end
   end
 end
