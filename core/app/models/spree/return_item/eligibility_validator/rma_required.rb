2c16612b2f0cbc3ac8ba1b84311fe28c5cdbbf2b
99cm <zyizyao@china-guide.com>
2018-09-06 15:33:58 -0400

    Update rma_required.rb

--- core/app/models/spree/return_item/eligibility_validator/rma_required.rb
+++ core/app/models/spree/return_item/eligibility_validator/rma_required.rb
@@ -1,16 +1,22 @@
-module Spree
-  class ReturnItem::EligibilityValidator::RMARequired < Spree::ReturnItem::EligibilityValidator::BaseValidator
-    def eligible_for_return?
-      if @return_item.return_authorization.present?
-        true
-      else
-        add_error(:rma_required, Spree.t('return_item_rma_ineligible'))
-        false
-      end
-    end
+# frozen_string_literal: true
 
-    def requires_manual_intervention?
-      false
+module Spree
+  class ReturnItem < Spree::Base
+    module EligibilityValidator
+      class RMARequired < Spree::ReturnItem::EligibilityValidator::BaseValidator
+        def eligible_for_return?
+          if @return_item.return_authorization.present?
+            true
+          else
+            add_error(:rma_required, I18n.t('spree.return_item_rma_ineligible'))
+            false
+          end
+        end
+
+        def requires_manual_intervention?
+          false
+        end
+      end
     end
   end
 end
