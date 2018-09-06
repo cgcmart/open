8c915244ed36348eb88baa6b8d7dca9e903131d6
99cm <zyizyao@china-guide.com>
2018-09-06 11:59:35 -0400

    Update user_logged_in.rb

--- core/app/models/spree/promotion/rules/user_logged_in.rb
+++ core/app/models/spree/promotion/rules/user_logged_in.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Spree
-  class Promotion
+  class Promotion < Spree::Base
     module Rules
       class UserLoggedIn < PromotionRule
         def applicable?(promotable)
