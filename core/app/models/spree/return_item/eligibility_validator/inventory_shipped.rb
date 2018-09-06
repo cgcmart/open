b2f65aac7f636e8257d7412509e5124cbd8f94b4
99cm <zyizyao@china-guide.com>
2018-09-06 15:30:22 -0400

    Update inventory_shipped.rb

--- core/app/models/spree/return_item/eligibility_validator/inventory_shipped.rb
+++ core/app/models/spree/return_item/eligibility_validator/inventory_shipped.rb
@@ -1,16 +1,22 @@
-module Spree
-  class ReturnItem::EligibilityValidator::InventoryShipped < Spree::ReturnItem::EligibilityValidator::BaseValidator
-    def eligible_for_return?
-      if @return_item.inventory_unit.shipped?
-        true
-      else
-        add_error(:inventory_unit_shipped, Spree.t('return_item_inventory_unit_ineligible'))
-        false
-      end
-    end
+# frozen_string_literal: true
 
-    def requires_manual_intervention?
-      @errors.present?
+module Spree
+  class ReturnItem < Spree::Base
+    module EligibilityValidator
+      class InventoryShipped < Spree::ReturnItem::EligibilityValidator::BaseValidator
+        def eligible_for_return?
+          if @return_item.inventory_unit.shipped?
+            true
+          else
+            add_error(:inventory_unit_shipped, I18n.t('spree.return_item_inventory_unit_ineligible'))
+            false
+          end
+        end
+
+        def requires_manual_intervention?
+          @errors.present?
+        end
+      end
     end
   end
 end
