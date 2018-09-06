49659895c9e61fb63c3c473d4cbfcd84428915ec
99cm <zyizyao@china-guide.com>
2018-09-06 11:51:48 -0400

    Update taxon.rb

--- core/app/models/spree/promotion/rules/taxon.rb
+++ core/app/models/spree/promotion/rules/taxon.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Spree
-  class Promotion
+  class Promotion < Spree::Base
     module Rules
       class Taxon < PromotionRule
         has_many :promotion_rule_taxons, class_name: 'Spree::PromotionRuleTaxon',
@@ -8,20 +10,38 @@ module Spree
         has_many :taxons, through: :promotion_rule_taxons, class_name: 'Spree::Taxon'
 
         MATCH_POLICIES = %w(any all)
-        preference :match_policy, default: MATCH_POLICIES.first
+
+        validates_inclusion_of :preferred_match_policy, in: MATCH_POLICIES
+        preference :match_policy, :string, default: MATCH_POLICIES.first
 
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
         end
 
         def eligible?(order, _options = {})
-          if preferred_match_policy == 'all'
-            unless (taxons.to_a - taxons_in_order_including_parents(order)).empty?
+          order_taxons = taxons_in_order(order)
+
+          case preferred_match_policy
+          when 'all'
+            matches_all = taxons.all? do |rule_taxon|
+              order_taxons.where(id: rule_taxon.self_and_descendants.ids).exists?
+            end
+
+            unless matches_all
               eligibility_errors.add(:base, eligibility_error_message(:missing_taxon))
             end
+          when 'any'
+            unless order_taxons.where(id: rule_taxon_ids_with_children).exists?
+              eligibility_errors.add(:base, eligibility_error_message(:no_matching_taxons))
+            end
+          when 'none'
+            if order_taxons.where(id: rule_taxon_ids_with_children).exists?
+              eligibility_errors.add(:base, eligibility_error_message(:has_excluded_taxon))
+            end
           else
-            order_taxons = taxons_in_order_including_parents(order)
-            unless taxons.any? { |taxon| order_taxons.include? taxon }
+            # Change this to an exception in a future version of Solidus
+            warn_invalid_match_policy(assume: 'any')
+            unless order_taxons.where(id: rule_taxon_ids_with_children).exists?
               eligibility_errors.add(:base, eligibility_error_message(:no_matching_taxons))
             end
           end
@@ -30,16 +50,30 @@ module Spree
         end
 
         def actionable?(line_item)
-          taxon_product_ids.include? line_item.variant.product_id
+          found = Spree::Classification.where(
+            product_id: line_item.variant.product_id,
+            taxon_id: taxon_ids
+          ).exists?
+
+          case preferred_match_policy
+          when 'any', 'all'
+            found
+          when 'none'
+            !found
+          else
+            # Change this to an exception in a future version of Solidus
+            warn_invalid_match_policy(assume: 'any')
+            found
+          end
         end
 
         def taxon_ids_string
           taxons.pluck(:id).join(',')
         end
 
-        def taxon_ids_string=(s)
-          ids = s.to_s.split(',').map(&:strip)
-          self.taxons = Spree::Taxon.find(ids)
+        def taxon_ids_string=(taxon_ids)
+          taxon_ids = taxon_ids.to_s.split(',').map(&:strip)
+          self.taxons = Spree::Taxon.find(taxon_ids)
         end
 
         private
@@ -50,22 +84,8 @@ module Spree
         end
 
         # ids of taxons rules and taxons rules children
-        def taxons_including_children_ids
-          taxons.inject([]) { |ids, taxon| ids += taxon.self_and_descendants.ids }
-        end
-
-        # taxons order vs taxons rules and taxons rules children
-        def order_taxons_in_taxons_and_children(order)
-          order_taxons(order).where(id: taxons_including_children_ids)
-        end
-
-        def taxons_in_order_including_parents(order)
-          order_taxons_in_taxons_and_children(order).inject([]) { |taxons, taxon| taxons << taxon.self_and_ancestors }.flatten.uniq
-        end
-
-        def taxon_product_ids
-          Spree::Product.joins(:taxons).where(spree_taxons: { id: taxons.pluck(:id) }).pluck(:id).uniq
-        end
+        def rule_taxon_ids_with_children
+          taxons.flat_map { |taxon| taxon.self_and_descendants.ids }.uniq
       end
     end
   end
