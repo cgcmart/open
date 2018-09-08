4bd9a1e908b94e0984bd3646c7dccfae7c817aaf
99cm <zyizyao@china-guide.com>
2018-09-08 13:17:35 -0400

    Update country.rb

--- core/app/models/spree/country.rb
+++ core/app/models/spree/country.rb
@@ -1,23 +1,18 @@
+# frozen_string_literal: true
+
 module Spree
   class Country < Spree::Base
     # we need to have this callback before any dependent: :destroy associations
     # https://github.com/rails/rails/issues/3458
     before_destroy :ensure_not_default
 
-    has_many :states, dependent: :destroy
-    has_many :addresses, dependent: :restrict_with_error
-    has_many :zone_members,
-             -> { where(zoneable_type: 'Spree::Country') },
-             class_name: 'Spree::ZoneMember',
-             dependent: :destroy,
-             foreign_key: :zoneable_id
+    has_many :states, -> { order(:name) }, dependent: :destroy
+    has_many :addresses, dependent: :nullify
+    has_many :prices, class_name: "Spree::Price", foreign_key: "country_iso", primary_key: "iso"
 
-    has_many :zones, through: :zone_members, class_name: 'Spree::Zone'
-
-    validates :name, :iso_name, presence: true, uniqueness: { case_sensitive: false }
+    validates :name, :iso_name, presence: true
 
     def self.default
-      country_id = Spree::Config[:default_country_id]
       default = find_by(id: country_id) if country_id.present?
       default || find_by(iso: 'US') || first
     end
@@ -34,7 +29,7 @@ module Spree
 
     def ensure_not_default
       if id.eql?(Spree::Config[:default_country_id])
-        errors.add(:base, Spree.t(:default_country_cannot_be_deleted))
+        errors.add(:base, I18n.t('spree.default_country_cannot_be_deleted'))
         throw(:abort)
       end
     end
