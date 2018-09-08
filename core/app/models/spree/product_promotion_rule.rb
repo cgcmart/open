ab6fb9782df4a1f61e4d36b3d23c7db928d3fcd0
99cm <zyizyao@china-guide.com>
2018-09-08 18:52:32 -0400

    Update product_promotion_rule.rb

--- core/app/models/spree/product_promotion_rule.rb
+++ core/app/models/spree/product_promotion_rule.rb
@@ -1,9 +1,8 @@
+# frozen_string_literal: true
+
 module Spree
   class ProductPromotionRule < Spree::Base
-    belongs_to :product, class_name: 'Spree::Product'
-    belongs_to :promotion_rule, class_name: 'Spree::PromotionRule'
-
-    validates :product, :promotion_rule, presence: true
-    validates :product_id, uniqueness: { scope: :promotion_rule_id }, allow_nil: true
+    belongs_to :product
+    belongs_to :promotion_rule
   end
 end
