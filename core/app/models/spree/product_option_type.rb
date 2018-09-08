6325190797ac1f4449be8957251e4916c162d74d
99cm <zyizyao@china-guide.com>
2018-09-08 18:50:32 -0400

    Update product_option_type.rb

--- core/app/models/spree/product_option_type.rb
+++ core/app/models/spree/product_option_type.rb
@@ -1,12 +1,9 @@
+# frozen_string_literal: true
+
 module Spree
   class ProductOptionType < Spree::Base
-    with_options inverse_of: :product_option_types do
-      belongs_to :product, class_name: 'Spree::Product'
-      belongs_to :option_type, class_name: 'Spree::OptionType'
-    end
+    belongs_to :product, class_name: 'Spree::Product', inverse_of: :product_option_types, touch: true
+    belongs_to :option_type, class_name: 'Spree::OptionType', inverse_of: :product_option_types
     acts_as_list scope: :product
-
-    validates :product, :option_type, presence: true
-    validates :product_id, uniqueness: { scope: :option_type_id }, allow_nil: true
   end
 end
