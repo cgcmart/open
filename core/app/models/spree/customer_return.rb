031538d2081f7454cb3598d5c60f3fe2396dd4a4
99cm <zyizyao@china-guide.com>
2018-09-08 13:44:00 -0400

    Update customer_return.rb

--- core/app/models/spree/customer_return.rb
+++ core/app/models/spree/customer_return.rb
@@ -1,34 +1,34 @@
+# frozen_string_literal: true
+
 module Spree
   class CustomerReturn < Spree::Base
-    include Spree::Core::NumberGenerator.new(prefix: 'CR', length: 9)
     belongs_to :stock_location
 
-    has_many :reimbursements, inverse_of: :customer_return
     has_many :return_items, inverse_of: :customer_return
     has_many :return_authorizations, through: :return_items
+    has_many :reimbursements, inverse_of: :customer_return
 
+    before_create :generate_number
     after_create :process_return!
 
-    validates :number, uniqueness: true
-    validates :return_items, :stock_location, presence: true
-    validate :must_have_return_authorization, on: :create
+    validates :return_items, presence: true
+    validates :stock_location, presence: true
     validate :return_items_belong_to_same_order
 
     accepts_nested_attributes_for :return_items
 
     extend DisplayMoney
-    money_methods pre_tax_total: { currency: Spree::Config[:currency] }
+    money_methods :total, :amount
 
-    self.whitelisted_ransackable_attributes = ['number']
-
+    delegate :currency, to: :order
     delegate :id, to: :order, prefix: true, allow_nil: true
 
-    def completely_decided?
-      !return_items.undecided.exists?
+    def total
+      return_items.map(&:total).sum
     end
 
-    def fully_reimbursed?
-      completely_decided? && return_items.accepted.includes(:reimbursement).all? { |return_item| return_item.reimbursement.try(:reimbursed?) }
+    def amount
+      return_items.sum(:amount)
     end
 
     # Temporarily tie a customer_return to one order
@@ -37,31 +37,35 @@ module Spree
       return_items.first.inventory_unit.order
     end
 
-    def pre_tax_total
-      return_items.sum(:pre_tax_amount)
+    def fully_reimbursed?
+      completely_decided? && return_items.accepted.includes(:reimbursement).all? { |return_item| return_item.reimbursement.try(:reimbursed?) }
+    end
+
+    def completely_decided?
+      !return_items.undecided.exists?
+    end
+
+    def process_return!
+      order.return! if order.all_inventory_units_returned?
     end
 
     private
 
-    def inventory_units
-      return_items.flat_map(&:inventory_unit)
-    end
-
-    def must_have_return_authorization
-      if item = return_items.find { |ri| ri.return_authorization.blank? }
-        errors.add(:base, Spree.t(:missing_return_authorization, item_name: item.inventory_unit.variant.name))
+    def generate_number
+      self.number ||= loop do
+        random = "CR#{Array.new(9){ rand(9) }.join}"
+        break random unless self.class.exists?(number: random)
       end
-    end
-
-    def process_return!
-      return_items.each(&:receive!)
-      order.return! if order.all_inventory_units_returned?
     end
 
     def return_items_belong_to_same_order
-      if return_items.any? { |return_item| return_item.inventory_unit.order_id != order_id }
-        errors.add(:base, Spree.t(:return_items_cannot_be_associated_with_multiple_orders))
+      if return_items.reject{ |return_item| return_item.inventory_unit.order_id == order_id }.any?
+        errors.add(:base, I18n.t('spree.return_items_cannot_be_associated_with_multiple_orders'))
       end
+    end
+
+    def inventory_units
+      return_items.flat_map(&:inventory_unit)
     end
   end
 end
