c077ff2abf03471e04880365d66c133e9c4e3c0a
99cm <zyizyao@china-guide.com>
2018-09-08 14:19:54 -0400

    Update log_entry.rb

--- core/app/models/spree/log_entry.rb
+++ core/app/models/spree/log_entry.rb
@@ -1,14 +1,8 @@
+# frozen_string_literal: true
+
 module Spree
   class LogEntry < Spree::Base
     belongs_to :source, polymorphic: true
-
-    # Fix for #1767
-    # If a payment fails, we want to make sure we keep the record of it failing
-    after_rollback :save_anyway, if: proc { !Rails.env.test? }
-
-    def save_anyway
-      Spree::LogEntry.create!(source: source, details: details)
-    end
 
     def parsed_details
       @details ||= YAML.load(details)
