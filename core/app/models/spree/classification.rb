85afdd6d4ec1bb2b0e7d3cc8826cbe263187197b
99cm <zyizyao@china-guide.com>
2018-09-08 11:55:39 -0400

    Update classification.rb

--- core/app/models/spree/classification.rb
+++ core/app/models/spree/classification.rb
@@ -1,15 +1,14 @@
+# frozen_string_literal: true
+
 module Spree
   class Classification < Spree::Base
     self.table_name = 'spree_products_taxons'
     acts_as_list scope: :taxon
 
-    with_options inverse_of: :classifications, touch: true do
-      belongs_to :product, class_name: 'Spree::Product'
-      belongs_to :taxon, class_name: 'Spree::Taxon'
-    end
+    belongs_to :product, class_name: 'Spree::Product', inverse_of: :classifications, touch: true
+    belongs_to :taxon, class_name: 'Spree::Taxon',inverse_of: :classifications, touch: true
 
-    validates :taxon, :product, presence: true
-    # For #3494
-    validates :taxon_id, uniqueness: { scope: :product_id, message: :already_linked, allow_blank: true }
+    # For https://github.com/spree/spree/issues/3494
+    validates_uniqueness_of :taxon_id, scope: :product_id, message: :already_linked
   end
 end
