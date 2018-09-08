c2311988bb42142cd6f1a15d2714f6acbef8f4a6
99cm <zyizyao@china-guide.com>
2018-09-08 14:40:01 -0400

    Update option_value_variant.rb

--- core/app/models/spree/option_value_variant.rb
+++ core/app/models/spree/option_value_variant.rb
@@ -1,9 +1,8 @@
+# frozen_string_literal: true
+
 module Spree
   class OptionValueVariant < Spree::Base
-    belongs_to :option_value, class_name: 'Spree::OptionValue'
-    belongs_to :variant, class_name: 'Spree::Variant'
-
-    validates :option_value, :variant, presence: true
-    validates :option_value_id, uniqueness: { scope: :variant_id }
+    belongs_to :option_value
+    belongs_to :variant
   end
 end
