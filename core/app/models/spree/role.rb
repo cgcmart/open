8186d6bf7d36d05eb2df1a0ce25151814075e380
99cm <zyizyao@china-guide.com>
2018-09-09 18:49:32 -0400

    Update role.rb

--- core/app/models/spree/role.rb
+++ core/app/models/spree/role.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 module Spree
   class Role < Spree::Base
     has_many :role_users, class_name: 'Spree::RoleUser', dependent: :destroy
-    has_many :users, through: :role_users, class_name: Spree.user_class.to_s
+    has_many :users, through: :role_users
 
-    validates :name, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
+    validates_uniqueness_of :name
+
+    def admin?
+      name == 'admin'
+    end
   end
 end
