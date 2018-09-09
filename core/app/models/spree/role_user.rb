89a7b66efd01fe650b0acf3eabed42016a099ed5
99cm <zyizyao@china-guide.com>
2018-09-09 18:55:17 -0400

    Update role_user.rb

--- core/app/models/spree/role_user.rb
+++ core/app/models/spree/role_user.rb
@@ -1,6 +1,19 @@
+# frozen_string_literal: true
+
 module Spree
   class RoleUser < Spree::Base
+    self.table_name = "spree_roles_users"
     belongs_to :role, class_name: 'Spree::Role'
-    belongs_to :user, class_name: Spree.user_class.to_s
+    belongs_to :user, class_name: Spree::UserClassHandle.new
+
+    after_create :auto_generate_spree_api_key
+
+    validates_uniqueness_of :role_id, scope: :user_id
+
+    private
+
+    def auto_generate_spree_api_key
+      user.try!(:auto_generate_spree_api_key)
+    end
   end
 end
