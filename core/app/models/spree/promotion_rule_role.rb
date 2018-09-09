4646aa3ce8194291bef56659e8afe5ed02d82c29
99cm <zyizyao@china-guide.com>
2018-09-09 17:27:46 -0400

    Create promotion_rule_role.rb

---
+++ core/app/models/spree/promotion_rule_role.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Spree
+  class PromotionRuleRole < ActiveRecord::Base
+    belongs_to :promotion_rule, class_name: 'Spree::PromotionRule'
+    belongs_to :role, class_name: 'Spree::Role'
+  end
+end
