a1c969f37ca1632a3cf78773905d25f731806d5d
99cm <zyizyao@china-guide.com>
2018-09-09 18:41:08 -0400

    Update reimbursement_tax_calculator.rb

--- core/app/models/spree/reimbursement_tax_calculator.rb
+++ core/app/models/spree/reimbursement_tax_calculator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Spree
   # Tax calculation is broken out at this level to allow easy integration with 3rd party
   # taxation systems.  Those systems are usually geared toward calculating all items at once
@@ -18,19 +20,15 @@ module Spree
       private
 
       def set_tax!(return_item)
-        calculated_refund = Spree::ReturnItem.refund_amount_calculator.new.compute(return_item)
-
-        percent_of_tax = if return_item.pre_tax_amount <= 0 || calculated_refund <= 0
-                           0
-                         else
-                           return_item.pre_tax_amount / calculated_refund
-        end
+        percent_of_tax = (return_item.amount <= 0) ? 0 : return_item.amount / Spree::ReturnItem.refund_amount_calculator.new.compute(return_item)
 
         additional_tax_total = percent_of_tax * return_item.inventory_unit.additional_tax_total
         included_tax_total   = percent_of_tax * return_item.inventory_unit.included_tax_total
 
-        return_item.update_attributes!(additional_tax_total: additional_tax_total,
-                                       included_tax_total:   included_tax_total)
+        return_item.update_attributes!({
+          additional_tax_total: additional_tax_total,
+          included_tax_total:   included_tax_total
+        })
       end
     end
   end
