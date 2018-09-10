68454d7ed65e88e7acf18cb975ff4e899e4d412c
99cm <zyizyao@china-guide.com>
2018-09-10 13:10:38 -0400

    Update tax_category.rb

--- core/app/models/spree/tax_category.rb
+++ core/app/models/spree/tax_category.rb
@@ -1,17 +1,26 @@
+# frozen_string_literal: true
+
 module Spree
   class TaxCategory < Spree::Base
     acts_as_paranoid
-    validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :deleted_at }
+    validates :name, presence: true
+    validates_uniqueness_of :name, unless: :deleted_at
 
-    has_many :tax_rates, dependent: :destroy, inverse_of: :tax_category
+    has_many :tax_rate_tax_categories,
+      class_name: 'Spree::TaxRateTaxCategory',
+      dependent: :destroy,
+      inverse_of: :tax_category
+    has_many :tax_rates, through: :tax_rate_tax_categories, class_name: 'Spree::TaxRate', inverse_of: :tax_category
 
-    before_save :set_default_category
+    after_save :ensure_one_default
 
-    def set_default_category
-      # set existing default tax category to false if this one has been marked as default
+    def self.default
+      find_by(is_default: true)
+    end
 
-      if is_default && tax_category = self.class.where(is_default: true).where.not(id: id).first
-        tax_category.update_columns(is_default: false, updated_at: Time.current)
+    def ensure_one_default
+      if is_default
+        Spree::TaxCategory.where(is_default: true).where.not(id: id).update_all(is_default: false, updated_at: Time.current)
       end
     end
   end
