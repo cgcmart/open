98204a9b771df1b58080c5c0dadcefc01757ce17
99cm <zyizyao@china-guide.com>
2018-09-10 12:11:19 -0400

    Update state_change.rb

--- core/app/models/spree/state_change.rb
+++ core/app/models/spree/state_change.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 module Spree
   class StateChange < Spree::Base
-    belongs_to :user, class_name: Spree.user_class.to_s, optional: true
+    belongs_to :user
     belongs_to :stateful, polymorphic: true
+    before_create :assign_user
 
     def <=>(other)
       created_at <=> other.created_at
+    end
+
+    def assign_user
+      true # don't stop the filters
     end
   end
 end
