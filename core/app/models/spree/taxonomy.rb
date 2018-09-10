eed80181e8ae8c1811e66be06b6896861325a509
99cm <zyizyao@china-guide.com>
2018-09-10 13:43:39 -0400

    Update taxonomy.rb

--- core/app/models/spree/taxonomy.rb
+++ core/app/models/spree/taxonomy.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 module Spree
   class Taxonomy < Spree::Base
     acts_as_list
 
-    validates :name, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
+    validates :name, presence: true
 
     has_many :taxons, inverse_of: :taxonomy
     has_one :root, -> { where parent_id: nil }, class_name: 'Spree::Taxon', dependent: :destroy
 
-    after_create :set_root
-    after_save :set_root_taxon_name
+    after_save :set_name
 
-    default_scope { order("#{table_name}.position, #{table_name}.created_at") }
+    default_scope -> { order(position: :asc) }
 
     private
 
-    def set_root
-      self.root ||= Taxon.create!(taxonomy_id: id, name: name)
-    end
-
-    def set_root_taxon_name
-      root.update_attributes(name: name)
+    def set_name
+      if root
+        root.update_columns(
+          name: name,
+          updated_at: Time.current
+        )
+      else
+        self.root = Spree::Taxon.create!(taxonomy_id: id, name: name)
+      end
     end
   end
 end
