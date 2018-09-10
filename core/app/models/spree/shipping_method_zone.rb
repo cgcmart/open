f5a6f4aa6595732a146ddc06e36e10839a05954b
99cm <zyizyao@china-guide.com>
2018-09-10 11:21:03 -0400

    Update shipping_method_zone.rb

--- core/app/models/spree/shipping_method_zone.rb
+++ core/app/models/spree/shipping_method_zone.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 module Spree
   class ShippingMethodZone < Spree::Base
-    belongs_to :shipping_method
     belongs_to :zone
+    belongs_to :shipping_method
   end
 end
