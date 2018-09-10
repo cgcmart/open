085d1c17950fabb5f6115799e43ef15018f3e43f
99cm <zyizyao@china-guide.com>
2018-09-10 11:11:00 -0400

    Update shipping_category.rb

--- core/app/models/spree/shipping_category.rb
+++ core/app/models/spree/shipping_category.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
+
 module Spree
   class ShippingCategory < Spree::Base
-    validates :name, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
-
-    with_options inverse_of: :shipping_category do
-      has_many :products
-      has_many :shipping_method_categories
-    end
+    validates :name, presence: true
+    has_many :products, inverse_of: :shipping_category
+    has_many :shipping_method_categories, inverse_of: :shipping_category
     has_many :shipping_methods, through: :shipping_method_categories
   end
 end
