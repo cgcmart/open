6bb7b191196ca5f0a60389ecea1fec4515115dc1
99cm <zyizyao@china-guide.com>
2018-09-10 12:17:54 -0400

    Update stock_movement.rb

--- core/app/models/spree/stock_movement.rb
+++ core/app/models/spree/stock_movement.rb
@@ -1,30 +1,21 @@
+# frozen_string_literal: true
+
 module Spree
   class StockMovement < Spree::Base
-    QUANTITY_LIMITS = {
-      max: 2**31 - 1,
-      min: -2**31
-    }.freeze
-
     belongs_to :stock_item, class_name: 'Spree::StockItem', inverse_of: :stock_movements
     belongs_to :originator, polymorphic: true
 
     after_create :update_stock_item_quantity
 
-    with_options presence: true do
-      validates :stock_item
-      validates :quantity, numericality: {
-        greater_than_or_equal_to: QUANTITY_LIMITS[:min],
-        less_than_or_equal_to: QUANTITY_LIMITS[:max],
-        only_integer: true
-      }
-    end
+    validates :stock_item, presence: true
+    validates :quantity, presence: true
 
     scope :recent, -> { order(created_at: :desc) }
 
     self.whitelisted_ransackable_attributes = ['quantity']
 
     def readonly?
-      persisted?
+      !new_record?
     end
 
     private
