40039cdbb2becbb4d3dd69ad08ed7ae1591c20fb
99cm <zyizyao@china-guide.com>
2018-09-12 10:07:13 -0400

    Update zone_member.rb

--- core/app/models/spree/zone_member.rb
+++ core/app/models/spree/zone_member.rb
@@ -1,12 +1,10 @@
+# frozen_string_literal: true
+
 module Spree
   class ZoneMember < Spree::Base
     belongs_to :zone, class_name: 'Spree::Zone', counter_cache: true, inverse_of: :zone_members
     belongs_to :zoneable, polymorphic: true
 
-    validates :zone, :zoneable, presence: true
-
-    scope :defunct_without_kind, ->(kind) do
-      where('zoneable_id IS NULL OR zoneable_type != ?', "Spree::#{kind.classify}")
-    end
+    delegate :name, to: :zoneable, allow_nil: true
   end
 end
