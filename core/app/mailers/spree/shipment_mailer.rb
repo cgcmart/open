1ae667108c7c467c426aed57586470a06cad0771
99cm <zyizyao@china-guide.com>
2018-09-12 11:43:14 -0400

    Update shipment_mailer.rb

--- core/app/mailers/spree/shipment_mailer.rb
+++ core/app/mailers/spree/shipment_mailer.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module Spree
   class ShipmentMailer < BaseMailer
     def shipped_email(shipment, resend = false)
       @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
-      mail(to: @shipment.order.email, from: from_address, subject: subject)
+      @store = @order.store
+      subject = (resend ? "[#{.t('spree.resend').upcase}] " : '')
+      subject += "#{@store.name} #{t('spree.shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
+      mail(to: @shipment.order.email, from: from_address(@store), subject: subject)
     end
   end
 end
