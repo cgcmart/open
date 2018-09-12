e7d6550105ffc277d9935215db8ad8ff0a682f4b
99cm <zyizyao@china-guide.com>
2018-09-12 10:34:28 -0400

    Update order_mailer.rb

--- core/app/mailers/spree/order_mailer.rb
+++ core/app/mailers/spree/order_mailer.rb
@@ -1,17 +1,36 @@
+# frozen_string_literal: true
+
 module Spree
   class OrderMailer < BaseMailer
     def confirm_email(order, resend = false)
-      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
-      mail(to: @order.email, from: from_address, subject: subject)
+      @order = order
+      @store = @order.store
+      subject = build_subject(t('.subject'), resend)
+
+      mail(to: @order.email, from: from_address(@store), subject: subject)
     end
 
     def cancel_email(order, resend = false)
-      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
-      mail(to: @order.email, from: from_address, subject: subject)
+      @order = order
+      @store = @order.store
+      subject = build_subject(t('.subject'), resend)
+
+      mail(to: @order.email, from: from_address(@store), subject: subject)
+    end
+
+    def inventory_cancellation_email(order, inventory_units, resend = false)
+      @order, @inventory_units = order, inventory_units
+      @store = @order.store
+      subject = build_subject(t('spree.order_mailer.inventory_cancellation.subject'), resend)
+
+      mail(to: @order.email, from: from_address(@store), subject: subject)
+    end
+
+    private
+
+    def build_subject(subject_text, resend)
+      resend_text = (resend ? "[#{t('spree.resend').upcase}] " : '')
+      "#{resend_text}#{@order.store.name} #{subject_text} ##{@order.number}"
     end
   end
 end
