352a2a3b089e3c9d4ab8030f5ffd168e271dccc7
99cm <zyizyao@china-guide.com>
2018-09-12 10:30:08 -0400

    Update base_mailer.rb

--- core/app/mailers/spree/base_mailer.rb
+++ core/app/mailers/spree/base_mailer.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Spree
   class BaseMailer < ActionMailer::Base
-    def from_address
-      Spree::Store.current.mail_from_address
+    def from_address(store)
+      store.mail_from_address
     end
 
     def money(amount, currency = Spree::Config[:currency])
@@ -15,18 +17,7 @@ module Spree
     helper_method :frontend_available?
 
     def mail(headers = {}, &block)
-      ensure_default_action_mailer_url_host
       super if Spree::Config[:send_core_emails]
-    end
-
-    private
-
-    # this ensures that ActionMailer::Base.default_url_options[:host] is always set
-    # this is only a fail-safe solution if developer didn't set this in environment files
-    # http://guides.rubyonrails.org/action_mailer_basics.html#generating-urls-in-action-mailer-views
-    def ensure_default_action_mailer_url_host
-      ActionMailer::Base.default_url_options ||= {}
-      ActionMailer::Base.default_url_options[:host] ||= Spree::Store.current.url
     end
   end
 end
