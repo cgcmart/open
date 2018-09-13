d575e990de10bee3e11d039e4a59989569ee8d56
99cm <zyizyao@china-guide.com>
2018-09-13 10:53:36 -0400

    Create next.rb

---
+++ core/app/services/spree/checkout/next.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Spree
+  module Checkout
+    class Next
+      def initialize(order)
+        @order = order
+      end
+
+      def call
+        order.next
+      end
+
+      private
+
+      attr_reader :order
+    end
+  end
+end
