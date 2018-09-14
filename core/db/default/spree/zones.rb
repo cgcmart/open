f453d566b7c066113a41831972fe28db5e366600
99cm <zyizyao@china-guide.com>
2018-09-14 11:55:33 -0400

    Update zones.rb

--- core/db/default/spree/zones.rb
+++ core/db/default/spree/zones.rb
@@ -1,10 +1,6 @@
-eu_vat = Spree::Zone.create!(name: "EU_VAT", description: "Countries that make up the EU VAT zone.", kind: 'country')
-north_america = Spree::Zone.create!(name: "North America", description: "USA + Canada", kind: 'country')
+# frozen_string_literal: true
 
-%w(PL FI PT RO DE FR SK HU SI IE AT ES IT BE SE LV BG GB LT CY LU MT DK NL EE HR CZ GR).
-each do |name|
-  eu_vat.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
-end
+north_america = Spree::Zone.create!(name: "North America", description: "USA + Canada", kind: 'country')
 
 %w(US CA).each do |name|
   north_america.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
