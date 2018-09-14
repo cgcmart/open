f2a5ff75f5142eac986ea18288480390622bb613
99cm <zyizyao@china-guide.com>
2018-09-14 11:50:00 -0400

    Create store_credit.rb

---
+++ core/db/default/spree/store_credit.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+Spree::StoreCreditCategory.find_or_create_by!(name: I18n.t('spree.store_credit_category.default'))
+
+Spree::PaymentMethod.create_with(
+  name: "Store Credit",
+  description: "Store credit",
+  active: true,
+  available_to_admin: false,
+  available_to_users: false
+).find_or_create_by!(
+  type: "Spree::PaymentMethod::StoreCredit"
+)
+
+Spree::StoreCreditType.create_with(priority: 1).find_or_create_by!(name: Spree::StoreCreditType::EXPIRING)
+Spree::StoreCreditType.create_with(priority: 2).find_or_create_by!(name: Spree::StoreCreditType::NON_EXPIRING)
+
+Spree::ReimbursementType.create_with(name: "Store Credit").find_or_create_by!(type: 'Spree::ReimbursementType::StoreCredit')
+
+Spree::StoreCreditCategory.find_or_create_by!(name: 'Gift Card')
+
+Spree::StoreCreditUpdateReason.find_or_create_by!(name: 'Credit Given In Error')
