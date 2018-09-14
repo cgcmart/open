25b6b098e9dbb9fe5f3af898f89c737761f04550
99cm <zyizyao@china-guide.com>
2018-09-14 11:44:40 -0400

    Update states.rb

--- core/db/default/spree/states.rb
+++ core/db/default/spree/states.rb
@@ -1,11 +1,14 @@
-Spree::Country.where(states_required: true).each do |country|
-  carmen_country = Carmen::Country.named(country.name)
-  next if !carmen_country
-  carmen_country.subregions.each do |subregion|
-    country.states.where(
-      name: subregion.name,
-      abbr: subregion.code,
-      country_id: country.id
-    ).first_or_create
+# frozen_string_literal: true
+
+ActiveRecord::Base.transaction do
+  Spree::Country.all.each do |country|
+    carmen_country = Carmen::Country.coded(country.iso)
+    next unless carmen_country.subregions?
+
+    carmen_country.subregions.each do |subregion|
+      Spree::State.where(abbr: subregion.code, country: country).first_or_create!(
+        name: subregion.name
+      )
+    end
   end
 end
