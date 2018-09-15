26c0350abd6753f815a8ff52841a6619055efd96
99cm <zyizyao@china-guide.com>
2018-09-14 21:37:59 -0400

    Delete 20180613080857_rename_guest_token_to_token_in_orders.rb

--- core/db/migrate/20180613080857_rename_guest_token_to_token_in_orders.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RenameGuestTokenToTokenInOrders < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :spree_orders, :guest_token, :token
-  end
-end
