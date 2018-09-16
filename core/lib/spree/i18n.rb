92dc1bbf82bbe3a196df73c72c21bb49e9a6dfe2
99cm <zyizyao@china-guide.com>
2018-09-16 13:22:37 -0400

    Update i18n.rb

--- core/lib/spree/i18n.rb
+++ core/lib/spree/i18n.rb
@@ -1,35 +1,34 @@
+# frozen_string_literal: true
+
 require 'i18n'
 require 'active_support/core_ext/array/extract_options'
-require 'spree/i18n/base'
+require 'action_view'
 
 module Spree
-  extend ActionView::Helpers::TranslationHelper
-  extend ActionView::Helpers::TagHelper
+  def self.i18n_available_locales
+    I18n.available_locales.select do |locale|
+      I18n.t('spree.i18n.this_file_language', locale: locale, fallback: false, default: nil)
+    end
+  end
+
+  class TranslationHelperWrapper # :nodoc:
+    include ActionView::Helpers::TranslationHelper
+  end
+
+  # This value is used as a count for the pluralization helpers related to I18n
+  # ex: Spree::Order.model_name.human(count: Spree::I18N_GENERIC_PLURAL)
+  # Related to Solidus issue #1164, this is needed to avoid problems with
+  # some pluralization calculators
+  I18N_GENERIC_PLURAL = 2.1
 
   class << self
     # Add spree namespace and delegate to Rails TranslationHelper for some nice
     # extra functionality. e.g return reasonable strings for missing translations
-    def translate(*args)
-      @virtual_path = virtual_path
-
-      options = args.extract_options!
-      options[:scope] = [*options[:scope]].unshift(:spree)
-      args << options
-      super(*args)
+    def translate(key, options = {})
+      options[:scope] = [:spree, *options[:scope]]
+      TranslationHelperWrapper.new.translate(key, options)
     end
 
-    alias t translate
-
-    def context
-      Spree::ViewContext.context
-    end
-
-    def virtual_path
-      if context
-        path = context.instance_variable_get('@virtual_path')
-
-        path.gsub(/spree/, '') if path
-      end
-    end
+    alias_method :t, :translate    
   end
 end
