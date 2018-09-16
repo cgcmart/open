5e1bd11db134cf4226fbf6fca5c71c8ff51007c7
99cm <zyizyao@china-guide.com>
2018-09-16 16:38:38 -0400

    Create class_constantizer.rb

---
+++ core/lib/spree/core/class_constantizer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Spree
+  module Core
+    module ClassConstantizer
+      class Set
+        include Enumerable
+
+        def initialize
+          @collection = ::Set.new
+        end
+
+        def <<(klass)
+          @collection << klass.to_s
+        end
+
+        def concat(klasses)
+          klasses.each do |klass|
+            self << klass
+          end
+        end
+
+        delegate :clear, :empty?, to: :@collection
+
+        def each
+          @collection.each do |klass|
+            yield klass.constantize
+          end
+        end
+      end
+    end
+  end
+end
