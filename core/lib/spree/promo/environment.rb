34e4b9a4b5813aa3310f89246b3994eb7ad73873
99cm <zyizyao@china-guide.com>
2018-09-16 18:39:17 -0400

    Create environment.rb

---
+++ core/lib/spree/promo/environment.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Spree
+  module Promo
+    class Environment
+      include Core::EnvironmentExtension
+
+      add_class_set :rules
+      add_class_set :actions
+      add_class_set :shipping_actions
+    end
+  end
+end
