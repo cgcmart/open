e1f290928c5bd2d61f893bc58b6617f8323a3d87
99cm <zyizyao@china-guide.com>
2018-09-17 09:57:33 -0400

    Update payment_factory.rb

--- core/lib/spree/testing_support/factories/payment_factory.rb
+++ core/lib/spree/testing_support/factories/payment_factory.rb
@@ -1,22 +1,45 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/payment_method_factory'
+require 'spree/testing_support/factories/credit_card_factory'
+require 'spree/testing_support/factories/order_factory'
+require 'spree/testing_support/factories/store_credit_factory'
+
 FactoryBot.define do
-  factory :payment, class: Spree::Payment do
-    amount 45.75
+  factory :payment, aliases: [:credit_card_payment], class: 'Spree::Payment' do
     association(:payment_method, factory: :credit_card_payment_method)
-    association(:source, factory: :credit_card)
+    source { create(:credit_card, user: order.user, address: order.bill_address) }
     order
-    state 'checkout'
-    response_code '12345'
+    state         { 'checkout' }
+    response_code { '12345' }
+
+    trait :completed do
+      state { 'completed' }
+    end
+
+    trait :failing do
+      response_code { '00000' }
+      association(:source, :failing, { factory: :credit_card })
+    end
 
     factory :payment_with_refund do
-      state 'completed'
-      after :create do |payment|
-        create(:refund, amount: 5, payment: payment)
+      transient do
+        refund_amount { 5 }
       end
+
+      amount { refund_amount }
+
+      state { 'completed' }
+
+      refunds { build_list :refund, 1, amount: refund_amount }
+    end
+
+    initialize_with do
+      order.payments.new(attributes)
     end
   end
 
   factory :check_payment, class: Spree::Payment do
-    amount 45.75
     association(:payment_method, factory: :check_payment_method)
     order
   end
