4607cb1cb3651551c2ff49c5b0303763bc65fe28
99cm <zyizyao@china-guide.com>
2018-09-17 12:52:15 -0400

    Update stock_movement_factory.rb

--- core/lib/spree/testing_support/factories/stock_movement_factory.rb
+++ core/lib/spree/testing_support/factories/stock_movement_factory.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/stock_item_factory'
+
 FactoryBot.define do
   factory :stock_movement, class: Spree::StockMovement do
-    quantity 1
-    action 'sold'
+    quantity { 1 }
+    action   { 'sold' }
     stock_item
   end
 
   trait :received do
-    action 'received'
+    action { 'received' }
   end
 end
