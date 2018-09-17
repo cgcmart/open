ddf5cc24e2fda73e1ea076fac558a6ca121755d6
99cm <zyizyao@china-guide.com>
2018-09-17 11:04:22 -0400

    Update prototype_factory.rb

--- core/lib/spree/testing_support/factories/prototype_factory.rb
+++ core/lib/spree/testing_support/factories/prototype_factory.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'spree/testing_support/factories/property_factory'
 
 FactoryBot.define do
   factory :prototype, class: Spree::Prototype do
-    name 'Baseball Cap'
+    name { 'Baseball Cap' }
     properties { [create(:property)] }
   end
   factory :prototype_with_option_types, class: Spree::Prototype do
-    name 'Baseball Cap'
+    name { 'Baseball Cap' }
     properties { [create(:property)] }
     option_types { [create(:option_type)] }
   end
