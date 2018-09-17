0c5b1989e73d3ea2fdb1c7a334d9ae5b7bfab885
99cm <zyizyao@china-guide.com>
2018-09-17 11:38:57 -0400

    Update shipping_method_factory.rb

--- core/lib/spree/testing_support/factories/shipping_method_factory.rb
+++ core/lib/spree/testing_support/factories/shipping_method_factory.rb
@@ -1,9 +1,32 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/calculator_factory'
+require 'spree/testing_support/factories/shipping_category_factory'
+require 'spree/testing_support/factories/zone_factory'
+
 FactoryBot.define do
-  factory :base_shipping_method, class: Spree::ShippingMethod do
-    zones { |_a| [Spree::Zone.global] }
-    name 'UPS Ground'
-    code 'UPS_GROUND'
-    display_on 'both'
+  factory(
+    :shipping_method,
+    aliases: [
+      :base_shipping_method
+    ],
+    class: 'Spree::ShippingMethod'
+  ) do
+    zones do
+      [Spree::Zone.find_by(name: 'GlobalZone') || FactoryBot.create(:global_zone)]
+    end
+
+    name       { 'UPS Ground' }
+    code       { 'UPS_GROUND' }
+    carrier { 'UPS' }
+    service_level { '1DAYGROUND' }
+
+    calculator { |s| s.association(:shipping_calculator, strategy: :build, preferred_amount: s.cost, preferred_currency: s.currency) }
+
+    transient do
+      cost { 10.0 }
+      currency { Spree::Config[:currency] }
+    end
 
     before(:create) do |shipping_method, _evaluator|
       if shipping_method.shipping_categories.empty?
@@ -11,11 +34,8 @@ FactoryBot.define do
       end
     end
 
-    factory :shipping_method, class: Spree::ShippingMethod do
-      association(:calculator, factory: :shipping_calculator, strategy: :build)
-    end
-
-    factory :free_shipping_method, class: Spree::ShippingMethod do
+    factory :free_shipping_method, class: 'Spree::ShippingMethod' do
+      cost { nil }
       association(:calculator, factory: :shipping_no_amount_calculator, strategy: :build)
     end
   end
