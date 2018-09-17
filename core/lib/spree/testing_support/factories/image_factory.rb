458c211a95b5eb76d0c882ea7f5cde11f4e24ef3
99cm <zyizyao@china-guide.com>
2018-09-16 21:22:10 -0400

    Update image_factory.rb

--- core/lib/spree/testing_support/factories/image_factory.rb
+++ core/lib/spree/testing_support/factories/image_factory.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :image, class: Spree::Image do
-    attachment { File.new(Spree::Core::Engine.root + 'spec/fixtures' + 'thinking-cat.jpg') }
+    attachment { File.new(Spree::Core::Engine.root + 'spec/fixtures/thinking-cat.jpg') }
   end
 end
