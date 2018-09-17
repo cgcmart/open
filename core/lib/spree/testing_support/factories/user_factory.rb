51a4ddb1f0f664b443c61eb903d07cf0f028dbc0
99cm <zyizyao@china-guide.com>
2018-09-17 19:46:29 -0400

    Update user_factory.rb

--- core/lib/spree/testing_support/factories/user_factory.rb
+++ core/lib/spree/testing_support/factories/user_factory.rb
@@ -1,20 +1,26 @@
-FactoryBot.define do
-  sequence :user_authentication_token do |n|
-    "xxxx#{Time.current.to_i}#{rand(1000)}#{n}xxxxxxxxxxxxx"
-  end
+# frozen_string_literal: true
 
-  factory :user, class: Spree.user_class do
-    email { generate(:random_email) }
-    login { email }
-    password 'secret'
+require 'spree/testing_support/sequences'
+require 'spree/testing_support/factories/role_factory'
+require 'spree/testing_support/factories/address_factory'
+
+FactoryBot.define do
+  factory :user, class: Spree::UserClassHandle.new do
+    email                 { generate(:email) }
+    password              { 'secret' }
     password_confirmation { password }
-    authentication_token { generate(:user_authentication_token) } if Spree.user_class.attribute_method? :authentication_token
+
+    trait :with_api_key do
+      after(:create) do |user, _|
+        user.generate_spree_api_key!
+      end
+    end
 
     factory :admin_user do
       spree_roles { [Spree::Role.find_by(name: 'admin') || create(:role, name: 'admin')] }
     end
 
-    factory :user_with_addresses, aliases: [:user_with_addreses] do
+    factory :user_with_addresses do
       ship_address
       bill_address
     end
