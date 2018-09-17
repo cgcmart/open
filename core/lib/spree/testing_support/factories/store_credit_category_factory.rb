eecd2d0efd6f31bc146dd815c109ed4b060f2c0d
99cm <zyizyao@china-guide.com>
2018-09-17 19:18:30 -0400

    Update store_credit_category_factory.rb

--- core/lib/spree/testing_support/factories/store_credit_category_factory.rb
+++ core/lib/spree/testing_support/factories/store_credit_category_factory.rb
@@ -1,9 +1,7 @@
-FactoryBot.define do
-  factory :store_credit_category, class: Spree::StoreCreditCategory do
-    name 'Exchange'
-  end
+# frozen_string_literal: true
 
-  factory :store_credit_gift_card_category, class: Spree::StoreCreditCategory do
-    name Spree::StoreCreditCategory::GIFT_CARD_CATEGORY_NAME
+FactoryBot.define do
+  factory :store_credit_category, class: 'Spree::StoreCreditCategory' do
+    name { "Exchange" }
   end
 end
