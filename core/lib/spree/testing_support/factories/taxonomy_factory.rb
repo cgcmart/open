1ef27e0f06e0492e8265f879faca3e906759cda2
99cm <zyizyao@china-guide.com>
2018-09-17 19:39:59 -0400

    Update taxonomy_factory.rb

--- core/lib/spree/testing_support/factories/taxonomy_factory.rb
+++ core/lib/spree/testing_support/factories/taxonomy_factory.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory :taxonomy, class: Spree::Taxonomy do
-    sequence(:name) { |n| "taxonomy_#{n}" }
+  factory :taxonomy, class: 'Spree::Taxonomy' do
+    name { 'Brand' }
   end
 end
