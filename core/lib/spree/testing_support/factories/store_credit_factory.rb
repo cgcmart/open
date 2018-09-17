bd9b9a227ba19b3a8156b7741476f53e18140cf9
99cm <zyizyao@china-guide.com>
2018-09-17 19:24:51 -0400

    Update store_credit_factory.rb

--- core/lib/spree/testing_support/factories/store_credit_factory.rb
+++ core/lib/spree/testing_support/factories/store_credit_factory.rb
@@ -1,17 +1,16 @@
-FactoryBot.define do
-  sequence(:store_credits_order_number) { |n| "R1000#{n}" }
+# frozen_string_literal: true
 
-  factory :store_credit, class: Spree::StoreCredit do
+require 'spree/testing_support/factories/store_credit_category_factory'
+require 'spree/testing_support/factories/store_credit_type_factory'
+require 'spree/testing_support/factories/user_factory'
+
+FactoryBot.define do
+  factory :store_credit, class: 'Spree::StoreCredit' do
     user
-    created_by { create(:user) }
-    category { create(:store_credit_category) }
+    association :created_by, factory: :user
+    association :category, factory: :store_credit_category
     amount { 150.00 }
     currency { 'USD' }
-    credit_type { create(:primary_credit_type) }
-  end
-
-  factory :store_credits_order_without_user, class: Spree::Order do
-    number { generate(:store_credits_order_number) }
-    bill_address
+    association :credit_type, factory: :primary_credit_type
   end
 end
