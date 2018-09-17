46e9c0f6d023c5a04d31a08f47ba4e5c4c6c0e83
99cm <zyizyao@china-guide.com>
2018-09-16 21:40:16 -0400

    Update inventory_unit_factory.rb

--- core/lib/spree/testing_support/factories/inventory_unit_factory.rb
+++ core/lib/spree/testing_support/factories/inventory_unit_factory.rb
@@ -1,17 +1,33 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/line_item_factory'
+require 'spree/testing_support/factories/variant_factory'
+require 'spree/testing_support/factories/order_factory'
+require 'spree/testing_support/factories/shipment_factory'
+
 FactoryBot.define do
   factory :inventory_unit, class: Spree::InventoryUnit do
+    transient do
+      order { nil }
+    end
+
     variant
-    order
-    line_item
-    state 'on_hand'
-    association(:shipment, factory: :shipment, state: 'pending')
+    line_itemdo
+      if order
+        build(:line_item, variant: variant, order: order)
+      else
+        build(:line_item, variant: variant)
+      end
+    end
+    state { 'on_hand' }
+    shipment { build(:shipment, state: 'pending', order: line_item.order) }
     # return_authorization
 
     # this trait usage increases build speed ~ 2x
     trait :without_assoc do
-      shipment nil
-      order nil
-      line_item nil
+      shipment  { nil }
+      order     { nil }
+      line_item { nil }
     end
   end
 end
