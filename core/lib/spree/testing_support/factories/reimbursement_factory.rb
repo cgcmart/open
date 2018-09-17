77c7fafda3f2da1320cd2719ee4d0a6d15d47aa6
99cm <zyizyao@china-guide.com>
2018-09-17 11:16:48 -0400

    Update reimbursement_factory.rb

--- core/lib/spree/testing_support/factories/reimbursement_factory.rb
+++ core/lib/spree/testing_support/factories/reimbursement_factory.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/customer_return_factory'
+
 FactoryBot.define do
   factory :reimbursement, class: Spree::Reimbursement do
     transient do
-      return_items_count 1
+      return_items_count { 1 }
     end
 
     customer_return { create(:customer_return_with_accepted_items, line_items_count: return_items_count) }
@@ -11,6 +15,7 @@ FactoryBot.define do
       if reimbursement.return_items.empty?
         reimbursement.return_items = reimbursement.customer_return.return_items
       end
+      reimbursement.total = reimbursement.return_items.map { |ri| ri.amount }.sum
     end
   end
 end
