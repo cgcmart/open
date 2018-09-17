cbcf8bedf343b73ef112b9e38b6f8af2ebfa767b
99cm <zyizyao@china-guide.com>
2018-09-17 19:32:50 -0400

    Update store_factory.rb

--- core/lib/spree/testing_support/factories/store_factory.rb
+++ core/lib/spree/testing_support/factories/store_factory.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     sequence(:code) { |i| "spree_#{i}" }
     sequence(:name) { |i| "Spree Test Store #{i}" }
     sequence(:url) { |i| "www.example#{i}.com" }
-    mail_from_address 'spree@example.org'
-    default_currency 'USD'
+    mail_from_address { 'spree@example.org' }
+    default_currency { 'USD' }
   end
 end
