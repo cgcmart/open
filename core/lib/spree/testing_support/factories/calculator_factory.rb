6d3d7dd892820f4d42a8893643636a123ded09de
99cm <zyizyao@china-guide.com>
2018-09-16 21:00:43 -0400

    Update calculator_factory.rb

--- core/lib/spree/testing_support/factories/calculator_factory.rb
+++ core/lib/spree/testing_support/factories/calculator_factory.rb
@@ -1,20 +1,26 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory :calculator, class: Spree::Calculator::FlatRate do
-    after(:create) { |c| c.set_preference(:amount, 10.0) }
+  factory :calculator, aliases: [:flat_rate_calculator], class: 'Spree::Calculator::FlatRate' do
+    preferred_amount { 10.0 }
   end
 
   factory :no_amount_calculator, class: Spree::Calculator::FlatRate do
-    after(:create) { |c| c.set_preference(:amount, 0) }
+    preferred_amount { 0 }
   end
 
   factory :default_tax_calculator, class: Spree::Calculator::DefaultTax do
   end
 
   factory :shipping_calculator, class: Spree::Calculator::Shipping::FlatRate do
-    after(:create) { |c| c.set_preference(:amount, 10.0) }
+    preferred_amount { 10.0 }
   end
 
-  factory :shipping_no_amount_calculator, class: Spree::Calculator::Shipping::FlatRate do
-    after(:create) { |c| c.set_preference(:amount, 0) }
+  factory :shipping_no_amount_calculator, class: 'Spree::Calculator::Shipping::FlatRate' do
+    preferred_amount { 0 }
+  end
+
+  factory :percent_on_item_calculator, class: 'Spree::Calculator::PercentOnLineItem' do
+    preferred_percent { 10 }
   end
 end
