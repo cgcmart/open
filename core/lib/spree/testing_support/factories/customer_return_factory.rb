267020830b148ef4898bb937a8f4ba9e32cca218
99cm <zyizyao@china-guide.com>
2018-09-16 21:18:06 -0400

    Update customer_return_factory.rb

--- core/lib/spree/testing_support/factories/customer_return_factory.rb
+++ core/lib/spree/testing_support/factories/customer_return_factory.rb
@@ -1,17 +1,23 @@
+# frozen_string_literal: true
+
+require 'spree/testing_support/factories/stock_location_factory'
+require 'spree/testing_support/factories/order_factory'
+require 'spree/testing_support/factories/return_item_factory'
+
 FactoryBot.define do
   factory :customer_return, class: Spree::CustomerReturn do
     association(:stock_location, factory: :stock_location)
 
     transient do
-      line_items_count 1
+      line_items_count   { 1 }
       return_items_count { line_items_count }
+      shipped_order { create :shipped_order, line_items_count: line_items_count }
+      return_authorization { create :return_authorization, order: shipped_order }
     end
 
     before(:create) do |customer_return, evaluator|
-      shipped_order = create(:shipped_order, line_items_count: evaluator.line_items_count)
-
-      shipped_order.inventory_units.take(evaluator.return_items_count).each do |inventory_unit|
-        customer_return.return_items << build(:return_item, inventory_unit: inventory_unit)
+      evaluator.shipped_order.inventory_units.take(evaluator.return_items_count).each do |inventory_unit|
+        customer_return.return_items << build(:return_item, inventory_unit: inventory_unit, return_authorization: evaluator.return_authorization)
       end
     end
 
