0f545042930a127b3205d0aa2c6e76807e021ed3
99cm <zyizyao@china-guide.com>
2018-09-18 10:12:11 -0400

    Create text.rb

---
+++ core/lib/spree/core/query_filters/text.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Spree
+  module Core
+    module QueryFilters
+      class Text
+        def initialize(attribute:)
+          @attribute = attribute
+        end
+
+        def call(scope:, filter:)
+          scope = eq(scope, filter[:eq])
+          contains(scope, filter[:contains])
+        end
+
+        private
+
+        attr_reader :attribute
+
+        def eq(scope, value)
+          return scope unless value
+
+          scope.where(attribute.eq(value))
+        end
+
+        def contains(scope, value)
+          return scope unless value
+
+          scope.where(attribute.matches("%#{value}%"))
+        end
+      end
+    end
+  end
+end
