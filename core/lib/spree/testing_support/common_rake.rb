2caeaf119570293d7516bb37b10aa3eeb3885292
99cm <zyizyao@china-guide.com>
2018-09-17 20:44:41 -0400

    Update common_rake.rb

--- core/lib/spree/testing_support/common_rake.rb
+++ core/lib/spree/testing_support/common_rake.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 unless defined?(Spree::InstallGenerator)
   require 'generators/spree/install/install_generator'
 end
@@ -5,37 +7,33 @@ end
 require 'generators/spree/dummy/dummy_generator'
 require 'generators/spree/dummy_model/dummy_model_generator'
 
-desc 'Generates a dummy app for testing'
 namespace :common do
   task :test_app, :user_class do |_t, args|
     args.with_defaults(user_class: 'Spree::LegacyUser')
-    require ENV['LIB_NAME'].to_s
+    require ENV['LIB_NAME']
 
     ENV['RAILS_ENV'] = 'test'
-    Rails.env = 'test'
 
     Spree::DummyGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", '--quiet']
     Spree::InstallGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", '--auto-accept', '--migrate=false', '--seed=false', '--sample=false', '--quiet', '--copy_views=false', "--user_class=#{args[:user_class]}"]
 
     puts 'Setting up dummy database...'
-    system("bundle exec rake db:drop db:create > #{File::NULL}")
-    Spree::DummyModelGenerator.start
-    system("bundle exec rake db:migrate > #{File::NULL}")
+
+    sh "bin/rails db:environment:set RAILS_ENV=test"
+    sh "bin/rails db:drop db:create db:migrate VERBOSE=false RAILS_ENV=test"
 
     begin
       require "generators/#{ENV['LIB_NAME']}/install/install_generator"
       puts 'Running extension installation generator...'
       "#{ENV['LIB_NAME'].camelize}::Generators::InstallGenerator".constantize.start(['--auto-run-migrations'])
     rescue LoadError
-      puts 'Skipping installation no generator to run...'
+      # No extension generator to run
     end
-
-    puts 'Precompiling assets...'
-    system("bundle exec rake assets:precompile > #{File::NULL}")
   end
 
   task :seed do |_t|
     puts 'Seeding ...'
-    system("bundle exec rake db:seed RAILS_ENV=test > #{File::NULL}")
+
+    sh "bundle exec rake db:seed RAILS_ENV=test"
   end
 end
