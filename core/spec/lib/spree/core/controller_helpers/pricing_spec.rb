67cd374277a28fec38a9a2bfb5d13aa676af81c2
99cm <zyizyao@china-guide.com>
2018-09-20 11:09:09 -0400

    Create pricing_spec.rb

---
+++ core/spec/lib/spree/core/controller_helpers/pricing_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+class FakesController < ApplicationController
+  include Spree::Core::ControllerHelpers::Pricing
+end
+
+RSpec.describe Spree::Core::ControllerHelpers::Pricing, type: :controller do
+  controller(FakesController) {}
+
+  before do
+    allow(controller).to receive(:current_store).and_return(store)
+  end
+
+  describe '#current_currency' do
+    subject { controller.current_currency }
+
+    context "when store default_currency is nil" do
+      let(:store) { nil }
+      it { Spree::Deprecation.silence { is_expected.to eq('USD') } }
+    end
+
+    context "when the current store default_currency empty" do
+      let(:store) { FactoryBot.create :store, default_currency: '' }
+
+      it { Spree::Deprecation.silence { is_expected.to eq('USD') } }
+    end
+
+    context "when the current store default_currency is a currency" do
+      let(:store) { FactoryBot.create :store, default_currency: 'EUR' }
+
+      it { Spree::Deprecation.silence { is_expected.to eq('EUR') } }
+    end
+  end
+
+  describe '#current_pricing_options' do
+    subject { controller.current_pricing_options }
+
+    let(:store) { FactoryBot.create(:store, default_currency: nil) }
+
+    it { is_expected.to be_a(Spree::Config.pricing_options_class) }
+
+    context "currency" do
+      subject { controller.current_pricing_options.currency }
+
+      context "when store default_currency is nil" do
+        let(:store) { nil }
+        it { is_expected.to eq('USD') }
+      end
+
+      context "when the current store default_currency empty" do
+        let(:store) { FactoryBot.create :store, default_currency: '' }
+
+        it { is_expected.to eq('USD') }
+      end
+
+      context "when the current store default_currency is a currency" do
+        let(:store) { FactoryBot.create :store, default_currency: 'EUR' }
+
+        it { is_expected.to eq('EUR') }
+      end
+    end
+
+    context "country_iso" do
+      subject { controller.current_pricing_options.country_iso }
+
+      let(:store) { FactoryBot.create(:store, cart_tax_country_iso: cart_tax_country_iso) }
+
+      context "when the store has a cart tax country set" do
+        let(:cart_tax_country_iso) { "DE" }
+        it { is_expected.to eq("DE") }
+      end
+
+      context "when the store has no cart tax country set" do
+        let(:cart_tax_country_iso) { nil }
+        it { is_expected.to be_nil }
+      end
+    end
+  end
+end
