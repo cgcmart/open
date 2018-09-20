df882ebaaf4c8101ca28ebc9c3861a23808b7706
99cm <zyizyao@china-guide.com>
2018-09-20 11:28:22 -0400

    Create preferences_spec.rb

---
+++ core/spec/lib/spree/core/testing_support/preferences_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Spree::TestingSupport::Preferences do
+  describe 'resetting the app configuration' do
+    before do
+      @original_spree_mails_from = Spree::Config.mails_from
+      @original_spree_searcher_class = Spree::Config.searcher_class
+      class MySearcherClass; end
+      include Spree::TestingSupport::Preferences
+      Spree::Config.mails_from = "hello@myserver.com"
+      Spree::Config.searcher_class = MySearcherClass
+    end
+
+    it 'resets normal preferences' do
+      expect(Spree::Config.mails_from).to eq("hello@myserver.com")
+      reset_spree_preferences
+      expect(Spree::Config.mails_from).to eq(@original_spree_mails_from)
+    end
+
+    it 'resets cached configuration instance variables' do
+      expect(Spree::Config.searcher_class).to eq(MySearcherClass)
+      reset_spree_preferences
+      expect(Spree::Config.searcher_class).to eq(@original_spree_searcher_class)
+    end
+  end
+end
