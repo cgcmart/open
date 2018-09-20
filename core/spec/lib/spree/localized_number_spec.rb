159d9d8a27deb27bf855aaf28b807ab41bde2037
99cm <zyizyao@china-guide.com>
2018-09-20 14:40:13 -0400

    Update localized_number_spec.rb

--- core/spec/lib/spree/localized_number_spec.rb
+++ core/spec/lib/spree/localized_number_spec.rb
@@ -1,16 +1,25 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::LocalizedNumber do
+require 'spec_helper'
+require 'spree/localized_number'
+
+RSpec.describe Spree::LocalizedNumber do
   context '.parse' do
     before do
       I18n.enforce_available_locales = false
       I18n.locale = I18n.default_locale
-      I18n.backend.store_translations(:de, number: { currency: { format: { delimiter: '.', separator: ',' } } })
+      I18n.backend.store_translations(:de, { number: { currency: { format: { delimiter: '.', separator: ',' } } } })
     end
 
     after do
       I18n.locale = I18n.default_locale
       I18n.enforce_available_locales = true
+    end
+
+    context 'with an empty string' do
+      it 'parses to 0' do
+        expect(subject.class.parse('')).to be 0.0
+      end
     end
 
     context 'with decimal point' do
@@ -30,22 +39,6 @@ describe Spree::LocalizedNumber do
       it 'uses the price as is' do
         I18n.locale = :de
         expect(subject.class.parse(1599.99)).to eq 1599.99
-      end
-    end
-
-    context 'string argument' do
-      it 'is not modified' do
-        I18n.locale = :de
-        number = '1.599,99'
-        number_bak = number.dup
-        subject.class.parse(number)
-        expect(number).to eql(number_bak)
-      end
-    end
-
-    context 'with empty string' do
-      it 'returns 0' do
-        expect(subject.class.parse('')).to be 0
       end
     end
   end
