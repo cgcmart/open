ada57b95fdf3b52d74127f72598bb8b739b2ad3e
99cm <zyizyao@china-guide.com>
2018-09-20 14:48:12 -0400

    Update migrations_spec.rb

--- core/spec/lib/spree/migrations_spec.rb
+++ core/spec/lib/spree/migrations_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 module Spree
-  describe Migrations do
-    subject { described_class.new(config, 'spree') }
-
+  RSpec.describe Migrations do
     let(:app_migrations) { ['.', '34_add_title.rb', '52_add_text.rb'] }
     let(:engine_migrations) { ['.', '334_create_orders.spree.rb', '777_create_products.spree.rb'] }
 
@@ -12,24 +12,25 @@ module Spree
     let(:engine_dir) { 'dir/db/migrate' }
     let(:app_dir) { "#{Rails.root}/db/migrate" }
 
+    
+    subject { described_class.new(config, 'spree') }
+
     before do
       expect(File).to receive(:directory?).with(app_dir).and_return true
     end
 
-    it 'warns about missing migrations' do
+    it 'detects missing migrations' do
       expect(Dir).to receive(:entries).with(app_dir).and_return app_migrations
       expect(Dir).to receive(:entries).with(engine_dir).and_return engine_migrations
 
-      silence_stream(STDOUT) do
-        expect(subject.check).to eq true
-      end
+      expect(subject.missing_migrations.size).to eq 2
     end
 
     context 'no missing migrations' do
       it 'says nothing' do
         expect(Dir).to receive(:entries).with(engine_dir).and_return engine_migrations
         expect(Dir).to receive(:entries).with(app_dir).and_return(app_migrations + engine_migrations)
-        expect(subject.check).to eq nil
+        expect(subject.missing_migrations.size).to eq 0
       end
     end
   end
