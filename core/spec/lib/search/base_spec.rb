44e7e9ac7b4a914c126d1db69ce37b631fcf36a3
99cm <zyizyao@china-guide.com>
2018-09-20 10:49:32 -0400

    Update base_spec.rb

--- core/spec/lib/search/base_spec.rb
+++ core/spec/lib/search/base_spec.rb
@@ -1,6 +1,9 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Core::Search::Base do
+require 'rails_helper'
+require 'spree/core/product_filters'
+
+RSpec.describe Spree::Core::Search::Base do
   before do
     include Spree::Core::ProductFilters
     @taxon = create(:taxon, name: 'Ruby on Rails')
@@ -12,18 +15,17 @@ describe Spree::Core::Search::Base do
 
   it 'returns all products by default' do
     params = { per_page: '' }
-    searcher = described_class.new(params)
+    searcher = Spree::Core::Search::Base.new(params)
     expect(searcher.retrieve_products.count).to eq(2)
   end
 
   context 'when include_images is included in the initalization params' do
+    let(:params) { { include_images: true, keyword: @product1.name, taxon: @taxon.id } }
     subject { described_class.new(params).retrieve_products }
 
-    let(:params) { { include_images: true, keyword: @product1.name, taxon: @taxon.id } }
-
     before do
-      @product1.master.images << create(:image, position: 2)
-      @product1.master.images << create(:image, position: 1)
+      @product1.master.images.create(attachment_file_name: 'Test', position: 2)
+      @product1.master.images.create(attachment_file_name: 'Test1', position: 1)
       @product1.reload
     end
 
@@ -37,24 +39,24 @@ describe Spree::Core::Search::Base do
     @product3 = create(:product, name: 'RoR Pants', price: 14.00)
 
     params = { per_page: '2' }
-    searcher = described_class.new(ActionController::Parameters.new(params))
+    searcher = Spree::Core::Search::Base.new(params)
     expect(searcher.retrieve_products.count).to eq(2)
 
     params[:page] = '2'
-    searcher = described_class.new(ActionController::Parameters.new(params))
+    searcher = Spree::Core::Search::Base.new(params)
     expect(searcher.retrieve_products.count).to eq(1)
   end
 
   it 'maps search params to named scopes' do
     params = { per_page: '', search: { 'price_range_any' => ['Under $10.00'] } }
-    searcher = described_class.new(ActionController::Parameters.new(params))
+    searcher = Spree::Core::Search::Base.new(params)
     expect(searcher.send(:get_base_scope).to_sql).to match(/<= 10/)
     expect(searcher.retrieve_products.count).to eq(1)
   end
 
   it 'maps multiple price_range_any filters' do
     params = { per_page: '', search: { 'price_range_any' => ['Under $10.00', '$10.00 - $15.00'] } }
-    searcher = described_class.new(ActionController::Parameters.new(params))
+    searcher = Spree::Core::Search::Base.new(params)
     expect(searcher.send(:get_base_scope).to_sql).to match(/<= 10/)
     expect(searcher.send(:get_base_scope).to_sql).to match(/between 10 and 15/i)
     expect(searcher.retrieve_products.count).to eq(2)
@@ -62,21 +64,21 @@ describe Spree::Core::Search::Base do
 
   it 'uses ransack if scope not found' do
     params = { per_page: '', search: { 'name_not_cont' => 'Shirt' } }
-    searcher = described_class.new(ActionController::Parameters.new(params))
+    searcher = Spree::Core::Search::Base.new(params)
     expect(searcher.retrieve_products.count).to eq(1)
   end
 
   it 'accepts a current user' do
     user = double
-    searcher = described_class.new({})
+    searcher = Spree::Core::Search::Base({})
     searcher.current_user = user
     expect(searcher.current_user).to eql(user)
   end
 
   it 'finds products in alternate currencies' do
     create(:price, currency: 'EUR', variant: @product1.master)
-    searcher = described_class.new({})
-    searcher.current_currency = 'EUR'
+    searcher = Spree::Core::Search::Base.new({})
+    searcher.pricing_options = Spree::Config.pricing_options_class.new(currency: 'EUR')
     expect(searcher.retrieve_products).to eq([@product1])
   end
 end
