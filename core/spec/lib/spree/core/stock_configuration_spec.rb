940fc9562052e0429e838d8d02263e5661167e18
99cm <zyizyao@china-guide.com>
2018-09-20 14:08:10 -0400

    Create stock_configuration_spec.rb

---
+++ core/spec/lib/spree/core/stock_configuration_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Spree::Core::StockConfiguration do
+  describe '#coordinator_class' do
+    let(:stock_configuration) { described_class.new }
+    subject { stock_configuration.coordinator_class }
+
+    it "returns Spree::Stock::Coordinator" do
+      is_expected.to be ::Spree::Stock::SimpleCoordinator
+    end
+
+    context "with another constant name assiged" do
+      MyCoordinator = Class.new
+      before { stock_configuration.coordinator_class = MyCoordinator.to_s }
+
+      it "returns the constant" do
+        is_expected.to be MyCoordinator
+      end
+    end
+  end
+  describe '#estimator_class' do
+    let(:stock_configuration) { described_class.new }
+    subject { stock_configuration.estimator_class }
+
+    it "returns Spree::Stock::Estimator" do
+      is_expected.to be ::Spree::Stock::Estimator
+    end
+
+    context "with another constant name assiged" do
+      MyEstimator = Class.new
+      before { stock_configuration.estimator_class = MyEstimator.to_s }
+
+      it "returns the constant" do
+        is_expected.to be MyEstimator
+      end
+    end
+  end
+end
