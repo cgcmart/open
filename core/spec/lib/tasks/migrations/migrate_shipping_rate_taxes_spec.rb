08cf0168a6918ec3a2ae429d707b2b79c0b3f825
99cm <zyizyao@china-guide.com>
2018-09-20 22:07:18 -0400

    Create migrate_shipping_rate_taxes_spec.rb

---
+++ core/spec/lib/tasks/migrations/migrate_shipping_rate_taxes_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'solidus:migrations:migrate_shipping_rate_taxes' do
+  describe 'up' do
+    include_context(
+      'rake',
+      task_path: Spree::Core::Engine.root.join('lib/tasks/migrations/migrate_shipping_rate_taxes.rake'),
+      task_name: 'solidus:migrations:migrate_shipping_rate_taxes:up',
+    )
+
+    it 'runs' do
+      expect { task.invoke }.to output(
+        "Adding persisted tax notes to historic shipping rates ... Success.\n"
+      ).to_stdout
+    end
+  end
+end
