207b107fff5b83f461b6aed899d54926131b1ca8
99cm <zyizyao@china-guide.com>
2018-09-21 11:16:12 -0400

    Update per_item_spec.rb

--- core/spec/models/spree/calculator/shipping/per_item_spec.rb
+++ core/spec/models/spree/calculator/shipping/per_item_spec.rb
@@ -1,16 +1,21 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'shared_examples/calculator_shared_examples'
 
 module Spree
   module Calculator::Shipping
-    describe PerItem, type: :model do
-      subject { PerItem.new(preferred_amount: 10) }
-
+    RSpec.describe PerItem, type: :model do
       let(:variant1) { build(:variant) }
       let(:variant2) { build(:variant) }
+
+      it_behaves_like 'a calculator with a description'
 
       let(:package) do
         build(:stock_package, variants_contents: { variant1 => 5, variant2 => 3 })
       end
+
+      subject { PerItem.new(preferred_amount: 10) }
 
       it 'correctly calculates per item shipping' do
         expect(subject.compute(package).to_f).to eq(80) # 5 x 10 + 3 x 10
