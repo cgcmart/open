174e5a653b4b2afb700944f2877694a094b0dd80
99cm <zyizyao@china-guide.com>
2018-09-21 19:43:12 -0400

    Update address_spec.rb

--- core/spec/models/spree/order/address_spec.rb
+++ core/spec/models/spree/order/address_spec.rb
@@ -1,6 +1,8 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Order, type: :model do
+require 'rails_helper'
+
+RSpec.describe Spree::Order, type: :model do
   let(:order) { Spree::Order.new }
 
   context 'validation' do
