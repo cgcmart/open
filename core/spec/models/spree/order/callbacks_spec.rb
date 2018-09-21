68a6f98c6acd7c24f5f0faeb678805a16f6f52e6
99cm <zyizyao@china-guide.com>
2018-09-21 19:54:22 -0400

    Update callbacks_spec.rb

--- core/spec/models/spree/order/callbacks_spec.rb
+++ core/spec/models/spree/order/callbacks_spec.rb
@@ -1,6 +1,8 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Order, type: :model do
+require 'rails_helper'
+
+RSpec.describe Spree::Order, type: :model do
   let(:order) { stub_model(Spree::Order) }
 
   before do
@@ -9,7 +11,7 @@ describe Spree::Order, type: :model do
 
   context 'validations' do
     context 'email validation' do
-      # Regression test for #1238
+      # Regression test for https://github.com/spree/spree/issues/1238
       it "o'brien@gmail.com is a valid email address" do
         order.state = 'address'
         order.email = "o'brien@gmail.com"
@@ -26,7 +28,7 @@ describe Spree::Order, type: :model do
         allow(order).to receive_messages user: user
       end
 
-      it 'assigns the email address of the user' do
+      it 'should assign the email address of the user' do
         order.run_callbacks(:create)
         expect(order.email).to eq(user.email)
       end
@@ -34,7 +36,7 @@ describe Spree::Order, type: :model do
   end
 
   context 'in the cart state' do
-    it 'does not validate email address' do
+    it 'should not validate email address' do
       order.state = 'cart'
       order.email = nil
       expect(order.error_on(:email).size).to eq(0)
