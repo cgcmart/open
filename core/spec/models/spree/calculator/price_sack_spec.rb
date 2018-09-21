d83bbc91aaab415fee6f437965d4e0ce324e0198
99cm <zyizyao@china-guide.com>
2018-09-21 13:55:20 -0400

    Update price_sack_spec.rb

--- core/spec/models/spree/calculator/price_sack_spec.rb
+++ core/spec/models/spree/calculator/price_sack_spec.rb
@@ -1,6 +1,9 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Calculator::PriceSack, type: :model do
+require 'rails_helper'
+require 'shared_examples/calculator_shared_examples'
+
+RSpec.describe Spree::Calculator::PriceSack, type: :model do
   let(:calculator) do
     calculator = Spree::Calculator::PriceSack.new
     calculator.preferred_minimal_amount = 5
@@ -9,20 +12,22 @@ describe Spree::Calculator::PriceSack, type: :model do
     calculator
   end
 
+  it_behaves_like 'a calculator with a description'
+
   let(:order) { stub_model(Spree::Order) }
   let(:shipment) { stub_model(Spree::Shipment, amount: 10) }
 
-  # Regression test for #714 and #739
+  # Regression test for https://github.com/spree/spree/issues/714 and https://github.com/spree/spree/issues/739
   it 'computes with an order object' do
     calculator.compute(order)
   end
 
-  # Regression test for #1156
+  # Regression test for https://github.com/spree/spree/issues/1156
   it 'computes with a shipment object' do
     calculator.compute(shipment)
   end
 
-  # Regression test for #2055
+  # Regression test for https://github.com/spree/spree/issues/2055
   it 'computes the correct amount' do
     expect(calculator.compute(2)).to eq(calculator.preferred_normal_amount)
     expect(calculator.compute(6)).to eq(calculator.preferred_discount_amount)
