5256174ebb91454faf18dd3363ccb9844f537681
99cm <zyizyao@china-guide.com>
2018-09-21 10:36:50 -0400

    Update shipment_mailer_spec.rb

--- core/spec/mailers/shipment_mailer_spec.rb
+++ core/spec/mailers/shipment_mailer_spec.rb
@@ -1,12 +1,8 @@
-require 'spec_helper'
-require 'email_spec'
+# frozen_string_literal: true
 
-describe Spree::ShipmentMailer, type: :mailer do
-  include EmailSpec::Helpers
-  include EmailSpec::Matchers
+require 'rails_helper'
 
-  before { create(:store) }
-
+RSpec.describe Spree::ShipmentMailer, type: :mailer do
   let(:order) { stub_model(Spree::Order, number: 'R12345') }
   let(:shipping_method) { stub_model(Spree::ShippingMethod, name: 'USPS') }
   let(:product) { stub_model(Spree::Product, name: %{The "BEST" product}, sku: 'SKU0001') }
@@ -20,31 +16,25 @@ describe Spree::ShipmentMailer, type: :mailer do
     shipment
   end
 
-  context ':from not set explicitly' do
-    it 'falls back to spree config' do
-      message = Spree::ShipmentMailer.shipped_email(shipment)
-      expect(message.from).to eq([Spree::Store.current.mail_from_address])
-    end
-  end
-
-  # Regression test for #2196
+  # Regression test for https://github.com/spree/spree/issues/2196
   it "doesn't include out of stock in the email body" do
     shipment_email = Spree::ShipmentMailer.shipped_email(shipment)
-    expect(shipment_email.body).not_to include(%q{Out of Stock})
+    expect(shipment_email.parts.first.body).not_to include(%q{Out of Stock})
+    expect(shipment_email.parts.first.body).to include(%{Your order has been shipped})
+    expect(shipment_email.subject).to eq "#{order.store.name} Shipment Notification ##{order.number}"
   end
 
-  it 'shipment_email accepts an shipment id as an alternative to an Shipment object' do
-    expect(Spree::Shipment).to receive(:find).with(shipment.id).and_return(shipment)
-    expect do
-      Spree::ShipmentMailer.shipped_email(shipment.id).body
-    end.not_to raise_error
+  context 'with resend option' do
+    subject do
+      Spree::ShipmentMailer.shipped_email(shipment, resend: true).subject
+    end
+    it { is_expected.to match /^\[RESEND\] / }
   end
 
   context 'emails must be translatable' do
     context 'shipped_email' do
       context 'pt-BR locale' do
         before do
-          I18n.enforce_available_locales = false
           pt_br_shipped_email = { spree: { shipment_mailer: { shipped_email: { dear_customer: 'Caro Cliente,' } } } }
           I18n.backend.store_translations :'pt-BR', pt_br_shipped_email
           I18n.locale = :'pt-BR'
@@ -52,7 +42,6 @@ describe Spree::ShipmentMailer, type: :mailer do
 
         after do
           I18n.locale = I18n.default_locale
-          I18n.enforce_available_locales = true
         end
 
         specify do
@@ -60,22 +49,6 @@ describe Spree::ShipmentMailer, type: :mailer do
           expect(shipped_email).to have_body_text('Caro Cliente,')
         end
       end
-    end
-  end
-
-  context 'shipped_email' do
-    let(:shipped_email) { Spree::ShipmentMailer.shipped_email(shipment) }
-
-    specify do
-      expect(shipped_email).to have_body_text(order.number)
-    end
-
-    specify do
-      expect(shipped_email).to have_body_text(shipping_method.name)
-    end
-
-    specify do
-      expect(shipped_email).to have_body_text("href=\"#{shipment.tracking_url}\"")
     end
   end
 end
