6ae9191f353619477a157b30188202ba56631813
99cm <zyizyao@china-guide.com>
2018-09-21 12:56:28 -0400

    Update flat_rate_spec.rb

--- core/spec/models/spree/calculator/flat_rate_spec.rb
+++ core/spec/models/spree/calculator/flat_rate_spec.rb
@@ -1,7 +1,12 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Calculator::FlatRate, type: :model do
+require 'rails_helper'
+require 'shared_examples/calculator_shared_examples'
+
+RSpec.describe Spree::Calculator::FlatRate, type: :model do
   let(:calculator) { Spree::Calculator::FlatRate.new }
+
+  it_behaves_like 'a calculator with a description'
 
   let(:order) do
     mock_model(
@@ -10,35 +15,35 @@ describe Spree::Calculator::FlatRate, type: :model do
   end
 
   context 'compute' do
-    it "computes the amount as the rate when currency matches the order's currency" do
+    it "should compute the amount as the rate when currency matches the order's currency" do
       calculator.preferred_amount = 25.0
       calculator.preferred_currency = 'GBP'
       allow(order).to receive_messages currency: 'GBP'
       expect(calculator.compute(order).round(2)).to eq(25.0)
     end
 
-    it "computes the amount as 0 when currency does not match the order's currency" do
+    it "should compute the amount as 0 when currency does not match the order's currency" do
       calculator.preferred_amount = 100.0
       calculator.preferred_currency = 'GBP'
       allow(order).to receive_messages currency: 'USD'
       expect(calculator.compute(order).round(2)).to eq(0.0)
     end
 
-    it 'computes the amount as 0 when currency is blank' do
+    it 'should compute the amount as 0 when currency is blank' do
       calculator.preferred_amount = 100.0
       calculator.preferred_currency = ''
       allow(order).to receive_messages currency: 'GBP'
       expect(calculator.compute(order).round(2)).to eq(0.0)
     end
 
-    it 'computes the amount as the rate when the currencies use different casing' do
+    it 'should compute the amount as the rate when the currencies use different casing' do
       calculator.preferred_amount = 100.0
       calculator.preferred_currency = 'gBp'
       allow(order).to receive_messages currency: 'GBP'
       expect(calculator.compute(order).round(2)).to eq(100.0)
     end
 
-    it 'computes the amount as 0 when there is no object' do
+    it 'should compute the amount as 0 when there is no object' do
       calculator.preferred_amount = 100.0
       calculator.preferred_currency = 'GBP'
       expect(calculator.compute.round(2)).to eq(0.0)
