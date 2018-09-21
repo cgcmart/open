e072fb0afd18d4c49efbd7442bcb12f67875a5b6
99cm <zyizyao@china-guide.com>
2018-09-21 11:13:01 -0400

    Update flat_rate_spec.rb

--- core/spec/models/spree/calculator/shipping/flat_rate_spec.rb
+++ core/spec/models/spree/calculator/shipping/flat_rate_spec.rb
@@ -1,9 +1,14 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'shared_examples/calculator_shared_examples'
 
 module Spree
   module Calculator::Shipping
-    describe FlatRate, type: :model do
+    RSpec.describe FlatRate, type: :model do
       subject { Calculator::Shipping::FlatRate.new(preferred_amount: 4.00) }
+
+      it_behaves_like 'a calculator with a description'
 
       it 'always returns the same rate' do
         expect(subject.compute(build(:stock_package_fulfilled))).to eq(4.00)
