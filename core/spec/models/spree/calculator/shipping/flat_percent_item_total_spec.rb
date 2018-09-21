76a2042c0def3435604b5679204b58c2b895620c
99cm <zyizyao@china-guide.com>
2018-09-21 11:10:35 -0400

    Update flat_percent_item_total_spec.rb

--- core/spec/models/spree/calculator/shipping/flat_percent_item_total_spec.rb
+++ core/spec/models/spree/calculator/shipping/flat_percent_item_total_spec.rb
@@ -1,12 +1,15 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'shared_examples/calculator_shared_examples'
 
 module Spree
   module Calculator::Shipping
-    describe FlatPercentItemTotal, type: :model do
-      subject { FlatPercentItemTotal.new(preferred_flat_percent: 10) }
-
+    RSpec.describe FlatPercentItemTotal, type: :model do
       let(:variant1) { build(:variant, price: 10.11) }
       let(:variant2) { build(:variant, price: 20.2222) }
+
+      it_behaves_like 'a calculator with a description'
 
       let(:line_item1) { build(:line_item, variant: variant1) }
       let(:line_item2) { build(:line_item, variant: variant2) }
@@ -15,8 +18,14 @@ module Spree
         build(:stock_package, variants_contents: { variant1 => 2, variant2 => 1 })
       end
 
+      subject { FlatPercentItemTotal.new(preferred_flat_percent: 10) }
+
       it 'rounds result correctly' do
         expect(subject.compute(package)).to eq(4.04)
+      end
+
+      it 'should return a bigdecimal' do
+        expect(subject.compute(package)).to be_a(BigDecimal)
       end
     end
   end
