a9e3b8b8898e991d1a1edd0f873ad1e91ca7745f
99cm <zyizyao@china-guide.com>
2018-09-22 09:31:14 -0400

    Update finalizing_spec.rb

--- core/spec/models/spree/order/finalizing_spec.rb
+++ core/spec/models/spree/order/finalizing_spec.rb
@@ -1,12 +1,13 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Order, type: :model do
+require 'rails_helper'
+
+RSpec.describe Spree::Order, type: :model do
   let(:order) { stub_model('Spree::Order') }
 
-  before { create(:store) }
-
   context '#finalize!' do
-    let(:order) { Spree::Order.create(email: 'test@example.com') }
+    let!(:store) { create(:store) }
+    let(:order) { Spree::Order.create(email: 'test@example.com', store: store) }
 
     before do
       order.update_column :state, 'complete'
@@ -17,23 +18,23 @@ describe Spree::Order, type: :model do
       order.finalize!
     end
 
-    it 'sells inventory units' do
+    it 'should sell inventory units' do
       order.shipments.each do |shipment| # rubocop:disable RSpec/IteratedExpectation
-        expect(shipment).to receive(:update!)
+        expect(shipment).to receive(:update_state)
         expect(shipment).to receive(:finalize!)
       end
       order.finalize!
     end
 
-    it 'decreases the stock for each variant in the shipment' do
+    it 'should decrease the stock for each variant in the shipment' do
       order.shipments.each do |shipment|
         expect(shipment.stock_location).to receive(:decrease_stock_for_variant)
       end
       order.finalize!
     end
 
-    it 'changes the shipment state to ready if order is paid' do
-      Spree::Shipment.create(order: order, stock_location: create(:stock_location))
+    it 'should change the shipment state to ready if order is paid' do
+      Spree::Shipment.create(order: order)
       order.shipments.reload
 
       allow(order).to receive_messages(paid?: true, complete?: true)
@@ -42,16 +43,15 @@ describe Spree::Order, type: :model do
       expect(order.shipment_state).to eq('ready')
     end
 
-    after { Spree::Config.set track_inventory_levels: true }
-    it 'does not sell inventory units if track_inventory_levels is false' do
+    it 'should not sell inventory units if track_inventory_levels is false' do
       Spree::Config.set track_inventory_levels: false
       expect(Spree::InventoryUnit).not_to receive(:sell_units)
       order.finalize!
     end
 
-    it 'sends an order confirmation email' do
+    it 'should send an order confirmation email' do
       mail_message = double 'Mail::Message'
-      expect(Spree::OrderMailer).to receive(:confirm_email).with(order.id).and_return mail_message
+      expect(Spree::OrderMailer).to receive(:confirm_email).with(order).and_return mail_message
       expect(mail_message).to receive :deliver_later
       order.finalize!
     end
@@ -62,17 +62,21 @@ describe Spree::Order, type: :model do
       expect(order.confirmation_delivered?).to be true
     end
 
-    it 'does not send duplicate confirmation emails' do
+    it 'should not send duplicate confirmation emails' do
       allow(order).to receive_messages(confirmation_delivered?: true)
       expect(Spree::OrderMailer).not_to receive(:confirm_email)
       order.finalize!
     end
 
-    it 'freezes all adjustments' do
+    it 'should freeze all adjustments' do
+      # Stub this method as it's called due to a callback
+      # and it's irrelevant to this test
       allow(Spree::OrderMailer).to receive_message_chain :confirm_email, :deliver_later
       adjustments = [double]
       expect(order).to receive(:all_adjustments).and_return(adjustments)
-      expect(adjustments).to all(receive(:close))
+      adjustments.each do |adj|
+        expect(adj).to receive(:finalize!)
+      end
       order.finalize!
     end
 
@@ -81,17 +85,12 @@ describe Spree::Order, type: :model do
         allow(order).to receive_messages is_risky?: true
       end
 
-      it 'changes state to risky' do
-        expect(order).to receive(:considered_risky!)
-        order.finalize!
-      end
-
       context 'and order is approved' do
         before do
           allow(order).to receive_messages approved?: true
         end
 
-        it 'leaves order in complete state' do
+        it 'should leave order in complete state' do
           order.finalize!
           expect(order.state).to eq 'complete'
         end
@@ -103,7 +102,7 @@ describe Spree::Order, type: :model do
         allow(order).to receive_messages is_risky?: false
       end
 
-      it 'sets completed_at' do
+      it 'should set completed_at' do
         order.finalize!
         expect(order.completed_at).to be_present
       end
