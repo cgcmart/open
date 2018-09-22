e26a968d08841f509cec5287a5b62aafa3ec51bc
99cm <zyizyao@china-guide.com>
2018-09-22 08:29:53 -0400

    Create payment_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/payment_serializer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class PaymentSerializer < BaseSerializer
+        set_type :payment
+
+        attributes :amount, :response_code, :number, :cvv_response_code, :cvv_response_message,
+                   :payment_method_id, :payment_method_name
+      end
+    end
+  end
+end
