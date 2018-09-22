bc4785db722c053cbbdb37ba13d15f0d37e1510b
99cm <zyizyao@china-guide.com>
2018-09-22 11:02:04 -0400

    Update risk_assessment_spec.rb

--- core/spec/models/spree/order/risk_assessment_spec.rb
+++ core/spec/models/spree/order/risk_assessment_spec.rb
@@ -1,15 +1,15 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::Order, type: :model do
+require 'rails_helper'
+
+RSpec.describe Spree::Order, type: :model do
   let(:order) { stub_model('Spree::Order') }
 
   describe '.is_risky?' do
     context 'Not risky order' do
       let(:order) { FactoryBot.create(:order, payments: [payment]) }
-
       context 'with avs_response == D' do
         let(:payment) { FactoryBot.create(:payment, avs_response: 'D') }
-
         it 'is not considered risky' do
           expect(order.is_risky?).to eq(false)
         end
@@ -17,7 +17,6 @@ describe Spree::Order, type: :model do
 
       context 'with avs_response == M' do
         let(:payment) { FactoryBot.create(:payment, avs_response: 'M') }
-
         it 'is not considered risky' do
           expect(order.is_risky?).to eq(false)
         end
@@ -25,7 +24,6 @@ describe Spree::Order, type: :model do
 
       context "with avs_response == ''" do
         let(:payment) { FactoryBot.create(:payment, avs_response: '') }
-
         it 'is not considered risky' do
           expect(order.is_risky?).to eq(false)
         end
@@ -33,7 +31,6 @@ describe Spree::Order, type: :model do
 
       context 'with cvv_response_code == M' do
         let(:payment) { FactoryBot.create(:payment, cvv_response_code: 'M') }
-
         it 'is not considered risky' do
           expect(order.is_risky?).to eq(false)
         end
@@ -41,7 +38,6 @@ describe Spree::Order, type: :model do
 
       context "with cvv_response_message == ''" do
         let(:payment) { FactoryBot.create(:payment, cvv_response_message: '') }
-
         it 'is not considered risky' do
           expect(order.is_risky?).to eq(false)
         end
@@ -51,7 +47,6 @@ describe Spree::Order, type: :model do
     context 'Risky order' do
       context 'AVS response message' do
         let(:order) { FactoryBot.create(:order, payments: [FactoryBot.create(:payment, avs_response: 'A')]) }
-
         it 'returns true if the order has an avs_response' do
           expect(order.is_risky?).to eq(true)
         end
@@ -59,7 +54,6 @@ describe Spree::Order, type: :model do
 
       context 'CVV response code' do
         let(:order) { FactoryBot.create(:order, payments: [FactoryBot.create(:payment, cvv_response_code: 'N')]) }
-
         it 'returns true if the order has an cvv_response_code' do
           expect(order.is_risky?).to eq(true)
         end
@@ -67,27 +61,10 @@ describe Spree::Order, type: :model do
 
       context "state == 'failed'" do
         let(:order) { FactoryBot.create(:order, payments: [FactoryBot.create(:payment, state: 'failed')]) }
-
         it "returns true if the order has state == 'failed'" do
           expect(order.is_risky?).to eq(true)
         end
       end
-    end
-  end
-
-  context 'is considered risky' do
-    let(:order) do
-      order = FactoryBot.create(:completed_order_with_pending_payment)
-      order.considered_risky!
-      order
-    end
-
-    it 'can be approved by a user' do
-      expect(order).to receive(:approve!)
-      order.approved_by(stub_model(Spree::LegacyUser, id: 1))
-      expect(order.approver_id).to eq(1)
-      expect(order.approved_at).to be_present
-      expect(order.approved?).to be true
     end
   end
 end
