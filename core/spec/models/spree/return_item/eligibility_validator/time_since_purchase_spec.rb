0ac4a19917cdc1c74a1372f0edab539d8ededaec
99cm <zyizyao@china-guide.com>
2018-09-22 22:07:53 -0400

    Update time_since_purchase_spec.rb

--- core/spec/models/spree/return_item/eligibility_validator/time_since_purchase_spec.rb
+++ core/spec/models/spree/return_item/eligibility_validator/time_since_purchase_spec.rb
@@ -1,34 +1,41 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::ReturnItem::EligibilityValidator::TimeSincePurchase, type: :model do
-  let(:inventory_unit) { create(:inventory_unit, order: create(:shipped_order)) }
-  let(:return_item)    { create(:return_item, inventory_unit: inventory_unit) }
-  let(:validator)      { Spree::ReturnItem::EligibilityValidator::TimeSincePurchase.new(return_item) }
+require 'rails_helper'
+
+RSpec.describe Spree::ReturnItem::EligibilityValidator::TimeSincePurchase, type: :model do
+  include ActiveSupport::Testing::TimeHelpers
+
+  let(:return_item) { instance_double(Spree::ReturnItem) }
+  let(:validator) { described_class.new(return_item) }
 
   describe '#eligible_for_return?' do
     subject { validator.eligible_for_return? }
 
+    let(:interval) { Spree::Config[:return_eligibility_number_of_days] }
+
+    before do
+      allow(return_item).
+        to receive_message_chain('inventory_unit.order.completed_at').
+        and_return(completed_at)
+    end
+
+    around(:each) do |e|
+      travel_to(Time.current) { e.run }
+    end
+
     context 'it is within the return timeframe' do
-      it 'returns true' do
-        completed_at = return_item.inventory_unit.order.completed_at - (Spree::Config[:return_eligibility_number_of_days].days / 2)
-        return_item.inventory_unit.order.update_attributes(completed_at: completed_at)
-        expect(subject).to be true
-      end
+      let(:completed_at) { 1.day.ago }
+      it { is_expected.to be_truthy }
     end
 
     context 'it is past the return timeframe' do
-      before do
-        completed_at = return_item.inventory_unit.order.completed_at - Spree::Config[:return_eligibility_number_of_days].days - 1.day
-        return_item.inventory_unit.order.update_attributes(completed_at: completed_at)
-      end
+      let(:completed_at) { interval.day.ago }
 
-      it 'returns false' do
-        expect(subject).to be false
-      end
+      it { is_expected.to be_falsy }
 
       it 'sets an error' do
         subject
-        expect(validator.errors[:number_of_days]).to eq Spree.t('return_item_time_period_ineligible')
+        expect(validator.errors[:number_of_days]).to eq I18n.t('spree.return_item_time_period_ineligible')
       end
     end
   end
