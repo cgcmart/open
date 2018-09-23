910069f3147c70c7c1eb2fea21f2c94983920df0
99cm <zyizyao@china-guide.com>
2018-09-23 16:04:20 -0400

    Update app_configuration_spec.rb

--- core/spec/models/spree/app_configuration_spec.rb
+++ core/spec/models/spree/app_configuration_spec.rb
@@ -1,26 +1,43 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::AppConfiguration, type: :model do
-  let (:prefs) { Rails.application.config.spree.preferences }
+require 'rails_helper'
+
+RSpec.describe Spree::AppConfiguration, type: :model do
+  let(:prefs) { Spree::Config }
 
   it 'is available from the environment' do
     prefs.layout = 'my/layout'
     expect(prefs.layout).to eq 'my/layout'
   end
 
-  it 'is available as Spree::Config for legacy access' do
-    Spree::Config.layout = 'my/layout'
-    expect(Spree::Config.layout).to eq 'my/layout'
+  it 'should be available as Spree::Config for legacy access' do
+    expect(Spree::Config).to be_a Spree::AppConfiguration
   end
 
   it 'uses base searcher class by default' do
-    prefs.searcher_class = nil
     expect(prefs.searcher_class).to eq Spree::Core::Search::Base
   end
 
-  describe 'admin_path' do
-    it { expect(Spree::Config).to have_preference(:admin_path) }
-    it { expect(Spree::Config.preferred_admin_path_type).to eq(:string) }
-    it { expect(Spree::Config.preferred_admin_path_default).to eq('/admin') }
+  it "uses variant search class by default" do
+    expect(prefs.variant_search_class).to eq Spree::Core::Search::Variant
+  end
+
+  it "uses variant price selector class by default" do
+    expect(prefs.variant_price_selector_class).to eq Spree::Variant::PriceSelector
+  end
+
+  it "has a getter for the pricing options class provided by the variant price selector class" do
+    expect(prefs.pricing_options_class).to eq Spree::Variant::PriceSelector.pricing_options_class
+  end
+
+  describe '#stock' do
+    subject { prefs.stock }
+    it { is_expected.to be_a Spree::Core::StockConfiguration }
+  end
+
+  describe '@default_country_iso_code' do
+    it 'is the USA by default' do
+      expect(prefs[:default_country_iso]).to eq('US')
+    end
   end
 end
