f98a7eab6e522fa1244af01f5ea226a52882c278
99cm <zyizyao@china-guide.com>
2018-09-23 12:21:25 -0400

    Update shipping_category_spec.rb

--- core/spec/models/spree/stock/splitter/shipping_category_spec.rb
+++ core/spec/models/spree/stock/splitter/shipping_category_spec.rb
@@ -1,41 +1,47 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 module Spree
   module Stock
-    module Splitter
-      describe ShippingCategory, type: :model do
-        subject(:result) do
-          described_class.new(packer).split(packages)
+    RSpec.describe Splitter::ShippingCategory, type: :model do
+      let(:order) { create(:order_with_line_items, line_items_count: 1) }
+      let(:line_item) { order.line_items.first }
+      let(:variant1) { build(:variant) }
+      let(:variant2) { build(:variant) }
+      let(:shipping_category_1) { create(:shipping_category, name: 'A') }
+      let(:shipping_category_2) { create(:shipping_category, name: 'B') }
+
+      def inventory_unit1
+        build(:inventory_unit, variant: variant1, order: order, line_item: line_item).tap do |inventory_unit|
+          inventory_unit.variant.product.shipping_category = shipping_category_1
         end
+      end
 
-        let(:packer) { build(:stock_packer) }
-
-        let(:packages) { [package1, package2] }
-        let(:package1) { Spree::Stock::Package.new(packer.stock_location) }
-        let(:package2) { Spree::Stock::Package.new(packer.stock_location) }
-
-        let(:variant1) { build_stubbed(:variant, product: product1) }
-        let(:product1) { build_stubbed(:product, shipping_category: shipping_category_1) }
-        let(:variant2) { build_stubbed(:variant, product: product2) }
-        let(:product2) { build_stubbed(:product, shipping_category: shipping_category_2) }
-
-        let(:shipping_category_1) { build_stubbed(:shipping_category, name: 'A') }
-        let(:shipping_category_2) { build_stubbed(:shipping_category, name: 'B') }
-
-        before do
-          package1.add_multiple(build_stubbed_list(:inventory_unit, 4, :without_assoc, variant: variant1))
-          package1.add_multiple(build_stubbed_list(:inventory_unit, 8, :without_assoc, variant: variant2))
-
-          package2.add_multiple(build_stubbed_list(:inventory_unit, 6, :without_assoc, variant: variant1))
-          package2.add_multiple(build_stubbed_list(:inventory_unit, 9, :without_assoc, variant: variant2), :backordered)
+      def inventory_unit2
+        build(:inventory_unit, variant: variant2, order: order, line_item: line_item).tap do |inventory_unit|
+          inventory_unit.variant.product.shipping_category = shipping_category_2
         end
+      end
 
-        it 'splits each package by shipping category' do
-          expect(result[0].quantity).to eq 4
-          expect(result[1].quantity).to eq 8
-          expect(result[2].quantity).to eq 6
-          expect(result[3].quantity).to eq 9
-        end
+      let(:stock_location) { mock_model(Spree::StockLocation) }
+
+      subject { described_class.new(stock_location) }
+
+      it 'splits each package by shipping category' do
+        package1 = Package.new(stock_location)
+        4.times { package1.add inventory_unit1 }
+        8.times { package1.add inventory_unit2 }
+
+        package2 = Package.new(stock_location)
+        6.times { package2.add inventory_unit1 }
+        9.times { package2.add inventory_unit2, :backordered }
+
+        packages = subject.split([package1, package2])
+        expect(packages[0].quantity).to eq 4
+        expect(packages[1].quantity).to eq 8
+        expect(packages[2].quantity).to eq 6
+        expect(packages[3].quantity).to eq 9
       end
     end
   end
