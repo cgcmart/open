4ef86af7ebf47a3c615aef1a9ca21a117e2e0cc6
99cm <zyizyao@china-guide.com>
2018-09-23 10:50:19 -0400

    Update availability_validator_spec.rb

--- core/spec/models/spree/stock/availability_validator_spec.rb
+++ core/spec/models/spree/stock/availability_validator_spec.rb
@@ -1,42 +1,139 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 module Spree
   module Stock
-    describe AvailabilityValidator, type: :model do
-      subject { described_class.new }
+    RSpec.describe AvailabilityValidator, type: :model do
+      let(:validator) { Spree::Stock::AvailabilityValidator.new }
 
-      let!(:line_item) { double(quantity: 5, variant_id: 1, variant: double.as_null_object, errors: double('errors'), inventory_units: []) }
-      let(:inventory_unit) { double('InventoryUnit') }
-      let(:inventory_units) { [inventory_unit] }
+      subject { validator.validate(line_item) }
 
-      before do
-        allow(inventory_unit).to receive_messages(pending?: false)
-        allow(inventory_unit).to receive_messages(quantity: 5)
+      shared_examples_for "fails validation" do
+        it "returns false" do
+          expect(subject).to eq false
+        end
+
+        it "adds a validation error" do
+          subject
+          display_name = line_item.variant.name.to_s
+          display_name += " (#{line_item.variant.options_text})" unless line_item.variant.options_text.blank?
+          expect(line_item.errors).to match_array ["Quantity selected of #{display_name.inspect} is not available."]
+        end
       end
 
-      it 'is valid when supply is sufficient' do
-        allow_any_instance_of(Stock::Quantifier).to receive_messages(can_supply?: true)
-        expect(line_item).not_to receive(:errors)
-        subject.validate(line_item)
+      shared_examples_for "passes validation" do
+        it "returns true" do
+          expect(subject).to eq true
+        end
+
+        it "doesn't add a validation error" do
+          subject
+          expect(line_item.errors).to be_empty
+        end
       end
 
-      it 'is invalid when supply is insufficent' do
-        allow_any_instance_of(Stock::Quantifier).to receive_messages(can_supply?: false)
-        expect(line_item.errors).to receive(:[]).with(:quantity).and_return []
-        subject.validate(line_item)
+      context "line_item is not part of a shipment" do
+        let(:line_item) { create(:line_item) }
+
+        context "has stock in all stock locations" do
+          before do
+            Spree::StockItem.where(variant_id: line_item.variant_id).update_all(count_on_hand: 10, backorderable: false)
+          end
+
+          include_examples "passes validation"
+        end
+
+        context "doesn't have stock in any stock location" do
+          before do
+            Spree::StockItem.where(variant_id: line_item.variant_id).update_all(count_on_hand: 0, backorderable: false)
+          end
+
+          include_examples "fails validation"
+        end
       end
 
-      it 'considers existing inventory_units sufficient' do
-        allow_any_instance_of(Stock::Quantifier).to receive_messages(can_supply?: false)
-        expect(line_item).not_to receive(:errors)
-        allow(line_item).to receive_messages(inventory_units: inventory_units)
-        subject.validate(line_item)
+      context "line_item is part of a shipment" do
+        let!(:order) { create(:order_with_line_items) }
+
+        context "has stock in one stock location" do
+          let(:line_item)         { order.line_items.first }
+
+          before do
+            line_item.variant.stock_items.update_all(count_on_hand: 10, backorderable: false)
+          end
+
+          include_examples "passes validation"
+        end
+
+        context "with stock in multiple locations" do
+          let(:line_item)         { order.line_items.first }
+          let(:variant)           { line_item.variant }
+          let!(:stock_location_1) { create(:stock_location, name: "Test Warehouse", active: false) }
+
+          before do
+            shipment = order.shipments.create(stock_location: stock_location_1)
+            order.contents.add(variant, 1, shipment: shipment)
+          end
+
+          context "but no stock in either location" do
+            before do
+              variant.stock_items.update_all(count_on_hand: 0, backorderable: false)
+            end
+            include_examples "fails validation"
+          end
+
+          context "but no stock in one location" do
+            before do
+              stock_location_1.stock_items.update_all(count_on_hand: 0, backorderable: false)
+            end
+
+            include_examples "fails validation"
+          end
+
+          context "with enough stock only across locations" do
+            before do
+              variant.stock_items.update_all(count_on_hand: 1, backorderable: false)
+            end
+            include_examples "passes validation"
+          end
+
+          context "but inventory units are finalized" do
+            before do
+              order.inventory_units.update_all(pending: false)
+            end
+
+            include_examples "passes validation"
+          end
+        end
       end
 
-      it 'is valid when the quantity is zero' do
-        expect(line_item).to receive(:quantity).and_return(0)
-        expect(line_item.errors).not_to receive(:[]).with(:quantity)
-        subject.validate(line_item)
+      context "line_item is split across two shipments" do
+        let!(:order) { create(:order_with_line_items) }
+        let(:line_item) { order.line_items.first }
+        let(:variant) { line_item.variant }
+        let(:stock_location) { order.shipments.first.stock_location }
+
+        before do
+          shipment2 = order.shipments.create!(stock_location: order.shipments.first.stock_location)
+          order.contents.add(variant, 1, shipment: shipment2)
+          variant.stock_items.first.update_columns(count_on_hand: count_on_hand, backorderable: false)
+        end
+
+        context "and there is just enough stock" do
+          let(:count_on_hand) { 2 }
+          include_examples "passes validation"
+        end
+
+        context "and there is not enough stock" do
+          let(:count_on_hand) { 1 }
+          include_examples "fails validation"
+        end
+
+        context "and there is no available stock" do
+          let(:count_on_hand) { 0 }
+          include_examples "fails validation"
+        end
       end
     end
   end
