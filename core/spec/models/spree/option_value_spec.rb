e743293d5844b8ac4d4ed7d028b7cb97448cd293
99cm <zyizyao@china-guide.com>
2018-09-24 11:52:57 -0400

    Update option_value_spec.rb

--- core/spec/models/spree/option_value_spec.rb
+++ core/spec/models/spree/option_value_spec.rb
@@ -1,13 +1,53 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::OptionValue, type: :model do
+require 'rails_helper'
+
+RSpec.describe Spree::OptionValue, type: :model do
+  include ActiveSupport::Testing::TimeHelpers
+
   context 'touching' do
-    it 'touches a variant' do
-      variant = create(:variant)
-      option_value = variant.option_values.first
-      variant.update_column(:updated_at, 1.day.ago)
-      option_value.touch
-      expect(variant.reload.updated_at).to be_within(3.seconds).of(Time.current)
+    let!(:variant) do
+      travel_to(1.day.ago) do
+        create(:variant)
+      end
+    end
+    let(:option_value) { variant.option_values.first }
+
+    it 'should touch a variant' do
+      now = Time.current
+      travel_to(now) do
+        option_value.touch
+        expect(variant.reload.updated_at).to be_within(1.second).of(now)
+      end
+    end
+  end
+
+  context "from the after_save hook" do
+      it "should not touch the variant if there are no changes" do
+        now = Time.current
+        travel_to(now) do
+          option_value.save!
+          expect(variant.reload.updated_at).to be <= 1.day.ago
+        end
+      end
+
+      it "should touch the variant if there are changes" do
+        now = Time.current
+        travel_to(now) do
+          option_value.name += "--1"
+          option_value.save!
+          expect(variant.reload.updated_at).to be_within(1.second).of(now)
+        end
+      end
+    end
+  end
+
+  describe "#presentation_with_option_type" do
+    let(:option_value) { build(:option_value) }
+    subject { option_value.presentation_with_option_type }
+
+    it "returns a string in the correct form" do
+      expect(subject).to eq "Size - S"
     end
   end
 end
