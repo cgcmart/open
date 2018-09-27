56b399c534c3a5f36a390859c049fe1393f3bccd
99cm <zyizyao@china-guide.com>
2018-09-27 16:18:53 -0400

    Update state_spec.rb

--- core/spec/models/spree/state_spec.rb
+++ core/spec/models/spree/state_spec.rb
@@ -1,18 +1,43 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::State, type: :model do
-  it 'can find a state by name or abbr' do
-    state = create(:state, name: 'California', abbr: 'CA')
-    expect(Spree::State.find_all_by_name_or_abbr('California')).to include(state)
-    expect(Spree::State.find_all_by_name_or_abbr('CA')).to include(state)
+require 'rails_helper'
+
+RSpec.describe Spree::State, type: :model do
+  describe '.with_name_or_abbr' do
+    subject do
+      Spree::State.with_name_or_abbr(search_term)
+    end
+
+    let!(:state) { create(:state, name: "California", abbr: "CA") }
+
+    context 'by invalid term' do
+      let(:search_term) { 'NonExistent' }
+      it { is_expected.to be_empty }
+    end
+
+    context 'by name' do
+      let(:search_term) { 'California' }
+      it { is_expected.to include(state) }
+    end
+
+    context 'by abbr' do
+      let(:search_term) { 'CA' }
+      it { is_expected.to include(state) }
+    end
+
+    context 'by case-insensitive abbr' do
+      let(:search_term) { 'CaLiFoRnIa' }
+      it { is_expected.to include(state) }
+    end
+
+    context 'by case-insensitive abbr' do
+      let(:search_term) { 'cA' }
+      it { is_expected.to include(state) }
+    end
   end
 
   it 'can find all states group by country id' do
     state = create(:state)
     expect(Spree::State.states_group_by_country_id).to eq(state.country_id.to_s => [[state.id, state.name]])
-  end
-
-  describe 'whitelisted_ransackable_attributes' do
-    it { expect(Spree::State.whitelisted_ransackable_attributes).to eq(%w(abbr)) }
   end
 end
