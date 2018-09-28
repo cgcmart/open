c6efbfea33c5692226584e6299a667db091a666c
99cm <zyizyao@china-guide.com>
2018-09-27 20:19:52 -0400

    Update tax_category_spec.rb

--- core/spec/models/spree/tax_category_spec.rb
+++ core/spec/models/spree/tax_category_spec.rb
@@ -1,6 +1,8 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Spree::TaxCategory, type: :model do
+require 'rails_helper'
+
+RSpec.describe Spree::TaxCategory, type: :model do
   context 'default tax category' do
     let(:tax_category) { create(:tax_category) }
     let(:new_tax_category) { create(:tax_category) }
@@ -9,7 +11,7 @@ describe Spree::TaxCategory, type: :model do
       tax_category.update_column(:is_default, true)
     end
 
-    it 'undefaults the previous default tax category' do
+    it 'should undefault the previous default tax category' do
       new_tax_category.update_attributes(is_default: true)
       expect(new_tax_category.is_default).to be true
 
@@ -17,7 +19,7 @@ describe Spree::TaxCategory, type: :model do
       expect(tax_category.is_default).to be false
     end
 
-    it 'undefaults the previous default tax category except when updating the existing default tax category' do
+    it 'should undefault the previous default tax category except when updating the existing default tax category' do
       tax_category.update_column(:description, 'Updated description')
 
       tax_category.reload
