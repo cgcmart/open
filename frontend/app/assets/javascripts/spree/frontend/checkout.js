114f4c3888b8a7951c31a00adb82a57c9c3c33bc
99cm <zyizyao@china-guide.com>
2018-09-28 16:19:45 -0400

    Update checkout.js

--- frontend/app/assets/javascripts/spree/frontend/checkout.js
+++ frontend/app/assets/javascripts/spree/frontend/checkout.js
@@ -1,18 +1,23 @@
 Spree.disableSaveOnClick = function () {
   $('form.edit_order').on('submit', function (event) {
     if ($(this).data('submitted') === true) {
-      event.preventDefault();
+      event.preventDefault()
     } else {
-      $(this).data('submitted', true);
-      $(this).find(':submit, :image').removeClass('primary').addClass('disabled');
+      $(this).data('submitted', true)
+      $(this).find(':submit, :image').removeClass('primary').addClass('disabled')
     }
-  });
-};
+  })
+}
 
 Spree.enableSave = function () {
-  $('form.edit_order').data('submitted', false).find(':submit, :image').attr('disabled', false).addClass('primary').removeClass('disabled');
-};
+  $('form.edit_order').data('submitted', false).find(':submit, :image').attr('disabled', false).addClass('primary').removeClass('disabled')
+}
 
-Spree.ready(function () {
-  return Spree.Checkout = {};
-});
+Spree.ready(function($) {
+  var termsCheckbox = $('#accept_terms_and_conditions')
+  termsCheckbox.change(function() {
+    var submitBtn = $(this.closest('form')).find(':submit')
+    submitBtn.prop('disabled', !this.checked)
+    submitBtn.toggleClass('disabled', !this.checked)
+  })
+})
