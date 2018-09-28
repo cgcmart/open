a275576ea9fbbc79739b3dcfdeff5fad69b8353e
99cm <zyizyao@china-guide.com>
2018-09-28 10:14:38 -0400

    Update spec_helper.rb

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -1,85 +1,37 @@
+# frozen_string_literal: true
+
 if ENV['COVERAGE']
-  # Run Coverage report
   require 'simplecov'
-  SimpleCov.start do
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Helpers', 'app/helpers'
-    add_group 'Mailers', 'app/mailers'
-    add_group 'Models', 'app/models'
-    add_group 'Views', 'app/views'
-    add_group 'Jobs', 'app/jobs'
-    add_group 'Libraries', 'lib'
-  end
+  SimpleCov.start('rails')
 end
 
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
-ENV['RAILS_ENV'] ||= 'test'
+require 'rspec/core'
 
-begin
-  require File.expand_path('../dummy/config/environment', __FILE__)
-rescue LoadError
-  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
-end
-
-require 'rspec/rails'
-require 'database_cleaner'
-require 'ffaker'
-
-Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
-
-require 'spree/testing_support/i18n' if ENV['CHECK_TRANSLATIONS']
-
-require 'spree/testing_support/factories'
 require 'spree/testing_support/preferences'
-require 'spree/testing_support/url_helpers'
-require 'spree/testing_support/kernel'
+require 'spree/config'
+require 'with_model'
 
 RSpec.configure do |config|
+  config.disable_monkey_patching!
   config.color = true
-  config.fail_fast = ENV['FAIL_FAST'] || false
-  config.fixture_path = File.join(__dir__, 'fixtures')
-  config.infer_spec_type_from_file_location!
-  config.mock_with :rspec
-  config.raise_errors_for_deprecations!
-
-  # Config for running specs while have transition period from Paperclip to ActiveStorage
-  if Rails.application.config.use_paperclip
-    config.filter_run_excluding :active_storage
-  else
-    config.filter_run_including :active_storage
-    config.run_all_when_everything_filtered = true
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = :expect
   end
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  config.before do
-    Rails.cache.clear
+  config.before :each do
     reset_spree_preferences
-  rescue Errno::ENOTEMPTY
   end
 
-  config.include FactoryBot::Syntax::Methods
   config.include Spree::TestingSupport::Preferences
-  config.include Spree::TestingSupport::Kernel
+  config.extend WithModel
 
-  # Clean out the database state before the tests run
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transaction
-  end
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
-  # Wrap all db isolated tests in a transaction
-  config.around(db: :isolate) do |example|
-    DatabaseCleaner.cleaning(&example)
-  end
-
-  config.around do |example|
-    Timeout.timeout(30, &example)
-  end
+  config.example_status_persistence_file_path = "./spec/examples.txt"
 
   config.order = :random
   Kernel.srand config.seed
