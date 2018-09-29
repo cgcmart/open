b86fbf5336bf302bec3332315807ffc70f38b741
99cm <zyizyao@china-guide.com>
2018-09-28 23:02:37 -0400

    Update store_controller.rb

--- frontend/app/controllers/spree/store_controller.rb
+++ frontend/app/controllers/spree/store_controller.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 module Spree
   class StoreController < Spree::BaseController
+    include Spree::Core::ControllerHelpers::Pricing
     include Spree::Core::ControllerHelpers::Order
-
-    skip_before_action :set_current_order, only: :cart_link
 
     def forbidden
       render 'spree/shared/forbidden', layout: Spree::Config[:layout], status: 403
@@ -14,13 +15,20 @@ module Spree
 
     def cart_link
       render partial: 'spree/shared/link_to_cart'
-      fresh_when(simple_current_order)
+      fresh_when(current_order, template: 'spree/shared/_link_to_cart')
     end
 
-    protected
+     private
 
     def config_locale
       Spree::Frontend::Config[:locale]
+    end
+
+    def lock_order
+      Spree::OrderMutex.with_lock!(@order) { yield }
+    rescue Spree::OrderMutex::LockFailed
+      flash[:error] = t('spree.order_mutex_error')
+      redirect_to spree.cart_path
     end
   end
 end
