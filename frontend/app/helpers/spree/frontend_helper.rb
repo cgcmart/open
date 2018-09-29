18ff4d77a0f735ff2dd7eed37586423f8db06a53
99cm <zyizyao@china-guide.com>
2018-09-29 08:28:41 -0400

    Update frontend_helper.rb

--- frontend/app/helpers/spree/frontend_helper.rb
+++ frontend/app/helpers/spree/frontend_helper.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+require 'spree/store_helper'
+require 'spree/checkout_helper'
+
 module Spree
   module FrontendHelper
     def body_class
@@ -26,36 +31,6 @@ module Spree
         and will be removed in Spree 3.6. Please update your code to avoid problems after update
       EOS
       spree_breadcrumbs(taxon, separator)
-    end
-
-    def checkout_progress(numbers: false)
-      states = @order.checkout_steps
-      items = states.each_with_index.map do |state, i|
-        text = Spree.t("order_state.#{state}").titleize
-        text.prepend("#{i.succ}. ") if numbers
-
-        css_classes = []
-        current_index = states.index(@order.state)
-        state_index = states.index(state)
-
-        if state_index < current_index
-          css_classes << 'completed'
-          text = link_to text, checkout_state_path(state)
-        end
-
-        css_classes << 'next' if state_index == current_index + 1
-        css_classes << 'active' if state == @order.state
-        css_classes << 'first' if state_index == 0
-        css_classes << 'last' if state_index == states.length - 1
-        # No more joined classes. IE6 is not a target browser.
-        # Hack: Stops <a> being wrapped round previous items twice.
-        if state_index < current_index
-          content_tag('li', text, class: css_classes.join(' '))
-        else
-          content_tag('li', content_tag('a', text), class: css_classes.join(' '))
-        end
-      end
-      content_tag('ul', raw(items.join("\n")), class: 'progress-steps nav nav-pills nav-justified', id: "checkout-step-#{@order.state}")
     end
 
     def flash_messages(opts = {})
