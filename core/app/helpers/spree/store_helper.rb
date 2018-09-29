6b3aa03161246b774deb2bf68ffad08e9cd5e6ca
99cm <zyizyao@china-guide.com>
2018-09-29 08:31:04 -0400

    Create store_helper.rb

---
+++ core/app/helpers/spree/store_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Spree
+  # Methods added to this helper will be available to all templates in the
+  # frontend.
+  module StoreHelper
+    # @return [Boolean] true when it is appropriate to show the store menu
+    def store_menu?
+      %w{thank_you}.exclude? params[:action]
+    end  
+  end
+end
