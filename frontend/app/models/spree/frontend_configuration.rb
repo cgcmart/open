a3429a7d348f95aadd369da2cf15ebcc84e2cb9b
99cm <zyizyao@china-guide.com>
2018-09-29 09:36:50 -0400

    Update frontend_configuration.rb

--- frontend/app/models/spree/frontend_configuration.rb
+++ frontend/app/models/spree/frontend_configuration.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 module Spree
   class FrontendConfiguration < Preferences::Configuration
     preference :coupon_codes_enabled, :boolean, default: true # Determines if we show coupon code form at cart and checkout
     preference :locale, :string, default: Rails.application.config.i18n.default_locale
+
+    # Add your terms and conditions in app/views/spree/checkout/_terms_and_conditions.en.html.erb
+    preference :require_terms_and_conditions_acceptance, :boolean, default: false
   end
 end
