6f3dd6c5c5eb210f92b8291cc5ff7d79769b198e
99cm <zyizyao@china-guide.com>
2018-10-05 10:28:31 -0400

    Update content_controller_spec.rb

--- frontend/spec/controllers/spree/content_controller_spec.rb
+++ frontend/spec/controllers/spree/content_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 describe Spree::ContentController, type: :controller do
   it 'displays CVV page' do
-    spree_get :cvv
+    get :cvv
     expect(response.response_code).to eq(200)
   end
 end
