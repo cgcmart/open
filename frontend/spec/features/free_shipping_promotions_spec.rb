5ae71b697ace56f12ace8c684261d6ef54513d60
99cm <zyizyao@china-guide.com>
2018-10-08 10:53:16 -0400

    Update free_shipping_promotions_spec.rb

--- frontend/spec/features/free_shipping_promotions_spec.rb
+++ frontend/spec/features/free_shipping_promotions_spec.rb
@@ -1,27 +1,30 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'Free shipping promotions', type: :feature, js: true do
+  let!(:store) { create(:store) }
   let!(:country) { create(:country, name: 'United States of America', states_required: true) }
   let!(:state) { create(:state, name: 'Alabama', country: country) }
-
-  before do
-    create(:zone)
+  let!(:zone) { create(:zone) }
+  let!(:shipping_method) do
     sm = create(:shipping_method)
     sm.calculator.preferred_amount = 10
     sm.calculator.save
+    sm
+  end
 
-    create(:check_payment_method)
-    create(:product, name: 'RoR Mug', price: 20)
-
-    promotion = Spree::Promotion.create!(name: 'Free Shipping',
-                                         starts_at: 1.day.ago,
-                                         expires_at: 1.day.from_now)
-
-    action = Spree::Promotion::Actions::FreeShipping.new
-    action.promotion = promotion
-    action.save
-
-    promotion.reload # so that promotion.actions is available
+  let!(:payment_method) { create(:check_payment_method) }
+  let!(:product) { create(:product, name: 'RoR Mug', price: 20) }
+  let!(:promotion) do
+    create(
+      :promotion,
+      apply_automatically: true,
+      Promotion_Actions: [Spree::Promotion::Actions::FreeShipping.new],
+      name: 'Free Shipping',
+      starts_at: 1.day.ago,
+      expires_at: 1.day.from_now
+    )
   end
 
   context 'free shipping promotion automatically applied' do
@@ -46,7 +49,7 @@ describe 'Free shipping promotions', type: :feature, js: true do
       click_button 'Save and Continue'
     end
 
-    # Regression test for #4428
+    # Regression test for https://github.com/spree/spree/issues/4428
     it 'applies the free shipping promotion' do
       within('#checkout-summary') do
         expect(page).to have_content('Shipping total:  $10.00')
