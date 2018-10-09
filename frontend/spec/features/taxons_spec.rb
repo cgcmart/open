5c07806014412dd08087c91dfea26998f81eafdb
99cm <zyizyao@china-guide.com>
2018-10-08 23:09:02 -0400

    Update taxons_spec.rb

--- frontend/spec/features/taxons_spec.rb
+++ frontend/spec/features/taxons_spec.rb
@@ -1,20 +1,20 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'viewing products', type: :feature, inaccessible: true do
   let!(:taxonomy) { create(:taxonomy, name: 'Category') }
   let!(:super_clothing) { taxonomy.root.children.create(name: 'Super Clothing') }
   let!(:t_shirts) { super_clothing.children.create(name: 'T-Shirts') }
-  let(:metas) { { meta_description: 'Brand new Ruby on Rails TShirts', meta_title: 'Ruby On Rails TShirt', meta_keywords: 'ror, tshirt, ruby' } }
-  let(:store_name) { ((first_store = Spree::Store.first) && first_store.name).to_s }
-
-  before do
-    t_shirts.children.create(name: 'XXL') # xxl
-
+  let!(:xxl) { t_shirts.children.create(name: 'XXL') }
+  let!(:product) do
     product = create(:product, name: 'Superman T-Shirt')
     product.taxons << t_shirts
   end
+  let(:metas) { { meta_description: 'Brand new Ruby on Rails TShirts', meta_title: 'Ruby On Rails TShirt', meta_keywords: 'ror, tshirt, ruby' } }
+  let(:store_name) { ((first_store = Spree::Store.first) && first_store.name).to_s }
 
-  # Regression test for #1796
+  # Regression test for https://github.com/spree/spree/issues/1796
   it "can see a taxon's products, even if that taxon has child taxons" do
     visit '/t/category/super-clothing/t-shirts'
     expect(page).to have_content('Superman T-Shirt')
@@ -57,7 +57,7 @@ describe 'viewing products', type: :feature, inaccessible: true do
       expect(page).to have_title('Category - T-Shirts - ' + store_name)
     end
 
-    # Regression test for #2814
+    # Regression test for https://github.com/spree/spree/issues/2814
     it "doesn't use meta_title as heading on page" do
       t_shirts.update_attributes metas
       visit '/t/category/super-clothing/t-shirts'
@@ -142,6 +142,27 @@ describe 'viewing products', type: :feature, inaccessible: true do
       tmp = page.all('#products .product-list-item a').map(&:text).flatten.compact
       tmp.delete('')
       expect(tmp.sort!).to eq(['Ruby on Rails Bag', 'Ruby on Rails Tote'])
+    end
+  end
+
+  # Regression test for https://github.com/solidusio/solidus/issues/2602
+  context 'root taxon page' do
+    it 'shows taxon previews' do
+      visit spree.nested_taxons_path(taxonomy.root)
+
+      expect(page).to have_css('#products.product-list-item', count: 2)
+      expect(page).to have_content('Superman T-Shirt', count: 2)
+    end
+
+    context 'with prices in other currency' do
+      before { Spree::Price.update_all(currency: 'CAD') }
+
+      it 'shows no products' do
+        visit spree.nested_taxons_path(taxonomy.root)
+
+        expect(page).to have_css('#products.product-list-item', count: 0)
+        expect(page).to have_no_content("Superman T-Shirt")
+      end
     end
   end
 end
