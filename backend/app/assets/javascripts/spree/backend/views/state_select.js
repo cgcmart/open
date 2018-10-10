405e46ecd36653ac06f972861f2f77bddc9854db
99cm <zyizyao@china-guide.com>
2018-10-10 16:22:59 -0400

    Update state_select.js

--- backend/app/assets/javascripts/spree/backend/views/state_select.js
+++ backend/app/assets/javascripts/spree/backend/views/state_select.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line camelcase, no-unused-vars
 Spree.Views.StateSelect = Backbone.View.extend({
   initialize: function() {
     this.states = {} // null object
 
-    this.$state_select = this.$('.js-state_id');
-    this.$state_input = this.$('.js-state_name');
+    this.$state_select = this.$('.js-state_id')
+    this.$state_input = this.$('.js-state_name')
 
     // read initial values from page
     this.model.set({
@@ -11,14 +12,14 @@ Spree.Views.StateSelect = Backbone.View.extend({
       state_id: this.$state_select.val()
     })
 
-    this.updateStates();
+    this.updateStates()
     this.listenTo(this.model, 'change:country_id', this.updateStates)
-    this.render();
+    this.render()
   },
 
   events: {
-    "change .js-state_name": "onChange",
-    "change .js-state_id": "onChange",
+    "change .js-state_name": 'onChange',
+    "change .js-state_id": 'onChange',
   },
 
   onChange: function() {
@@ -29,13 +30,13 @@ Spree.Views.StateSelect = Backbone.View.extend({
   },
 
   updateStates: function() {
-    this.stopListening(this.states);
-    var country_id = this.model.get("country_id");
+    this.stopListening(this.states)
+    var country_id = this.model.get('country_id')
     if (country_id) {
-      this.states = Spree.Views.StateSelect.stateCache(country_id);
-      this.listenTo(this.states, "sync", this.render);
+      this.states = Spree.Views.StateSelect.stateCache(country_id)
+      this.listenTo(this.states, 'sync', this.render)
     }
-    this.render();
+    this.render()
   },
 
   render: function() {
@@ -43,29 +44,29 @@ Spree.Views.StateSelect = Backbone.View.extend({
     this.$state_input.hide().prop('disabled', true);
 
     if (!this.model.get('country_id') || !this.states.fetched) {
-      this.$state_select.show();
+      this.$state_select.show()
     } else if (this.states.length) {
-      var $state_select = this.$state_select;
+      var $state_select = this.$state_select
       this.states.each(function(state) {
         $state_select.append(
-          $('<option>').prop('value', state.id).text(state.get("name"))
-        );
+          $('<option>').prop('value', state.id).text(state.get('name'))
+        )
       })
-      this.$state_select.val(this.model.get("state_id"))
-      this.$state_select.show().prop("disabled", false);
+      this.$state_select.val(this.model.get('state_id'))
+      this.$state_select.show().prop('disabled', false)
     } else {
-      this.$state_input.show().prop('disabled', false);
+      this.$state_input.show().prop('disabled', false)
     }
   }
 })
 
 Spree.Views.StateSelect.stateCache = _.memoize(function(country_id) {
   var states = new Spree.Collections.States([], {country_id: country_id})
-  states.fetched = false;
+  states.fetched = false
   states.fetch({
     success: function() {
-      states.fetched = true;
+      states.fetched = true
     }
-  });
-  return states;
-});
+  })
+  return states
+})
