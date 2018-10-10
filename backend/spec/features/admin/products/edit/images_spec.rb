9ae9382344798dc991b4342cc1ff8fb83a4419d9
99cm <zyizyao@china-guide.com>
2018-10-09 22:05:58 -0400

    Update images_spec.rb

--- backend/spec/features/admin/products/edit/images_spec.rb
+++ backend/spec/features/admin/products/edit/images_spec.rb
@@ -13,13 +13,11 @@ describe 'Product Images', type: :feature, js: true do
     # Otherwise this would result in this error:
     # undefined method `processors' for \"48x48>\
     Spree::Image.attachment_definitions[:attachment][:styles].symbolize_keys!
-    allow(ENV).to receive(:[]).and_call_original
-    allow(ENV).to receive(:[]).with(:SPREE_USE_PAPERCLIP).and_return(true)
   end
 
   context 'uploading, editing, and deleting an image' do
-    it 'should allow an admin to upload and edit an image for a product' do
-      Spree::Image.attachment_definitions[:attachment].delete :storage if Rails.application.config.use_paperclip
+    it 'allows an admin to upload and edit an image for a product' do
+      Spree::Image.attachment_definitions[:attachment].delete :storage
 
       create(:product)
 
