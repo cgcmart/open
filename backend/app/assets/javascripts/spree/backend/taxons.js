5aadc3303f5db35ad493a704843e440405325f22
99cm <zyizyao@china-guide.com>
2018-10-11 13:39:08 -0400

    Update taxons.js

--- backend/app/assets/javascripts/spree/backend/taxons.js
+++ backend/app/assets/javascripts/spree/backend/taxons.js
@@ -1,13 +1,13 @@
 //= require solidus_admin/Sortable
 
 Spree.ready(function() {
-  var productTemplate = HandlebarsTemplates['products/sortable'];
+  var productTemplate = HandlebarsTemplates['products/sortable']
   var productListTemplate = function(products) {
-    return _.map(products, productTemplate).join('') || "<h4>" + Spree.translations.no_results + "</h4>";
-  };
+    return _.map(products, productTemplate).join('') || '<h4>' + Spree.translations.no_results + '</h4>'
+  }
 
   var saveSort = function(e) {
-    var item = e.item;
+    var item = e.item
     Spree.ajax({
       url: Spree.routes.classifications_api,
       method: 'PUT',
@@ -16,18 +16,18 @@ Spree.ready(function() {
         taxon_id: $('#taxon_id').val(),
         position: e.newIndex
       }
-    });
-  };
+    })
+  }
 
   var formatTaxon = function(taxon) {
-    return Select2.util.escapeMarkup(taxon.pretty_name);
-  };
+    return Select2.util.escapeMarkup(taxon.pretty_name)
+  }
 
   $('#taxon_id').select2({
-    dropdownCssClass: "taxon_select_box",
+    dropdownCssClass: 'taxon_select_box',
     placeholder: Spree.translations.find_a_taxon,
     ajax: {
-      url: Spree.routes.taxons_search,
+      url: Spree.routes.taxons_api,
       params: {
         "headers": {
           "X-Spree-Token": Spree.api_key
@@ -46,12 +46,12 @@ Spree.ready(function() {
         return {
           results: data['taxons'],
           more: data.current_page < data.pages
-        };
+        }
       }
     },
     formatResult: formatTaxon,
     formatSelection: formatTaxon
-  });
+  })
 
   $('#taxon_id').on("change", function(e) {
     Spree.ajax({
@@ -61,15 +61,44 @@ Spree.ready(function() {
         simple: 1
       },
       success: function(data) {
-        $('#taxon_products').html(productListTemplate(data.products));
+        $('#taxon_products').html(productListTemplate(data.products))
 
         var el = document.querySelector('#taxon_products')
 
         new Sortable(el, {
-          draggable: ".sort_item",
+          draggable: '.sort_item',
           onEnd: saveSort
-        });
+        })
       }
-    });
-  });
-});
+    })
+  })
+
+  taxonProducts.on('click', '.js-delete-product', function (e) {
+    var currentTaxonId = $('#taxon_id').val()
+    var product = $(this).parents('.product')
+    var productId = product.data('product-id')
+    var productTaxons = String(product.data('taxons')).split(',').map(Number)
+    var productIndex = productTaxons.indexOf(parseFloat(currentTaxonId))
+    productTaxons.splice(productIndex, 1)
+    var taxonIds = productTaxons.length > 0 ? productTaxons : ['']
+    $.ajax({
+      url: Spree.routes.products_api + '/' + productId,
+      data: {
+        product: {
+          taxon_ids: taxonIds
+        },
+        token: Spree.api_key
+      },
+      type: 'PUT'
+    }).done(function () {
+      product.fadeOut(400, function (e) {
+        product.remove()
+      })
+    })
+  })
+  $('.variant_autocomplete').variantAutocomplete()
+
+  function formatTaxon (taxon) {
+    return Select2.util.escapeMarkup(taxon.pretty_name)
+  }
+})
