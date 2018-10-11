1206f9705490c240fbf9993b496c4ab1c15d5462
99cm <zyizyao@china-guide.com>
2018-10-11 10:46:12 -0400

    Update form.js

--- backend/app/assets/javascripts/spree/backend/views/zones/form.js
+++ backend/app/assets/javascripts/spree/backend/views/zones/form.js
@@ -4,13 +4,13 @@ Spree.Views.Zones.Form = Backbone.View.extend({
   },
 
   render: function() {
-    var kind = this.$('[name="zone[kind]"]:checked').val() || 'state';
-    this.$('[name="zone[kind]"]').val([kind]);
+    var kind = this.$('[name="zone[kind]"]:checked').val() || 'state'
+    this.$('[name="zone[kind]"]').val([kind])
 
-    $('#state_members').toggleClass('hidden', kind !== 'state');
-    $('#state_members :input').prop('disabled', kind !== 'state');
+    $('#state_members').toggleClass('hidden', kind !== 'state')
+    $('#state_members :input').prop('disabled', kind !== 'state')
 
-    $('#country_members').toggleClass('hidden', kind !== 'country');
-    $('#country_members :input').prop('disabled', kind !== 'country');
+    $('#country_members').toggleClass('hidden', kind !== 'country')
+    $('#country_members :input').prop('disabled', kind !== 'country')
   }
 })
