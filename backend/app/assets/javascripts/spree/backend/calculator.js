fb1ca43ad7a4c10579ebd62192d8c4683c25feb5
99cm <zyizyao@china-guide.com>
2018-10-11 11:25:33 -0400

    Update calculator.js

--- backend/app/assets/javascripts/spree/backend/calculator.js
+++ backend/app/assets/javascripts/spree/backend/calculator.js
@@ -1,19 +1,19 @@
 Spree.CalculatorEditView = Backbone.View.extend({
   events: {
-    "change .js-calculator-type": "render",
+    "change .js-calculator-type": 'render',
   },
 
   initialize: function() {
-    this.render();
+    this.render()
   },
 
   render: function() {
-    var selected_class = this.$('.js-calculator-type option:selected').val();
+    var selected_class = this.$('.js-calculator-type option:selected').val()
     this.$('.js-calculator-preferences').each(function() {
-      var selected = ($(this).data('calculator-type') === selected_class);
-      $(this).find(':input').prop("disabled", !selected);
-      $(this).toggle(selected);
-    });
+      var selected = ($(this).data('calculator-type') === selected_class)
+      $(this).find(':input').prop('disabled', !selected)
+      $(this).toggle(selected)
+    })
   }
 })
 
@@ -21,6 +21,6 @@ Spree.ready(function() {
   $('.js-calculator-fields').each(function() {
     new Spree.CalculatorEditView({
       el: this
-    });
-  });
-});
+    })
+  })
+})
