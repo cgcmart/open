1ddee1724229bac8fffbd711b46a59969ad07d6e
99cm <zyizyao@china-guide.com>
2018-10-11 10:43:14 -0400

    Update zone.js

--- backend/app/assets/javascripts/spree/backend/zone.js
+++ backend/app/assets/javascripts/spree/backend/zone.js
@@ -2,7 +2,7 @@ Spree.ready(function(){
   if($('.js-zones-form').length) {
     var view = new Spree.Views.Zones.Form({
       el: $('.js-zones-form')
-    });
+    })
     view.render()
   }
-});
+})
