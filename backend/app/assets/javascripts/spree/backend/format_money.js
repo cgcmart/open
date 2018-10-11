4c440b2b8e95347805204a1848252d619629e30c
99cm <zyizyao@china-guide.com>
2018-10-11 12:24:51 -0400

    Update format_money.js

--- backend/app/assets/javascripts/spree/backend/format_money.js
+++ backend/app/assets/javascripts/spree/backend/format_money.js
@@ -2,10 +2,10 @@
 //= require solidus_admin/accounting
 
 Spree.formatMoney = function(amount, currency) {
-  var currencyInfo = Spree.currencyInfo[currency];
+  var currencyInfo = Spree.currencyInfo[currency]
 
-  var thousand = Spree.t('currency_delimiter');
-  var decimal = Spree.t('currency_separator');
+  var thousand = Spree.t('currency_delimiter')
+  var decimal = Spree.t('currency_separator')
 
-  return accounting.formatMoney(amount, currencyInfo[0], currencyInfo[1], thousand, decimal, currencyInfo[2]);
+  return accounting.formatMoney(amount, currencyInfo[0], currencyInfo[1], thousand, decimal, currencyInfo[2])
 }
