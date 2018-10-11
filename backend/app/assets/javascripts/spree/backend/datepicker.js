1e3f2a23746d5557da4e7d0e9146a29a38175a6f
99cm <zyizyao@china-guide.com>
2018-10-11 12:17:32 -0400

    Update datepicker.js

--- backend/app/assets/javascripts/spree/backend/datepicker.js
+++ backend/app/assets/javascripts/spree/backend/datepicker.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 Spree.ready(function(){
   flatpickr.localize({
@@ -8,11 +8,11 @@ Spree.ready(function(){
     months: {
       longhand: Spree.t('month_names')
     }
-  });
+  })
 
   $('.datepicker').flatpickr({
     allowInput: true
-  });
+  })
 
   // Handle range dates
   if ($('.date-range-filter .datepicker-from, .date-range-filter .datepicker-to').length) {
@@ -23,21 +23,21 @@ Spree.ready(function(){
     // wrong order.
     var swapDates = function($other, otherInstance, compareDates) {
       return function(selectedDates, dateStr, instance) {
-        var date = instance.parseDate(dateStr);
-        var otherDateStr = $other.val();
-        var otherDate = instance.parseDate(otherDateStr);
+        var date = instance.parseDate(dateStr)
+        var otherDateStr = $other.val()
+        var otherDate = instance.parseDate(otherDateStr)
 
         if (date && otherDate && compareDates(date, otherDate)) {
-          instance.setDate(otherDate);
-          otherInstance.setDate(date);
+          instance.setDate(otherDate)
+          otherInstance.setDate(date)
         }
       }
     }
 
-    var $left = $('.date-range-filter .datepicker-from');
-    var $right = $('.date-range-filter .datepicker-to');
-    var leftInstance = $left[0]._flatpickr;
-    var rightInstance = $right[0]._flatpickr;
+    var $left = $('.date-range-filter .datepicker-from')
+    var $right = $('.date-range-filter .datepicker-to')
+    var leftInstance = $left[0]._flatpickr
+    var rightInstance = $right[0]._flatpickr
     var leftSwapDates = swapDates($right, rightInstance, function(date, otherDate) {
       return date > otherDate
     })
@@ -50,6 +50,6 @@ Spree.ready(function(){
 
     // Execute swap dates check in order to correct possible wrong order at page
     // load
-    leftSwapDates(null, $left.val(), leftInstance);
+    leftSwapDates(null, $left.val(), leftInstance)
   }
-});
+})
