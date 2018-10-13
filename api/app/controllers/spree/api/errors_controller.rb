a89965d620bbdf11b6c645ac86c27282cbff5bb8
99cm <zyizyao@china-guide.com>
2018-10-12 23:09:00 -0400

    Create errors_controller.rb

---
+++ api/app/controllers/spree/api/errors_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Spree
+  module Api
+    class ErrorsController < ActionController::Base
+      def render_404
+        render 'spree/api/errors/not_found', status: 404
+      end
+    end
+  end
+end
