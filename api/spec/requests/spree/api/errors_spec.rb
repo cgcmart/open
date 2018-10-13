9b2ce79cbb5a910dc540c8ff07e398868d2ecf10
99cm <zyizyao@china-guide.com>
2018-10-13 11:22:22 -0400

    Create errors_spec.rb

---
+++ api/spec/requests/spree/api/errors_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'API Errors Spec', type: :request do
+  context 'unexisting API route' do
+    it 'returns 404' do
+      get '/api/prods'
+
+      expect(response).to eq('/api/prods.json')
+      follow_redirect!
+
+      expect(response.status).to eq 404
+    end
+  end
+end
