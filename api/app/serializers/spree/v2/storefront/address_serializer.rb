767fa216cec6d2dc8bec9cb1076be2f0c32a13af
99cm <zyizyao@china-guide.com>
2018-10-12 22:45:26 -0400

    Create address_serializer.rb

---
+++ api/app/serializers/spree/v2/storefront/address_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Spree
+  module V2
+    module Storefront
+      class AddressSerializer < BaseSerializer
+        set_type :address
+
+        attributes :firstname, :lastname, :address1, :address2, :city, :zipcode, :phone, :state_name,
+                   :company, :country_name, :country_iso3
+
+        attribute :state_code, &:state_abbr
+      end
+    end
+  end
+end
