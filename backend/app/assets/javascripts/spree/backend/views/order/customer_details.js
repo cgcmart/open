9e7e7d1c164fbefa1521273013a1cdbf10778d86
99cm <zyizyao@china-guide.com>
2018-10-14 12:28:28 -0400

    Update customer_details.js

--- backend/app/assets/javascripts/spree/backend/views/order/customer_details.js
+++ backend/app/assets/javascripts/spree/backend/views/order/customer_details.js
@@ -4,22 +4,22 @@ Spree.Views.Order.CustomerDetails = Backbone.View.extend({
       new Spree.Views.Order.Address({
         model: this.model.get("bill_address"),
         el: this.$('.js-billing-address')
-      });
+      })
 
     this.shipAddressView =
       new Spree.Views.Order.Address({
         model: this.model.get("ship_address"),
         el: this.$('.js-shipping-address')
-      });
+      })
 
     this.customerSelectView =
       new Spree.Views.Order.CustomerSelect({
         el: this.$('#customer_search')
-      });
-    this.listenTo(this.customerSelectView, "select", this.onSelectCustomer);
+      })
+    this.listenTo(this.customerSelectView, "select", this.onSelectCustomer)
 
-    this.onGuestCheckoutChanged();
-    this.onChange();
+    this.onGuestCheckoutChanged()
+    this.onChange()
 
     this.listenTo(this.model, "change", this.render)
     this.render()
@@ -54,14 +54,14 @@ Spree.Views.Order.CustomerDetails = Backbone.View.extend({
 
   render: function() {
     var user_id = this.model.get("user_id") || $("#user_id").val()
-    this.$("#user_id").val(user_id);
+    this.$("#user_id").val(user_id)
     this.$('#guest_checkout_true')
-      .prop("checked", !user_id);
+      .prop("checked", !user_id)
     this.$('#guest_checkout_false')
       .prop("checked", !!user_id)
-      .prop("disabled", !user_id);
+      .prop("disabled", !user_id)
 
-    this.$('#shipping').toggleClass("hidden", !!this.model.get("use_billing"));
+    this.$('#shipping').toggleClass("hidden", !!this.model.get("use_billing"))
     this.$('#order_email').val(this.model.get("email"))
   }
 })
