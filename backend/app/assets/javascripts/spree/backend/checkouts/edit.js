a2c977ebf6cc779d4bf5b7daf034bc5f3bd1a636
99cm <zyizyao@china-guide.com>
2018-10-14 12:32:11 -0400

    Update edit.js

--- backend/app/assets/javascripts/spree/backend/checkouts/edit.js
+++ backend/app/assets/javascripts/spree/backend/checkouts/edit.js
@@ -3,10 +3,10 @@ Spree.ready(function() {
     var order = new Spree.Models.Order({
       bill_address: {},
       ship_address: {}
-    });
+    })
     new Spree.Views.Order.CustomerDetails({
       el: $(".js-customer-details"),
       model: order
-    });
+    })
   }
-});
+})
