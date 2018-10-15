9e37243556a2f2ece98d73a66375db92b520fa6d
99cm <zyizyao@china-guide.com>
2018-10-15 10:34:51 -0400

    Update highlight_negative_numbers.js

--- backend/app/assets/javascripts/spree/backend/highlight_negative_numbers.js
+++ backend/app/assets/javascripts/spree/backend/highlight_negative_numbers.js
@@ -1,14 +1,14 @@
 Spree.ready(function() {
   // Highlight negative numbers in red.
   document.querySelector('body').addEventListener('input', function(e){
-    var el = e.target;
-    var isInputNumber = el instanceof HTMLInputElement && el.type == 'number';
+    var el = e.target
+    var isInputNumber = el instanceof HTMLInputElement && el.type == 'number'
     if (isInputNumber) {
       if (el.value < 0) {
-        el.classList.add("negative");
+        el.classList.add("negative")
       } else {
-        el.classList.remove("negative");
+        el.classList.remove("negative")
       }
     }
-  });
-});
+  })
+})
