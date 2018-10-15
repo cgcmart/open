da70995f402404188f24a5fb13a94aff9a08080d
99cm <zyizyao@china-guide.com>
2018-10-15 10:42:11 -0400

    Update locale_selection.js

--- backend/app/assets/javascripts/spree/backend/locale_selection.js
+++ backend/app/assets/javascripts/spree/backend/locale_selection.js
@@ -1,16 +1,16 @@
 Spree.ready(function() {
-  var localeSelect = document.querySelector('.js-locale-selection');
-  if (!localeSelect) return;
+  var localeSelect = document.querySelector('.js-locale-selection')
+  if (!localeSelect) return
 
   localeSelect.addEventListener('change', function() {
     Spree.ajax({
-      type: "PUT",
-      dataType: "json",
-      url: Spree.pathFor("admin/locale/set"),
+      type: 'PUT',
+      dataType: 'json',
+      url: Spree.pathFor('admin/locale/set'),
       data: { switch_to_locale: localeSelect.value },
       success: function(data) {
-        document.location.href = data.location;
+        document.location.href = data.location
       }
-    });
-  });
-});
+    })
+  })
+})
