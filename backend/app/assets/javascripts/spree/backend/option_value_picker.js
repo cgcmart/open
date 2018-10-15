1e23d1028a0362ad2b1e6fdd5c256a8f9bd33934
99cm <zyizyao@china-guide.com>
2018-10-15 11:22:46 -0400

    Update option_value_picker.js

--- backend/app/assets/javascripts/spree/backend/option_value_picker.js
+++ backend/app/assets/javascripts/spree/backend/option_value_picker.js
@@ -1,45 +1,45 @@
 $.fn.optionValueAutocomplete = function (options) {
-  'use strict';
+  'use strict'
 
   // Default options
   options = options || {}
-  var multiple = typeof(options['multiple']) !== 'undefined' ? options['multiple'] : true;
-  var productSelect = options['productSelect'];
+  var multiple = typeof(options['multiple']) !== 'undefined' ? options['multiple'] : true
+  var productSelect = options['productSelect']
 
-  function formatOptionValue(option_value) {
-    return Select2.util.escapeMarkup(option_value.name);
+  function formatOptionValue(optionValue) {
+    return Select2.util.escapeMarkup(optionValue.name)
   }
 
   this.select2({
     minimumInputLength: 3,
     multiple: multiple,
     initSelection: function (element, callback) {
-      $.get(Spree.routes.option_value_search, {
+      $.get(Spree.routes.option_value_api, {
         ids: element.val().split(','),
         token: Spree.api_key
       }, function (data) {
-        callback(multiple ? data : data[0]);
-      });
+        callback(multiple ? data : data[0])
+      })
     },
     ajax: {
-      url: Spree.routes.option_value_search,
+      url: Spree.routes.option_value_api,
       datatype: 'json',
       data: function (term, page) {
         // Note: This doesn't work. variants_product_id isn't an allowed filter
-        var productId = typeof(productSelect) !== 'undefined' ? $(productSelect).select2('val') : null;
+        var productId = typeof(productSelect) !== 'undefined' ? $(productSelect).select2('val') : null
         return {
           q: {
             name_cont: term,
             variants_product_id_eq: productId
           },
           token: Spree.api_key
-        };
+        }
       },
       results: function (data, page) {
-        return { results: data };
+        return { results: data }
       }
     },
     formatResult: formatOptionValue,
     formatSelection: formatOptionValue
-  });
-};
+  })
+}
