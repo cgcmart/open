fd93063b9d40d0d0232bab00615ca94eff774c63
99cm <zyizyao@china-guide.com>
2018-10-16 13:29:00 -0400

    Update stock_item.js

--- backend/app/assets/javascripts/spree/backend/models/stock_item.js
+++ backend/app/assets/javascripts/spree/backend/models/stock_item.js
@@ -1,6 +1,6 @@
 Spree.Models.StockItem = Backbone.Model.extend({
   paramRoot: 'stock_item',
   urlRoot: function() {
-    return Spree.routes.stock_items_api(this.get('stock_location_id'));
+    return Spree.routes.stock_items_api(this.get('stock_location_id'))
   }
-});
+})
