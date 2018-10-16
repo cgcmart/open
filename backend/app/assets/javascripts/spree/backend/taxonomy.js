133a5a28b1184793d6fbef57a1d84e59a9350de9
99cm <zyizyao@china-guide.com>
2018-10-16 10:23:12 -0400

    Update taxonomy.js

--- backend/app/assets/javascripts/spree/backend/taxonomy.js
+++ backend/app/assets/javascripts/spree/backend/taxonomy.js
@@ -1,8 +1,8 @@
 //= require 'solidus_admin/Sortable'
 
 Handlebars.registerHelper('isRootTaxon', function() {
-  return this.parent_id == null;
-});
+  return this.parent_id == null
+})
 
 var TaxonTreeView = Backbone.View.extend({
   create_taxon: function(attributes) {
@@ -14,7 +14,7 @@ var TaxonTreeView = Backbone.View.extend({
         taxon: attributes
       },
       complete: this.redraw_tree
-    });
+    })
   },
 
   update_taxon: function(id, attributes) {
@@ -26,7 +26,7 @@ var TaxonTreeView = Backbone.View.extend({
         taxon: attributes
       },
       error: this.redraw_tree
-    });
+    })
   },
 
   delete_taxon: function(id) {
@@ -35,11 +35,11 @@ var TaxonTreeView = Backbone.View.extend({
       dataType: "json",
       url: this.model.url() + "/taxons/" + id,
       error: this.redraw_tree
-    });
+    })
   },
 
   render: function() {
-    var taxons_template = HandlebarsTemplates["taxons/tree"];
+    var taxons_template = HandlebarsTemplates["taxons/tree"]
     this.$el.html(taxons_template({
       taxons: [this.model.get("root")]
     }));
@@ -56,14 +56,14 @@ var TaxonTreeView = Backbone.View.extend({
 
     var lists = this.$('ul');
     for(var i = 0; i < lists.length; i++) {
-      new Sortable(lists[i], sortableOptions);
+      new Sortable(lists[i], sortableOptions)
     }
   },
 
   redraw_tree: function() {
     this.model.fetch({
       url: this.model.url() + '?set=nested'
-    });
+    })
   },
 
   handle_move: function(e) {
@@ -71,28 +71,28 @@ var TaxonTreeView = Backbone.View.extend({
     this.update_taxon(el.data('taxon-id'), {
       parent_id: el.parent().closest('li').data('taxon-id'),
       child_index: el.index()
-    });
+    })
   },
 
   handle_delete: function(e) {
     var el;
-    el = $(e.target).closest('li');
+    el = $(e.target).closest('li')
     if (confirm(Spree.translations.are_you_sure_delete)) {
-      this.delete_taxon(el.data('taxon-id'));
-      el.remove();
+      this.delete_taxon(el.data('taxon-id'))
+      el.remove()
     }
   },
 
   handle_add_child: function(e) {
-    var el = $(e.target).closest('li');
-    var parent_id = el.data('taxon-id');
-    this.create_taxon({name: 'New node', parent_id: parent_id, child_index: 0});
+    var el = $(e.target).closest('li')
+    var parent_id = el.data('taxon-id')
+    this.create_taxon({name: 'New node', parent_id: parent_id, child_index: 0})
   },
 
   handle_create: function(e) {
     e.preventDefault();
     var parent_id = this.model.get("root").id;
-    this.create_taxon({name: 'New node', parent_id: parent_id, child_index: 0});
+    this.create_taxon({name: 'New node', parent_id: parent_id, child_index: 0})
   },
 
   events: {
@@ -101,21 +101,21 @@ var TaxonTreeView = Backbone.View.extend({
   },
 
   initialize: function() {
-    _.bindAll(this, 'redraw_tree', 'handle_create');
-    $('.add-taxon-button').on('click', this.handle_create);
-    this.listenTo(this.model, 'sync', this.render);
-    this.redraw_tree();
+    _.bindAll(this, 'redraw_tree', 'handle_create')
+    $('.add-taxon-button').on('click', this.handle_create)
+    this.listenTo(this.model, 'sync', this.render)
+    this.redraw_tree()
   }
-});
+})
 
 Spree.ready(function() {
   if ($('#taxonomy_tree').length) {
     var model = new Spree.Models.Taxonomy({
       id: $('#taxonomy_tree').data("taxonomy-id")
-    });
+    })
     new TaxonTreeView({
       el: $('#taxonomy_tree'),
       model: model
-    });
+    })
   }
-});
+})
