86261976cc038301bfa8369ca85387516e204705
99cm <zyizyao@china-guide.com>
2018-10-17 13:46:15 -0400

    Update shipping_methods_controller_spec.rb

--- backend/spec/controllers/spree/admin/shipping_methods_controller_spec.rb
+++ backend/spec/controllers/spree/admin/shipping_methods_controller_spec.rb
@@ -6,7 +6,7 @@ describe Spree::Admin::ShippingMethodsController, type: :controller do
   stub_authorization!
 
   # Regression test for https://github.com/spree/spree/issues/1240
-  it 'should not hard-delete shipping methods' do
+  it 'does not hard-delete shipping methods' do
     shipping_method = stub_model(Spree::ShippingMethod)
     allow(Spree::ShippingMethod).to receive_messages find: shipping_method
     expect(shipping_method.deleted_at).to be_nil
