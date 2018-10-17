14bfc78e69144b0d4373e488508e3b3008db9387
99cm <zyizyao@china-guide.com>
2018-10-17 11:40:08 -0400

    Update details_adjustments.js

--- backend/app/assets/javascripts/spree/backend/views/order/details_adjustments.js
+++ backend/app/assets/javascripts/spree/backend/views/order/details_adjustments.js
@@ -1,39 +1,39 @@
 Spree.Views.Order.DetailsAdjustments = Backbone.View.extend({
   initialize: function() {
-    this.listenTo(this.model, "change", this.render);
+    this.listenTo(this.model, 'change', this.render)
     this.render()
   },
 
   adjustmentTotals: function() {
-    var totals = {};
-    var collection = this.collection ? this.collection.chain() : _.chain([this.model]);
+    var totals = {}
+    var collection = this.collection ? this.collection.chain() : _.chain([this.model])
     collection
       .map(function(item){ return item.get("adjustments") || [] })
       .flatten(true)
       .each(function(adjustment){
-        var label = adjustment.label;
+        var label = adjustment.label
 
         /* Fixme: because this is done in JS, we only have floating point math */
-        totals[label] = (totals[label] || 0);
-        totals[label] += Number(adjustment.amount);
-      });
-    return totals;
+        totals[label] = (totals[label] || 0)
+        totals[label] += Number(adjustment.amount)
+      })
+    return totals
   },
 
   render: function() {
-    var model = this.model;
-    var tbody = this.$('tbody');
+    var model = this.model
+    var tbody = this.$('tbody')
     var adjustmentTotals = this.adjustmentTotals()
 
     tbody.empty();
     _.each(adjustmentTotals, function(amount, label) {
-       var html = HandlebarsTemplates["orders/details_adjustment_row"]({
+       var html = HandlebarsTemplates['orders/details_adjustment_row']({
          label: label,
          amount: Spree.formatMoney(amount, model.get("currency"))
-       });
-       tbody.append(html);
-    });
+       })
+       tbody.append(html)
+    })
 
-    this.$el.toggleClass("hidden", _.isEmpty(adjustmentTotals));
+    this.$el.toggleClass('hidden', _.isEmpty(adjustmentTotals))
   }
 })
