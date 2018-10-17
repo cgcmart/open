34f12f79c676047f467889a90789e425c504500d
99cm <zyizyao@china-guide.com>
2018-10-17 09:33:24 -0400

    Update activation.js

--- backend/app/assets/javascripts/spree/backend/promotions/activation.js
+++ backend/app/assets/javascripts/spree/backend/promotions/activation.js
@@ -1,26 +1,26 @@
 Spree.PromotionActivationView = Backbone.View.extend({
   events: {
-    "change [name=activation_type]": "render"
+    "change [name=activation_type]": 'render'
   },
 
   initialize: function(){
-    this.render();
+    this.render()
   },
 
   render: function(){
-    var activation_type = this.$("[name=activation_type]:checked").val();
+    var activation_type = this.$("[name=activation_type]:checked").val()
     this.$('[data-activation-type]').each(function(){
-      var selected = ($(this).data('activation-type') === activation_type);
-      $(this).find(':input').prop("disabled", !selected);
-      $(this).toggle(selected);
-    });
+      var selected = ($(this).data('activation-type') === activation_type)
+      $(this).find(':input').prop('disabled', !selected)
+      $(this).toggle(selected)
+    })
   }
-});
+})
 
 Spree.ready(function(){
-  if($("#js_promotion_activation").length) {
+  if($('#js_promotion_activation').length) {
     new Spree.PromotionActivationView({
-      el: $("#js_promotion_activation")
-    });
+      el: $('#js_promotion_activation')
+    })
   }
-});
+})
