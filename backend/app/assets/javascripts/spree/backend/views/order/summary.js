c4d25c60e9bacd9fa4819a785550dfcc34ccb64f
99cm <zyizyao@china-guide.com>
2018-10-17 10:25:39 -0400

    Update summary.js

--- backend/app/assets/javascripts/spree/backend/views/order/summary.js
+++ backend/app/assets/javascripts/spree/backend/views/order/summary.js
@@ -1,14 +1,14 @@
 Spree.Views.Order.Summary = Backbone.View.extend({
   initialize: function () {
-    this.listenTo(this.model, "change", this.render);
+    this.listenTo(this.model, 'change', this.render)
     this.render()
   },
 
   render: function () {
     this.$('dd.order-state').html(this.renderState('order_state', this.model.get("state")))
 
-    this.$("#item_total").text(this.model.get("display_item_total"));
-    this.$("#order_total").text(this.model.get("display_total"));
+    this.$("#item_total").text(this.model.get("display_item_total"))
+    this.$("#order_total").text(this.model.get("display_total"))
 
     this.$('.order-shipment_total').toggleClass("hidden", !Number(this.model.get("ship_total")))
     this.$('dd.order-shipment_total').text(this.model.get("display_ship_total"))
@@ -20,17 +20,17 @@ Spree.Views.Order.Summary = Backbone.View.extend({
     this.$('dd.order-additional_tax_total').text(this.model.get("display_additional_tax_total"))
 
     this.$('.order-shipment_state').toggleClass("hidden", !this.model.get("completed_at"))
-    this.$('dd.order-shipment_state').html(this.renderState('shipment_state', this.model.get("shipment_state")))
+    this.$('dd.order-shipment_state').html(this.renderState('shipment_states', this.model.get("shipment_state")))
 
     this.$('.order-payment_state').toggleClass("hidden", !this.model.get("completed_at"))
-    this.$('dd.order-payment_state').html(this.renderState('payment_state', this.model.get("payment_state")))
+    this.$('dd.order-payment_state').html(this.renderState('payment_states', this.model.get("payment_state")))
   },
 
   renderState: function(translation_key, value) {
-    var state_name = Spree.translations[translation_key][value] || value;
+    var state_name = Spree.translations[translation_key][value] || value
     return $('<span>')
       .addClass('state')
       .addClass(value)
-      .text(state_name);
+      .text(state_name)
   }
-});
+})
