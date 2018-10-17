70dd649c2fb6e9166b4fa82b67601b6cb5fab56b
99cm <zyizyao@china-guide.com>
2018-10-17 16:10:50 -0400

    Update option_types_spec.rb

--- backend/spec/features/admin/products/option_types_spec.rb
+++ backend/spec/features/admin/products/option_types_spec.rb
@@ -5,13 +5,13 @@ require 'spec_helper'
 describe 'Option Types', type: :feature do
   stub_authorization!
 
-  before(:each) do
+  before do
     visit spree.admin_path
     click_nav 'Products'
   end
 
   context 'listing option types' do
-    it 'should list existing option types' do
+    it 'lists existing option types' do
       create(:option_type, name: 'tshirt-color', presentation: 'Color')
       create(:option_type, name: 'tshirt-size', presentation: 'Size')
       click_link 'Option Types'
@@ -25,7 +25,7 @@ describe 'Option Types', type: :feature do
   end
 
   context 'creating a new option type' do
-    it 'should allow an admin to create a new option type', js: true do
+    it 'allows an admin to create a new option type', js: true do
       click_link 'Option Types'
       click_link 'new_option_type_link'
       expect(page).to have_content('New Option Type')
@@ -43,7 +43,7 @@ describe 'Option Types', type: :feature do
   end
 
   context 'editing an existing option type' do
-    it 'should allow an admin to update an existing option type' do
+    it 'allows an admin to update an existing option type' do
       create(:option_type, name: 'tshirt-color', presentation: 'Color')
       create(:option_type, name: 'tshirt-size', presentation: 'Size')
       click_link 'Option Types'
