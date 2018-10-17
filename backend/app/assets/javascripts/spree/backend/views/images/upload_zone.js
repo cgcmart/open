9eb027951c0bc4849a0335c455cb9fdd505ad238
99cm <zyizyao@china-guide.com>
2018-10-17 11:31:55 -0400

    Update upload_zone.js

--- backend/app/assets/javascripts/spree/backend/views/images/upload_zone.js
+++ backend/app/assets/javascripts/spree/backend/views/images/upload_zone.js
@@ -7,33 +7,33 @@ Spree.Views.Images.UploadZone = Backbone.View.extend({
   },
 
   upload: function(file) {
-    var progressModel = new Spree.Models.ImageUpload({file: file});
+    var progressModel = new Spree.Models.ImageUpload({file: file})
 
-    this.collection.add(progressModel);
+    this.collection.add(progressModel)
 
-    progressModel.previewFile();
-    progressModel.uploadFile();
+    progressModel.previewFile()
+    progressModel.uploadFile()
   },
 
   dragClass: 'with-images',
 
   onDragOver: function(e) {
-    this.el.classList.add(this.dragClass);
-    e.preventDefault();
+    this.el.classList.add(this.dragClass)
+    e.preventDefault()
   },
 
   onDragLeave: function() {
-    this.el.classList.remove(this.dragClass);
+    this.el.classList.remove(this.dragClass)
   },
 
   onDrop: function(e) {
-    this.el.classList.remove(this.dragClass);
-    e.preventDefault();
+    this.el.classList.remove(this.dragClass)
+    e.preventDefault()
 
-    _.each(e.originalEvent.dataTransfer.files, this.upload, this);
+    _.each(e.originalEvent.dataTransfer.files, this.upload, this)
   },
 
   onFileBrowserSelect: function(e) {
-    _.each(e.target.files, this.upload, this);
+    _.each(e.target.files, this.upload, this)
   }
-});
+})
