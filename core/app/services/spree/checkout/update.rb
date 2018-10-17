fe105e4c26bd7f612d9f18a62c99471765c60ab0
99cm <zyizyao@china-guide.com>
2018-10-17 17:56:09 -0400

    Create update.rb

---
+++ core/app/services/spree/checkout/update.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Spree
+  module Checkout
+    class Update
+      prepend Spree::ServiceModule::Base
+
+      def call(order:, params:, permitted_attributes:, request_env:)
+        return success(order) if order.update_from_params(params, permitted_attributes, request_env)
+
+        failure(order, order.errors.full_messages.join(', '))
+      end
+    end
+  end
+end
