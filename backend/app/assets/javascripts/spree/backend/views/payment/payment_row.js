e05f3c2dbc52f0b0920cf6c9f5ec268fff0db181
99cm <zyizyao@china-guide.com>
2018-10-17 11:51:13 -0400

    Update payment_row.js

--- backend/app/assets/javascripts/spree/backend/views/payment/payment_row.js
+++ backend/app/assets/javascripts/spree/backend/views/payment/payment_row.js
@@ -8,29 +8,29 @@ Spree.Views.Payment.PaymentRow = Backbone.View.extend({
 
   onEdit: function(e) {
     e.preventDefault();
-    this.$el.addClass("editing");
+    this.$el.addClass('editing')
   },
 
   onCancel: function(e) {
-    e.preventDefault();
-    this.$el.removeClass("editing");
+    e.preventDefault()
+    this.$el.removeClass('editing')
   },
 
   onSave: function(e) {
     var view = this;
-    var amount = this.$(".js-edit-amount").val();
+    var amount = this.$('.js-edit-amount').val()
     var options = {
       success: function(model, response, options) {
-        view.$(".js-display-amount").text(model.attributes.display_amount);
-        view.$el.removeClass("editing");
+        view.$('.js-display-amount').text(model.attributes.display_amount)
+        view.$el.removeClass('editing')
       },
       error: function(model, response, options) {
-        show_flash('error', response.responseJSON.error);
+        show_flash('error', response.responseJSON.error)
       }
-    };
-    e.preventDefault();
+    }
+    e.preventDefault()
     this.model.save({
       amount: amount
-    }, options);
+    }, options)
   }
-});
+})
