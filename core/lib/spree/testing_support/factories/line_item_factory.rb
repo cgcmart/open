21d54a096138af32b11eb16978c2aea80908b536
99cm <zyizyao@china-guide.com>
2018-10-17 17:10:27 -0400

    Update line_item_factory.rb

--- core/lib/spree/testing_support/factories/line_item_factory.rb
+++ core/lib/spree/testing_support/factories/line_item_factory.rb
@@ -6,7 +6,7 @@ require 'spree/testing_support/factories/product_factory'
 FactoryBot.define do
   factory :line_item, class: Spree::LineItem do
     quantity 1
-    price { BigDecimal.new('10.00') }
+    price { BigDecimal('10.00') }
     order
     currency { order.currency }
     transient do
