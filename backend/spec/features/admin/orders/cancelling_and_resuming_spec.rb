3fc296820cf04cd89ce96ed79dcb010889e7742a
99cm <zyizyao@china-guide.com>
2018-10-17 15:12:25 -0400

    Update cancelling_and_resuming_spec.rb

--- backend/spec/features/admin/orders/cancelling_and_resuming_spec.rb
+++ backend/spec/features/admin/orders/cancelling_and_resuming_spec.rb
@@ -6,18 +6,15 @@ describe 'Cancelling + Resuming', type: :feature do
   stub_authorization!
 
   let(:user) { build_stubbed(:user, id: 123, spree_api_key: 'fake') }
-
-  before do
-    allow(user).to receive(:has_spree_role?).and_return(true)
-    allow_any_instance_of(Spree::Admin::BaseController).to receive(:try_spree_current_user).and_return(user)
-  end
-
   let(:order) do
     order = create(:order)
     order.update_columns({ state: 'complete', completed_at: Time.current })
     order
   end
 
+  before do
+    allow_any_instance_of(Spree::Admin::BaseController).to receive(:try_spree_current_user).and_return(user)
+  end
   
   it 'can cancel an order' do
     visit spree.edit_admin_order_path(order.number)
