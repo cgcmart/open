b98349b839b1db5ed32e0adf81a673ccfd44ab26
99cm <zyizyao@china-guide.com>
2018-10-17 11:34:59 -0400

    Update address.js

--- backend/app/assets/javascripts/spree/backend/views/order/address.js
+++ backend/app/assets/javascripts/spree/backend/views/order/address.js
@@ -1,16 +1,16 @@
 Spree.Views.Order.Address = Backbone.View.extend({
   initialize: function(options) {
     // read initial values from page
-    this.onChange();
+    this.onChange()
 
-    this.render();
-    this.listenTo(this.model, "change", this.render);
+    this.render()
+    this.listenTo(this.model, "change", this.render)
 
     this.stateSelect =
       new Spree.Views.StateSelect({
         model: this.model,
         el: this.$el
-      });
+      })
   },
 
   events: {
@@ -22,27 +22,27 @@ Spree.Views.Order.Address = Backbone.View.extend({
   },
 
   eachField: function(callback){
-    var view = this;
+    var view = this
     var fields = ["firstname", "lastname", "company", "address1", "address2",
-      "city", "zipcode", "phone", "country_id"];
+      "city", "zipcode", "phone", "country_id"]
     _.each(fields, function(field) {
-      var el = view.$('[name$="[' + field + ']"]');
-      if (el.length) callback(field, el);
-    });
+      var el = view.$('[name$="[' + field + ']"]')
+      if (el.length) callback(field, el)
+    })
   },
 
   getValues: function() {
-    var attributes = {};
+    var attributes = {}
     this.eachField(function(name, el) {
-      attributes[name] = el.val();
-    });
-    return attributes;
+      attributes[name] = el.val()
+    })
+    return attributes
   },
 
   render: function() {
-    var model = this.model;
+    var model = this.model
     this.eachField(function(name, el) {
       el.val(model.get(name))
     })
   }
-});
+})
