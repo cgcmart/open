780e06aced2e8e55ee64205e7feddadc44867f51
99cm <zyizyao@china-guide.com>
2018-10-21 11:47:54 -0400

    Update select2_locale_zh-CN.js

--- backend/vendor/assets/javascripts/solidus_admin/select2_locales/select2_locale_zh-CN.js
+++ backend/vendor/assets/javascripts/solidus_admin/select2_locales/select2_locale_zh-CN.js
@@ -2,7 +2,7 @@
  * Select2 Chinese translation
  */
 (function ($) {
-    "use strict";
+  'use strict'
     $.fn.select2.locales['zh-CN'] = {
         formatNoMatches: function () { return "没有找到匹配项"; },
         formatInputTooShort: function (input, min) { var n = min - input.length; return "请再输入" + n + "个字符";},
@@ -10,7 +10,7 @@
         formatSelectionTooBig: function (limit) { return "你只能选择最多" + limit + "项"; },
         formatLoadMore: function (pageNumber) { return "加载结果中…"; },
         formatSearching: function () { return "搜索中…"; }
-    };
+    }
 
-    $.extend($.fn.select2.defaults, $.fn.select2.locales['zh-CN']);
-})(jQuery);
+    $.extend($.fn.select2.defaults, $.fn.select2.locales['zh-CN'])
+})(jQuery)
