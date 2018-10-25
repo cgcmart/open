bdf05d42ab4297187eba36ace7b5ec5790a3715a
99cm <zyizyao@china-guide.com>
2018-10-25 10:56:03 -0400

    Update stores_spec.rb

--- backend/spec/features/admin/stores_spec.rb
+++ backend/spec/features/admin/stores_spec.rb
@@ -8,14 +8,14 @@ describe 'Stores', type: :feature do
   context 'when adding a store' do
     before { visit spree.new_admin_store_path }
 
-    it 'admin should be able to set the default_currency' do
+    it 'admin is able to set the default_currency' do
       expect(find('#store_default_currency').value).to eq ''
 
-      fill_in 'store_name', with: 'Solidus Store'
-      fill_in 'store_code', with: 'solidus'
-      fill_in 'store_url', with: 'example.solidus.io'
-      fill_in 'store_mail_from_address', with: 'from@solidus.io'
-      select 'EUR', from: 'store_default_currency'
+      fill_in 'store_name', with: 'CGCMart Store'
+      fill_in 'store_code', with: 'cgcmart'
+      fill_in 'store_url', with: 'example.cgcmart.com'
+      fill_in 'store_mail_from_address', with: 'from@cgcmart.com'
+      select 'CNY', from: 'store_default_currency'
       click_button 'Create'
 
       @store = Spree::Store.last
@@ -28,7 +28,7 @@ describe 'Stores', type: :feature do
     let(:store) { create :store, default_currency: 'AUD' }
     before { visit spree.edit_admin_store_path(store) }
 
-    it 'admin should be able to change the default_currency' do
+    it 'admin is able to change the default_currency' do
       select 'EUR', from: 'store_default_currency'
       click_button 'Update'
       expect(store.reload.default_currency).to eq 'EUR'
