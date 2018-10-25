2683c04382ee7f1f255c882e7552e0e353be2f7c
99cm <zyizyao@china-guide.com>
2018-10-25 11:01:56 -0400

    Update migrate_shipping_rate_taxes_spec.rb

--- core/spec/lib/tasks/migrations/migrate_shipping_rate_taxes_spec.rb
+++ core/spec/lib/tasks/migrations/migrate_shipping_rate_taxes_spec.rb
@@ -2,12 +2,12 @@
 
 require 'rails_helper'
 
-RSpec.describe 'solidus:migrations:migrate_shipping_rate_taxes' do
+RSpec.describe 'open:migrations:migrate_shipping_rate_taxes' do
   describe 'up' do
     include_context(
       'rake',
       task_path: Spree::Core::Engine.root.join('lib/tasks/migrations/migrate_shipping_rate_taxes.rake'),
-      task_name: 'solidus:migrations:migrate_shipping_rate_taxes:up',
+      task_name: 'open:migrations:migrate_shipping_rate_taxes:up',
     )
 
     it 'runs' do
