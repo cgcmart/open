5d8dc8c3799b8ad340758decf2309475a7c14802
99cm <zyizyao@china-guide.com>
2018-10-28 13:16:09 -0400

    Add Spree.open_version
    Spree.version is no longer appropriate as we are not spree, have tremendous commits since a fork of Spree 3.7, and prefer a
    different versioning scheme, following up newer Ruby and Rails versions.

--- core/lib/spree/core/version.rb
+++ core/lib/spree/core/version.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 module Spree
-  def self.version
-    '3.7.0'
+  def self.open_version
+    "2.5.0"
+  end
+
+  def self.open_gem_version
+    Gem::Version.new(open_version)
   end
 end
