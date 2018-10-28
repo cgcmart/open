a871c7d9686a125187fec81ec4b80973ebc67a9d
99cm <zyizyao@china-guide.com>
2018-10-27 22:31:06 -0400

    Update spree.rb

--- lib/spree.rb
+++ lib/spree.rb
@@ -1,15 +1,3 @@
-require 'spree_core'
-require 'spree_api'
-require 'spree_backend'
-require 'spree_frontend'
-require 'spree_sample'
+# frozen_string_literal: true
 
-begin
-  require 'protected_attributes'
-  puts '*' * 75
-  puts '[FATAL] Spree does not work with the protected_attributes gem installed!'
-  puts 'You MUST remove this gem from your Gemfile. It is incompatible with Spree.'
-  puts '*' * 75
-  exit
-rescue LoadError
-end
+require 'open'
