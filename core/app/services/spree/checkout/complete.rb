da1be9351686085fbf5fee8fcaacea92719176ff
Leo Wang <zyizyao@china-guide.com>
2018-11-14 11:33:13 -0500

    Update complete.rb

--- core/app/services/spree/checkout/complete.rb
+++ core/app/services/spree/checkout/complete.rb
@@ -11,7 +11,7 @@ module Spree
         if order.reload.complete?
           success(order)
         else
-          failure(order, order.errors.full_messages.join(', '))
+          failure(order)
         end
       end
 
