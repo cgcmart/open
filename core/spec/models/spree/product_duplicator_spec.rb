f76e2b2c1aaf353ddb4e3ba305f2883b5e6f2f12
Leo Wang <zyizyao@china-guide.com>
2018-11-15 11:27:53 -0500

    Update product_duplicator_spec.rb

--- core/spec/models/spree/product_duplicator_spec.rb
+++ core/spec/models/spree/product_duplicator_spec.rb
@@ -4,18 +4,26 @@ require 'rails_helper'
 
 module Spree
   RSpec.describe Spree::ProductDuplicator, type: :model do
-    let(:product) { create(:product, properties: [create(:property, name: 'MyProperty')]) }
+    let!(:product) { create(:product, properties: [create(:property, name: 'MyProperty')]) }
     let!(:duplicator) { Spree::ProductDuplicator.new(product) }
 
-    let(:image) { File.open(File.expand_path('../../fixtures/thinking-cat.jpg', __dir__)) }
-    let(:params) { { viewable_id: product.master.id, viewable_type: 'Spree::Variant', attachment: image, alt: 'position 1', position: 1 } }
+    let(:file) { File.open(File.expand_path('../../fixtures/thinking-cat.jpg', __dir__)) }
+    let(:params) { { viewable_id: product.master.id, viewable_type: 'Spree::Variant', attachment: file, alt: 'position 1', position: 1 } }
 
     before do
-      Spree::Image.create(params)
+      new_image = Spree::Image.new(params)
+      new_image.attachment.attach(io: file, filename: File.basename(file))
+      new_image.save!
     end
 
     it 'will duplicate the product' do
       expect { duplicator.duplicate }.to change { Spree::Product.count }.by(1)
+    end
+
+    it 'will duplicate already duplicated product' do
+      Timecop.scale(3600)
+      expect { 3.times { duplicator.duplicate } }.to change { Spree::Product.count }.by(3)
+      Timecop.return
     end
 
     context 'when image duplication enabled' do
