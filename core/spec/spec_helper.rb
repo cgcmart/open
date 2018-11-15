0f0409473e7c1d05f4e32ffedde738ca5ce28c6d
Leo Wang <zyizyao@china-guide.com>
2018-11-15 12:30:23 -0500

    Update spec_helper.rb

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -9,7 +9,7 @@ if ENV['COVERAGE']
     add_group 'Services', 'app/services'
     add_group 'Sorters', 'app/sorters'
     add_group 'Validators', 'app/validators'
-    add_group 'Libraries', 'lib'
+    add_group 'Libraries', 'lib/spree'
 
     add_filter '/bin/'
     add_filter '/db/'
