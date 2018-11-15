7889d8dc1731fc22863422d05182364afa07e2e1
Leo Wang <zyizyao@china-guide.com>
2018-11-15 17:33:56 -0500

    Removed deprecated current_currency

--- core/spec/lib/spree/core/controller_helpers/pricing_spec.rb
+++ core/spec/lib/spree/core/controller_helpers/pricing_spec.rb
@@ -13,27 +13,6 @@ RSpec.describe Spree::Core::ControllerHelpers::Pricing, type: :controller do
     allow(controller).to receive(:current_store).and_return(store)
   end
 
-  describe '#current_currency' do
-    subject { controller.current_currency }
-
-    context "when store default_currency is nil" do
-      let(:store) { nil }
-      it { Spree::Deprecation.silence { is_expected.to eq('USD') } }
-    end
-
-    context "when the current store default_currency empty" do
-      let(:store) { FactoryBot.create :store, default_currency: '' }
-
-      it { Spree::Deprecation.silence { is_expected.to eq('USD') } }
-    end
-
-    context "when the current store default_currency is a currency" do
-      let(:store) { FactoryBot.create :store, default_currency: 'EUR' }
-
-      it { Spree::Deprecation.silence { is_expected.to eq('EUR') } }
-    end
-  end
-
   describe '#current_pricing_options' do
     subject { controller.current_pricing_options }
 
