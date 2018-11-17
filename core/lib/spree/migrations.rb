7ba59e25bb6fb9eeffc4d87762d1ecb91753d18a
Leo Wang <zyizyao@china-guide.com>
2018-11-17 17:42:26 -0500

    Update migrations.rb

--- core/lib/spree/migrations.rb
+++ core/lib/spree/migrations.rb
@@ -27,6 +27,7 @@ module Spree
         engine_in_app = app_migrations.map do |file_name|
           name, engine = file_name.split('.', 2)
           next unless match_engine?(engine)
+
           name
         end.compact
 
@@ -54,6 +55,7 @@ module Spree
     def app_migrations
       Dir.entries(app_dir).map do |file_name|
         next if ['.', '..'].include? file_name
+
         name = file_name.split('_', 2).last
         name.empty? ? next : name
       end.compact! || []
