039f30600d3c9ebc443e00eef94f911464d62e00
Leo Wang <zyizyao@china-guide.com>
2018-11-17 17:04:25 -0500

    Update dummy_generator.rb

--- core/lib/generators/spree/dummy/dummy_generator.rb
+++ core/lib/generators/spree/dummy/dummy_generator.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-require "rails/generators/rails/app/app_generator"
+require 'rails/generators/rails/app/app_generator'
 require 'active_support/core_ext/hash'
 require 'spree/core/version'
 
 module Spree
   # @private
   class DummyGenerator < Rails::Generators::Base
-    desc "Creates blank Rails application, installs Open and all sample data"
+    desc 'Creates blank Rails application, installs Open and all sample data'
 
     class_option :lib_name, default: ''
     class_option :database, default: ''
 
     def self.source_paths
-      paths = self.superclass.source_paths
+      paths = superclass.source_paths
       paths << File.expand_path('templates', __dir__)
       paths.flatten
     end
@@ -43,21 +43,22 @@ module Spree
       opts[:skip_test] = true
       opts[:skip_yarn] = true
 
-      puts "Generating dummy Rails application..."
+      puts 'Generating dummy Rails application...'
       invoke Rails::Generators::AppGenerator,
-        [ File.expand_path(dummy_path, destination_root) ], opts
+        [File.expand_path(dummy_path, destination_root)], opts
     end
 
     def test_dummy_config
       @lib_name = options[:lib_name]
       @database = options[:database]
 
-      template "rails/database.yml", "#{dummy_path}/config/database.yml", force: true
-      template "rails/boot.rb", "#{dummy_path}/config/boot.rb", force: true
-      template "rails/application.rb.tt", "#{dummy_path}/config/application.rb", force: true
-      template "rails/routes.rb", "#{dummy_path}/config/routes.rb", force: true
-      template "rails/test.rb", "#{dummy_path}/config/environments/test.rb", force: true
-      template "rails/script/rails", "#{dummy_path}/spec/dummy/script/rails", force: true
+      template 'rails/database.yml', "#{dummy_path}/config/database.yml", force: true
+      template 'rails/boot.rb', "#{dummy_path}/config/boot.rb", force: true
+      template 'rails/application.rb.tt', "#{dummy_path}/config/application.rb", force: true
+      template 'rails/routes.rb', "#{dummy_path}/config/routes.rb", force: true
+      template 'rails/test.rb', "#{dummy_path}/config/environments/test.rb", force: true
+      template 'rails/script/rails', "#{dummy_path}/spec/dummy/script/rails", force: true
+      template 'initializers/devise.rb', "#{dummy_path}/config/initializers/devise.rb", force: true
     end
 
     def test_dummy_inject_extension_requirements
@@ -72,31 +73,31 @@ module Spree
 
     def test_dummy_clean
       inside dummy_path do
-        remove_file ".gitignore"
-        remove_file "doc"
-        remove_file "Gemfile"
-        remove_file "lib/tasks"
-        remove_file "app/assets/images/rails.png"
-        remove_file "app/assets/javascripts/application.js"
-        remove_file "public/index.html"
-        remove_file "public/robots.txt"
-        remove_file "README"
-        remove_file "test"
-        remove_file "vendor"
-        remove_file "spec"
+        remove_file '.gitignore'
+        remove_file 'doc'
+        remove_file 'Gemfile'
+        remove_file 'lib/tasks'
+        remove_file 'app/assets/images/rails.png'
+        remove_file 'app/assets/javascripts/application.js'
+        remove_file 'public/index.html'
+        remove_file 'public/robots.txt'
+        remove_file 'README'
+        remove_file 'test'
+        remove_file 'vendor'
+        remove_file 'spec'
       end
     end
 
     def inject_content_security_policy
       inside dummy_path do
         inject_into_file 'config/initializers/content_security_policy.rb', %Q[
-  p.script_src  :self, :https, :unsafe_inline, :http, :unsafe_eval
+          p.script_src  :self, :https, :unsafe_inline, :http, :unsafe_eval
         ], before: /^end/, verbose: true
       end
     end
 
-    attr :lib_name
-    attr :database
+    attr_reader :lib_name
+    attr_reader :database
 
     protected
 
@@ -121,13 +122,13 @@ end
     def application_definition
       @application_definition ||= begin
         dummy_application_path = File.expand_path("#{dummy_path}/config/application.rb", destination_root)
-        unless options[:pretend] || !File.exists?(dummy_application_path)
+        unless options[:pretend] || !File.exist?(dummy_application_path)
           contents = File.read(dummy_application_path)
           contents[(contents.index("module #{module_name}"))..-1]
         end
       end
     end
-    alias :store_application_definition! :application_definition
+    alias store_application_definition! application_definition
 
     def camelized
       @camelized ||= name.gsub(/\W/, '_').squeeze('_').camelize
@@ -138,7 +139,7 @@ end
     end
 
     def gemfile_path
-      core_gems = ["spree/core", "spree/api", "spree/backend", "spree/frontend"]
+      core_gems = ['spree/core', 'spree/api', 'spree/backend', 'spree/frontend']
 
       if core_gems.include?(lib_name)
         '../../../../../Gemfile'
