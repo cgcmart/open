d85b55f783b9284390944c65f8e4a33f34568b1b
Leo Wang <zyizyao@china-guide.com>
2018-11-19 11:50:00 -0500

    Update updating_spec.rb

--- core/spec/models/spree/order/updating_spec.rb
+++ core/spec/models/spree/order/updating_spec.rb
@@ -5,11 +5,13 @@ require 'rails_helper'
 RSpec.describe Spree::Order, type: :model do
   let(:order) { create(:order) }
 
-  context "#update!" do
+  context '#update!' do
     context 'when there are update hooks' do
       before { Spree::Order.register_update_hook :foo }
+
       after { Spree::Order.update_hooks.clear }
-      it 'should call each of the update hooks' do
+
+      it 'calls each of the update hooks' do
         expect(order).to receive :foo
         order.recalculate
       end
