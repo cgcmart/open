4553cc2f63e95695c8764f08085568d921bb5d17
Leo Wang <zyizyao@china-guide.com>
2018-11-19 10:45:37 -0500

    Update tiered_flat_rate_spec.rb

--- core/spec/models/spree/calculator/tiered_flat_rate_spec.rb
+++ core/spec/models/spree/calculator/tiered_flat_rate_spec.rb
@@ -14,11 +14,13 @@ RSpec.describe Spree::Calculator::TieredFlatRate, type: :model do
     context 'when tiers is a hash' do
       context 'and the key is not a positive number' do
         before { calculator.preferred_tiers = { 'nope' => 20 } }
+
         it { is_expected.to be false }
       end
 
       context 'and the key is an integer' do
         before { calculator.preferred_tiers = { 20 => 20 } }
+
         it 'converts successfully' do
           is_expected.to be true
           expect(calculator.preferred_tiers).to eq({ BigDecimal('20') => BigDecimal('20') })
@@ -27,6 +29,7 @@ RSpec.describe Spree::Calculator::TieredFlatRate, type: :model do
 
       context 'and the key is a float' do
         before { calculator.preferred_tiers = { 20.5 => 20.5 } }
+
         it 'converts successfully' do
           is_expected.to be true
           expect(calculator.preferred_tiers).to eq({ BigDecimal('20.5') => BigDecimal('20.5') })
@@ -35,6 +38,7 @@ RSpec.describe Spree::Calculator::TieredFlatRate, type: :model do
 
       context 'and the key is a string number' do
         before { calculator.preferred_tiers = { '20' => 20 } }
+
         it 'converts successfully' do
           is_expected.to be true
           expect(calculator.preferred_tiers).to eq({ BigDecimal('20') => BigDecimal('20') })
@@ -43,6 +47,7 @@ RSpec.describe Spree::Calculator::TieredFlatRate, type: :model do
 
       context 'and the key is a numeric string with spaces' do
         before { calculator.preferred_tiers = { ' 20 ' => 20 } }
+
         it 'converts successfully' do
           is_expected.to be true
           expect(calculator.preferred_tiers).to eq({ BigDecimal('20') => BigDecimal('20') })
@@ -51,6 +56,7 @@ RSpec.describe Spree::Calculator::TieredFlatRate, type: :model do
 
       context 'and the key is a string number with decimals' do
         before { calculator.preferred_tiers = { '20.5' => 20.5 } }
+
         it 'converts successfully' do
           is_expected.to be true
           expect(calculator.preferred_tiers).to eq({ BigDecimal('20.5') => BigDecimal('20.5') })
