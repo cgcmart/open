6d3ac0352fd473e47348cacc076aad91cc3b567c
Leo Wang <zyizyao@china-guide.com>
2018-11-19 11:41:04 -0500

    Update state_machine_spec.rb

--- core/spec/models/spree/order/state_machine_spec.rb
+++ core/spec/models/spree/order/state_machine_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Spree::Order, type: :model do
           create(:payment, state: 'checkout', order: order)
         end
 
-        it 'should finalize order when transitioning to complete state' do
+        it 'finalizes order when transitioning to complete state' do
           order.complete!
           expect(order).to be_complete
           expect(order).to be_completed
@@ -28,7 +28,7 @@ RSpec.describe Spree::Order, type: :model do
             create(:payment, :failing, state: 'checkout', order: order)
           end
 
-          it 'should not complete the order' do
+          it 'does not complete the order' do
             expect(order.complete).to be false
             expect(order.state).to eq('confirm')
           end
@@ -36,15 +36,15 @@ RSpec.describe Spree::Order, type: :model do
       end
     end
 
-    context "when current state is address" do
+    context 'when current state is address' do
       before do
         order.ensure_updated_shipments
         order.next!
         expect(order.all_adjustments).to be_empty
-        expect(order.state).to eq "address"
+        expect(order.state).to eq 'address'
       end
 
-      it "adjusts tax rates when transitioning to delivery" do
+      it 'adjusts tax rates when transitioning to delivery' do
         expect(Spree::TaxCalculator::Default).to receive(:new).once.with(order).and_call_original
         order.next!
       end
@@ -56,7 +56,7 @@ RSpec.describe Spree::Order, type: :model do
     states = [:pending, :backorder, :ready]
 
     states.each do |shipment_state|
-      it "should be true if shipment_state is #{shipment_state}" do
+      it "is true if shipment_state is #{shipment_state}" do
         expect(order).to be_completed
         order.shipment_state = shipment_state
 
@@ -65,7 +65,7 @@ RSpec.describe Spree::Order, type: :model do
     end
 
     (Spree::Shipment.state_machine.states.keys - states).each do |shipment_state|
-      it "should be false if shipment_state is #{shipment_state}" do
+      it "is false if shipment_state is #{shipment_state}" do
         expect(order).to be_completed
         order.shipment_state = shipment_state
         expect(order).not_to be_can_cancel
@@ -77,27 +77,27 @@ RSpec.describe Spree::Order, type: :model do
     let!(:order) { create(:completed_order_with_totals) }
     let!(:shipment) { order.shipments.first }
 
-    it "is setup correctly" do
+    it 'is setup correctly' do
       expect(order).to be_completed
       expect(order).to be_complete
       expect(order).to be_allow_cancel
     end
 
-    it 'should send a cancel email' do
+    it 'sends a cancel email' do
       perform_enqueued_jobs do
         order.cancel!
       end
 
       mail = ActionMailer::Base.deliveries.last
-      expect(mail.subject).to include "Cancellation"
+      expect(mail.subject).to include 'Cancellation'
     end
 
     context 'resets payment state' do
-      let!(:payment) { create(:payment, order: order, amount: order.total, state: "completed") }
+      let!(:payment) { create(:payment, order: order, amount: order.total, state: 'completed') }
 
-      context "without shipped items" do
-        it "should set payment state to 'void'" do
-          expect { order.cancel! }.to change{ order.reload.payment_state }.to("void")
+      context 'without shipped items' do
+        it "sets payment state to 'void'" do
+          expect { order.cancel! }.to change{ order.reload.payment_state }.to('void')
         end
       end
 
@@ -106,16 +106,16 @@ RSpec.describe Spree::Order, type: :model do
           order.shipments[0].ship!
         end
 
-        it 'should not alter the payment state' do
+        it 'does not alter the payment state' do
           expect(order).to_not be_allow_cancel
           expect(order.cancel).to be false
           expect(order.payment_state).to eql 'paid'
         end
       end
 
-      it 'should automatically refund all payments' do
+      it 'automatically refunds all payments' do
         expect(order).to be_allow_cancel
-        expect { order.cancel! }.to change{ payment.reload.state }.to("void")
+        expect { order.cancel! }.to change{ payment.reload.state }.to('void')
       end
     end
   end
