ed5cfd03b0e7cd7c5d3eca85c292fefe09f0709c
Leo Wang <zyizyao@china-guide.com>
2018-11-23 11:57:19 -0500

    Update reimbursements_spec.rb

--- backend/spec/features/admin/reimbursements_spec.rb
+++ backend/spec/features/admin/reimbursements_spec.rb
@@ -6,12 +6,12 @@ describe 'Promotions', type: :feature do
   stub_authorization!
   let!(:reimbursement) { create(:reimbursement) }
 
-  it "should display the reimbursements table" do
+  it 'displays the reimbursements table' do
     visit spree.admin_order_reimbursement_path(reimbursement.order, reimbursement)
     expect(page).to have_css('table thead tr th', text: 'Product')
     expect(page).to have_css('table thead tr th', text: 'Preferred Reimbursement Type')
     expect(page).to have_css('table thead tr th', text: 'Reimbursement Type Override')
-    expect(page).to have_css('table thead tr th', text: 'Exchange for')
+    expect(page).to have_css('table thead tr th', text: 'Exchange For')
     expect(page).to have_css('table thead tr th', text: 'Amount Before Sales Tax')
     expect(page).to have_css('table thead tr th', text: 'Total')
   end
