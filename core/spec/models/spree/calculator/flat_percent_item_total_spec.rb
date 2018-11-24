4a5ca45edaaa3dcf6f930e5f589378476fad9c91
Leo Wang <zyizyao@china-guide.com>
2018-11-24 17:31:00 -0500

    Update flat_percent_item_total_spec.rb

--- core/spec/models/spree/calculator/flat_percent_item_total_spec.rb
+++ core/spec/models/spree/calculator/flat_percent_item_total_spec.rb
@@ -5,19 +5,27 @@ require 'shared_examples/calculator_shared_examples'
 
 RSpec.describe Spree::Calculator::FlatPercentItemTotal, type: :model do
   let(:calculator) { Spree::Calculator::FlatPercentItemTotal.new }
-  let(:line_item) { mock_model Spree::LineItem }
+  let(:line_item) { create(:line_item) }
 
   it_behaves_like 'a calculator with a description'
 
   before { allow(calculator).to receive_messages preferred_flat_percent: 10 }
 
   context 'compute' do
-    it 'should round result correctly' do
+    it 'rounds result correctly' do
       allow(line_item).to receive_messages amount: 31.08
       expect(calculator.compute(line_item)).to eq 3.11
 
       allow(line_item).to receive_messages amount: 31.00
       expect(calculator.compute(line_item)).to eq 3.10
+    end
+
+    it 'rounds result based on order currency' do
+      line_item.order.currency = 'JPY'
+      allow(line_item).to receive_messages amount: 31.08
+      expect(calculator.compute(line_item)).to eq 3
+      allow(line_item).to receive_messages amount: 31.00
+      expect(calculator.compute(line_item)).to eq 3
     end
 
     it 'returns object.amount if computed amount is greater' do
