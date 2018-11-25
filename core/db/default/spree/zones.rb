babec9e6116b64f475857661206761835e69e4d5
Leo Wang <zyizyao@china-guide.com>
2018-11-25 18:31:08 -0500

    Update zones.rb

--- core/db/default/spree/zones.rb
+++ core/db/default/spree/zones.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-north_america = Spree::Zone.create!(name: "North America", description: "USA + Canada", kind: 'country')
+north_america = Spree::Zone.where(name: 'North America', description: 'USA + Canada', kind: 'country').first_or_create!
 
 %w(US CA).each do |name|
-  north_america.zone_members.create!(zoneable: Spree::Country.find_by!(iso: name))
+  north_america.zone_members.where(zoneable: Spree::Country.find_by!(iso: name)).first_or_create!
 end
