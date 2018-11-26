8a51137c926b3f3b1e6066acc5c93841d6edeadb
Leo Wang <zyizyao@china-guide.com>
2018-11-25 19:01:13 -0500

    Update stores.rb

--- core/db/default/spree/stores.rb
+++ core/db/default/spree/stores.rb
@@ -2,9 +2,9 @@
 
 unless Spree::Store.where(code: 'sample-store').exists?
   Spree::Store.create!(
-    name: "Sample Store",
-    code: "sample-store",
-    url: "example.com",
-    mail_from_address: "store@example.com"
+    name: 'Sample Store',
+    code: 'sample-store',
+    url: 'example.com',
+    mail_from_address: 'store@example.com'
   )
 end
