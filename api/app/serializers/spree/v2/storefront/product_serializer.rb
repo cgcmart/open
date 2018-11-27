a6aaac8a7cd87bb27f562b5423b0aa8ea81de857
Leo Wang <zyizyao@china-guide.com>
2018-11-27 14:34:53 -0500

    Update product_serializer.rb

--- api/app/serializers/spree/v2/storefront/product_serializer.rb
+++ api/app/serializers/spree/v2/storefront/product_serializer.rb
@@ -10,19 +10,19 @@ module Spree
                    :available_on, :slug, :meta_description, :meta_keywords,
                    :updated_at
 
-        attribute :purchasable,   &:purchasable?
-        attribute :in_stock,      &:in_stock?
+        attribute :purchasable, &:purchasable?
+        attribute :in_stock, &:in_stock?
         attribute :backorderable, &:backorderable?
 
         has_many :variants
         has_many :option_types
         has_many :product_properties
 
-        has_one  :default_variant,
-          object_method_name: :master,
-          id_method_name:     :master_id,
-          record_type:        :variant,
-          serializer:         :variant
+        has_one :default_variant,
+          object_method_name: :default_variant,
+          id_method_name: :default_variant_id,
+          record_type: :variant,
+          serializer: :variant
       end
     end
   end
