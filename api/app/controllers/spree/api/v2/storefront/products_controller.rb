5d992b20ebf006beeb130460ee7f9b98672ce108
Leo Wang <zyizyao@china-guide.com>
2018-11-27 10:59:36 -0500

    Update products_controller.rb

--- api/app/controllers/spree/api/v2/storefront/products_controller.rb
+++ api/app/controllers/spree/api/v2/storefront/products_controller.rb
@@ -69,10 +69,6 @@ module Spree
             Spree::Product.includes(scope_includes)
           end
 
-          def resource_includes
-            request_includes || default_resource_includes
-          end
-
           def default_resource_includes
             %i[
               variants
