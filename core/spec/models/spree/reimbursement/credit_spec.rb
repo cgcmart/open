c7aa1ff8499352d4db925810e1a2093dc8c76c86
Leo Wang <zyizyao@china-guide.com>
2018-12-01 21:19:57 -0500

    Update credit_spec.rb

--- core/spec/models/spree/reimbursement/credit_spec.rb
+++ core/spec/models/spree/reimbursement/credit_spec.rb
@@ -13,7 +13,7 @@ module Spree
 
         before { allow(reimbursement).to receive(:credits).and_return([credit_double, credit_double]) }
 
-        it 'should sum the amounts of all of the reimbursements credits' do
+        it 'sums the amounts of all of the reimbursements credits' do
           expect(subject).to eq BigDecimal('199.98')
         end
       end
@@ -22,7 +22,7 @@ module Spree
     describe '#description' do
       let(:credit) { Spree::Reimbursement::Credit.new(amount: 100, creditable: mock_model(Spree::PaymentMethod::Check)) }
 
-      it "should be the creditable's class name" do
+      it "is the creditable's class name" do
         expect(credit.description).to eq 'Check'
       end
     end
@@ -30,7 +30,7 @@ module Spree
     describe '#display_amount' do
       let(:credit) { Spree::Reimbursement::Credit.new(amount: 100) }
 
-      it 'should be a money object' do
+      it 'is a money object' do
         expect(credit.display_amount).to eq Spree::Money.new(100, currency: 'USD')
       end
     end
