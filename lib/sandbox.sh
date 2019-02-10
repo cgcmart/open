d24fd29b21f2157ea442e4df4a4fbfe7a6c1fa0a
99cm <dotcom@china-guide.com>
2019-02-10 08:54:39 -0500

    Ensure sqlite3 version to match ActiveRecord SQLite adapter requirement

--- lib/sandbox.sh
+++ lib/sandbox.sh
@@ -50,6 +50,7 @@ group :test, :development do
 end
 RUBY
 
+sed -i -e "s/gem 'sqlite3'/gem 'sqlite3', '~> 1.3.6'/g" Gemfile
 bundle install --gemfile Gemfile
 bundle exec rails db:drop || true
 bundle exec rails db:create
