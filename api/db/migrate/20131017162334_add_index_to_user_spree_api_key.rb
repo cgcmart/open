827dfc35f3b3363faef782c7d49f16b726d80b7c
99cm <dotcom@china-guide.com>
2019-02-24 01:10:54 -0500

    Update migration

--- api/db/migrate/20131017162334_add_index_to_user_spree_api_key.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddIndexToUserSpreeApiKey < ActiveRecord::Migration[5.2]
-  def change
-    unless defined?(User)
-      add_index :spree_users, :spree_api_key
-    end
-  end
-end
--- api/db/migrate/20190224010301_add_spree_api_key_to_spree_users.rb
+++ api/db/migrate/20190224010301_add_spree_api_key_to_spree_users.rb
@@ -3,7 +3,8 @@
 class AddApiKeyToSpreeUsers < ActiveRecord::Migration[5.2]
   def change
     unless defined?(User)
-      add_column :spree_users, :api_key, :string, limit: 40
+      add_column :spree_users, :spree_api_key, :string, limit: 48
+	  add_index :spree_users, :spree_api_key
     end
   end
 end
